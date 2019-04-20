392b7e2d1fbb443a8489ca2713d2e504cd0e2a23
tic40 <ccpzjoh@gmail.com>
2019-04-20 23:31:02 +0900

    Fix

--- tenka1beginner2019/c/main.cpp
+++ tenka1beginner2019/c/main.cpp
@@ -2,14 +2,12 @@
 using namespace std;
 
 int main() {
-  int n, last = 0, min = 0;
+  int n, last, min;
   char white = '.', black = '#';
   string s;
   cin >> n >> s;
 
-  for (int i = 0; i < s.size(); i++) {
-    if (s.at(i) == white) last++;
-  }
+  for (int i = 0; i < s.size(); i++) if (s.at(i) == white) last++;
   min = last;
   for (int i = 1; i <= s.size(); i++) {
     s.at(i - 1) == black ? last++ : last--;
