f9463ae1e7c1a4f7390f39d9ada5f17e5ac982b4
tic40 <ccpzjoh@gmail.com>
2019-06-18 09:00:01 +0900

    Clean

--- abc130/c/main.cpp
+++ abc130/c/main.cpp
@@ -1,10 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i, n) for(int i = 0; i < n; i++)
-#define REPR(i, n) for(int i = n; i >= 0; i--)
-#define FOR(i, m, n) for(int i = m; i < n; i++)
-typedef long long ll;
-
 int main() {
   int w, h, x, y;
   cin >> w >> h >> x >> y;
