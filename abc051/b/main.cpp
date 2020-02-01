6f4cbf16deef9a438ac922193f59c3bee02aa24e
tic40 <ccpzjoh@gmail.com>
2020-02-01 14:09:48 +0900

    Update

--- abc051/b/main.cpp
+++ abc051/b/main.cpp
@@ -5,11 +5,6 @@ typedef long long ll;
 
 int main() {
   int k,s,ans=0; cin>>k>>s;
-  REP(x,k+1) {
-    REP(y,k+1) {
-      int z = s-(x+y);
-      if (0<=z && z<=k) ans++;
-    }
-  }
+  REP(x,k+1) { REP(y,k+1) { int z = s-(x+y); if (0<=z && z<=k) ans++; } }
   cout << ans << endl;
 }
