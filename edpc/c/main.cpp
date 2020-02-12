0d568b1373acc46a2a0dbb28bd36566c3074ec5b
tic40 <ccpzjoh@gmail.com>
2020-02-12 21:15:20 +0900

    Add edpc c

--- edpc/c/main.cpp
+++ edpc/c/main.cpp
@@ -3,6 +3,22 @@ using namespace std;
 #define REP(i, n) for(int i = 0; i < n; i++)
 typedef long long ll;
 
-int main() {
+int dp[100001][3];
 
+int main() {
+  int n; cin >> n;
+  int a[n][3];
+  REP(i,n) REP(j,3) cin >> a[i][j];
+
+  REP(i,n) {
+    REP(j,3) {
+      REP(k,3) {
+        if (j==k) continue;
+        dp[i+1][j] = max(dp[i+1][j], dp[i][k] + a[i][j]);
+      }
+    }
+  }
+  int ans = 0;
+  REP(i,3) ans = max(ans, dp[n][i]);
+  cout << ans << endl;
 }
