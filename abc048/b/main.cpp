9cbc21a62e0f6002d0c972769cbf550e5c904236
tic40 <ccpzjoh@gmail.com>
2020-03-18 01:35:56 +0900

    Fix

--- abc048/b/main.cpp
+++ abc048/b/main.cpp
@@ -3,16 +3,10 @@ using namespace std;
 #define REP(i, n) for(int i = 0; i < n; i++)
 typedef long long ll;
 
-ll a,b,x;
-
-ll f(ll n) {
-  if (n >= 0) return n / x + 1;
-  return 0;
-}
-
 int main() {
+  ll a,b,x;
   cin >> a >> b >> x;
-  cout << f(b) << endl;
-  cout << f(a-1) << endl;
-  cout << f(b) - f(a-1) << endl;
+  if (a==0) cout << b/x+1 << endl;
+  else cout << b/x - (a-1)/x << endl;
+  return 0;
 }
