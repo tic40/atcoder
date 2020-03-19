404d10356e374d7676c5c48726f8e45e7add4b31
tic40 <ccpzjoh@gmail.com>
2020-03-19 21:49:46 +0900

    Add abc070

---
+++ abc070/c/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, n) for(int i = 0; i < n; i++)
+#define COUT(x) cout<<(x)<<endl
+#define dump(x)  cout << #x << " = " << (x) << endl;
+using ll = long long;
+using P = pair<int,int>;
+using Graph = vector<vector<int>>;
+using M = map<int,int>;
+using PQ = priority_queue<int>;
+using PQG = priority_queue<int, vector<int>, greater<int>>;
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
+ll gcd(ll a, ll b) { return b?gcd(b,a%b):a;}
+ll lcm(ll a, ll b) { return a/gcd(a,b)*b;}
+
+int main() {
+  ll n; cin >> n;
+  ll x, t;
+  REP(i,n) {
+    cin >> t;
+    x = i == 0 ? t : lcm(x,t);
+  }
+  cout << x << endl;
+  return 0;
+}
