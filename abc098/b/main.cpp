b119af8224a46599e71ddf1931acfcb568a67393
tic40 <ccpzjoh@gmail.com>
2020-05-06 00:46:56 +0900

    Add abc098

---
+++ abc098/b/main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, n) for(int i = 0; i < n; i++)
+#define COUT(x) cout<<(x)<<endl
+#define dump(x)  cout << #x << " = " << (x) << endl;
+using ll = long long;
+using P = pair<int,int>;
+using Graph = vector<vector<int>>;
+using M = map<int,int>;
+using PQ = priority_queue<int>;
+using PQG = priority_queue<int, vector<int>, greater<int>>;
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
+
+int main() {
+  int n; string s;
+  cin >> n >> s;
+
+  int ans = 0;
+  for (int i = 1; i < n; i++) {
+    map<char,int> m;
+    int cnt = 0;
+    REP(j,i) m[s[j]]=1;
+    for (int j = i; j < n; j++) {
+      if (m[s[j]]!=1) continue;
+      m[s[j]] = 2;
+      cnt++;
+    }
+    ans = max(ans,cnt);
+  }
+  COUT(ans);
+  return 0;
+}
