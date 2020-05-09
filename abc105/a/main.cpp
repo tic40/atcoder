64c2ab24c53659bb0c910eb10a6fc91c5441bbcb
tic40 <ccpzjoh@gmail.com>
2020-05-09 22:46:40 +0900

    Add abc105

---
+++ abc105/a/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, n) for(int i = 0; i < n; i++)
+#define COUT(x) cout<<(x)<<endl
+#define dump(x)  cout << #x << " = " << (x) << endl;
+using ll = long long;
+using P = pair<int,int>;
+using Graph = vector<vector<int>>;
+using M = map<int,int>;
+using PQ = priority_queue<int>;
+using PQG = priority_queue<int, vector<int>, greater<int>>;
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
+
+int main() {
+  int n,k; cin >> n >> k;
+  COUT(n%k?1:0);
+  return 0;
+}
---
+++ abc105/b/main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, n) for(int i = 0; i < n; i++)
+#define COUT(x) cout<<(x)<<endl
+#define dump(x)  cout << #x << " = " << (x) << endl;
+using ll = long long;
+using P = pair<int,int>;
+using Graph = vector<vector<int>>;
+using M = map<int,int>;
+using PQ = priority_queue<int>;
+using PQG = priority_queue<int, vector<int>, greater<int>>;
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
+
+int main() {
+  int n; cin >> n;
+  bool ok = false;
+  for (int i = 0; i <= n/4; i++) {
+    if ( (n-(4*i))%7==0 ) ok = true;
+  }
+  COUT(ok?"Yes":"No");
+  return 0;
+}
