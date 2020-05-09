fe1a65c0d6da0845bbabf819982b8579c790c56c
tic40 <ccpzjoh@gmail.com>
2020-05-09 14:30:17 +0900

    Add abc103

---
+++ abc103/b/main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, n) for(int i = 0; i < n; i++)
+#define COUT(x) cout<<(x)<<endl
+#define dump(x)  cout << #x << " = " << (x) << endl;
+using ll = long long;
+using P = pair<int,int>;
+using Graph = vector<vector<int>>;
+using M = map<int,int>;
+using PQ = priority_queue<int>;
+using PQG = priority_queue<int, vector<int>, greater<int>>;
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
+
+int main() {
+  string s,t; cin >> s >> t;
+  REP(i,s.size()) {
+    if (s==t) { COUT("Yes"); return 0; };
+    rotate(s.begin(), s.begin()+1, s.end());
+  }
+  COUT("No");
+  return 0;
+}
