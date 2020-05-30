de2406ef1c8239c52921e800051912a8903006fb
tic40 <ccpzjoh@gmail.com>
2020-05-30 15:37:37 +0900

    Add abc138

---
+++ abc138/a/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, n) for(int i = 0; i < n; i++)
+#define COUT(x) cout<<(x)<<endl
+#define dump(x) cout<<#x<<" = "<<(x)<<endl;
+#define Yes(x) cout<<(x?"Yes":"No")<<endl;
+#define YES(x) cout<<(x?"YES":"NO")<<endl;
+using ll = long long;
+using P = pair<int,int>;
+using Graph = vector<vector<int>>;
+using M = map<int,int>;
+using PQ = priority_queue<int>;
+using PQG = priority_queue<int, vector<int>, greater<int>>;
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
+
+int main() {
+  int a; cin >> a;
+  string s; cin >> s;
+  COUT(a>=3200?s:"red");
+  return 0;
+}
