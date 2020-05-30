67e132458a18a84aec6b5c3a8c02ded40926db43
tic40 <ccpzjoh@gmail.com>
2020-05-30 15:37:45 +0900

    Add abc119

---
+++ abc119/a/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, n) for(int i = 0; i < n; i++)
+#define COUT(x) cout<<(x)<<endl
+#define dump(x) cout<<#x<<" = "<<(x)<<endl;
+#define Yes(x) cout<<(x?"Yes":"No")<<endl;
+#define YES(x) cout<<(x?"YES":"NO")<<endl;
+using ll = long long;
+using P = pair<int,int>;
+using Graph = vector<vector<int>>;
+using M = map<int,int>;
+using PQ = priority_queue<int>;
+using PQG = priority_queue<int, vector<int>, greater<int>>;
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
+
+int main() {
+  string s; cin >> s;
+  string m = s.substr(5,2);
+  string d = s.substr(8,2);
+  int sum = stoi(m) * 30 + stoi(d);
+  if (sum <= 4*30+30) COUT("Heisei");
+  else COUT("TBD");
+  return 0;
+}
