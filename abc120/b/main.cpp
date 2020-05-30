cd26a4c894e56e299fb0f18cfd74181a86c905e5
tic40 <ccpzjoh@gmail.com>
2020-05-30 15:59:15 +0900

    Add abc120

---
+++ abc120/b/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, n) for(int i = 0; i < n; i++)
+#define COUT(x) cout<<(x)<<endl
+#define dump(x) cout<<#x<<" = "<<(x)<<endl;
+#define Yes(x) cout<<(x?"Yes":"No")<<endl;
+#define YES(x) cout<<(x?"YES":"NO")<<endl;
+using ll = long long;
+using P = pair<int,int>;
+using Graph = vector<vector<int>>;
+using M = map<int,int>;
+using PQ = priority_queue<int>;
+using PQG = priority_queue<int, vector<int>, greater<int>>;
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
+
+int main() {
+  int a,b,k; cin >> a >> b >> k;
+  int i = 100;
+  while(i>0) {
+    if (a%i==0 && b%i==0) {
+      k--;
+      if(k == 0) { COUT(i); break; }
+    }
+    i--;
+  }
+  return 0;
+}
