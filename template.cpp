7858849dc8e68e859e4175b2be45ad386d7a00f6
tic40 <ccpzjoh@gmail.com>
2020-05-31 12:36:24 +0900

    Update template

--- template.cpp
+++ template.cpp
@@ -1,20 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i, n) for(int i = 0; i < n; i++)
+#define REP(i, n) for(int i=0;i<n;i++)
 #define COUT(x) cout<<(x)<<endl
 #define dump(x) cout<<#x<<" = "<<(x)<<endl;
 #define Yes(x) cout<<(x?"Yes":"No")<<endl;
 #define YES(x) cout<<(x?"YES":"NO")<<endl;
+#define ALL(x) (x).begin(),(x).end()
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
 using ll = long long;
 using P = pair<int,int>;
 using Graph = vector<vector<int>>;
 using M = map<int,int>;
 using PQ = priority_queue<int>;
 using PQG = priority_queue<int, vector<int>, greater<int>>;
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
 const int INF = 1e9;
 const int MOD = 1e9+7;
 const ll LINF = 1e18;
 
 int main() {
+  IOS;
+
   return 0;
 }
