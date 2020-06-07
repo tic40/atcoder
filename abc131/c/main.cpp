1dc550289e8ec0ea8b00f1443132666441f7b2c0
tic40 <ccpzjoh@gmail.com>
2020-06-07 15:04:04 +0900

    add abc131

---
+++ abc131/c/main.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ALL(x) (x).begin(),(x).end()
+#define COUT(x) cout<<(x)<<"\n"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define REP(i, n) for(int i=0;i<n;i++)
+#define YES(x) cout<<(x?"YES":"NO")<<"\n"
+#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
+#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
+#define endl "\n"
+using G = vector<vector<int>>;
+using M = map<int,int>;
+using P = pair<int,int>;
+using PQ = priority_queue<int>;
+using PQG = priority_queue<int,vector<int>,greater<int>>;
+using V = vector<int>;
+using ll = long long;
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
+
+ll gcd(ll a, ll b) { return b?gcd(b,a%b):a; }
+ll lcm(ll a, ll b) { return a/gcd(a,b)*b; }
+
+int main() {
+  IOS;
+  ll a,b,c,d;
+  cin >> a >> b >> c >> d; a--;
+  ll cd = lcm(c,d);
+
+  ll adivc = a/c;
+  ll adivd = a/d;
+  ll adivcd = a/cd;
+
+  ll bdivc = b/c;
+  ll bdivd = b/d;
+  ll bdivcd = b/cd;
+
+  COUT(b-a-((bdivc+bdivd-bdivcd) - (adivc+adivd-adivcd)));
+  return 0;
+}
