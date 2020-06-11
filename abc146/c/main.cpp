d3491dcef8c96b2d6808d18272daa2d74cde6711
tic40 <ccpzjoh@gmail.com>
2020-06-11 23:37:15 +0900

    Add abc146

---
+++ abc146/c/main.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ALL(x) (x).begin(),(x).end()
+#define COUT(x) cout<<(x)<<"\n"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define REP(i, n) for(int i=0;i<n;i++)
+#define YES(x) cout<<(x?"YES":"NO")<<"\n"
+#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
+#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
+#define endl "\n"
+using G = vector<vector<int>>;
+using M = map<int,int>;
+using P = pair<int,int>;
+using PQ = priority_queue<int>;
+using PQG = priority_queue<int,vector<int>,greater<int>>;
+using V = vector<int>;
+using ll = long long;
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
+
+int main() {
+  IOS;
+  ll a,b,x;
+  cin >> a >> b >> x;
+
+  auto d = [&](ll c) {
+    int res = 0;
+    while(c) { c /= 10; res++; }
+    return res;
+  };
+  auto f = [&](ll c) {
+    return a*c + b*d(c);
+  };
+
+  ll l = 0, r = 1e9+1;
+  while(r-l > 1) {
+    ll c = (l+r)/2;
+    if (f(c) <= x) l = c;
+    else r = c;
+  }
+  COUT(l);
+  return 0;
+}
