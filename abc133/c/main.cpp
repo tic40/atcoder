129162433308d877592a204136020373abb3f485
tic40 <ccpzjoh@gmail.com>
2020-07-06 22:48:04 +0900

    Update

--- abc133/c/main.cpp
+++ abc133/c/main.cpp
@@ -3,10 +3,9 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-	ll l, r;
-	cin >> l >> r;
+	ll l, r; cin >> l >> r;
 	int ans = 2019;
-  if (r-l >= 2019) { cout << 0 << endl; return 0; }
+
   for (ll i = l; i < r; i++) {
     for (ll j = i + 1; j <= r; j++) {
       ans = min(ans, (int)(i*j%2019));
@@ -14,4 +13,5 @@ int main(){
     }
   }
   cout << ans << endl;
+  return 0;
 }
