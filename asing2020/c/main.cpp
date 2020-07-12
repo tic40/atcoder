4291117a3e2f82364b07ed82359242b04f31e399
tic40 <ccpzjoh@gmail.com>
2020-07-12 12:59:58 +0900

    Add asing2020

---
+++ asing2020/c/main.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ALL(x) (x).begin(),(x).end()
+#define COUT(x) cout<<(x)<<"\n"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define REP(i, n) for(int i=0;i<n;i++)
+#define YES(x) cout<<(x?"YES":"NO")<<"\n"
+#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
+#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
+#define endl "\n"
+using G = vector<vector<int>>;
+using M = map<int,int>;
+using P = pair<int,int>;
+using PQ = priority_queue<int>;
+using PQG = priority_queue<int,vector<int>,greater<int>>;
+using V = vector<int>;
+using ll = long long;
+using edge = struct { int to; int cost; };
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
+
+int main() {
+  IOS;
+  int n; cin >> n;
+
+  M m;
+  for (int x = 1; x <= 100; x++) {
+    for (int y = 1; y <= 100; y++) {
+      for (int z = 1; z <= 100; z++) {
+        int calc = pow(x+y,2) + pow(y+z,2) + pow(z+x, 2);
+        m[calc]++;
+      }
+    }
+  }
+  REP(i,n) {
+    COUT(m[(i+1)*2]);
+  }
+
+  return 0;
+}
