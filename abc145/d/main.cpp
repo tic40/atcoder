f62fa9210233325638274c3e1cbf058100fca607
tic40 <ccpzjoh@gmail.com>
2020-07-18 13:23:22 +0900

    Add abc145 d

---
+++ abc145/d/main.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ALL(x) (x).begin(),(x).end()
+#define COUT(x) cout<<(x)<<"\n"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define REP(i, n) for(int i=0;i<n;i++)
+#define YES(x) cout<<(x?"YES":"NO")<<"\n"
+#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
+#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
+#define endl "\n"
+using G = vector<vector<int>>;
+using M = map<int,int>;
+using P = pair<int,int>;
+using PQ = priority_queue<int>;
+using PQG = priority_queue<int,vector<int>,greater<int>>;
+using V = vector<int>;
+using ll = long long;
+using edge = struct { int to; int cost; };
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+const int INF = 1e9;
+const int mod = 1e9+7;
+const ll LINF = 1e18;
+
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+  // for prime mod
+  mint inv() const { return pow(mod-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+mint choose(int n, int a) {
+  mint x = 1, y = 1;
+  REP(i,a) {
+    x *= n-i;
+    y *= i+1;
+  }
+  return x / y;
+}
+
+int main() {
+  IOS;
+  int X,Y; cin >> X >> Y;
+  mint ans = 0;
+  REP(b, 1000005) {
+    int a = X - (2*b);
+    if (a < 0) break;
+    if (Y != (2*a) + b) continue;
+    ans += choose(a+b, b);
+  }
+  COUT(ans);
+  return 0;
+}
