33d69369761c406413cdb6bf34726184426248a4
tic40 <ccpzjoh@gmail.com>
2020-07-24 00:35:31 +0900

    Add abc026

---
+++ abc026/b/main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ALL(x) (x).begin(),(x).end()
+#define COUT(x) cout<<(x)<<"\n"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define REP(i, n) for(int i=0;i<n;i++)
+#define YES(x) cout<<(x?"YES":"NO")<<"\n"
+#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
+#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
+#define endl "\n"
+using G = vector<vector<int>>;
+using M = map<int,int>;
+using P = pair<int,int>;
+using PQ = priority_queue<int>;
+using PQG = priority_queue<int,vector<int>,greater<int>>;
+using V = vector<int>;
+using ll = long long;
+using edge = struct { int to; int cost; };
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
+const double pi = 3.141592653589793;
+
+int main() {
+  IOS;
+  int n; cin >> n;
+  vector<int> r(n);
+  REP(i,n) cin >> r[i];
+  sort(ALL(r),greater<int>());
+
+  double ans;
+  REP(i,n) {
+    double area = r[i]*r[i]*pi;
+    ans = i%2 ? ans - area : ans + area;
+  }
+  cout << fixed << setprecision(10) << ans << endl;
+  return 0;
+}
