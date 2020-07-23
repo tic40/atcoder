c8d1e1a5ff46126fc46af2f971c8544aea5dde21
tic40 <ccpzjoh@gmail.com>
2020-07-23 16:48:19 +0900

    Add abc019

---
+++ abc019/c/main.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ALL(x) (x).begin(),(x).end()
+#define COUT(x) cout<<(x)<<"\n"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define REP(i, n) for(int i=0;i<n;i++)
+#define YES(x) cout<<(x?"YES":"NO")<<"\n"
+#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
+#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
+#define endl "\n"
+using G = vector<vector<int>>;
+using M = map<int,long long int>;
+using P = pair<int,int>;
+using PQ = priority_queue<int>;
+using PQG = priority_queue<int,vector<int>,greater<int>>;
+using V = vector<int>;
+using ll = long long;
+using edge = struct { int to; int cost; };
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
+
+int main() {
+  IOS;
+  int n; cin >> n;
+  vector<int> a(n);
+
+  M m;
+  REP(i,n) {
+    cin >> a[i];
+    m[a[i]]++;
+  }
+
+  int ans = 0;
+  sort(ALL(a));
+  REP(i,n) {
+    if (m[a[i]] > 0) ans++;
+    if (m[a[i]] == -1) continue;
+    for (int j = a[i]; j <= 1e9; j*=2) m[j] = -1;
+  }
+  COUT(ans);
+  return 0;
+}
