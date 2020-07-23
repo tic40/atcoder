c499da66d4ddd95d1f55f3b39ff552c358147945
tic40 <ccpzjoh@gmail.com>
2020-07-23 12:20:41 +0900

    Update abc161 d

--- abc161/d/main.cpp
+++ abc161/d/main.cpp
@@ -33,33 +33,3 @@ int main() {
   COUT(ans[k-1]);
   return 0;
 }
-
-
-
-
-
-
-
-
-
-
-  /*
-  int k; cin >> k;
-  vector<ll> a;
-  for (int i=1; i<=9; i++) a.push_back(i);
-  while(1) {
-    if (k <= a.size()) { cout << a[k-1] << endl; return 0; }
-    k-=a.size();
-    vector<ll> old;
-    swap(old,a);
-    for (ll x : old) {
-      for (int i = -1; i<=1; i++) {
-        int d = x%10+i;
-        if (d < 0 || d > 9) continue;
-        ll nx = x*10 + d;
-        a.push_back(nx);
-      }
-    }
-  }
-  */
-}
