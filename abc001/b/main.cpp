f46e6864876b6ef01193517bf75619ca7c11ec12
tic40 <ccpzjoh@gmail.com>
2020-07-23 12:32:49 +0900

    Add abc001

---
+++ abc001/b/main.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ALL(x) (x).begin(),(x).end()
+#define COUT(x) cout<<(x)<<"\n"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define REP(i, n) for(int i=0;i<n;i++)
+#define YES(x) cout<<(x?"YES":"NO")<<"\n"
+#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
+#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
+#define endl "\n"
+using G = vector<vector<int>>;
+using M = map<int,int>;
+using P = pair<int,int>;
+using PQ = priority_queue<int>;
+using PQG = priority_queue<int,vector<int>,greater<int>>;
+using V = vector<int>;
+using ll = long long;
+using edge = struct { int to; int cost; };
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
+
+int main() {
+  // IOS;
+  int m; cin >> m;
+  int ans;
+  if (m < 100) {
+    ans = 0;
+  } else if (m <= 5000) {
+    ans = m*10/1000;
+  } else if(m <= 30000) {
+    ans = m/1000+50;
+  } else if (m <= 70000) {
+    ans = (m/1000-30)/5+80;
+  } else {
+    ans = 89;
+  }
+  printf("%02d\n", ans);
+  return 0;
+}
