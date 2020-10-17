d5afc6b12f7d2e010267a45b2d3e3680a43f5fd9
tic40 <ccpzjoh@gmail.com>
2020-10-17 22:02:04 +0900

    Add abc180

---
+++ abc180/b/main.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ALL(x) (x).begin(),(x).end()
+#define COUT(x) cout<<(x)<<"\n"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define REP(i,n) for(int i=0;i<n;i++)
+#define YES(x) cout<<(x?"YES":"NO")<<"\n"
+#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
+#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
+#define endl "\n"
+using G = vector<vector<int>>;
+using M = map<int,int>;
+using P = pair<int,int>;
+using PQ = priority_queue<int>;
+using PQG = priority_queue<int,vector<int>,greater<int>>;
+using V = vector<int>;
+using ll = long long;
+using edge = struct { int to; int cost; };
+template<class T>bool chmax(T &a,const T &b) {if(a<b){a=b; return 1;} return 0;}
+template<class T>bool chmin(T &a,const T &b) {if(b<a){a=b; return 1;} return 0;}
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
+
+int main() {
+  IOS;
+  int n; cin >> n;
+  ll x[n];
+  REP(i,n) cin >> x[i];
+
+  ll ans = 0;
+  REP(i,n) { ans += abs(x[i]); }
+  COUT(ans);
+
+  ans = 0;
+  REP(i,n) { ans += abs(x[i] * x[i]); }
+  cout << fixed << setprecision(10) << sqrt(ans) << endl;
+
+  ans = 0;
+  REP(i,n) chmax(ans, abs(x[i]));
+  COUT(ans);
+  return 0;
+}
