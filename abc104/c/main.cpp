ca148e7319bc0af8f44483894c448f78dddc744b
tic40 <ccpzjoh@gmail.com>
2020-11-25 09:16:07 +0900

    Add abc104 c

--- abc104/c/main.cpp
+++ abc104/c/main.cpp
@@ -14,5 +14,39 @@ const int MOD = 1e9+7;
 const ll LINF = 1e18;
 
 int main() {
+  int d,g;
+  cin >> d >> g;
+  int p[d],c[d];
+  REP(i,d) cin >> p[i] >> c[i];
+
+  int ans = INF;
+  for (int bit = 0; bit < (1 << d); bit++) {
+    int cnt = 0, sum = 0;
+    REP(i,d) {
+      if (bit >> i & 1) {
+        sum += 100 * (i+1) * p[i] + c[i];
+        cnt += p[i];
+      }
+    }
+
+    if (g <= sum) {
+      ans = min(ans, cnt);
+    } else {
+      int mxidx;
+      for (int i = d-1; 0 <= i; i--) {
+        if (bit >> i & 1) continue;
+        mxidx = i;
+        break;
+      }
+
+      REP(j,p[mxidx]) {
+        sum += 100 * (mxidx+1);
+        cnt++;
+        if (g <= sum) ans = min(ans, cnt);
+      }
+    }
+  }
+
+  COUT(ans);
   return 0;
 }
