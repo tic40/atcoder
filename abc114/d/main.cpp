37adfe272859ab97c7e50f9066caa3d53283e74e
tic40 <ccpzjoh@gmail.com>
2020-12-08 00:38:04 +0900

    Add abc114 d

---
+++ abc114/d/main.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ALL(x) (x).begin(),(x).end()
+#define COUT(x) cout<<(x)<<"\n"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define REP(i,n) for(int i=0;i<n;i++)
+#define YES(x) cout<<(x?"YES":"NO")<<"\n"
+#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
+#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
+#define endl "\n"
+using G = vector<vector<int>>;
+using M = map<int,int>;
+using P = pair<int,int>;
+using PQ = priority_queue<int>;
+using PQG = priority_queue<int,vector<int>,greater<int>>;
+using V = vector<int>;
+using ll = long long;
+using edge = struct { int to; int cost; };
+template<class T>bool chmax(T &a,const T &b) {if(a<b){a=b; return 1;} return 0;}
+template<class T>bool chmin(T &a,const T &b) {if(b<a){a=b; return 1;} return 0;}
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
+
+vector<pair<ll, ll>> prime_factorize(ll n) {
+  vector<pair<ll,ll>> res;
+  for (ll a = 2; a * a <= n; a++) {
+    if (n % a != 0) continue;
+    ll ex = 0;
+    while (n % a == 0) { ++ex; n /= a; }
+    res.push_back({a, ex});
+  }
+  if (n != 1) res.push_back({n, 1});
+  return res;
+}
+
+ll dp[100][76]; // dp[i][j]: i個目までの素因数で約数の個数がj個
+
+int main() {
+  IOS;
+  int n; cin >> n;
+  map<int, int> m;
+
+  for (int i = 1; i <= n; i++) {
+    auto x = prime_factorize(i);
+    for (auto v: x) m[v.first]+=v.second;
+  }
+
+  vector<int> nums;
+  for (auto v: m) nums.push_back(v.second);
+
+  dp[0][1]=1;
+  REP(i,nums.size()) {
+    REP(j,76) {
+      REP(cnt, nums[i]+1) {
+        dp[i+1][min(76, j*(cnt+1))] += dp[i][j];
+      }
+    }
+  }
+  COUT(dp[nums.size()][75]);
+
+  return 0;
+}
