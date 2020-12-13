245427a2b1d1a8fb7458d796863adc1fff8965f6
tic40 <ccpzjoh@gmail.com>
2020-12-13 22:50:18 +0900

    Add abc185

---
+++ abc185/b/main.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ALL(x) (x).begin(),(x).end()
+#define COUT(x) cout<<(x)<<"\n"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define REP(i,n) for(int i=0;i<n;i++)
+#define YES(x) cout<<(x?"YES":"NO")<<"\n"
+#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
+#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
+#define endl "\n"
+using G = vector<vector<int>>;
+using M = map<int,int>;
+using P = pair<int,int>;
+using PQ = priority_queue<int>;
+using PQG = priority_queue<int,vector<int>,greater<int>>;
+using V = vector<int>;
+using ll = long long;
+using edge = struct { int to; int cost; };
+template<class T>bool chmax(T &a,const T &b) {if(a<b){a=b; return 1;} return 0;}
+template<class T>bool chmin(T &a,const T &b) {if(b<a){a=b; return 1;} return 0;}
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
+
+int main() {
+  IOS;
+  int n,m,t;
+  cin >> n >> m >> t;
+  int ok = true, mx=n, now=0;
+  int a,b;
+  REP(i,m) {
+    cin >> a >> b;
+    n -= a-now;
+    if (n <= 0) ok = false;
+    n = min(mx,n+(b-a));
+    now = b;
+  }
+  n-=(t-b);
+  if (n<=0) ok = false;
+  Yes(ok);
+  return 0;
+}
