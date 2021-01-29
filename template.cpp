6260d88b8a523ae8178249fc861e9e05f47dcf3a
tic40 <ccpzjoh@gmail.com>
2021-01-30 02:01:47 +0900

    Update template

--- template.cpp
+++ template.cpp
@@ -21,6 +21,67 @@ template<class T>bool chmin(T &a,const T &b) {if(b<a){a=b; return 1;} return 0;}
 const int INF = 1e9;
 const int MOD = 1e9+7;
 const ll LINF = 1e18;
+const double PI = 3.141592653589793;
+
+ll gcd(ll a, ll b) { return b?gcd(b,a%b):a; }
+ll lcm(ll a, ll b) { return a/gcd(a,b)*b; }
+ll extGCD(ll a, ll b, ll &x, ll &y) {
+  if (b == 0) { x = 1; y = 0; return a; }
+  ll d = extGCD(b, a%b, y, x);
+  y -= a/b * x;
+  return d;
+}
+
+// 二項係数
+const int comMAX = 100005;
+ll fac[comMAX], finv[comMAX], inv[comMAX];
+void comInit() { // テーブルを作る前処理
+  fac[0] = fac[1] = 1;
+  finv[0] = finv[1] = 1;
+  inv[1] = 1;
+  for (int i = 2; i < comMAX; i++){
+    fac[i] = fac[i - 1] * i % MOD;
+    inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
+    finv[i] = finv[i - 1] * inv[i] % MOD;
+  }
+  return;
+}
+ll com(int n, int k) { // 二項係数計算
+  if (n < k) return 0;
+  if (n < 0 || k < 0) return 0;
+  return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
+
+struct mint {
+  ll x;
+  mint(ll x=0):x((x%MOD+MOD)%MOD){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += MOD-a.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= MOD; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+  // for prime MOD
+  mint inv() const { return pow(MOD-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
 
 int main() {
   IOS;
