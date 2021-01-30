37c43bf55cfcce51bf59b584492cdde96ea13ce8
tic40 <ccpzjoh@gmail.com>
2021-01-30 10:07:23 +0900

    Update

--- template.cpp
+++ template.cpp
@@ -85,6 +85,36 @@ struct mint {
 istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
 ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
 
+struct UnionFind {
+  vector<int> d;
+  UnionFind(int n=0): d(n, -1) {}
+  int root(int x) {
+    if (d[x] < 0) return x;
+    return d[x] = root(d[x]);
+  }
+  bool unite(int x, int y) {
+    x = root(x); y = root(y);
+    if (x == y) return false;
+    if (d[x] > d[y]) swap(x,y);
+    d[x] += d[y];
+    d[y] = x;
+    return true;
+  }
+  bool same(int x, int y) { return root(x) == root(y); }
+  int size(int x) { return -d[root(x)]; }
+};
+
+int binary_search(vector<int> a, int key) {
+  int ng = -1, ok = a.size();
+  while (abs(ok-ng)>1) {
+    int mid = (ok+ng)/2;
+    // 条件: key以上の場合
+    if (a[mid]>=key) ok = mid;
+    else ng = mid;
+  }
+  return ok;
+}
+
 
 void solve() {
   return;
