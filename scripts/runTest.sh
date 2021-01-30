0f61d00f12ebf48a12bdd5c0e36ca456e7d05c99
tic40 <ccpzjoh@gmail.com>
2021-01-30 10:13:15 +0900

    Move scripts

--- scripts/runTest.sh
+++ scripts/runTest.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 filePattern=${1:-test*}
 
+echo "===start runTest.sh==="
+
 g++ -std=gnu++1y -o a.out main.cpp
+
+echo "===start test==="
 find . -type f -name "$filePattern" | sort | while read -r fname
 do
   echo "[run $fname]"
   ./a.out < $fname
 done
+echo "===end test==="
