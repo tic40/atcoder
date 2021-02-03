3662a414eed496899c1fffe03160977dbae48302
tic40 <ccpzjoh@gmail.com>
2021-02-03 22:39:23 +0900

    Add abc126 e

---
+++ abc126/e/main.cpp
@@ -0,0 +1,143 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ALL(x) (x).begin(),(x).end()
+#define COUT(x) cout<<(x)<<"\n"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define REP(i,n) for(int i=0;i<n;i++)
+#define YES(x) cout<<(x?"YES":"NO")<<"\n"
+#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
+#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
+#define endl "\n"
+using G = vector<vector<int>>;
+using M = map<int,int>;
+using P = pair<int,int>;
+using PQ = priority_queue<int>;
+using PQG = priority_queue<int,vector<int>,greater<int>>;
+using V = vector<int>;
+using ll = long long;
+using edge = struct { int to; int cost; };
+template<class T>bool chmax(T &a,const T &b) {if(a<b){a=b; return 1;} return 0;}
+template<class T>bool chmin(T &a,const T &b) {if(b<a){a=b; return 1;} return 0;}
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
+const double PI = 3.141592653589793;
+
+ll gcd(ll a, ll b) { return b?gcd(b,a%b):a; }
+ll lcm(ll a, ll b) { return a/gcd(a,b)*b; }
+ll extGCD(ll a, ll b, ll &x, ll &y) {
+  if (b == 0) { x = 1; y = 0; return a; }
+  ll d = extGCD(b, a%b, y, x);
+  y -= a/b * x;
+  return d;
+}
+
+const int comMAX = 100005;
+// 二項係数テーブル
+ll fac[comMAX], finv[comMAX], inv[comMAX];
+// 二項係数テーブル前処理
+void comInit() {
+  fac[0] = fac[1] = 1;
+  finv[0] = finv[1] = 1;
+  inv[1] = 1;
+  for (int i = 2; i < comMAX; i++){
+    fac[i] = fac[i - 1] * i % MOD;
+    inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
+    finv[i] = finv[i - 1] * inv[i] % MOD;
+  }
+  return;
+}
+// 二項係数計算
+ll com(int n, int k) {
+  if (n < k) return 0;
+  if (n < 0 || k < 0) return 0;
+  return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
+
+struct mint {
+  ll x;
+  mint(ll x=0):x((x%MOD+MOD)%MOD){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += MOD-a.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= MOD; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+  // for prime MOD
+  mint inv() const { return pow(MOD-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+struct UnionFind {
+  vector<int> d;
+  UnionFind(int n=0): d(n, -1) {}
+  int root(int x) {
+    if (d[x] < 0) return x;
+    return d[x] = root(d[x]);
+  }
+  bool unite(int x, int y) {
+    x = root(x); y = root(y);
+    if (x == y) return false;
+    if (d[x] > d[y]) swap(x,y);
+    d[x] += d[y];
+    d[y] = x;
+    return true;
+  }
+  bool same(int x, int y) { return root(x) == root(y); }
+  int size(int x) { return -d[root(x)]; }
+};
+
+int binary_search(vector<int> a, int key) {
+  int ng = -1, ok = a.size();
+  while (abs(ok-ng)>1) {
+    int mid = (ok+ng)/2;
+    // 条件: key以上の場合
+    if (a[mid]>=key) ok = mid;
+    else ng = mid;
+  }
+  return ok;
+}
+
+int n,m;
+vector<int> x,y,z;
+
+void solve() {
+  UnionFind uf(n);
+  REP(i,m) uf.unite(x[i],y[i]);
+
+  set<int> st;
+  REP(i,n) st.insert(uf.root(i));
+
+  COUT(st.size());
+  return;
+}
+
+int main() {
+  IOS;
+  cin >> n >> m;
+  x.resize(m); y.resize(m); z.resize(m);
+  REP(i,m) {
+    cin >> x[i] >> y[i] >> z[i];
+    x[i]--; y[i]--;
+  }
+
+  solve();
+  return 0;
+}
