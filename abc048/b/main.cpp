90a90d0f12bfbede5ff97af5a07ec88361f80307
tic40 <ccpzjoh@gmail.com>
2021-02-14 23:24:37 +0900

    Update

--- abc048/b/main.cpp
+++ abc048/b/main.cpp
@@ -1,12 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i, n) for(int i = 0; i < n; i++)
-typedef long long ll;
+#define REP(i,n) for(int i=0;i<n;i++)
+using ll = long long;
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
 
 int main() {
   ll a,b,x;
   cin >> a >> b >> x;
-  if (a==0) cout << b/x+1 << endl;
-  else cout << b/x - (a-1)/x << endl;
+  ll ans = b/x - max(0LL, a-1)/x;
+
+  if (a == 0) { cout << ans + 1 << endl; }
+  else cout << ans << endl;
+
   return 0;
 }
