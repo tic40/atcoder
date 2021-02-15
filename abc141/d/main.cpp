f3bc4605c1460e15415b9e65698a320644037e05
tic40 <ccpzjoh@gmail.com>
2021-02-15 21:41:06 +0900

    Update

--- abc141/d/main.cpp
+++ abc141/d/main.cpp
@@ -1,40 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ALL(x) (x).begin(),(x).end()
-#define COUT(x) cout<<(x)<<"\n"
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define REP(i, n) for(int i=0;i<n;i++)
-#define YES(x) cout<<(x?"YES":"NO")<<"\n"
-#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
-#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
-#define endl "\n"
-using G = vector<vector<int>>;
-using M = map<int,int>;
-using P = pair<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int,vector<int>,greater<int>>;
-using V = vector<int>;
+#define REP(i,n) for(int i=0;i<n;i++)
 using ll = long long;
-template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
-template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
 const int INF = 1e9;
 const int MOD = 1e9+7;
 const ll LINF = 1e18;
 
 int main() {
-  IOS;
   int n,m; cin >> n >> m;
-  PQ pq;
+  priority_queue<int> q;
+
+  int a;
   REP(i,n) {
-    int a; cin >> a;
-    pq.push(a);
+    cin >> a;
+    q.push(a);
   }
+
   REP(i,m) {
-    int a = pq.top(); pq.pop();
-    pq.push(a/2);
+    q.push(q.top()/2);
+    q.pop();
   }
+
   ll ans = 0;
-  REP(i,n) { ans+=pq.top(); pq.pop(); }
-  COUT(ans);
+  REP(i,n) {
+    ans += q.top();
+    q.pop();
+  }
+
+  cout << ans << endl;
   return 0;
 }
