c4eb2a8243a8cb3f1ee0417c43c1468120e94ea5
tic40 <ccpzjoh@gmail.com>
2021-02-17 09:37:17 +0900

    Add nikkei2019-2-qual

---
+++ nikkei2019-2-qual/b/main.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i,n) for(int i=0;i<n;i++)
+using ll = long long;
+const int INF = 1e9;
+const int MOD = 998244353;
+const ll LINF = 1e18;
+
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%MOD+MOD)%MOD){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += MOD-a.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= MOD; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+  // for prime MOD
+  mint inv() const { return pow(MOD-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+int main() {
+  int n; cin >> n;
+  map<int, int> m;
+  REP(i,n) {
+    int d; cin >> d;
+    if (i == 0 && d != 0) {
+      cout << 0 << endl;
+      return 0;
+    }
+    m[d]++;
+  }
+
+  if (m[0] != 1) {
+    cout << 0 << endl;
+    return 0;
+  }
+
+  mint ans = 1;
+  REP(i,n) {
+    if (i == 0) continue;
+    mint x = m[i-1];
+    ans *= x.pow(m[i]);
+  }
+  cout << ans << endl;
+
+  return 0;
+}
