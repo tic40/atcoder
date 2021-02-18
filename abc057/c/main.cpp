737a1962802de518d3a0a44c0932f81cea98e241
tic40 <ccpzjoh@gmail.com>
2021-02-18 23:27:03 +0900

    Update

--- abc057/c/main.cpp
+++ abc057/c/main.cpp
@@ -1,21 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i, n) for(int i = 0; i < n; i++)
+#define REP(i,n) for(int i=0;i<n;i++)
 using ll = long long;
-
-int digits(ll n) {
-  int d = 0;
-  while(n > 0) { n /= 10; d++; }
-  return d;
-}
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
 
 int main() {
   ll n; cin >> n;
-  int ans = digits(n);
-  for(ll i = 1; i*i <= n; i++) {
-    if (n%i != 0) continue;
-    ans = min(ans, max(digits(i), digits(n/i)));
+
+  int mx = 0;
+  for (int i = 1; i*i <= n; i++) {
+    if (n%i == 0) mx = n/i;
   }
+
+  int ans = 0;
+  while(0 < mx) {
+    mx /= 10;
+    ans++;
+  }
+
   cout << ans << endl;
   return 0;
 }
