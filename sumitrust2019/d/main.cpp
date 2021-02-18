482b44e36c6d8878f89268505da888af846f3a19
tic40 <ccpzjoh@gmail.com>
2021-02-18 09:18:45 +0900

    Update

--- sumitrust2019/d/main.cpp
+++ sumitrust2019/d/main.cpp
@@ -1,45 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ALL(x) (x).begin(),(x).end()
-#define COUT(x) cout<<(x)<<"\n"
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define REP(i, n) for(int i=0;i<n;i++)
-#define YES(x) cout<<(x?"YES":"NO")<<"\n"
-#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
-#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
-#define endl "\n"
-using G = vector<vector<int>>;
-using M = map<int,int>;
-using P = pair<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int,vector<int>,greater<int>>;
-using V = vector<int>;
+#define REP(i,n) for(int i=0;i<n;i++)
 using ll = long long;
-using edge = struct { int to; int cost; };
-template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
-template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
 const int INF = 1e9;
 const int MOD = 1e9+7;
 const ll LINF = 1e18;
 
+int n;
+string s;
+bool dp[30005][4][1000];
+
 int main() {
-  IOS;
-  int n;
-  string s;
   cin >> n >> s;
 
-  int ans = 0;
-  REP(i,10) REP(j,10) REP(k,10) {
-    int secret[3] = {i,j,k};
-    int cnt = 0;
-    REP(i,n) {
-      if (s[i]-'0' == secret[cnt]) {
-        cnt++;
-        if (cnt == 3) { ans++; break; }
+  dp[0][0][0] = true;
+
+  REP(i,n) REP(j,4) REP(k,1000) {
+    if (!dp[i][j][k]) continue;
+    dp[i+1][j][k] = true;
+    if (j <= 2) {
+      int x = k*10+s[i]-'0';
+      if (0 <= x && x <= 999) {
+        dp[i+1][j+1][k*10+s[i]-'0'] = true;
       }
     }
   }
 
-  COUT(ans);
+  int ans = 0;
+  REP(k,1000) {
+    if (dp[n][3][k]) ans++;
+  }
+  cout << ans << endl;
   return 0;
 }
