fa2b2c62f2c1bc0c4c155e0c870146d284c6211d
tic40 <ccpzjoh@gmail.com>
2021-02-26 13:26:53 +0900

    Update abc049 c

--- abc049/c/main.cpp
+++ abc049/c/main.cpp
@@ -1,10 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i, n) for(int i = 0; i < n; i++)
-typedef long long ll;
+#define REP(i,n) for(int i=0;i<n;i++)
+using ll = long long;
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
 
 int main() {
-  string s;
-  cin >> s;
-  cout << (regex_match(s, regex("(dream(er)?|eraser?)+")) ? "YES" : "NO") << endl;
+  string s; cin >> s;
+
+  bool ok = regex_match(s,regex("(dream|dreamer|erase|eraser)+"));
+  cout << (ok ? "YES" : "NO") << endl;
+  return 0;
 }
