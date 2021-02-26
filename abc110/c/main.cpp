a5f087e12a5d205b0d835751afdad90762fa62e6
tic40 <ccpzjoh@gmail.com>
2021-02-26 09:23:35 +0900

    Update abc110 c

--- abc110/c/main.cpp
+++ abc110/c/main.cpp
@@ -1,39 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i, n) for(int i = 0; i < n; i++)
-#define COUT(x) cout<<(x)<<endl
-#define dump(x) cout<<#x<<" = "<<(x)<<endl;
-#define Yes(x) cout<<(x?"Yes":"No")<<endl;
+#define REP(i,n) for(int i=0;i<n;i++)
 using ll = long long;
-using P = pair<int,int>;
-using Graph = vector<vector<int>>;
-using M = map<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int, vector<int>, greater<int>>;
 const int INF = 1e9;
 const int MOD = 1e9+7;
 const ll LINF = 1e18;
 
 int main() {
-  string s,t; cin >> s >> t;
+  string s,t;
+  cin >> s >> t;
+  int n = s.size();
 
-  vector<int> start(26, -1);
-  vector<int> goal(26, -1);
+  map<char,char> sm,gm;
 
-  REP(i,s.size()) {
-    int a = s[i] - 'a';
-    int b = t[i] - 'a';
-
-    if (start[a] != -1 || goal[b] != -1) {
-      if (start[a] != b || goal[b] != a) {
-        COUT("No");
-        return 0;
-      }
+  string ans = "Yes";
+  REP(i,n) {
+    if (sm[s[i]] || gm[t[i]]) {
+      if (sm[s[i]] != t[i] || gm[t[i]] != s[i]) ans = "No";
     }
-    start[a] = b;
-    goal[b] = a;
+    sm[s[i]] = t[i];
+    gm[t[i]] = s[i];
   }
 
-  COUT("Yes");
+  cout << ans << endl;
   return 0;
 }
