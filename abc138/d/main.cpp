e370cbe322049882c61057618616eec5dc5680ea
tic40 <ccpzjoh@gmail.com>
2021-02-27 12:52:22 +0900

    Update abc138 d

--- abc138/d/main.cpp
+++ abc138/d/main.cpp
@@ -1,59 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ALL(x) (x).begin(),(x).end()
-#define COUT(x) cout<<(x)<<"\n"
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define REP(i, n) for(int i=0;i<n;i++)
-#define YES(x) cout<<(x?"YES":"NO")<<"\n"
-#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
-#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
-#define endl "\n"
-using G = vector<vector<int>>;
-using M = map<int,int>;
-using P = pair<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int,vector<int>,greater<int>>;
-using V = vector<int>;
+#define REP(i,n) for(int i=0;i<n;i++)
 using ll = long long;
-using edge = struct { int to; int cost; };
-template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
-template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
 const int INF = 1e9;
 const int MOD = 1e9+7;
 const ll LINF = 1e18;
 
-vector<int> ans;
-G g(200005);
+int n,q;
+vector<int> a,b,p,x;
+vector<vector<int>> g;
+vector<int> s;
 
-void dfs(int v, int p = -1) {
-  for (int u: g[v]) {
-    if (u == p) continue;
-    ans[u] += ans[v];
-    dfs(u,v);
+void dfs(int now, int from = -1) {
+  for(int ni: g[now]) {
+    if (ni == from) continue;
+    s[ni]+=s[now];
+    dfs(ni, now);
   }
+  return;
+}
+
+void solve() {
+  g.resize(n);
+  REP(i,n-1) {
+    g[a[i]].push_back(b[i]);
+    g[b[i]].push_back(a[i]);
+  }
+
+  // 累積和
+  s.resize(n);
+  REP(i,q) s[p[i]] += x[i];
+
+  dfs(0);
+  REP(i,n) cout << s[i] << " ";
+  return;
 }
 
 int main() {
-  IOS;
-  int n,q; cin >> n >> q;
+  cin >> n >> q;
+  a.resize(n-1),b.resize(n-1);
+  p.resize(q); x.resize(q);
   REP(i,n-1) {
-    int a,b; cin >> a >> b;
-    a--; b--;
-    g[a].push_back(b);
-    g[b].push_back(a);
+    cin >> a[i] >> b[i];
+    a[i]--; b[i]--;
   }
-  ans.resize(n);
   REP(i,q) {
-    int p,x; cin >> p >> x;
-    p--;
-    ans[p] += x;
+    cin >> p[i] >> x[i];
+    p[i]--;
   }
-  dfs(0);
 
-  REP(i,n) {
-    if (i > 0) cout << " ";
-    cout << ans[i];
-  }
-  cout << endl;
+  solve();
   return 0;
 }
