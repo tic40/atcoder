10492bcbc2cd9ff605bf8b9a7b8387b3d449d1c6
tic40 <ccpzjoh@gmail.com>
2021-02-27 13:21:51 +0900

    Update abc107 c

--- abc107/c/main.cpp
+++ abc107/c/main.cpp
@@ -1,30 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i, n) for(int i = 0; i < n; i++)
-#define COUT(x) cout<<(x)<<endl
-#define dump(x)  cout << #x << " = " << (x) << endl;
+#define REP(i,n) for(int i=0;i<n;i++)
 using ll = long long;
-using P = pair<int,int>;
-using Graph = vector<vector<int>>;
-using M = map<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int, vector<int>, greater<int>>;
 const int INF = 1e9;
 const int MOD = 1e9+7;
 const ll LINF = 1e18;
 
+int n,k;
+vector<int> x;
+
+void solve() {
+  int ans = INF;
+  REP(i,n-k+1) {
+    int l = x[i];
+    int r = x[i+k-1];
+    int d;
+    if (l <= 0 && r <= 0) {
+      d = abs(l);
+    } else if (0 < l && 0 < r) {
+      d = r;
+    } else {
+      d = min(abs(l)*2+abs(r), abs(r)*2+abs(l));
+    }
+    ans = min(ans, d);
+  }
+
+  cout << ans << endl;
+  return;
+}
+
 int main() {
-  int n,k; cin >> n >> k;
-  int x[n];
+  cin >> n >> k;
+  x.resize(n);
   REP(i,n) cin >> x[i];
 
-  int ans = INF;
-  REP(l,n-k+1) {
-    int r = l+k-1;
-    int totl = abs(x[l]) + abs(x[r]-x[l]);
-    int totr = abs(x[r]) + abs(x[r]-x[l]);
-    ans = min(ans, min(totl, totr));
-  }
-  COUT(ans);
+  solve();
   return 0;
 }
