b2ea5c7dae3dfc9b704e59417df6ce792b27c152
tic40 <ccpzjoh@gmail.com>
2021-03-01 09:35:19 +0900

    Update abc134 d

--- abc134/d/main.cpp
+++ abc134/d/main.cpp
@@ -1,43 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ALL(x) (x).begin(),(x).end()
-#define COUT(x) cout<<(x)<<"\n"
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define REP(i, n) for(int i=0;i<n;i++)
-#define YES(x) cout<<(x?"YES":"NO")<<"\n"
-#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
-#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
-#define endl "\n"
-using G = vector<vector<int>>;
-using M = map<int,int>;
-using P = pair<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int,vector<int>,greater<int>>;
-using V = vector<int>;
+#define REP(i,n) for(int i=0;i<n;i++)
 using ll = long long;
-using edge = struct { int to; int cost; };
-template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
-template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
 const int INF = 1e9;
 const int MOD = 1e9+7;
 const ll LINF = 1e18;
 
-int main() {
-  IOS;
-  int n; cin >> n;
-  vector<int> a(n+1),b(n+1);
-  REP(i,n) cin >> a[i+1];
+int n;
+vector<int> a;
 
-  vector<int> ans;
-  for (int i=n; i>0; i--) {
-    int sum = 0;
-    for(int j=i*2; j<=n; j+=i) sum+=b[j];
-    if (sum%2 != a[i]) { b[i]++; ans.push_back(i); }
+void solve() {
+  vector<int> x(n+1);
+
+  for(int i = n; 0 < i; i--) {
+    int sum = a[i];
+    for(int j = i*2; j <= n; j+=i) sum += x[j];
+    x[i] = sum%2;
   }
 
-  reverse(ALL(ans));
-  COUT(ans.size());
-  for (int x: ans) cout << x << " ";
-  cout << endl;
+  vector<int> ans;
+  REP(i,n+1) if(x[i]) ans.push_back(i);
+
+  cout << ans.size() << endl;
+  for(int v: ans) cout << v << " ";
+
+  return;
+}
+
+int main() {
+  cin >> n;
+  a.resize(n+1);
+  for(int i = 1; i<=n; i++) cin >> a[i];
+
+  solve();
   return 0;
 }
