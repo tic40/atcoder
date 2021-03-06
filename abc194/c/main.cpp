7e448866b773388f461df332ab71857126e17465
tic40 <ccpzjoh@gmail.com>
2021-03-06 23:05:02 +0900

    Update c

--- abc194/c/main.cpp
+++ abc194/c/main.cpp
@@ -1,179 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ALL(x) (x).begin(),(x).end()
-#define COUT(x) cout<<(x)<<"\n"
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define REP(i,n) for(int i=0;i<n;i++)
-#define YES(x) cout<<(x?"YES":"NO")<<"\n"
-#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
-#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
-#define endl "\n"
-#define sz(x) int(x.size())
-using G = vector<vector<int>>;
-using M = map<int,int>;
-using P = pair<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int,vector<int>,greater<int>>;
-using V = vector<int>;
 using ll = long long;
-using edge = struct { int to; int cost; };
-template<class T>bool chmax(T &a,const T &b) {if(a<b){a=b; return 1;} return 0;}
-template<class T>bool chmin(T &a,const T &b) {if(b<a){a=b; return 1;} return 0;}
 const int INF = 1e9;
 const int MOD = 1e9+7;
 const ll LINF = 1e18;
-const double PI = 3.141592653589793;
 
-struct mint {
-  ll x;
-  mint(ll x=0):x((x%MOD+MOD)%MOD){}
-  mint operator-() const { return mint(-x);}
-  mint& operator+=(const mint a) {
-    if ((x += a.x) >= MOD) x -= MOD;
-    return *this;
+int main() {
+  int n;
+  cin >> n;
+  map<int,int> m;
+  REP(i,n) {
+    int a; cin >> a;
+    m[a]++;
   }
-  mint& operator-=(const mint a) {
-    if ((x += MOD-a.x) >= MOD) x -= MOD;
-    return *this;
-  }
-  mint& operator*=(const mint a) { (x *= a.x) %= MOD; return *this;}
-  mint operator+(const mint a) const { return mint(*this) += a;}
-  mint operator-(const mint a) const { return mint(*this) -= a;}
-  mint operator*(const mint a) const { return mint(*this) *= a;}
-  mint pow(ll t) const {
-    if (!t) return 1;
-    mint a = pow(t>>1);
-    a *= a;
-    if (t&1) a *= *this;
-    return a;
-  }
-  // for prime MOD
-  mint inv() const { return pow(MOD-2);}
-  mint& operator/=(const mint a) { return *this *= a.inv();}
-  mint operator/(const mint a) const { return mint(*this) /= a;}
-};
-istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
-ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
 
-struct UnionFind {
-  vector<int> d;
-  UnionFind(int n=0): d(n, -1) {}
-  int root(int x) {
-    if (d[x] < 0) return x;
-    return d[x] = root(d[x]);
-  }
-  bool unite(int x, int y) {
-    x = root(x); y = root(y);
-    if (x == y) return false;
-    if (d[x] > d[y]) swap(x,y);
-    d[x] += d[y];
-    d[y] = x;
-    return true;
-  }
-  bool same(int x, int y) { return root(x) == root(y); }
-  int size(int x) { return -d[root(x)]; }
-};
-
-
-ll gcd(ll a, ll b) { return b?gcd(b,a%b):a; }
-ll lcm(ll a, ll b) { return a/gcd(a,b)*b; }
-ll extGCD(ll a, ll b, ll &x, ll &y) {
-  if (b == 0) { x = 1; y = 0; return a; }
-  ll d = extGCD(b, a%b, y, x);
-  y -= a/b * x;
-  return d;
-}
-
-const int comMAX = 200005;
-// 二項係数テーブル
-ll fac[comMAX], finv[comMAX], inv[comMAX];
-bool comInitialized = false;
-// 二項係数テーブル初期化
-void _comInit() {
-  fac[0] = fac[1] = 1;
-  finv[0] = finv[1] = 1;
-  inv[1] = 1;
-  for (int i = 2; i < comMAX; i++){
-    fac[i] = fac[i - 1] * i % MOD;
-    inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
-    finv[i] = finv[i - 1] * inv[i] % MOD;
-  }
-  comInitialized = true;
-  return;
-}
-
-// 二項係数テーブルからnCk算出
-ll com(int n, int k) {
-  if (!comInitialized) _comInit();
-  if (n < k) return 0;
-  if (n < 0 || k < 0) return 0;
-  return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
-}
-
-// mint nCk
-mint choose(int n, int k) {
-  mint x = 1, y = 1;
-  REP(i,k) {
-    x *= n-i;
-    y *= i+1;
-  }
-  return x / y;
-}
-
-ll binary_search(vector<ll> a, ll key) {
-  ll ng = -1, ok = a.size();
-  while (abs(ok-ng)>1) {
-    ll mid = (ok+ng)/2;
-    // 条件: key以上の場合
-    if (a[mid]>=key) ok = mid;
-    else ng = mid;
-  }
-  return ok;
-}
-
-// 素因数分解
-vector<pair<ll, ll>> prime_factorize(ll n) {
-  vector<pair<ll,ll>> res;
-  for (ll a = 2; a * a <= n; a++) {
-    if (n % a != 0) continue;
-    ll ex = 0;
-    while (n % a == 0) { ++ex; n /= a; }
-    res.push_back({a, ex});
-  }
-  if (n != 1) res.push_back({n, 1});
-  return res;
-}
-
-// 約数列挙
-vector<ll> divisor(ll n) {
-  vector<ll> ret;
-  for (ll i = 1; i*i <= n; i++) {
-    if (n%i) continue;
-
-    ret.push_back(i);
-    if (i * i != n) ret.push_back(n / i);
-  }
-  sort(ret.begin(),ret.end());
-  return ret;
-}
-
-ll divisorCnt(ll n) {
-  ll cnt = 0;
-  for (ll i = 1; i*i <= n; i++) {
-    if (n%i) continue;
-    cnt += i*i == n ? 1 : 2;
-  }
-  return cnt;
-}
-
-int n;
-vector<int> a;
-
-void solve() {
   ll ans = 0;
-  map<int, int> m;
-  REP(i,n) m[a[i]]++;
-
   for(int i = -200; i <= 200; i++) {
     for(int j = i+1; j <= 200; j++) {
       ll add = (i-j)*(i-j);
@@ -182,15 +24,5 @@ void solve() {
   }
 
   cout << ans << endl;
-  return;
-}
-
-int main() {
-  IOS;
-  cin >> n;
-  a.resize(n);
-  REP(i,n) cin >> a[i];
-
-  solve();
   return 0;
 }
