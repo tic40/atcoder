a9b3290e63342e82195e019c1c2bdbad076f2849
tic40 <ccpzjoh@gmail.com>
2021-03-07 12:09:22 +0900

    Update

--- abc194/c/main.cpp
+++ abc194/c/main.cpp
@@ -2,13 +2,9 @@
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 using ll = long long;
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
 
 int main() {
-  int n;
-  cin >> n;
+  int n; cin >> n;
   map<int,int> m;
   REP(i,n) {
     int a; cin >> a;
@@ -18,8 +14,7 @@ int main() {
   ll ans = 0;
   for(int i = -200; i <= 200; i++) {
     for(int j = i+1; j <= 200; j++) {
-      ll add = (i-j)*(i-j);
-      ans += add*m[i]*m[j];
+      ans += pow(i-j,2)*m[i]*m[j];
     }
   }
 
