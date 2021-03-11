d6984a7c6123895455d6cb9629585e6190058647
tic40 <ccpzjoh@gmail.com>
2021-03-11 10:20:18 +0900

    Update

--- abc147/c/main.cpp
+++ abc147/c/main.cpp
@@ -1,43 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i, n) for(int i = 0; i < n; i++)
-#define REPR(i, n) for(int i = n; i >= 0; i--)
-#define FOR(i, m, n) for(int i = m; i < n; i++)
-typedef long long ll;
+#define REP(i,n) for(int i=0;i<n;i++)
+using ll = long long;
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
 
-int N;
-int A[20];
-int x[20][20];
-int y[20][20];
-
-int counter(int x) {
-  if(x == 0) return 0;
-  return counter(x >> 1) + (x & 1);
-}
+int n;
+int s[15][15];
 
 void solve() {
-  cin >> N;
-  for(int i = 1; i <= N; i++) {
-    cin >> A[i];
-    for(int j = 1; j <= A[i]; j++) {
-      cin >> x[i][j] >> y[i][j];
-    }
-  }
   int ans = 0;
-  for(int bits = 1; bits < (1 << N); bits++) {
-    bool ok = true;
-    for(int i = 1; i <= N; i++) {
-      if(!(bits & (1 << (i-1)))) continue;
-      for(int j = 1; j <= A[i]; j++) {
-        if(((bits >> (x[i][j]-1)) & 1) ^ y[i][j]) ok = false;
-      }
+  for (int bit = 0; bit < (1<<n); bit++) {
+    vector<int> x(n);
+    REP(i,n) if ((bit >> i) & 1) x[i] = 1;
+
+    int ok = true;
+    REP(i,n) REP(j,n) {
+      if (!x[i] || s[i][j] == -1) continue;
+      if (s[i][j] != x[j]) ok = false; // 証言が矛盾
     }
-    if(ok) ans = max(ans, counter(bits));
+
+    if (ok) {
+      int cnt = 0;
+      for(auto v: x) if (v) cnt++;
+      ans = max(ans, cnt);
+    }
   }
+
   cout << ans << endl;
   return;
 }
 
 int main() {
+  REP(i,15) REP(j,15) s[i][j] = -1;
+
+  cin >> n;
+  REP(i,n) {
+    int a; cin >> a;
+    REP(j,a) {
+      int x,y; cin >> x >> y;
+      x--;
+      s[i][x] = y;
+    }
+  }
+
   solve();
+  return 0;
 }
