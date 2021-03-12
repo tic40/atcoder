d82ff0ff3eeb9f231e211a7092aaf64188a0126e
tic40 <ccpzjoh@gmail.com>
2021-03-12 09:11:19 +0900

    Update

--- abc084/d/main.cpp
+++ abc084/d/main.cpp
@@ -1,31 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ALL(x) (x).begin(),(x).end()
-#define COUT(x) cout<<(x)<<"\n"
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define REP(i, n) for(int i=0;i<n;i++)
-#define YES(x) cout<<(x?"YES":"NO")<<"\n"
-#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
-#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
-#define endl "\n"
-using G = vector<vector<int>>;
-using M = map<int,int>;
-using P = pair<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int,vector<int>,greater<int>>;
-using V = vector<int>;
+#define REP(i,n) for(int i=0;i<n;i++)
 using ll = long long;
-using edge = struct { int to; int cost; };
-template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
-template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
 const int INF = 1e9;
 const int MOD = 1e9+7;
 const ll LINF = 1e18;
 
+int q;
+vector<int> l,r;
+
 vector<int> eratosthenes(int n) {
   vector<int> primes(n+1);
   for(int i=2; i<=n; i++) {
-    if(primes[i] >= 0) {
+    if(0 <= primes[i]) {
       for(int j=2*i; j<=n; j+=i) primes[j] = -1;
       primes[i] = 1;
     }
@@ -33,23 +20,23 @@ vector<int> eratosthenes(int n) {
   return primes;
 }
 
-int main() {
-  IOS;
-  int q; cin >> q;
-  const int N = 100005;
+void solve() {
+  vector<int> e = eratosthenes(100000);
+  vector<int> dp(100005, 0);
+  for (int i = 3; i <= 100000; i+=2) {
+    dp[i+2] = dp[i];
+    if (e[i] == 1 && e[(i+1)/2] == 1) dp[i+2]++;
+  }
 
-  vector<int> primes = eratosthenes(N);
-  vector<int> s(N,0);
-  REP(i,100001) {
-    s[i+1] = s[i];
-    if (primes[i] > 0 && primes[(i+1)/2] > 0) {
-      s[i+1]++;
-    }
-  }
-  REP(i,q) {
-    int l,r;
-    cin >> l >> r;
-    COUT(s[r+1] - s[l]);
-  }
+  REP(i,q) cout << dp[r[i]+2] - dp[l[i]] << endl;
+  return;
+}
+
+int main() {
+  cin >> q;
+  l.resize(q); r.resize(q);
+  REP(i,q) cin >> l[i] >> r[i];
+
+  solve();
   return 0;
 }
