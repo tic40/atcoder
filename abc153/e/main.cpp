6e908eb4cab174420475d63f2e89c4fb81771a89
tic40 <ccpzjoh@gmail.com>
2021-03-12 21:38:13 +0900

    Add e

--- abc153/e/main.cpp
+++ abc153/e/main.cpp
@@ -1,22 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i, n) for(int i = 0; i < n; i++)
-typedef long long ll;
+#define REP(i,n) for(int i=0;i<n;i++)
+using ll = long long;
+const int INF = 1e9;
 
-cons int INF = 1001001001;
+int h,n;
+vector<int> a,b;
+
+void solve() {
+  vector<int> dp(h+1, INF);
+  dp[0] = 0;
+
+  REP(i,h+1) REP(j,n) {
+    int ni = min(h, i + a[j]);
+    dp[ni] = min(dp[ni], dp[i] + b[j]);
+  }
+
+  cout << dp[h] << endl;
+  return;
+}
 
 int main() {
-  int h,n;
   cin >> h >> n;
-  vector<int> db(h+1, INF);
-  dp[0] = 0;
-  REP(i,n) {
-    int a,b;
-    cin >> a >> b;
-    REP(j,h) {
-      int nj = min(j+a,h);
-      dp[nj] = min(dp[nj],dp[j]+b);
-    }
-  }
-  cout << dp[h] << endl;
+  a.resize(n); b.resize(n);
+  REP(i,n) cin >> a[i] >> b[i];
+
+  solve();
+  return 0;
 }
