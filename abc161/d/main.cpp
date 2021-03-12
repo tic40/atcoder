3197bcf715d8a2d490ae315ae337155ffd720e8f
tic40 <ccpzjoh@gmail.com>
2021-03-12 16:15:34 +0900

    Update

--- abc161/d/main.cpp
+++ abc161/d/main.cpp
@@ -1,35 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i, n) for(int i = 0; i < n; i++)
-#define COUT(x) cout<<(x)<<endl
-#define dump(x)  cout << #x << " = " << (x) << endl;
+#define REP(i,n) for(int i=0;i<n;i++)
 using ll = long long;
-using P = pair<int,int>;
-using Graph = vector<vector<int>>;
-using M = map<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int, vector<int>, greater<int>>;
 const int INF = 1e9;
 const int MOD = 1e9+7;
 const ll LINF = 1e18;
 
-int main() {
-  int k; cin >> k;
-  vector<ll> ans;
+int k;
+vector<ll> r;
+
+void bfs() {
   queue<ll> q;
 
-  for (int i = 1; i<10; i++) q.push(i);
+  for(int i = 1; i < 10; i++) q.push(i);
+
+  auto push = [&](ll x) {
+    ll nx = x*10 + x%10;
+    if (0 < nx%10) q.push(nx-1);
+    q.push(nx);
+    if (nx%10 < 9) q.push(nx+1);
+  };
 
   while(!q.empty()) {
-    if (ans.size() > k) break;
-    ll n = q.front(); q.pop();
-    ans.push_back(n);
-
-    if (f > 0) q.push(n*10+(f-1));
-    q.push(n*10+f);
-    if (f < 9) q.push(n*10+f+1);
+    if (k < r.size()) break;
+    ll x = q.front(); q.pop();
+    r.push_back(x);
+    push(x);
   }
+  return;
+}
 
-  COUT(ans[k-1]);
+int main() {
+  cin >> k;
+  bfs();
+  cout << r[k-1] << endl;
   return 0;
 }
