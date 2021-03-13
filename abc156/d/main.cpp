4d87e4dcd75961c14542ba58d2f0baa50a392902
tic40 <ccpzjoh@gmail.com>
2021-03-13 13:27:46 +0900

    Update

--- abc156/d/main.cpp
+++ abc156/d/main.cpp
@@ -1,37 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i, n) for(int i = 0; i < n; i++)
+#define REP(i,n) for(int i=0;i<n;i++)
 using ll = long long;
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
 
-const int mod = 1000000007;
 struct mint {
   ll x; // typedef long long ll;
-  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint(ll x=0):x((x%MOD+MOD)%MOD){}
   mint operator-() const { return mint(-x);}
   mint& operator+=(const mint a) {
-    if ((x += a.x) >= mod) x -= mod;
+    if ((x += a.x) >= MOD) x -= MOD;
     return *this;
   }
   mint& operator-=(const mint a) {
-    if ((x += mod-a.x) >= mod) x -= mod;
+    if ((x += MOD-a.x) >= MOD) x -= MOD;
     return *this;
   }
-  mint& operator*=(const mint a) {
-    (x *= a.x) %= mod;
-    return *this;
-  }
-  mint operator+(const mint a) const {
-    mint res(*this);
-    return res+=a;
-  }
-  mint operator-(const mint a) const {
-    mint res(*this);
-    return res-=a;
-  }
-  mint operator*(const mint a) const {
-    mint res(*this);
-    return res*=a;
-  }
+  mint& operator*=(const mint a) { (x *= a.x) %= MOD; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
   mint pow(ll t) const {
     if (!t) return 1;
     mint a = pow(t>>1);
@@ -39,28 +29,13 @@ struct mint {
     if (t&1) a *= *this;
     return a;
   }
-
-  // for prime mod
-  mint inv() const {
-    return pow(mod-2);
-  }
-  mint& operator/=(const mint a) {
-    return (*this) *= a.inv();
-  }
-  mint operator/(const mint a) const {
-    mint res(*this);
-    return res/=a;
-  }
+  // for prime MOD
+  mint inv() const { return pow(MOD-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
 };
-
-// 繰り返し二乗法
-mint f(int n) {
-  if (n == 0) return 1;
-  mint x = f(n/2);
-  x *= x;
-  if (n % 2 == 1) x *= 2;
-  return x;
-}
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
 
 mint choose(int n, int a) {
   mint x = 1, y = 1;
@@ -71,9 +46,21 @@ mint choose(int n, int a) {
   return x / y;
 }
 
+mint f(int n) {
+  if (n == 0) return 1;
+  mint x = f(n/2);
+  x *= x;
+  if (n % 2 == 1) x *= 2;
+  return x;
+}
+
 int main() {
   int n,a,b;
   cin >> n >> a >> b;
-  mint ans = f(n) - 1 - choose(n,a) - choose(n,b);
-  cout << ans.x << endl;
+
+  mint tot = f(n);
+  mint ans = tot - (choose(n,a) + choose(n,b)) - 1;
+
+  cout << ans << endl;
+  return 0;
 }
