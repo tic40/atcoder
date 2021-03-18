33050790075225bbbc1d694c5363d4389278a6b5
tic40 <ccpzjoh@gmail.com>
2021-03-18 23:54:45 +0900

    Update abc152 d

--- abc152/d/main.cpp
+++ abc152/d/main.cpp
@@ -1,29 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i, n) for(int i = 0; i < n; i++)
-typedef long long ll;
+#define REP(i,n) for(int i=0;i<n;i++)
 typedef pair<int,int> P;
+using ll = long long;
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
 
 P f(int x) {
-  // a:末尾, b:先頭
   int a = x%10, b = 0;
-  while(x) { b = x; x /= 10; }
-  return P(a,b);
+  while(x) {
+    b = x%10;
+    x/=10;
+  }
+  return P(a, b);
 }
 
 int main() {
-  int n;
+  int n, ans = 0;
   cin >> n;
-  map<P,int> freq;
+  map<P, int> freq;
+
+  for(int i = 1; i <= n; i++) freq[f(i)]++;
   for(int i = 1; i <= n; i++) {
     P p = f(i);
-    freq[p]++;
+    P rp = P(p.second, p.first);
+    ans += freq[rp];
   }
-  ll ans = 0;
-  for (int i = 1; i <= n; i++) {
-    P p = f(i);
-    P q(p.second, p.first);
-    ans += freq[q];
-  }
+
   cout << ans << endl;
+  return 0;
 }
