37d90d3318e068c77aeb9fd9806e8347005ae1a5
tic40 <ccpzjoh@gmail.com>
2021-03-25 08:50:18 +0900

    Update abc129 d

--- abc129/d/main.cpp
+++ abc129/d/main.cpp
@@ -1,53 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ALL(x) (x).begin(),(x).end()
-#define COUT(x) cout<<(x)<<"\n"
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define REP(i, n) for(int i=0;i<n;i++)
-#define YES(x) cout<<(x?"YES":"NO")<<"\n"
-#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
-#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
-#define endl "\n"
-using G = vector<vector<int>>;
-using M = map<int,int>;
-using P = pair<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int,vector<int>,greater<int>>;
-using V = vector<int>;
-using ll = long long;
-using edge = struct { int to; int cost; };
-template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
-template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
+#define REP(i,n) for(int i=0;i<n;i++)
 
-int main() {
-  IOS;
-  int h,w; cin >> h >> w;
-  vector<string> s(h);
-  REP(i,h) cin >> s[i];
+int h,w;
+vector<string> s;
+int hori[2000][2000]; // 横方向
+int vert[2000][2000]; // 縦方向
 
-  vector<vector<int>> ver(h+1,vector<int>(w+1)), hor(h+1,vector<int>(w+1));
-
+void solve() {
   REP(i,h) REP(j,w) {
     if (s[i][j] == '#') continue;
-    ver[i+1][j+1] = ver[i][j+1]+1;
-    hor[i+1][j+1] = hor[i+1][j]+1;
+    hori[i][j] = j == 0 ? 1 : hori[i][j-1]+1;
+    vert[i][j] = i == 0 ? 1 : vert[i-1][j]+1;
   }
 
-  for (int i = h; i > 0; i--) {
-    for (int j = w; j > 0; j--) {
-      if (ver[i-1][j-1] == 0) continue;
-      chmax(ver[i-1][j-1], ver[i][j-1]);
-      chmax(hor[i-1][j-1], hor[i-1][j]);
-    }
+  // 逆方向からmaxを取っていく
+  for(int i=h-2; 0<=i; i--) for(int j=w-2; 0<=j; j--) {
+    if (s[i][j] == '#') continue;
+    hori[i][j] = max(hori[i][j], hori[i][j+1]);
+    vert[i][j] = max(vert[i][j], vert[i+1][j]);
   }
 
-  int ans = 1;
-  REP(i,h) REP(j,w) {
-    chmax(ans, ver[i+1][j+1]+hor[i+1][j+1]-1);
-  }
-  COUT(ans);
+  int ans = 0;
+  REP(i,h) REP(j,w) ans = max(ans, hori[i][j] + vert[i][j]-1);
+  cout << ans << endl;
+  return;
+}
+
+int main() {
+  cin >> h >> w;
+  s.resize(h);
+  REP(i,h) cin >> s[i];
+
+  solve();
   return 0;
 }
