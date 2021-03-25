0e620b2052e5938f3e9a5cb6f7db4db2c181e58f
tic40 <ccpzjoh@gmail.com>
2021-03-25 10:00:11 +0900

    Update abc042 c

--- abc042/c/main.cpp
+++ abc042/c/main.cpp
@@ -1,51 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ALL(x) (x).begin(),(x).end()
-#define COUT(x) cout<<(x)<<"\n"
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define REP(i, n) for(int i=0;i<n;i++)
-#define YES(x) cout<<(x?"YES":"NO")<<"\n"
-#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
-#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
-#define endl "\n"
-using G = vector<vector<int>>;
-using M = map<int,int>;
-using P = pair<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int,vector<int>,greater<int>>;
-using V = vector<int>;
+#define REP(i,n) for(int i=0;i<n;i++)
 using ll = long long;
-using edge = struct { int to; int cost; };
-template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
-template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
 const int INF = 1e9;
 const int MOD = 1e9+7;
 const ll LINF = 1e18;
 
+int n,k;
 string s;
-int k, ans = INF;
-vector<int> l;
+vector<int> d;
 
-void dfs(string t = "") {
+int dfs(string t = "") {
   if (s.size() <= t.size()) {
-    if (stoi(s) <= stoi(t)) chmin(ans, stoi(t));
-    if (s.size() < t.size()) return;
+    int ti = stoi(t);
+    if (n <= ti) return ti;
+    if (s.size() < t.size()) return INF;
   }
-  for (int v: l) dfs(t + (char)('0' + v));
+
+  int ans = INF;
+  for (char c: d) {
+    string nt = t + char('0'+c);
+    ans = min(ans, dfs(nt));
+  }
+  return ans;
 }
 
 int main() {
-  IOS;
-  cin >> s >> k;
-  M m;
+  cin >> n >> k;
+  vector<int> tmp(10);
   REP(i,k) {
-    int d; cin >> d;
-    m[d]++;
+    int _; cin >> _;
+    tmp[_] = 1;
   }
+  REP(i,10) if (tmp[i] == 0) d.push_back(i);
+  s = to_string(n);
 
-  REP(i,10) if (m[i] == 0) l.push_back(i);
-  dfs();
-  COUT(ans);
-
+  cout << dfs() << endl;
   return 0;
 }
