7724e1aeedd9cc28b380ff5e2aebe27633a37300
tic40 <ccpzjoh@gmail.com>
2021-03-30 09:13:40 +0900

    Update

--- panasonic2020/d/main.cpp
+++ panasonic2020/d/main.cpp
@@ -1,43 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ALL(x) (x).begin(),(x).end()
-#define COUT(x) cout<<(x)<<"\n"
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define REP(i, n) for(int i=0;i<n;i++)
-#define YES(x) cout<<(x?"YES":"NO")<<"\n"
-#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
-#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
-#define endl "\n"
-using G = vector<vector<int>>;
-using M = map<int,int>;
-using P = pair<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int,vector<int>,greater<int>>;
-using V = vector<int>;
+#define REP(i,n) for(int i=0;i<n;i++)
 using ll = long long;
-using edge = struct { int to; int cost; };
-template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
-template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
 const int INF = 1e9;
 const int MOD = 1e9+7;
 const ll LINF = 1e18;
 
 int n;
+void dfs(string s, char mx) {
+  if (s.size() == n) {
+    cout << s << endl;
+    return;
+  }
 
-void dfs(string s = "", char mx = 'a') {
-  if (s.size() == n) { COUT(s); return; }
-
-  int p = mx-'a';
-  REP(i,p+1) {
-    string ns = s + (char)('a' + i);
-    char nmx = (char)('a' + p+(i==p));
-    dfs(ns, nmx);
+  for(char c = 'a'; c <= mx+1; c++) {
+    string t = s;
+    t += c;
+    dfs(t, max(mx,c));
   }
 }
 
 int main() {
-  IOS;
   cin >> n;
-  dfs();
+  dfs("", 'a'-1);
+
   return 0;
 }
