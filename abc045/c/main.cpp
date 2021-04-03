04496c770b0cac2aa29034a5c737130ac28d0d1d
tic40 <ccpzjoh@gmail.com>
2021-04-03 15:05:33 +0900

    Update abc045 c

--- abc045/c/main.cpp
+++ abc045/c/main.cpp
@@ -1,15 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+#define REP(i,n) for(int i=0;i<n;i++)
+using ll = long long;
+
 string s;
 
-ll solve(int i, ll left, ll sum) {
-  return s[i] == '\0'
-    ? left+sum
-    : solve(i+1, left*10 + (s[i]-'0'), sum) + solve(i+1, s[i]-'0', sum + left);
+ll dfs(int i, ll left, ll sum) {
+  if (i == s.size()) return left + sum;
+
+  int cur = s[i]-'0';
+  ll res1 = dfs(i+1, left*10+cur, sum);
+  ll res2 = dfs(i+1, cur, sum+left);
+  return res1 + res2;
 }
 
 int main() {
   cin >> s;
-  cout << solve(1, s[0]-'0', 0) << endl;
+  cout << dfs(1,s[0]-'0',0) << endl;
+  return 0;
 }
