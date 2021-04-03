b993d52d447deb901a9a7a553478bc635785a92e
tic40 <ccpzjoh@gmail.com>
2021-04-03 14:19:23 +0900

    Update abc140 d

--- abc140/d/main.cpp
+++ abc140/d/main.cpp
@@ -1,34 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ALL(x) (x).begin(),(x).end()
-#define COUT(x) cout<<(x)<<"\n"
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define REP(i, n) for(int i=0;i<n;i++)
-#define YES(x) cout<<(x?"YES":"NO")<<"\n"
-#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
-#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
-#define endl "\n"
-using G = vector<vector<int>>;
-using M = map<int,int>;
-using P = pair<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int,vector<int>,greater<int>>;
-using V = vector<int>;
+#define REP(i,n) for(int i=0;i<n;i++)
 using ll = long long;
-using edge = struct { int to; int cost; };
-template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
-template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
+
+int n,k;
+string s;
+
+void solve() {
+  int ans = n-1, cnt = 0;
+  REP(i,n-1) if (s[i+1] != s[i]) cnt++;
+
+  ans -= max(0, cnt - k*2);
+  cout << ans << endl;
+  return;
+}
 
 int main() {
-  IOS;
-  int n,k; cin >> n >> k;
-  string s; cin >> s;
+  cin >> n >> k >> s;
 
-  int ans=0;
-  REP(i,n-1) if (s[i+1] == s[i]) ans++;
-  COUT( min(ans+2*k, n-1) );
+  solve();
   return 0;
 }
