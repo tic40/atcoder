eedd747c32b572c8f4cbde3c79896ccfa6c7b328
tic40 <ccpzjoh@gmail.com>
2021-04-10 11:38:10 +0900

    Update abc048 c

--- abc048/c/main.cpp
+++ abc048/c/main.cpp
@@ -1,16 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i, n) for(int i = 0; i < n; i++)
-typedef long long ll;
+#define REP(i,n) for(int i=0;i<n;i++)
+using ll = long long;
 
 int main() {
-  ll ans=0;
-  int n,x,a,b=0;
-  cin >> n >> x;
-  REP(i,n) {
-    cin >> a;
-    int over = max(0, a+b-x);
-    ans += over; b = a-over;
+  int n,x; cin >> n >> x;
+  vector<int> a(n);
+  REP(i,n) cin >> a[i];
+
+  ll ans = 0;
+  REP(i,n-1) {
+    if (x < a[i]+a[i+1]) {
+      int diff = a[i] + a[i+1] - x;
+      ans += diff;
+      a[i+1] = max(0, a[i+1]-diff);
+    }
   }
+
   cout << ans << endl;
+  return 0;
 }
