e9c1ea978d24137af0db54242192475165bff74e
tic40 <ccpzjoh@gmail.com>
2021-05-03 09:56:34 +0900

    Update

--- abc199/d/main.cpp
+++ abc199/d/main.cpp
@@ -5,48 +5,53 @@ using ll = long long;
 
 int n,m;
 vector<vector<int>> g;
-vector<int> used,col,vs;
-ll now;
+vector<int> used, col;
+vector<int> vs;
+int now;
 
 void dfs(int v) {
   if (used[v]) return;
-  used[v]=1;
-  vs.push_back(v);
 
-  for (int u: g[v]) dfs(u);
+  used[v] = 1;
+  vs.push_back(v);
+  for(int u: g[v]) dfs(u);
   return;
 }
 
 void dfs2(int i) {
+  // 最後までたどり着いたらnow(通りの数)をインクリメントする
   if (i == vs.size()) {
-    now++;
-    return;
+    now++; return;
   }
+
   int v = vs[i];
   REP(c,3) {
     col[v] = c;
     bool ok = true;
+    // 隣接する頂点の色が同じでないかどうか
     for (int u: g[v]) {
       if (col[u] == c) ok = false;
     }
-    if (!ok) continue;
-    dfs2(i+1);
+    if (ok) dfs2(i+1);
   }
+  // colを元に戻す
   col[v] = -1;
   return;
 }
 
 void solve() {
-  ll ans = 1;
   used.resize(n);
   col.resize(n,-1);
+
+  ll ans = 1;
   REP(i,n) {
     if (used[i]) continue;
-    ans*=3;
+
     vs = vector<int>();
     dfs(i);
-    col[vs[0]] = 0;
     now = 0;
+    ans *= 3;
+    col[vs[0]]=0;
     dfs2(1);
     ans *= now;
   }
@@ -54,7 +59,6 @@ void solve() {
   cout << ans << endl;
   return;
 }
-
 
 int main() {
   cin >> n >> m;
