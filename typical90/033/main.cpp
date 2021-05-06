5b23b7c7f2604cd7fe3a962a5cc7f54019e12040
tic40 <ccpzjoh@gmail.com>
2021-05-07 08:38:21 +0900

    Solve 033

---
+++ typical90/033/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i,n) for(int i=0;i<n;i++)
+
+int main() {
+  int h,w;
+  cin >> h >> w;
+
+  // コーナーケース
+  if (h==1 || w==1) cout << h*w << endl;
+  else cout << ((h+1)/2) * ((w+1)/2) << endl;
+  return 0;
+}
