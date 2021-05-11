e2fd1414226cab7be6e30a120d459e6ae7b737ed
tic40 <ccpzjoh@gmail.com>
2021-05-12 00:17:39 +0900

    Update

--- abc070/d/main.cpp
+++ abc070/d/main.cpp
@@ -1,60 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ALL(x) (x).begin(),(x).end()
-#define COUT(x) cout<<(x)<<"\n"
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define REP(i, n) for(int i=0;i<n;i++)
-#define YES(x) cout<<(x?"YES":"NO")<<"\n"
-#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
-#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
-#define endl "\n"
-using G = vector<vector<int>>;
-using M = map<int,int>;
-using P = pair<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int,vector<int>,greater<int>>;
-using V = vector<int>;
+#define REP(i,n) for(int i=0;i<n;i++)
 using ll = long long;
-using edge = struct { int to; int cost; };
-template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
-template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
 const int INF = 1e9;
 const int MOD = 1e9+7;
 const ll LINF = 1e18;
+using P = pair<int, int>;
 
-ll depth[100005];
-vector<edge> g[100005];
+int n,q,k;
+vector<vector<P>> g;
+vector<ll> x,y,d;
 
-void dfs(int v, int p, ll d) {
-  depth[v] = d;
-  for (auto e : g[v]) {
-    if (e.to == p) continue;
-    dfs(e.to, v, d + e.cost);
+void dfs(int cur) {
+  for(auto v: g[cur]) {
+    int to = v.first;
+    int c = v.second;
+    ll nd = d[cur]+c;
+    if (nd < d[to]) {
+      d[to] = nd;
+      dfs(to);
+    }
   }
+  return;
+}
+
+void solve() {
+  d.resize(n, LINF);
+  d[k] = 0;
+  dfs(k);
+  REP(i,q) cout << d[x[i]] + d[y[i]] << endl;
+
+  return;
 }
 
 int main() {
-  IOS;
-
-  int n; cin >> n;
+  cin >> n;
+  g.resize(n);
+  int a,b,c;
   REP(i,n-1) {
-    int a,b,c;
     cin >> a >> b >> c;
     a--; b--;
-    g[a].push_back({b, c});
-    g[b].push_back({a, c});
+    g[a].emplace_back(P({b, c}));
+    g[b].emplace_back(P({a, c}));
   }
-  int q,k; cin >> q >> k;
+
+  cin >> q >> k;
   k--;
-
-  dfs(k, -1, 0);
-
+  x.resize(q);
+  y.resize(q);
   REP(i,q) {
-    int x,y;
-    cin >> x >> y;
-    x--; y--;
-    cout << depth[x] + depth[y] << endl;
+    cin >> x[i] >> y[i];
+    x[i]--; y[i]--;
   }
 
+  solve();
   return 0;
 }
