1cc0f83cf03cfa666e90c441fc0fc053b8d707cf
tic40 <ccpzjoh@gmail.com>
2021-05-15 13:46:24 +0900

    Update

--- abc109/d/main.cpp
+++ abc109/d/main.cpp
@@ -1,52 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ALL(x) (x).begin(),(x).end()
-#define COUT(x) cout<<(x)<<"\n"
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define REP(i, n) for(int i=0;i<n;i++)
-#define YES(x) cout<<(x?"YES":"NO")<<"\n"
-#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
-#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
-#define endl "\n"
-using G = vector<vector<int>>;
-using M = map<int,int>;
-using P = pair<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int,vector<int>,greater<int>>;
-using V = vector<int>;
-using ll = long long;
-using edge = struct { int to; int cost; };
-template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
-template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
+#define REP(i,n) for(int i=0;i<n;i++)
+
+int h,w;
+int a[500][500];
 
 int main() {
-  IOS;
-  int h,w; cin >> h >> w;
-  int a[h][w];
+  cin >> h >> w;
   REP(i,h) REP(j,w) cin >> a[i][j];
 
-  vector<tuple<int,int,int,int>> ans;
-  REP(i,h-1) REP(j,w) {
-    if(a[i][j]%2==0) continue;
-    a[i][j]--; a[i+1][j]++;
-    ans.push_back( make_tuple(i,j,i+1,j) );
-  }
-  REP(j,w-1) {
-    if (a[h-1][j]%2==0) continue;
-    a[h-1][j]--; a[h-1][j+1]++;
-    ans.push_back( make_tuple(h-1,j,h-1,j+1) );
+  vector<vector<int>> ans;
+  auto rec = [&](int y, int x, int ny, int nx) {
+    if (h <= ny || w <= nx) return false;
+    ans.push_back({ y, x, ny, nx });
+    a[y][x]--;
+    a[ny][nx]++;
+    return true;
+  };
+
+  REP(i,h) REP(j,w) {
+    // 偶数枚はスキップ
+    if (a[i][j] % 2 == 0) continue;
+    // 右方向に配る
+    if (rec(i,j,i,j+1)) continue;
+    // 下方向に配る
+    rec(i,j,i+1,j);
   }
 
-  COUT(ans.size());
-  for(auto x: ans) {
-    cout
-      << get<0>(x)+1 << " "
-      << get<1>(x)+1 << " "
-      << get<2>(x)+1 << " "
-      << get<3>(x)+1 << endl;
+  // output
+  cout << ans.size() << endl;
+  for(auto v: ans) {
+    REP(i,4) {
+      if (i != 0) cout << " ";
+      cout << v[i]+1;
+    }
+    cout << endl;
   }
 
   return 0;
