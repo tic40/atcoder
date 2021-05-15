f21e6622d8020d1abf1ba3e3236d875b6df5b347
tic40 <ccpzjoh@gmail.com>
2021-05-15 14:30:09 +0900

    Update

--- abc105/c/main.cpp
+++ abc105/c/main.cpp
@@ -1,34 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i, n) for(int i = 0; i < n; i++)
-#define COUT(x) cout<<(x)<<endl
-#define dump(x)  cout << #x << " = " << (x) << endl;
-using ll = long long;
-using P = pair<int,int>;
-using Graph = vector<vector<int>>;
-using M = map<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int, vector<int>, greater<int>>;
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
+#define REP(i,n) for(int i=0;i<n;i++)
 
 int main() {
   int n; cin >> n;
+  vector<int> ans;
 
-  if (n == 0) { COUT(0); return 0; }
-
-  vector<int> a;
-  while(n != 0) {
-    int tmp = n % 2;
-    if (tmp < 0) tmp+=2;
-
-    a.push_back(tmp);
-    n = (n-tmp) / -2;
+  while (n != 0) {
+    if (n%2 != 0) {
+      ans.push_back(1);
+      n--;
+    } else {
+      ans.push_back(0);
+    }
+    n /= -2;
   }
 
-  reverse(a.begin(), a.end());
-  for(auto v: a) cout << v;
+  reverse(ans.begin(), ans.end());
+  if (ans.size()) for(int v: ans) cout << v;
+  else cout << 0;
   cout << endl;
 
   return 0;
