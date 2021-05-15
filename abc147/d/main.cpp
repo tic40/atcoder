409ff6fd2f1b1b6e79cc8e179dfadd764f09cd3e
tic40 <ccpzjoh@gmail.com>
2021-05-15 12:19:16 +0900

    Update

--- abc147/d/main.cpp
+++ abc147/d/main.cpp
@@ -1,44 +1,78 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ALL(x) (x).begin(),(x).end()
-#define COUT(x) cout<<(x)<<"\n"
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define REP(i, n) for(int i=0;i<n;i++)
-#define YES(x) cout<<(x?"YES":"NO")<<"\n"
-#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
-#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
-#define endl "\n"
-using G = vector<vector<int>>;
-using M = map<int,int>;
-using P = pair<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int,vector<int>,greater<int>>;
-using V = vector<int>;
+#define REP(i,n) for(int i=0;i<n;i++)
 using ll = long long;
-using edge = struct { int to; int cost; };
-template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
-template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
 const int INF = 1e9;
 const int MOD = 1e9+7;
 const ll LINF = 1e18;
 
-int main() {
-  IOS;
-  int n; cin >> n;
-  vector<ll> a(n);
-  REP(i,n) cin >> a[i];
-  ll ans = 0;
-
-  // 60bitまで
-  REP(i,60) {
-    ll x = 0;
-    REP(j,n) if (a[j]>>i&1) x++;
-    ll now = x*(n-x)%MOD; // (1の数)*(0の数): 0と1のペアを選ぶ
-    REP(j,i) now = now*2%MOD;
-    ans += now;
-    ans %= MOD;
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%MOD+MOD)%MOD){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= MOD) x -= MOD;
+    return *this;
   }
+  mint& operator-=(const mint a) {
+    if ((x += MOD-a.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= MOD; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+  // for prime MOD
+  mint inv() const { return pow(MOD-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
 
-  COUT(ans);
+int n;
+vector<ll> a;
+
+// 愚直
+void solve1() {
+  mint x = 0;
+  REP(i,n) for(int j = i+1; j < n; j++) {
+    x += a[i] ^ a[j];
+  }
+  cout << x << endl;
+  return;
+}
+
+void solve2() {
+  mint x = 1;
+  mint ans = 0;
+  // bitの桁ごとに計算する
+  REP(i,60) {
+    ll zero = 0, one = 0;
+    REP(j,n) {
+      if (a[j] >> i & 1) one++;
+      else zero++;
+    }
+    ans += x * zero * one;
+    x = x*2;
+  }
+  cout << ans << endl;
+  return;
+}
+
+int main() {
+  cin >> n;
+  a.resize(n);
+  REP(i,n) cin >> a[i];
+
+  // solve1();
+  solve2();
   return 0;
 }
