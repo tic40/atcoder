23e2995b0b367fc30c4bf958079b64d59f53e434
tic40 <ccpzjoh@gmail.com>
2021-05-15 14:36:09 +0900

    Update

--- abc105/c/main.cpp
+++ abc105/c/main.cpp
@@ -2,23 +2,27 @@
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 
+vector<int> convert_base(int n, int base=10) {
+  vector<int> a;
+  while(n) {
+    // baseが負のときを考慮してabsを取る
+    int m = abs(n%base);
+    a.push_back(m);
+    // baseが負のとき
+    if (base < 0 && m) n--;
+    n /= base;
+  }
+  reverse(a.begin(), a.end());
+  return a;
+}
+
 int main() {
   int n; cin >> n;
-  vector<int> ans;
+  auto ans = convert_base(n, -2);
 
-  while (n != 0) {
-    if (n%2 != 0) {
-      ans.push_back(1);
-      n--;
-    } else {
-      ans.push_back(0);
-    }
-    n /= -2;
-  }
-
-  reverse(ans.begin(), ans.end());
   if (ans.size()) for(int v: ans) cout << v;
   else cout << 0;
+
   cout << endl;
 
   return 0;
