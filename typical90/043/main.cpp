cc1bf5b5c0b83e361b4e02338b7f5ac505a68b13
tic40 <ccpzjoh@gmail.com>
2021-05-20 23:21:26 +0900

    SUpdate

--- typical90/043/main.cpp
+++ typical90/043/main.cpp
@@ -1,10 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
-using ll = long long;
 const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
 
 int h,w;
 int sy,sx,gy,gx;
