a0f0c200f50672dc27ebc50b5f047b5aa56df4b7
tic40 <ccpzjoh@gmail.com>
2021-07-11 12:31:15 +0900

    Update

--- typical90/039/main.cpp
+++ typical90/039/main.cpp
@@ -2,34 +2,29 @@
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 using ll = long long;
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
 
 int n;
+vector<int> a,b,dp;
 vector<vector<int>> g;
-vector<int> a,b,dp(100000);
 
 void dfs(int cur, int pre) {
   dp[cur] = 1;
   for(int nv: g[cur]) {
     if (nv == pre) continue;
     dfs(nv, cur);
-    dp[cur]+=dp[nv];
+    dp[cur] += dp[nv];
   }
   return;
 }
 
-void solve() {
+ll solve() {
   dfs(0, -1);
-
   ll ans = 0;
-  REP(i,n) {
+  REP(i,n-1) {
     ll r = min(dp[a[i]], dp[b[i]]);
     ans += r * (n-r);
   }
-  cout << ans << endl;
-  return;
+  return ans;
 }
 
 int main() {
@@ -37,13 +32,15 @@ int main() {
   a.resize(n);
   b.resize(n);
   g.resize(n);
+  dp.resize(n);
+
   REP(i,n-1) {
     cin >> a[i] >> b[i];
-    a[i]--; b[i]--;
+    a[i]--, b[i]--;
     g[a[i]].push_back(b[i]);
     g[b[i]].push_back(a[i]);
   }
 
-  solve();
+  cout << solve() << endl;
   return 0;
 }
