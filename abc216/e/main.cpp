2f221823a264413fd1c9dd58c1aea6e33dcf46dc
tic40 <ccpzjoh@gmail.com>
2021-09-01 00:25:34 +0900

    Update

--- abc216/e/main.cpp
+++ abc216/e/main.cpp
@@ -2,41 +2,38 @@
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 using ll = long long;
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
 
-ll n,k;
+int n,k;
 vector<ll> a;
 
 ll binary_search() {
-  ll ok = 2 * INF, ng = 0;
+  ll ok = 2*1e9; // k回以下乗れるときの数
+  ll ng = 0; // k回以下乗れないときの数
 
   while (abs(ok-ng)>1) {
     ll mid = (ok+ng)/2;
-    ll cnt = 0;
-    // 数列に含まれるmid以上の値はk個以下か
+    ll cnt = 0; // 選んだ数
     REP(i,n) cnt += max(0LL, a[i]-mid+1);
+
     if (cnt <= k) ok = mid;
     else ng = mid;
   }
   return ok;
 }
 
-
 void solve() {
   sort(a.begin(), a.end());
+  ll p = binary_search();
 
-  ll m = binary_search();
-  ll ans = 0, cnt = 0;
+  ll ans = 0;
+  int cnt = 0;
   REP(i,n) {
-    if (m <= a[i]) {
-      ll d = a[i]-m+1;
-      ans += (a[i] + m) * d / 2;
-      cnt += d;
+    if (p <= a[i]) {
+      ans += (a[i] + p) * (0, a[i]-p+1) / 2;
+      cnt += a[i] - p + 1;
     }
   }
-  if (cnt < k) ans += (m-1) * (k-cnt);
+  if (0 < k-cnt) ans += (p-1) * (k-cnt);
 
   cout << ans << endl;
   return;
