b681119ae1d5389a5528db08fbfec5991df7e31d
tic40 <ccpzjoh@gmail.com>
2021-08-31 09:24:15 +0900

    Update

--- abc216/d/main.cpp
+++ abc216/d/main.cpp
@@ -3,56 +3,46 @@ using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 
 int n,m;
-vector<int> k;
 vector<queue<int>> a;
-// mem[i] := iは色番号, 配列で筒番号を持つ
 vector<vector<int>> mem;
+queue<int> que;
 
 void solve() {
-  // 筒の先頭に同じ色がある色番号を入れる
-  queue<int> que;
-
-  REP(i,n) {
-    if (mem[i].size() == 2) que.push(i);
-  }
-
   while(que.size()) {
-    int now = que.front(); que.pop();
-    for(int p: mem[now]) {
-      a[p].pop();
-      if (a[p].size()) {
-        mem[a[p].front()].push_back(p);
-        if (mem[a[p].front()].size() == 2) {
-          que.push(a[p].front());
-        }
+    int color = que.front(); que.pop();
+    for(int v: mem[color]) {
+      a[v].pop();
+      if (a[v].size()) {
+        int ncolor = a[v].front();
+        mem[ncolor].push_back(v);
+        if (mem[ncolor].size() == 2) que.push(ncolor);
       }
     }
   }
 
-  for(auto p: a) {
-    if (p.size()) {
-      cout << "No" << endl;
-      return;
-    }
+  int ok = true;
+  REP(i,m) {
+    if (a[i].size()) ok = false;
   }
 
-  cout << "Yes" << endl;
+  cout << (ok ? "Yes" : "No") << endl;
   return;
 }
 
 int main() {
   cin >> n >> m;
-  k.resize(m);
   a.resize(m);
-  mem.resize(n);
+  mem.resize(n+1);
+
   REP(i,m) {
-    cin >> k[i];
-    REP(j,k[i]) {
+    int k; cin >> k;
+    REP(j,k) {
       int _a; cin >> _a;
-      _a--;
       a[i].push(_a);
     }
-    mem[a[i].front()].push_back(i);
+    int color = a[i].front();
+    mem[color].push_back(i);
+    if (mem[color].size() == 2) que.push(color);
   }
 
   solve();
