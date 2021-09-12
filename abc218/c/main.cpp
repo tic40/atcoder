902b4d7755da8eb6a8514d5dea2a48fddb12940b
tic40 <ccpzjoh@gmail.com>
2021-09-12 12:01:15 +0900

    Update

--- abc218/c/main.cpp
+++ abc218/c/main.cpp
@@ -1,50 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
+using P = pair<int,int>;
 
-int n;
-vector<pair<int,int>> sp,tp;
+void move_to_origin(vector<P> &p) {
+  sort(p.begin(), p.end());
+  auto [a,b] = p[0];
+  for (auto &v: p) v = { v.first-a, v.second-b };
+  return;
+}
 
-void solve() {
+void solve(vector<P> sp, vector<P> tp) {
+  // 原点に平行移動
+  move_to_origin(sp);
+  move_to_origin(tp);
+
   REP(i,4) {
-    sort(sp.begin(), sp.end());
-    sort(tp.begin(), tp.end());
-
-    // 原点に移動する
-    int s_first = sp[0].first;
-    int s_second = sp[0].second;
-    REP(i,sp.size()) {
-      sp[i] = { sp[i].first - s_first, sp[i].second - s_second };
-    }
-    int t_first = tp[0].first;
-    int t_second = tp[0].second;
-    REP(i,tp.size()) {
-      tp[i] = { tp[i].first - t_first, tp[i].second - t_second };
-    }
-
-    if (sp == tp) { // 一致したら終了
-      cout << "Yes" << endl;
-      return;
-    }
+    // 一致したら終了
+    if (sp == tp) { cout << "Yes" << endl; return; }
 
     // 90度回転させる
-    REP(i,sp.size()) sp[i] = { -sp[i].second, sp[i].first };
+    for (auto &v: sp) v = { -v.second, v.first };
+    move_to_origin(sp);
   }
+
   cout << "No" << endl;
   return;
 }
 
 int main() {
-  cin >> n;
+  int n; cin >> n;
+
+  vector<P> sp,tp;
   REP(i,n) REP(j,n) {
     char c; cin >> c;
-    if (c == '#') sp.push_back({i,j});
+    if (c == '#') sp.emplace_back(P{i,j});
   }
   REP(i,n) REP(j,n) {
     char c; cin >> c;
-    if (c == '#') tp.push_back({i,j});
+    if (c == '#') tp.emplace_back(P{i,j});
   }
 
-  solve();
+  solve(sp,tp);
   return 0;
 }
