7aa791603f4e2abe01c579b946a6ad2cecf8070e
tic40 <ccpzjoh@gmail.com>
2021-09-27 22:40:51 +0900

    Update

--- template_simple.cpp
+++ template_simple.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
+#define REP(i,n) for(int i=0;i<(int)(n);i++)
+#define ALL(x) x.begin(), x.end()
 using ll = long long;
 const int INF = 1e9;
-const int MOD = 1e9+7;
 const ll LINF = 1e18;
+const int MOD = 1e9+7;
 
 int main() {
   return 0;
