75f580374319100eeb3e7b54b77c3ef23dfa6d5e
tic40 <ccpzjoh@gmail.com>
2021-10-15 02:00:57 +0900

    Solve 442

---
+++ algo-method/442/main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i,n) for(int i=0;i<(int)(n);i++)
+
+vector<int> quiq_sort(vector<int> a) {
+  int mid = a.size() / 2;
+  vector<int> l,r;
+
+  REP(i,a.size()) {
+    if (i == mid) continue;
+
+    if (a[i] < a[mid]) l.push_back(a[i]);
+    else r.push_back(a[i]);
+  }
+
+  vector<int> res;
+  if (l.size()) for (int v: quiq_sort(l)) res.push_back(v);
+  res.push_back(a[mid]);
+  if (r.size()) for (int v: quiq_sort(r)) res.push_back(v);
+  return res;
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  REP(i,n) cin >> a[i];
+
+  auto ans = quiq_sort(a);
+  for (int v: ans) cout << v << " ";
+  cout << endl;
+
+  return 0;
+}
