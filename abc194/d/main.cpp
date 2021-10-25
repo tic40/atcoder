560097c15207256f56c7f03be716208454098c7a
tic40 <ccpzjoh@gmail.com>
2021-10-25 23:23:54 +0900

    Solve 194 d

---
+++ abc194/d/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i,n) for(int i=0;i<(int)(n);i++)
+
+int main() {
+  int n; cin >> n;
+  double ans = 0;
+  for(int i = 1; i < n; i++) ans += (double)n/(n-i);
+  printf("%0.6f\n", ans);
+  return 0;
+}
