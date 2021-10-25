a1035c60310d113f9c623cf09659bb445121b0fe
tic40 <ccpzjoh@gmail.com>
2021-10-26 00:07:47 +0900

    Update

--- abc194/c/main.cpp
+++ abc194/c/main.cpp
@@ -1,23 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
+#define REP(i,n) for(int i=0;i<(int)(n);i++)
+#define ALL(x) x.begin(), x.end()
 using ll = long long;
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9+7;
 
 int main() {
   int n; cin >> n;
-  map<int,int> m;
-  REP(i,n) {
-    int a; cin >> a;
-    m[a]++;
-  }
+  vector<ll> a(n), b(n);
+  REP(i,n) cin >> a[i];
+  REP(i,n) b[i] = a[i]*a[i];
+
+  vector<ll> as(n+1), bs(n+1);
+  REP(i,n) as[i+1] = as[i]+a[i];
+  REP(i,n) bs[i+1] = bs[i]+b[i];
 
   ll ans = 0;
-  for(int i = -200; i <= 200; i++) {
-    for(int j = i+1; j <= 200; j++) {
-      ans += pow(i-j,2)*m[i]*m[j];
-    }
+  REP(i,n-1) {
+    ans += b[i]*(n-1-i);
+    ans += bs[n] - bs[i+1];
+    ans -= 2*a[i]*(as[n]-as[i+1]);
   }
-
   cout << ans << endl;
   return 0;
 }
