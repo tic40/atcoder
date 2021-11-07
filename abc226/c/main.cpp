458ff8604d94ec32be217bfb16abb3327d314288
tic40 <ccpzjoh@gmail.com>
2021-11-08 00:28:27 +0900

    Update

--- abc226/c/main.cpp
+++ abc226/c/main.cpp
@@ -1,11 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define REP(i,n) for(int i=0;i<(int)(n);i++)
-#define ALL(x) x.begin(), x.end()
 using ll = long long;
-const int INF = 1e9;
-const ll LINF = 1e18;
-const int MOD = 1e9+7;
 
 int n;
 vector<ll> t;
@@ -13,14 +9,13 @@ vector<vector<int>> g;
 vector<bool> ok;
 
 ll dfs(int cur) {
-  ll res = 0;
-  if (ok[cur]) return res;
+  if (ok[cur]) return 0;
 
+  ll res = t[cur];
   for(int v: g[cur]) {
-    if (ok[v]) continue; // 習得済み
+    if (ok[v]) continue;
     res += dfs(v);
   }
-  res += t[cur];
   ok[cur] = true;
   return res;
 }
@@ -41,8 +36,6 @@ int main() {
     }
   }
 
-  ll ans = dfs(n-1);
-  cout << ans << endl;
-
+  cout << dfs(n-1) << endl;
   return 0;
 }
