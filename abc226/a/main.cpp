7af34284bf1203e2e7aaeb80cdbede8c53cbbe4b
tic40 <ccpzjoh@gmail.com>
2021-11-07 23:30:39 +0900

    Update

--- abc226/a/main.cpp
+++ abc226/a/main.cpp
@@ -3,7 +3,15 @@ using namespace std;
 #define REP(i,n) for(int i=0;i<(int)(n);i++)
 
 int main() {
+  /*
   double n; cin >> n;
   cout << round(n) << endl;
+  */
+  // 誤差を考慮
+  int a,b;
+  scanf("%d.%d", &a, &b);
+  if (b/100 >= 5) a++;
+  cout << a << endl;
+
   return 0;
 }
