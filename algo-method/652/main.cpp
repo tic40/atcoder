05a1a7e063e4bc623d08436cd1f72297b05db3c6
tic40 <ccpzjoh@gmail.com>
2021-11-13 20:12:00 +0900

    Solve 652

---
+++ algo-method/652/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i,n) for(int i=0;i<(int)(n);i++)
+#define ALL(x) x.begin(), x.end()
+using ll = long long;
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9+7;
+
+int main() {
+  ll n; cin >> n;
+  ll ans = (1 + n)*n/2;
+  cout << ans << endl;
+  return 0;
+}
