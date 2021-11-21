f4d8346ff689fa772d18569678cf3a424e9dc3f6
tic40 <ccpzjoh@gmail.com>
2021-11-21 23:16:20 +0900

    Update

--- abc228/d/main.cpp
+++ abc228/d/main.cpp
@@ -6,14 +6,14 @@ using ll = long long;
 const int INF = 1e9;
 
 int q;
-ll n = 1048576; // 2^20 = 1048576
+ll n = 1<<20;
 vector<ll> a(n,-1);
 
 int main() {
   cin >> q;
 
   set<int> st;
-  REP(i,n+1) st.insert(i);
+  REP(i,n) st.insert(i);
 
   REP(i,q) {
     int t;
@@ -22,15 +22,9 @@ int main() {
     if (t == 1) {
       ll h = x % n;
       auto it = st.lower_bound(h);
-      if (*it < n) {
-        a[*it] = x;
-        st.erase(*it);
-      } else {
-        h = 0;
-        auto it = st.lower_bound(h);
-        a[*it] = x;
-        st.erase(*it);
-      }
+      if (it == st.end()) it = st.begin();
+      a[*it] = x;
+      st.erase(*it);
     } else {
       cout << a[x%n] << endl;
     }
