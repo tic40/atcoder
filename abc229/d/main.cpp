7d5dfcf7267d893baff01dc7841dde30aefd631e
tic40 <ccpzjoh@gmail.com>
2021-11-28 11:40:40 +0900

    Update

--- abc229/d/main.cpp
+++ abc229/d/main.cpp
@@ -9,21 +9,21 @@ const ll LINF = 1e18;
 const int MOD = 1e9+7;
 
 int main() {
-  string s; int k;
+  string s;
+  int k;
   cin >> s >> k;
   int n = s.size();
 
   vector<int> cnt(n+1);
-  REP(i,n) {
-    cnt[i+1] = cnt[i];
-    if (s[i] == '.') cnt[i+1]++;
-  }
+  REP(i,n) cnt[i+1] = (s[i] == '.') ? cnt[i]+1 : cnt[i];
 
-  int ans = 0;
-  int r = 0;
+  int ans = 0, r = 0;
   REP(l,n) {
-    while(r < n && cnt[r+1] - cnt[l] <= k) r++;
-    ans = max(ans, r-l);
+    while(r < n) {
+      if (cnt[r+1] - cnt[l] > k) break;
+      r++;
+    }
+    ans = max(ans,r-l);
   }
 
   cout << ans << endl;
