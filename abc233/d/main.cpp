bf9f09e05f0355222d234e509917907ef555de4f
tic40 <ccpzjoh@gmail.com>
2021-12-25 23:08:37 +0900

    Update

--- abc233/d/main.cpp
+++ abc233/d/main.cpp
@@ -10,16 +10,21 @@ int main() {
   cin >> n >> k;
   vector<ll> a(n);
   REP(i,n) cin >> a[i];
-
-  vector<ll> s(n+1);
+  vector<ll> s(n+1); // 累積和
   REP(i,n) s[i+1] = s[i]+a[i];
 
-  map<ll,ll> mp;
-  ll ans = 0;
+  map<ll,int> mp;
+  mp[0]++;
+  REP(i,n) mp[s[i+1]]++;
 
+  ll ans = 0;
   REP(i,n) {
-    mp[s[i]]++;
-    ll d = s[i+1] - k;
+    /*
+      s[i+x] - s[i] = k になるものを探す
+      s[i+x] = k + s[i]
+    */
+    mp[s[i]]--;
+    ll d = k + s[i];
     ans += mp[d];
   }
 
