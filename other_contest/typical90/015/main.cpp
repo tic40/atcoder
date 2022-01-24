aeb83934eed7e4f957c702cd6ce6f3b059c63497
tic40 <ccpzjoh@gmail.com>
2022-01-24 22:03:01 +0900

    Update

--- other_contest/typical90/015/main.cpp
+++ other_contest/typical90/015/main.cpp
@@ -3,13 +3,13 @@
 using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<(int)(n);i++)
-using mint = modint1000000007;
 using ll = long long;
+const int MOD = 1e9+7;
+using mint = modint1000000007;
 
-const int MOD = 1000000007;
-const int MAX = 100005;
+const int MAX = 200005;
 
-ll fac[MAX], finv[MAX], inv[MAX];
+long long fac[MAX], finv[MAX], inv[MAX];
 void comInit() { // テーブルを作る前処理
   fac[0] = fac[1] = 1;
   finv[0] = finv[1] = 1;
@@ -22,14 +22,15 @@ void comInit() { // テーブルを作る前処理
   return;
 }
 
-ll com(int n, int k) { // 二項係数計算
+long long com(int n, int k) {
   if (n < k) return 0;
   if (n < 0 || k < 0) return 0;
   return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
 }
 
+int n;
 int main() {
-  int n; cin >> n;
+  cin >> n;
   comInit();
 
   for(int k = 1; k <= n; k++) {
@@ -37,9 +38,10 @@ int main() {
     for(int i = 1; i <= n/k+1; i++) {
       // 1..nからk以上の差になるものをi個選ぶ場合
 
-      int s1 = n-(k-1)*(i-1);
-      int s2 = i;
-      sum += com(s1,s2);
+      /*
+        https://twitter.com/e869120/status/1382827276673306624/photo/1
+      */
+      sum += com(n-(k-1)*(i-1), i);
     }
     cout << sum.val() << endl;
   }
