2bc5de7534a98dffcc992cded51d5263d165dc59
tic40 <ccpzjoh@gmail.com>
2022-01-31 01:39:02 +0900

    Update

--- abc/abc237/e/main.cpp
+++ abc/abc237/e/main.cpp
@@ -4,36 +4,35 @@ using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<(int)(n);i++)
 using ll = long long;
-const int INF = 1e9;
+const ll LINF = 1e18;
 
 int n,m;
 vector<ll> h;
 ll ans = 0;
 
-using Edge = struct { ll to; ll cost; };
+using Edge = struct { int to; ll cost; };
 vector<vector<Edge>> g;
-using P = pair<ll,ll>;
+using P = pair<ll,int>;
 
 // s: スタート位置
 vector<ll> dijkstra(int s) {
-  vector<ll> dist(g.size(), -INF);
+  vector<ll> dist(g.size(), LINF);
   priority_queue<P, vector<P>, greater<P>> q;
 
-  auto push = [&](ll nx, ll to) {
-    if (dist[to] >= nx) return;
-    dist[to] = nx;
+  // cost: toに移動するときのコスト, to: 移動先
+  auto push = [&](ll cost, ll to) {
+    if (dist[to] <= cost) return;
+    dist[to] = cost;
     q.emplace(dist[to], to);
   };
-  // スタート位置をコスト0で追加
   push(0, s); // [現在位置のコスト, 現在位置]
 
-  while (!q.empty()) {
-    P p = q.top(); q.pop();
-    // auto d = p.first,
-    auto v = p.second;
+  while (q.size()) {
+    auto [d,v] = q.top(); q.pop();
+    // if (dist[v] != d) continue;
     for (Edge e: g[v]) {
-      ll nx = dist[v]+e.cost;
-      push(nx, e.to);
+      ll cost = dist[v]+e.cost;
+      push(cost, e.to);
     }
   }
 
@@ -50,21 +49,14 @@ int main() {
     int u,v; cin >> u >> v;
     u--; v--;
 
-    int uv = 0;
-    if (h[u] < h[v]) uv = -2*(h[v]-h[u]);
-    if (h[v] < h[u]) uv = (h[u]-h[v]);
-
-    int vu = 0;
-    if (h[v] < h[u]) vu = -2*(h[u]-h[v]);
-    if (h[u] < h[v]) vu = (h[v]-h[u]);
-
-    g[u].emplace_back((Edge){v, uv});
-    g[v].emplace_back((Edge){u, vu});
+    g[u].emplace_back((Edge){v, max(0LL, h[v]-h[u]) });
+    g[v].emplace_back((Edge){u, max(0LL, h[u]-h[v]) });
   }
 
   auto dist = dijkstra(0);
+
   ll ans = 0;
-  for(auto v: dist) ans = max(ans, v);
+  REP(i,n) ans = max(ans, h[0]-h[i]-dist[i]);
   cout << ans << endl;
   return 0;
 }
