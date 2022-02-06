aef1c4f06e9020d298b8a36761743ce8b0026ed3
tic40 <ccpzjoh@gmail.com>
2022-02-06 11:35:31 +0900

    Update

--- abc/abc238/d/main.cpp
+++ abc/abc238/d/main.cpp
@@ -26,16 +26,40 @@ void solve1() {
   return;
 }
 
+map<pair<ll, ll>, bool> mp;
+bool f(ll a, ll s) {
+  if (s == 0) return a == 0;
+
+  auto p = make_pair(a,s);
+  if (mp.count(p)) return mp[p];
+
+  // 1の位を全部試す
+  REP(x,2) REP(y,2) {
+    // AND側の条件
+    // x&y がaのひとけた目と一致している必要
+    if ( (x & y) != (a & 1) ) continue;
+    if (s-x-y < 0) continue;
+    if ( ((s-x-y) & 1) != 0) continue;
+    if ( f(a>>1, (s-x-y)>>1) ) return mp[p] = true;
+  }
+  return mp[p] = false;
+};
+
+// 桁DP解法
 void solve2() {
+  REP(i,t) {
+    mp = map<pair<ll, ll>, bool>(); // mapをクリアする
+    cout << (f(a[i],s[i]) ? "Yes" : "No") << endl;
+  }
   return;
 }
 
 int main() {
   cin >> t;
-  a.resize(t);
-  s.resize(t);
+  a.resize(t); s.resize(t);
   REP(i,t) cin >> a[i] >> s[i];
 
-  solve1();
+  // solve1();
+  solve2();
   return 0;
 }
