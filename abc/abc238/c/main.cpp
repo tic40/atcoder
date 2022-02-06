eee80dcac8c164887524531132e3f7ba5b2178ae
tic40 <ccpzjoh@gmail.com>
2022-02-06 10:54:26 +0900

    Update c

--- abc/abc238/c/main.cpp
+++ abc/abc238/c/main.cpp
@@ -7,18 +7,16 @@ using ll = long long;
 using mint = modint998244353;
 
 mint sum(ll x) {
-  return mint(1+x) * x / 2;
+  return (mint)(1+x)*x/2;
 }
 
 int main() {
   ll n; cin >> n;
-
   ll l = 1;
-  mint ans;
+
+  mint ans = 0;
   while(l <= n) {
-    ll r = l*10;
-    // [l, r)
-    ll len = min(n+1,r)-l;
+    ll len = min(n+1,l*10) - l;
     ans += sum(len);
     l *= 10;
   }
