e39a45ed4e1001fc9900b7443b055b90978da4d7
tic40 <ccpzjoh@gmail.com>
2022-02-22 00:13:45 +0900

    Update

--- abc/abc240/d/main.cpp
+++ abc/abc240/d/main.cpp
@@ -3,50 +3,29 @@
 using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<(int)(n);i++)
-using ll = long long;
-const int INF = 1e9;
-const ll LINF = 1e18;
-const int MOD = 1e9+7;
-
-int n;
-vector<int> a;
-
-void solve() {
-  stack<pair<int,int>> x;
-
-  int tot = 0;
-  REP(i,n) {
-    tot++;
-
-    if (x.size() == 0) {
-      x.push({ a[i], 1 });
-      cout << tot << endl;
-      continue;
-    }
-
-    auto [prev, prev_num] = x.top();
-    if (a[i] == prev) {
-      int nv = prev_num+1;
-      x.pop();
-      x.push({ a[i], nv });
-      if (a[i] == nv) {
-        x.pop();
-        tot -= nv;
-      }
-    } else {
-      x.push({ a[i], 1 });
-    }
-
-    cout << tot << endl;
-  }
-  return;
-}
 
 int main() {
-  cin >> n;
-  a.resize(n);
-  REP(i,n) cin >> a[i];
+  int n; cin >> n;
 
-  solve();
+  stack<pair<int,int>> st;
+  st.push({-1, -1}); // 番人
+  int sz = 0;
+
+  REP(i,n) {
+    int a; cin >> a;
+    sz++;
+
+    auto [prev, num] = st.top();
+    if (a == prev) {
+      st.pop();
+      num++;
+      if (a == num) sz -= num;
+      else st.push({ a, num });
+    } else {
+      st.push({ a, 1 });
+    }
+    cout << sz << endl;
+  }
+
   return 0;
 }
