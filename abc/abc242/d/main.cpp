683458d317e26ce8a31eec7254fd14bac6afca3e
tic40 <ccpzjoh@gmail.com>
2022-03-09 21:56:01 +0900

    Update d

--- abc/abc242/d/main.cpp
+++ abc/abc242/d/main.cpp
@@ -4,32 +4,26 @@ using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<(int)(n);i++)
 using ll = long long;
-
 string s;
-
-char g(char c, ll add) {
-  return char('A' + (c - 'A' + add) % 3);
-}
 
 char f(ll t, ll k) {
   if (t == 0) return s[k];
-  if (k == 0) return g(s[0], t);
+  if (k == 0) return 'A' + (s[0] - 'A' + t) % 3;
 
-  if (k % 2 == 0) {
-    return g( f(t-1, k/2), 1 );
-  } else {
-    return g( f(t-1, (k-1)/2), 2 );
-  }
+  char c = f(t-1, k/2);
+  return (k%2 == 0) ?
+    'A' + (c - 'A' + 1) % 3
+    : 'A' + (c - 'A' + 2) % 3;
 }
 
 int main() {
   int q; cin >> s >> q;
 
   REP(i,q) {
-    ll t,k;
-    cin >> t >> k; k--;
+    ll t,k; cin >> t >> k;
+    k--;
+
     cout << f(t,k) << endl;
   }
-
   return 0;
 }
