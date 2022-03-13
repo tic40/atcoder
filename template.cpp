b10770eee790b0cdf5abf8508ff48542685717db
tic40 <ccpzjoh@gmail.com>
2022-03-13 10:49:14 +0900

    Update

--- template.cpp
+++ template.cpp
@@ -4,9 +4,11 @@ using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<(int)(n);i++)
 using ll = long long;
-const int INF = 1e9;
-const ll LINF = 1e18;
+const int INF = 1e9+5;
+const ll LINF = 1e18+5;
 const int MOD = 1e9+7;
+// using mint = modint998244353;
+// using mint = modint1000000007;
 
 int main() {
   return 0;
