65eaf528953d318a7a3f5224b8161d74b27b7ab4
tic40 <ccpzjoh@gmail.com>
2022-03-13 10:50:13 +0900

    Update

--- abc/abc243/c/main.cpp
+++ abc/abc243/c/main.cpp
@@ -5,31 +5,21 @@ using namespace std;
 #define REP(i,n) for(int i=0;i<(int)(n);i++)
 const int INF = 1e9+5;
 
-int n;
-string s;
-vector<int> x,y;
+int main() {
+  int n; cin >> n;
+  vector<int> x(n),y(n);
+  REP(i,n) cin >> x[i] >> y[i];
+  string s; cin >> s;
 
-void solve() {
-  map<int, vector<int>> mp; // <y軸の位置, 右向きの人の左橋と左向きの人の右端x位置>
-  REP(i,n) mp[y[i]] = vector<int>{ INF, -INF };
+  map<int,pair<int,int>> mp;
+  REP(i,n) mp[y[i]] = { INF, -INF };
   REP(i,n) {
-    if (s[i] == 'R') mp[ y[i] ][0] = min(mp[ y[i] ][0], x[i]);
-    if (s[i] == 'L') mp[ y[i] ][1] = max(mp[ y[i] ][1], x[i]);
+    if (s[i] == 'R') mp[y[i]].first = min( mp[y[i]].first, x[i] );
+    if (s[i] == 'L') mp[y[i]].second = max( mp[y[i]].second, x[i] );
   }
 
   bool ok = false;
-  for(auto [k,v]: mp) if (v[0] < v[1]) ok = true;
+  for(auto [k,v]: mp) ok |= (v.first < v.second);
   cout << (ok ? "Yes" : "No") << endl;
-
-  return;
-}
-
-int main() {
-  cin >> n;
-  x.resize(n); y.resize(n);
-  REP(i,n) cin >> x[i] >> y[i];
-  cin >> s;
-
-  solve();
   return 0;
 }
