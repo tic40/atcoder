56a60c1f804b4cc98e8cbb76d9a2649fde465f48
tic40 <ccpzjoh@gmail.com>
2022-03-16 20:08:12 +0900

    Update

--- abc/abc241/e/main.cpp
+++ abc/abc241/e/main.cpp
@@ -2,51 +2,49 @@
 #include <atcoder/all>
 using namespace atcoder;
 using namespace std;
-#define REP(i,n) for(int i=0;i<(n);++i)
+#define REP(i,n) for(int i=0;i<(n);i++)
 using ll = long long;
 
 int n; ll k;
 vector<int> a;
 
-void solve1() {
-  map<ll,pair<ll,ll>> mp; // pair<idx,x>
+ll solve1() {
+  map<int,pair<int,ll>> mp;
   ll x = 0;
   REP(i,k) {
-    ll r = x%n;
+    ll r = x % n;
     if (mp.count(r)) {
-      auto [mi, mx] = mp[r];
-      ll p = i - mi; // p: ループの長さ
-      if ((k-i)%p == 0) { // ちょうどループで終わるところで終了する
+      auto [mi,mx] = mp[r];
+      int p = i - mi;
+      if ( (k - i) % p == 0) { // ループが割り切れるところで打ち切ると計算が楽
         x += (x-mx) * ((k-i)/p);
         break;
       }
     }
+
     mp[r] = {i,x};
-    x += a[x%n];
+    x += a[r];
   }
 
-  cout << x << endl;
+  return x;
 }
 
-// doubling
-void solve2() {
+// ダブリング解
+ll solve2() {
   int logK = 1;
   while ((1LL << logK) <= k) logK++;
-
   vector<vector<ll>> dp(logK, vector<ll>(n));
   REP(i,n) dp[0][i] = a[i];
 
   REP(i,logK-1) REP(j,n) {
-    dp[i + 1][j] = dp[i][j] + dp[i][ (j+dp[i][j])%n ];
+    dp[i+1][j] = dp[i][j] + dp[i][ (j + dp[i][j])%n ];
   }
 
   ll x = 0;
   REP(i,logK) {
-    if ((k >> i) & 1) x += dp[i][x%n];
+    if (k >> i & 1) x += dp[i][x % n];
   }
-
-  cout << x << endl;
-  return;
+  return x;
 }
 
 int main() {
@@ -54,7 +52,7 @@ int main() {
   a.resize(n);
   REP(i,n) cin >> a[i];
 
-  // solve1();
-  solve2();
+  cout << solve1() << endl;
+  cout << solve2() << endl;
   return 0;
 }
