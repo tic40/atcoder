1b25fe7b61bd11715f06b56b6f8e0ed6e9a03fc7
tic40 <ccpzjoh@gmail.com>
2022-03-27 19:59:19 +0900

    Update

--- abc/abc223/d/main.cpp
+++ abc/abc223/d/main.cpp
@@ -1,44 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<(int)(n);i++)
-#define ALL(x) x.begin(), x.end()
-using ll = long long;
-
-int n,m;
-vector<vector<int>> to;
-vector<int> deg;
-
-void solve() {
-  priority_queue<int,vector<int>,greater<int>> q;
-  REP(i,n) if (deg[i] == 0) q.push(i);
-  vector<int> ans;
-  while(q.size()) {
-    int v = q.top(); q.pop();
-    ans.push_back(v);
-    for (int u: to[v]) {
-      deg[u]--;
-      if (deg[u] == 0) q.push(u);
-    }
-  }
-
-  if ((int)ans.size() != n) cout << -1 << endl;
-  else {
-    for(int v: ans) cout << v+1 << " ";
-  }
-  return;
-}
+#define REP(i,n) for(int i=0;i<(n);i++)
 
 int main() {
-  cin >> n >> m;
-  to.resize(n);
-  deg.resize(n);
+  int n,m; cin >> n >> m;
+  vector<vector<int>> g(n);
+  vector<int> deg(n);
+
   REP(i,m) {
     int a,b; cin >> a >> b;
     a--; b--;
-    to[a].push_back(b);
+    g[a].push_back(b);
     deg[b]++;
   }
 
-  solve();
+  priority_queue<int, vector<int>, greater<int>> q;
+  // 入次数が0のものをキューに入れる
+  REP(i,n) if (deg[i] == 0) q.push(i);
+  vector<int> ans;
+
+  while(q.size()) {
+    int v = q.top(); q.pop();
+    ans.push_back(v);
+    for(int nv: g[v]) {
+      deg[nv]--;
+      if (deg[nv] == 0) q.push(nv);
+    }
+  }
+
+  if ((int)ans.size() == n) {
+    for(int v: ans) cout << v+1 << endl;
+  } else {
+    cout << -1 << endl;
+  }
   return 0;
 }
