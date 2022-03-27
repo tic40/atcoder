efc0ceeac1f7889f370151c350fa3310cbec10cc
tic40 <ccpzjoh@gmail.com>
2022-03-27 20:37:11 +0900

    Solve f

---
+++ abc/abc245/f/main.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace atcoder;
+using namespace std;
+#define REP(i,n) for(int i=0;i<(n);i++)
+
+int main() {
+  int n,m;
+  cin >> n >> m;
+  scc_graph g(n);
+  vector<vector<int>> to(n);
+  REP(i,m) {
+    int u,v; cin >> u >> v;
+    u--; v--;
+    g.add_edge(u,v);
+    to[u].push_back(v);
+  }
+
+  auto d = g.scc();
+  int s = d.size();
+  vector<int> di(n); // 頂点のグループ番号
+  REP(i,s) for(int v: d[i]) di[v] = i;
+  vector<bool> dp(s);
+
+  // トポロジカルソートされたものを後ろから見ていくので逆順にする
+  reverse(d.begin(),d.end());
+
+  REP(i,s) {
+    dp[i] = d[i].size() > 1;
+
+    if (d[i].size() == 1) {
+      for(int u: to[d[i][0]]) {
+        if (dp[di[u]]) dp[i] = true;
+      }
+    }
+  }
+
+  int ans = 0;
+  REP(i,s) if (dp[i]) ans += d[i].size();
+  cout << ans << endl;
+  return 0;
+}
