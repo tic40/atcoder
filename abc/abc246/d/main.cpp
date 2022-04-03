28aa0b71f13f0a5ff243c83713b215195063b3b1
tic40 <ccpzjoh@gmail.com>
2022-04-04 00:16:47 +0900

    Solve e

--- abc/abc246/d/main.cpp
+++ abc/abc246/d/main.cpp
@@ -2,13 +2,11 @@
 using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
 using ll = long long;
-const ll LINF = 8e18;
 
 ll f(ll a,ll b) { return a*a*a + a*a*b + a*b*b + b*b*b; }
-
 int main() {
   ll n; cin >> n;
-  ll ans = LINF;
+  ll ans = 9e18;
 
   ll a = 0, b = 1e6;
   while(a <= b) {
@@ -20,6 +18,7 @@ int main() {
       a++;
     }
   }
+
   cout << ans << endl;
   return 0;
 }
