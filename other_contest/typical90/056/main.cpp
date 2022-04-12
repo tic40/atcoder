490e9c7de26fdac76d0a5c1aee1e93f4b3fe8493
tic40 <ccpzjoh@gmail.com>
2022-04-12 23:29:03 +0900

    Fix

--- other_contest/typical90/056/main.cpp
+++ other_contest/typical90/056/main.cpp
@@ -1,48 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
-
-int n,s;
-vector<int> a,b;
-bool dp[105][100005];
-
-void solve() {
-  dp[0][0] = true;
-
-  REP(i,n) REP(j,s) {
-    if (dp[i][j]) {
-      dp[i+1][j+a[i]] = true;
-      dp[i+1][j+b[i]] = true;
-    }
-  }
-
-  if (!dp[n][s]) {
-    cout << "Impossible" << endl;
-    return;
-  }
-
-  // DP復元
-  string ans;
-  for (int i = n-1; 0 <= i; i--) {
-    if (dp[i][s-a[i]]) {
-      s -= a[i];
-      ans += "A";
-    } else if (dp[i][s-b[i]]) {
-      s -= b[i];
-      ans += "B";
-    }
-  }
-
-  reverse(ans.begin(), ans.end());
-  cout << ans << endl;
-  return;
-}
+#define REP(i,n) for(int i=0;i<(n);i++)
 
 int main() {
-  cin >> n >> s;
-  a.resize(n); b.resize(n);
+  int n,s; cin >> n >> s;
+  vector<int> a(n),b(n);
   REP(i,n) cin >> a[i] >> b[i];
 
-  solve();
+  vector<vector<bool>> dp(n+1, vector<bool>(s+1, false));
+  dp[0][0] = true;
+  REP(i,n) REP(j,s+1) {
+    if (dp[i][j] && j + a[i] <= s) dp[i+1][j+a[i]] = true;
+    if (dp[i][j] && j + b[i] <= s) dp[i+1][j+b[i]] = true;
+  }
+
+  if (!dp[n][s]) { cout << "Impossible" << endl; return 0; }
+
+  int now = s;
+  string ans;
+  for(int i = n-1; 0 <= i; i--) {
+    if (0 <= now - a[i] && dp[i][now - a[i]]) {
+      now -= a[i];
+      ans += 'A';
+    } else {
+      now -= b[i];
+      ans += 'B';
+    }
+  }
+
+  reverse(ans.begin(),ans.end());
+  cout << ans << endl;
   return 0;
 }
