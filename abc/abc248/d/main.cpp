1a5a89af1fda66231a0dc55bee219eb50be12bed
tic40 <ccpzjoh@gmail.com>
2022-04-18 11:13:01 +0900

    Update

--- abc/abc248/d/main.cpp
+++ abc/abc248/d/main.cpp
@@ -4,19 +4,22 @@ using namespace std;
 
 int main() {
   int n; cin >> n;
+  vector<int> a(n);
+  REP(i,n) cin >> a[i];
   vector<vector<int>> g(n+1);
-  REP(i,n) {
-    int a; cin >> a;
-    g[a].push_back(i);
-  }
+  REP(i,n) g[a[i]].push_back(i);
+
   int q; cin >> q;
   REP(i,q) {
-    int l,r,x; cin >> l >> r >> x;
+    int l,r,x;
+    cin >> l >> r >> x;
     l--; r--;
 
-    auto it1 = lower_bound(g[x].begin(),g[x].end(),l);
-    auto it2 = upper_bound(g[x].begin(),g[x].end(),r) - 1;
-    cout << (it2-it1) + 1 << endl;
+    vector<int>& now = g[x];
+    auto itl = lower_bound(now.begin(), now.end(), l);
+    auto itr = upper_bound(now.begin(), now.end(), r) - 1;
+    cout << itr - itl + 1 << endl;
   }
+
   return 0;
 }
