5b1a1f40e3d65ef9a1d41157fb77f6f59b81ba2c
tic40 <ccpzjoh@gmail.com>
2022-04-23 19:57:40 +0900

    Update

--- abc/abc248/e/main.cpp
+++ abc/abc248/e/main.cpp
@@ -4,46 +4,33 @@ using namespace std;
 using ll = long long;
 using P = pair<int,int>;
 
-bool colinear(P i, P j, P k) {
-  auto [x1,y1] = i;
-  auto [x2,y2] = j;
-  auto [x3,y3] = k;
-  // (x2 - x1) / (y2 - y1) = (x3 - x1) / (y3 - y1)
-  ll v1 = (y2 - y1) * (x3 - x1);
-  ll v2 = (x2 - x1) * (y3 - y1);
-  return v1 == v2;
-}
-
 int main() {
   int n,k; cin >> n >> k;
-  vector<P> p(n);
-  REP(i,n) {
-    int x,y; cin >> x >> y;
-    p[i] = {x,y};
-  }
+  vector<int> x(n),y(n);
+  REP(i,n) cin >> x[i] >> y[i];
 
   if (k == 1) { cout << "Infinity" << endl; return 0; }
 
-  vector<vector<bool>> flag(n, vector<bool>(n));
   int ans = 0;
+  vector<vector<bool>> used(n,vector<bool>(n));
 
-  REP(i,n) for(int j = i+1; j < n; j++) {
-    if (flag[i][j]) continue;
-    int cnt = 2;
-    vector<int> list = {i,j};
-    for (int l = j + 1; l < n; l++) {
-      if (colinear(p[i],p[j],p[l])) {
-        cnt++;
-        list.push_back(l);
+  REP(i,n) {
+    for(int j = i+1; j < n; j++) {
+      if (used[i][j]) continue;
+      vector<int> list = {i,j};
+      int cnt = 2;
+
+      for(int l = j+1; l < n; l++) {
+        ll v1 = (x[l] - x[i]) * (y[j] - y[i]);
+        ll v2 = (y[l] - y[i]) * (x[j] - x[i]);
+        if (v1 == v2) { cnt++; list.push_back(l); }
       }
-    }
 
-    REP(l, cnt) for (int m = l + 1; m < cnt; m++) {
-      flag[list[l]][list[m]] = true;
+      for(int l: list) for(int m: list) used[l][m] = true;
+      if (k <= cnt) ans++;
     }
-
-    if (k <= cnt) ans++;
   }
+
   cout << ans << endl;
   return 0;
 }
