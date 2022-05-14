40822e5e1f2ce502e90b002c00147f140427ced6
tic40 <ccpzjoh@gmail.com>
2022-05-15 00:41:45 +0900

    Update

--- abc/abc251/e/main.cpp
+++ abc/abc251/e/main.cpp
@@ -4,32 +4,22 @@ using namespace std;
 using ll = long long;
 const ll LINF = 1e18+5;
 
+ll dp[300005][2][2]; // [i][j][k] := i番目, j支払ったかどうか, k 最初か最後どちらを決め打ちで支払ったか
+
 int main() {
   int n; cin >> n;
   vector<int> a(n);
   REP(i,n) cin >> a[i];
 
-  // 最初を決め打ち
-  vector<vector<ll>> dp1(n+1, vector<ll> (2));
-  dp1[1][0] = LINF;
-  dp1[1][1] = a[0];
-  for(int i = 1; i < n; i++) {
-    dp1[i+1][0] = dp1[i][1];
-    dp1[i+1][1] = min(dp1[i][0], dp1[i][1])+a[i];
-  }
-  ll ans = min(dp1[n][1], dp1[n][0]);
+  REP(i,n) {
+    if (i == 0) dp[i+1][0][0] = LINF;
+    else dp[i+1][0][0] = dp[i][1][0];
+    dp[i+1][1][0] = min(dp[i][0][0],dp[i][1][0]) + a[i];
 
-  // 最後を決め打ち
-  vector<vector<ll>> dp2(n+1, vector<ll> (2));
-  dp2[0][0] = LINF;
-  dp2[0][1] = a[n-1];
-  for(int i = 0; i < n-1; i++) {
-    dp2[i+1][0] = dp2[i][1];
-    dp2[i+1][1] = min(dp2[i][0], dp2[i][1])+a[i];
+    dp[i+1][0][1] = dp[i][1][1];
+    dp[i+1][1][1] = min(dp[i][0][1],dp[i][1][1]) + a[i];
   }
 
-  ans = min(ans, min(dp2[n-1][1], dp2[n-1][0]));
-  cout << ans << endl;
-
+  cout << min(dp[n][1][1], min(dp[n][0][0],dp[n][1][0])) << endl;
   return 0;
 }
