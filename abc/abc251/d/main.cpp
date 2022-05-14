b47130890b3f1021ca838ff70698649f8a4d96a0
tic40 <ccpzjoh@gmail.com>
2022-05-15 01:04:51 +0900

    Update

--- abc/abc251/d/main.cpp
+++ abc/abc251/d/main.cpp
@@ -3,10 +3,7 @@ using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
 
 int main() {
-  int _; cin >> _;
   cout << 99*3 << endl;
-  REP(i,99) cout << i+1 << " ";
-  REP(i,99) cout << (i+1)*1e2 << " ";
-  REP(i,99) cout << (i+1)*1e4 << " ";
+  REP(i,99) cout << i+1 << " " << (i+1)*1e2 << " " << (i+1)*1e4 << " ";
   return 0;
 }
