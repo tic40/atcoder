ab64b4d74821e41b91527de4732b5e8d8739561a
tic40 <ccpzjoh@gmail.com>
2022-05-14 23:07:01 +0900

    Fix

--- abc/abc251/d/main.cpp
+++ abc/abc251/d/main.cpp
@@ -8,7 +8,6 @@ int main() {
   for(int i = 1; i <= 99; i++) ans.push_back(i);
   for(int i = 100; i <= 9900; i+=100) ans.push_back(i);
   for(int i = 10000; i <= 990000; i+=10000) ans.push_back(i);
-  ans.push_back(10000);
 
   cout << ans.size() << endl;
   for(int v: ans) cout << v << " ";
