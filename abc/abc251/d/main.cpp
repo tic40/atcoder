752af1996a43f438ae944acca5379a541d1550fd
tic40 <ccpzjoh@gmail.com>
2022-05-14 23:09:27 +0900

    Update

--- abc/abc251/d/main.cpp
+++ abc/abc251/d/main.cpp
@@ -3,14 +3,10 @@ using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
 
 int main() {
-  int w; cin >> w;
-  vector<int> ans;
-  for(int i = 1; i <= 99; i++) ans.push_back(i);
-  for(int i = 100; i <= 9900; i+=100) ans.push_back(i);
-  for(int i = 10000; i <= 990000; i+=10000) ans.push_back(i);
-
-  cout << ans.size() << endl;
-  for(int v: ans) cout << v << " ";
-
+  int _; cin >> _;
+  cout << 99*3 << endl;
+  REP(i,99) cout << i+1 << " ";
+  REP(i,99) cout << (i+1)*1e2 << " ";
+  REP(i,99) cout << (i+1)*1e4 << " ";
   return 0;
 }
