c533e01567b368082326d9871b2b3766b00699a6
tic40 <ccpzjoh@gmail.com>
2022-05-15 16:57:29 +0900

    Update

--- other_contest/edpc/b/main.cpp
+++ other_contest/edpc/b/main.cpp
@@ -1,25 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i, n) for(int i = 0; i < n; i++)
-typedef long long ll;
-const int INF = 1001001001;
+#define REP(i,n) for(int i=0;i<(n);i++)
+const int INF = 1e9;
 
 int main() {
-  int n,k;
-  cin >> n >> k;
-  int h[n];
-  REP(i, n) cin >> h[i];
+  int n,k; cin >> n >> k;
+  vector<int> h(n);
+  REP(i,n) cin >> h[i];
 
-  int dp[n];
-  REP(i,n+1) dp[i] = INF;
-
+  vector<int> dp(n, INF);
   dp[0] = 0;
-  for (int i = 1; i < n; i++) {
-    for (int j = 1; j <= k; j++) {
-      if (i-j < 0) break;
-      int cost = dp[i-j] + abs(h[i-j]-h[i]);
-      dp[i] = min(dp[i],cost);
-    }
+  REP(i,n) for (int j = 1; j <= k; j++) {
+    if (i-j < 0) break;
+    dp[i] = min(dp[i], dp[i-j] + abs(h[i]-h[i-j]));
   }
+
   cout << dp[n-1] << endl;
+  return 0;
 }
