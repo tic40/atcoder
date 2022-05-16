19663ee03ae0d1373a401065a7ade426fd66496a
tic40 <ccpzjoh@gmail.com>
2022-05-16 23:29:59 +0900

    Update

--- other_contest/edpc/f/main.cpp
+++ other_contest/edpc/f/main.cpp
@@ -1,29 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i, n) for(int i = 0; i < n; i++)
-typedef long long ll;
+#define REP(i,n) for(int i=0;i<(n);i++)
 
 int main() {
   string s,t; cin >> s >> t;
-  int S=s.size(), T=t.size();
-  int dp[S+1][T+1];
-  memset(dp, 0, sizeof(dp));
+  int ns = s.size(), nt = t.size();
 
-  REP(i,S) REP(j,T) {
-    dp[i+1][j+1] = max(dp[i+1][j],dp[i][j+1]);
+  vector<vector<int>> dp(ns+1, vector<int> (nt+1));
+  REP(i,ns) REP(j,nt) {
     if (s[i] == t[j]) dp[i+1][j+1] = dp[i][j]+1;
+    else dp[i+1][j+1] = max(dp[i+1][j],dp[i][j+1]);
   }
 
-  int i=S,j=T;
+  int i = ns, j = nt;
   string ans;
-  while (i>0 && j>0) {
-    if(dp[i][j] == dp[i-1][j]) i--;
+  while(0 < i && 0 < j) {
+    if (dp[i][j] == dp[i-1][j]) i--;
     else if (dp[i][j] == dp[i][j-1]) j--;
     else {
-      i--; j--; ans.push_back(s[i]);
+      i--; j--;
+      ans += s[i];
     }
   }
 
-  reverse(ans.begin(), ans.end());
+  reverse(ans.begin(),ans.end());
   cout << ans << endl;
+  return 0;
 }
