c26fc9cf25522da0f6791658aaf926dea57a9591
tic40 <ccpzjoh@gmail.com>
2022-05-25 23:36:52 +0900

    Update

--- abc/abc252/d/main.cpp
+++ abc/abc252/d/main.cpp
@@ -7,15 +7,14 @@ int main() {
   int n; cin >> n;
   vector<int> a(n);
   REP(i,n) cin >> a[i];
-
   sort(a.begin(),a.end());
+
   ll ans = 0;
-  for(int j = 1; j < n-1; j++) {
-    auto it1 = lower_bound(a.begin(),a.begin()+j, a[j])-1;
-    auto it2 = upper_bound(a.begin()+j,a.end(), a[j]);
-    int lidx = it1 - a.begin();
-    int ridx = it2 - a.begin();
-    ans += (ll)(lidx+1) * (n-ridx);
+  for(int j = 1; j < n; j++) {
+    auto it1 = lower_bound(a.begin(), a.begin()+j, a[j]) - 1;
+    auto it2 = upper_bound(a.begin()+j, a.end(), a[j]);
+    ans += (it1 - a.begin() + 1) * (n - (it2 - a.begin()));
   }
   cout << ans << endl;
+  return 0;
 }
