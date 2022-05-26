2737a543d6184d4c75856ad8263c2fbb9bc28882
tic40 <ccpzjoh@gmail.com>
2022-05-26 10:10:51 +0900

    Update

--- abc/abc252/e/main.cpp
+++ abc/abc252/e/main.cpp
@@ -2,8 +2,11 @@
 using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
 using ll = long long;
-const ll LINF = 1e18;
-struct Edge { int id, to, cost; };
+const ll LINF = 1e18+5;
+struct Edge {
+  int id,to,cost;
+  Edge(int id, int to, int cost): id(id),to(to),cost(cost) {}
+};
 using P = pair<ll,int>;
 
 int main() {
@@ -12,30 +15,30 @@ int main() {
   REP(i,m) {
     int a,b,c; cin >> a >> b >> c;
     a--; b--;
-    g[a].emplace_back(Edge{i+1,b,c});
-    g[b].emplace_back(Edge{i+1,a,c});
+    g[a].emplace_back(i+1,b,c);
+    g[b].emplace_back(i+1,a,c);
   }
 
-  vector<ll> d(n, LINF);
-  vector<int> from(n,-1);
   priority_queue<P, vector<P>, greater<P>> q;
-  d[0] = 0;
+  vector<ll> dist(n,LINF);
+  vector<int> from(n);
   q.emplace(0,0);
+  dist[0] = 0;
 
   while(q.size()) {
-    auto [c, v] = q.top(); q.pop();
-    if (d[v] != c) continue;
-    for(Edge edge: g[v]) {
-      ll nc = c + edge.cost;
-      if (d[edge.to] <= nc) continue;
-      d[edge.to] = nc;
-      from[edge.to] = edge.id;
-      q.emplace(nc, edge.to);
+    auto [c,v] = q.top(); q.pop();
+    if (dist[v] < c) continue;
+    for(Edge e: g[v]) {
+      ll nc = c + e.cost;
+      if (nc < dist[e.to]) {
+        dist[e.to] = nc;
+        q.emplace(nc,e.to);
+        from[e.to] = e.id;
+      }
     }
   }
 
   for(int i = 1; i < n; i++) cout << from[i] << " ";
   cout << endl;
-
   return 0;
 }
