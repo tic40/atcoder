821ebc4f1a3b3a7059db64b630f4e38faa9da63d
tic40 <ccpzjoh@gmail.com>
2022-06-05 23:56:11 +0900

    Update

--- abc/abc254/d/main.cpp
+++ abc/abc254/d/main.cpp
@@ -6,22 +6,24 @@ int main() {
   int n; cin >> n;
   vector<bool> sq(n+1);
   for(int i = 1; i*i <= n; i++) sq[i*i] = true;
-  vector<vector<int>> d(n+1);
 
+  // d[i] = iの約数の配列
+  vector<vector<int>> d(n+1);
   for(int i = 1; i <= n; i++) {
     for(int j = i; j <= n; j+=i) d[j].push_back(i);
   }
 
+  // 各 i/f(i) の数
   vector<int> cnt(n+1);
-
   for(int i = 1; i <= n; i++) {
-    int f = 0;
-    for(int v: d[i]) if (sq[v]) f = v;
-    cnt[i/f]++;
+    int t = 0;
+    for(int v: d[i]) if (sq[v]) t = v;
+    cnt[i/t]++;
   }
 
   int ans = 0;
-  for(int i = 1; i <= n; i++) ans += cnt[i]*cnt[i];
+  // cnt個の重複順列 cnt[i]^2
+  for(int v: cnt) ans += v*v;
   cout << ans << endl;
   return 0;
 }
