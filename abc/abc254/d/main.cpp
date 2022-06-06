9d243c0f908778d76cdf92053c6abde405f35fb2
tic40 <ccpzjoh@gmail.com>
2022-06-06 20:02:23 +0900

    Update

--- abc/abc254/d/main.cpp
+++ abc/abc254/d/main.cpp
@@ -2,8 +2,7 @@
 using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
 
-int main() {
-  int n; cin >> n;
+void solve1(int n) {
   vector<bool> sq(n+1);
   for(int i = 1; i*i <= n; i++) sq[i*i] = true;
 
@@ -25,5 +24,28 @@ int main() {
   // cnt個の重複順列 cnt[i]^2
   for(int v: cnt) ans += v*v;
   cout << ans << endl;
+  return;
+}
+
+void solve2(int n) {
+  int ans = 0;
+  for(int i = 1; i <= n; i++) {
+    int k = i;
+    for(int p = 2; p*p <= k; p++) {
+      while(k % (p*p) == 0) k /= (p*p);
+    }
+    // 平方数で割れるだけ割って残った数がkになっている
+    // k*j が平方数になるには、k*k、k*k*(平方数)が条件となるので、それをカウントする
+    for (int p = 1; k*(p*p) <= n; p++) ans++;
+  }
+  cout << ans << endl;
+  return;
+}
+
+int main() {
+  int n; cin >> n;
+  // solve1(n);
+  solve2(n);
+
   return 0;
 }
