b0c2491c51f68e7b47cd638f5681b34215094e95
tic40 <ccpzjoh@gmail.com>
2022-06-11 00:38:30 +0900

    Update

--- abc/abc254/e/main.cpp
+++ abc/abc254/e/main.cpp
@@ -3,17 +3,10 @@ using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
 const int INF = 1e9+5;
 
-int main() {
-  int n,m; cin >> n >> m;
-  vector<vector<int>> g(n);
-  REP(i,m) {
-    int a,b; cin >> a >> b;
-    a--; b--;
-    g[a].push_back(b);
-    g[b].push_back(a);
-  }
+int n,m,q;
+vector<vector<int>> g;
 
-  int q; cin >> q;
+void solve1() {
   REP(i,q) {
     int x,k; cin >> x >> k;
     x--;
@@ -31,8 +24,57 @@ int main() {
         ans += y+1;
       }
     }
-
     cout << ans << endl;
   }
+
+}
+
+void solve2() {
+  vector<int> dist(n,INF);
+  REP(i,q) {
+    int x,k; cin >> x >> k;
+    x--;
+
+    queue<int> que;
+    que.push(x);
+    dist[x] = 0;
+    vector<int> vs;
+    while(que.size()) {
+      int cur = que.front(); que.pop();
+      vs.push_back(cur);
+      if (k <= dist[cur]) continue;
+
+      for(int v: g[cur]) {
+        if (dist[cur]+1 < dist[v]) {
+          dist[v] = dist[cur]+1;
+          que.push(v);
+        }
+      }
+    }
+
+    int ans = 0;
+    for(int v: vs) {
+      ans += v+1;
+      dist[v] = INF;
+    }
+    cout << ans << endl;
+  }
+
+}
+
+int main() {
+  cin >> n >> m;
+  g.resize(n);
+  REP(i,m) {
+    int a,b; cin >> a >> b;
+    a--; b--;
+    g[a].push_back(b);
+    g[b].push_back(a);
+  }
+
+  cin >> q;
+  // solve1();
+  solve2();
+
   return 0;
 }
