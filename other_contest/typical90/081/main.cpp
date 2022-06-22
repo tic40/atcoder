318a8dc948faf85e78b1e320cae642c86e1b4ab8
tic40 <ccpzjoh@gmail.com>
2022-06-22 23:06:16 +0900

    Update

--- other_contest/typical90/081/main.cpp
+++ other_contest/typical90/081/main.cpp
@@ -2,34 +2,27 @@
 using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
 
+int m[5005][5005];
 int main() {
   int n,k; cin >> n >> k;
-  vector<vector<int>> d(5005,vector<int>(5005));
   REP(i,n) {
     int a,b; cin >> a >> b;
-    d[a][b]++;
+    m[a][b]++;
   }
 
   // 二次元累積和
-  for(int i = 1; i <= 5000; i++) {
-    for(int j = 1; j <= 5000; j++) {
-      d[i][j] += d[i-1][j];
-    }
-  }
-  for(int i = 1; i <= 5000; i++) {
-    for(int j = 1; j <= 5000; j++) {
-      d[i][j] += d[i][j-1];
-    }
-  }
+  REP(i,5000) REP(j,5000) m[i][j+1] += m[i][j];
+  REP(i,5000) REP(j,5000) m[i+1][j] += m[i][j];
 
   int ans = 0;
   REP(i,5001) REP(j,5001) {
     int lx = max(0,j-k-1);
     int ty = max(0,i-k-1);
-    int cnt = d[i][j];
-    cnt -= d[ty][j];
-    cnt -= d[i][lx];
-    cnt += d[ty][lx];
+
+    int cnt = m[i][j];
+    cnt -= m[ty][j];
+    cnt -= m[i][lx];
+    cnt += m[ty][lx];
     ans = max(ans,cnt);
   }
 
