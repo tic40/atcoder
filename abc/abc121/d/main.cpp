4d2603a462157693d0de97d9f2a606331d60b8f0
tic40 <ccpzjoh@gmail.com>
2022-06-26 22:46:35 +0900

    Update

--- abc/abc121/d/main.cpp
+++ abc/abc121/d/main.cpp
@@ -1,16 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i, n) for(int i = 0; i < n; i++)
+#define REP(i,n) for(int i=0;i<(n);i++)
 using ll = long long;
 
+// 排他的論理和 0^1^2...x-1^x
 ll f(ll x) {
+  // 任意の偶数 n について n ^ (n + 1) = 1 の性質がある
+  // これを利用して、例えばx=5の場合は (0^1)^(2^3)^5 = 1^1^5 となる
   ll res = ((x+1) / 2) % 2;
-  return x%2 == 0 ? res^x : res;
+  return (x+1) % 2 ? res^x : res;
 }
 
 int main() {
   ll a,b; cin >> a >> b;
 
-  cout << (f(a-1) ^ f(b)) << endl;
+  // f(a,b) = f(0, a-1) ^ f(0, b) が成り立つ
+  ll ans = f(a-1) ^ f(b);
+  cout << ans << endl;
   return 0;
 }
