c45962bf2211a336cba05cca1d6e0b58e40f03d6
tic40 <ccpzjoh@gmail.com>
2022-06-26 18:59:36 +0900

    Update

--- abc/abc112/c/main.cpp
+++ abc/abc112/c/main.cpp
@@ -1,44 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
+#define REP(i,n) for(int i=0;i<(n);i++)
 using ll = long long;
 
-int n;
-vector<int> x,y,h;
-
-void solve() {
-  int si = -1;
-  REP(i,n) {
-    if (h[i] > 0) { si = i; break; }
-  }
-
-  // cx, cy を全探索する
-  REP(cx,101) REP(cy,101) {
-    ll ch = h[si] + abs(cx - x[si]) + abs(cy - y[si]);
-
-    bool ok = true;
-    REP(i,n) {
-      ll tmp = abs(cx - x[i]) + abs(cy - y[i]);
-      if (h[i] > 0 && ch - h[i] != tmp) ok = false;
-      if (h[i] == 0 && ch > tmp) ok = false;
-    }
-
-    if (ok) {
-      cout << cx << " " << cy << " " << ch << endl;
-      return;
-    }
-  }
-
-  return;
-}
-
 int main() {
-  cin >> n;
-  x.resize(n);
-  y.resize(n);
-  h.resize(n);
+  int n; cin >> n;
+  vector<int> x(n),y(n),h(n);
   REP(i,n) cin >> x[i] >> y[i] >> h[i];
 
-  solve();
+  int ti = -1;
+  REP(i,n) if (h[i] > 0) { ti = i; break; }
+
+  REP(cx,101) REP(cy,101) {
+    int th = h[ti] + abs(x[ti]-cx) + abs(y[ti]-cy);
+
+    bool ok = [&]() {
+      REP(i,n) {
+        if (h[i] != max(0, th - abs(x[i]-cx) - abs(y[i]-cy))) return false;
+      }
+      return true;
+    }();
+
+    if (ok) {
+      cout << cx << " " << cy << " " << th << endl;
+      return 0;
+    }
+  }
   return 0;
 }
