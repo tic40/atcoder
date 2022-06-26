ff5ded67307f317bdb88186f755746fb6d783600
tic40 <ccpzjoh@gmail.com>
2022-06-26 22:58:53 +0900

    Update

--- abc/abc169/c/main.cpp
+++ abc/abc169/c/main.cpp
@@ -1,28 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ALL(x) (x).begin(),(x).end()
-#define COUT(x) cout<<(x)<<"\n"
-#define REP(i, n) for(int i=0;i<n;i++)
-#define YES(x) cout<<(x?"YES":"NO")<<"\n"
-#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
-#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
-#define endl "\n"
-using G = vector<vector<int>>;
-using M = map<int,int>;
-using P = pair<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int,vector<int>,greater<int>>;
-using V = vector<int>;
+#define REP(i,n) for(int i=0;i<(n);i++)
 using ll = long long;
-template<class T>bool CHMAX(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
-template<class T>bool CHMIN(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
 
 int main() {
-  ll a; double b; cin >> a >> b;
-  ll b2 = (ll)(b*100.0+0.5);
-  COUT(a*b2/100);
+  ll a; string b;
+  cin >> a >> b;
+  ll x = (b[0]-'0')*100 + (b[2]-'0')*10 + b[3]-'0';
+
+  cout << a*x / 100 << endl;
   return 0;
 }
