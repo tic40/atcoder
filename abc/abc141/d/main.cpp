fec1cd2aa0872b0605a3667d901a3b1da8628e2e
tic40 <ccpzjoh@gmail.com>
2022-06-27 20:51:09 +0900

    Update

--- abc/abc141/d/main.cpp
+++ abc/abc141/d/main.cpp
@@ -1,20 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
+#define REP(i,n) for(int i=0;i<(n);i++)
 using ll = long long;
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
 
 int main() {
   int n,m; cin >> n >> m;
-  priority_queue<int> q;
-
-  int a;
-  REP(i,n) {
-    cin >> a;
-    q.push(a);
-  }
+  vector<int> a(n);
+  REP(i,n) cin >> a[i];
+  priority_queue<int, vector<int>, less<int>> q;
+  REP(i,n) q.push(a[i]);
 
   REP(i,m) {
     q.push(q.top()/2);
@@ -22,10 +16,7 @@ int main() {
   }
 
   ll ans = 0;
-  REP(i,n) {
-    ans += q.top();
-    q.pop();
-  }
+  while(q.size()) { ans += q.top(); q.pop(); }
 
   cout << ans << endl;
   return 0;
