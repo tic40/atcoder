bb3f62d3094aaccf2781053716eef6900188d99c
tic40 <ccpzjoh@gmail.com>
2022-06-28 00:26:14 +0900

    Update

--- abc/abc053/d/main.cpp
+++ abc/abc053/d/main.cpp
@@ -1,39 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ALL(x) (x).begin(),(x).end()
-#define COUT(x) cout<<(x)<<"\n"
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define REP(i, n) for(int i=0;i<n;i++)
-#define YES(x) cout<<(x?"YES":"NO")<<"\n"
-#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
-#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
-#define endl "\n"
-using G = vector<vector<int>>;
-using M = map<int,int>;
-using P = pair<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int,vector<int>,greater<int>>;
-using V = vector<int>;
-using ll = long long;
-template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
-template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
+#define REP(i,n) for(int i=0;i<(n);i++)
 
 int main() {
-  IOS;
-  int n,a; cin >> n;
-  M m;
-  int sum=0;
-  REP(i,n) {
-    cin >> a;
-    m[a]++;
-    if (m[a]>1) sum++;
-  }
-  int ans = n-sum;
-  if (sum % 2) ans--;
+  int n; cin >> n;
+  vector<int> a(n);
+  REP(i,n) cin >> a[i];
 
-  COUT(ans);
+  map<int,int> mp;
+  for(int v: a) mp[v]++;
+
+  int cnt = 0;
+  for(auto v: mp) {
+    if (v.second == 2 || v.second % 2 == 0) cnt++;
+  }
+
+  int ans = mp.size() - cnt%2;
+  cout << ans << endl;
   return 0;
 }
