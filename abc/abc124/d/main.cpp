4e088dda5820a2f7dbc67d92c39651ee53fc60d0
tic40 <ccpzjoh@gmail.com>
2022-06-27 20:04:07 +0900

    Update

--- abc/abc124/d/main.cpp
+++ abc/abc124/d/main.cpp
@@ -1,36 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
-using ll = long long;
 
 int main() {
-  int n,k; string s;
+  int n,k;
+  string s;
   cin >> n >> k >> s;
 
   // ランレングス圧縮
   vector<pair<char,int>> vs;
-  vs.push_back({ s[0], 1 });
+  vs.emplace_back(s[0], 1);
   for(int i = 1; i < n; i++) {
-    if (s[i] == s[i-1]) vs.back().second++;
-    else vs.push_back({ s[i], 1 });
+    if (s[i-1] == s[i]) vs.back().second++;
+    else vs.emplace_back(s[i], 1);
   }
 
-  int zero = 0;
-  int now = 0;
-  int ans = 0;
-  int l = 0;
+  // しゃくとり
+  int ans = 0, sum = 0, l = 0, zero = 0;
   REP(i,(int)vs.size()) {
     if (vs[i].first == '0') zero++;
-    now += vs[i].second;
+    sum += vs[i].second;
 
     while(k < zero) {
       if (vs[l].first == '0') zero--;
-      now -= vs[l].second;
+      sum -= vs[l].second;
       l++;
     }
-    ans = max(ans,now);
-  }
 
+    ans = max(ans,sum);
+  }
   cout << ans << endl;
+
   return 0;
 }
