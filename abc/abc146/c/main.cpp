c85fa5d3a14193a939d13c8b120f0b1dff44cc8f
tic40 <ccpzjoh@gmail.com>
2022-06-28 00:02:19 +0900

    Update

--- abc/abc146/c/main.cpp
+++ abc/abc146/c/main.cpp
@@ -1,46 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ALL(x) (x).begin(),(x).end()
-#define COUT(x) cout<<(x)<<"\n"
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define REP(i, n) for(int i=0;i<n;i++)
-#define YES(x) cout<<(x?"YES":"NO")<<"\n"
-#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
-#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
-#define endl "\n"
-using G = vector<vector<int>>;
-using M = map<int,int>;
-using P = pair<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int,vector<int>,greater<int>>;
-using V = vector<int>;
+#define REP(i,n) for(int i=0;i<(n);i++)
 using ll = long long;
-template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
-template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
 
 int main() {
-  IOS;
   ll a,b,x;
   cin >> a >> b >> x;
 
-  auto d = [&](ll c) {
-    int res = 0;
-    while(c) { c /= 10; res++; }
-    return res;
-  };
-  auto f = [&](ll c) {
-    return a*c + b*d(c);
-  };
+  ll ok = 0, ng = 1e9+1;
+  while(abs(ok-ng)>1) {
+    ll mid = (ok+ng)/2;
+    ll d = to_string(mid).size();
 
-  ll l = 0, r = 1e9+1;
-  while(r-l > 1) {
-    ll c = (l+r)/2;
-    if (f(c) <= x) l = c;
-    else r = c;
+    if (a * mid <= x-b*d) ok = mid;
+    else ng = mid;
   }
-  COUT(l);
+
+  cout << ok << endl;
   return 0;
 }
