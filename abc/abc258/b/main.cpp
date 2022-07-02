de99153a209661c5eca61a5b851d2ddf2c5455b6
tic40 <ccpzjoh@gmail.com>
2022-07-03 01:06:26 +0900

    Update

--- abc/abc258/b/main.cpp
+++ abc/abc258/b/main.cpp
@@ -3,42 +3,29 @@ using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
 using ll = long long;
 
-int n;
-vector<vector<int>> a;
-ll ans = 0;
-
-const vector<pair<int,int>> moves = {
-  { -1, -1 }, { 0, -1 }, { 1, -1 },
-  { -1, 0 }, { 1, 0 }, { 1, 1 },
-  { 0, 1 }, { -1, 1 }
-};
-
-void dfs(int i, int j, int cnt, int dir, ll now) {
-  if (cnt == n) { ans = max(ans,now); return; }
-
-  auto [ti,tj] = moves[dir];
-  int ni = ti+i;
-  int nj = tj+j;
-  if (ni == -1) ni = n-1;
-  if (nj == -1) nj = n-1;
-  if (ni == n) ni = 0;
-  if (nj == n) nj = 0;
-
-  ll v = now * 10 + a[ni][nj];
-  dfs(ni, nj, cnt+1, dir, v);
-  return;
-}
-
 int main() {
-  cin >> n;
-  a.resize(n,vector<int> (n));
+  int n; cin >> n;
+  vector a(n,vector<int>(n));
   REP(i,n) {
     string s; cin >> s;
     REP(j,n) a[i][j] = s[j]-'0';
   }
 
+  ll ans = 0;
   REP(i,n) REP(j,n) {
-    REP(d,8) dfs(i,j,1,d,a[i][j]);
+    for(int di = -1; di <= 1; di++) for(int dj = -1; dj <= 1; dj++) {
+      if (di == 0 && dj == 0) continue;
+      ll tot = 0;
+      int ci = i, cj = j;
+      REP(k,n) {
+        tot = tot * 10 + a[ci][cj];
+        ci += di;
+        cj += dj;
+        ci = (ci + n) % n;
+        cj = (cj + n) % n;
+      }
+      ans = max(ans,tot);
+    }
   }
 
   cout << ans << endl;
