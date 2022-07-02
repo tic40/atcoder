34da5f77f0ccd3ea69a07a6f4c7e1eed7d6b73f4
tic40 <ccpzjoh@gmail.com>
2022-07-03 00:43:16 +0900

    Update template

--- template.cpp
+++ template.cpp
@@ -4,8 +4,9 @@ using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
 using ll = long long;
-const int INF = 1e9+5;
-const ll LINF = 1e18+5;
+using P = pair<int,int>;
+const int INF = numeric_limits<int>::max();
+const ll LINF = numeric_limits<ll>::max();
 const int MOD = 1e9+7;
 // using mint = modint998244353; // modint1000000007;
 
