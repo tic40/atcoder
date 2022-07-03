9b699a8f690a3734a417d4bc9184906f99dc2f61
tic40 <ccpzjoh@gmail.com>
2022-07-03 23:09:59 +0900

    Update

--- abc/abc133/d/main.cpp
+++ abc/abc133/d/main.cpp
@@ -1,31 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
-using ll = long long;
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
-
-int n;
-vector<ll> a;
-
-void solve() {
-  ll offset = 0;
-  REP(i,n) offset = a[i]*2 - offset;
-
-  vector<int> ans(n);
-  ans[0] = offset / 2;
-  REP(i,n-1) ans[i+1] = a[i]*2 - ans[i];
-
-  REP(i,n) cout << ans[i] << " ";
-  return;
-}
+#define REP(i,n) for(int i=0;i<(n);i++)
 
 int main() {
-  cin >> n;
-  a.resize(n);
+  int n; cin >> n;
+  vector<int> a(n);
   REP(i,n) cin >> a[i];
 
-  solve();
+  // 各山に降る量をxとすると、
+  // x1 = a1 - a2 + a3 - a4 ... + an となる
+  vector<int> x(n);
+  REP(i,n) x[0] += i%2 ? -a[i] : a[i];
+
+  for (int i = 1; i < n; i++) {
+    x[i] = 2*a[i-1] - x[i-1];
+  }
+
+  for (int v: x) cout << v << " ";
+  cout << endl;
+
   return 0;
 }
