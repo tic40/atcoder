f43ac9801bf62fbb9bc381da25f0650704aa11fa
tic40 <ccpzjoh@gmail.com>
2022-07-03 15:15:16 +0900

    Update

--- abc/abc112/d/main.cpp
+++ abc/abc112/d/main.cpp
@@ -1,18 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
+#define REP(i,n) for(int i=0;i<(n);i++)
 using ll = long long;
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
 
 int main() {
-  ll n,m;
-  cin >> n >> m;
-  ll ans = 0;
+  int n,m; cin >> n >> m;
 
-  for (ll a = 1; a*a <= m; a++) {
-    if (m%a != 0) continue;
+  ll ans = 0;
+  for(ll a = 1; a*a <= m; a++) {
+    if (m % a) continue; // mがaの約数でなければskip
 
     ll b = m/a;
     if (a*n <= m) ans = max(ans, a);
