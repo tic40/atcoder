d42041cff2fc49ed0f637bf1aa7bf1d5e9d4c882
tic40 <ccpzjoh@gmail.com>
2022-07-05 23:56:18 +0900

    Update

--- abc/abc140/d/main.cpp
+++ abc/abc140/d/main.cpp
@@ -1,23 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
-using ll = long long;
-
-int n,k;
-string s;
-
-void solve() {
-  int ans = n-1, cnt = 0;
-  REP(i,n-1) if (s[i+1] != s[i]) cnt++;
-
-  ans -= max(0, cnt - k*2);
-  cout << ans << endl;
-  return;
-}
+#define REP(i,n) for(int i=0;i<(n);i++)
 
 int main() {
+  int n,k; string s;
   cin >> n >> k >> s;
 
-  solve();
+  int cnt = 0;
+  REP(i,n-1) if (s[i] == s[i+1]) cnt++;
+  int ans = min(k*2+cnt, n-1);
+  cout << ans << endl;
   return 0;
 }
