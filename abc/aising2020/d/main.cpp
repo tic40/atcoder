260040e14d7a85f98dcee7064fa7506c827b03a8
tic40 <ccpzjoh@gmail.com>
2022-07-06 10:24:35 +0900

    Update

--- abc/aising2020/d/main.cpp
+++ abc/aising2020/d/main.cpp
@@ -1,65 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ALL(x) (x).begin(),(x).end()
-#define COUT(x) cout<<(x)<<"\n"
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define REP(i, n) for(int i=0;i<n;i++)
-#define YES(x) cout<<(x?"YES":"NO")<<"\n"
-#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
-#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
-#define endl "\n"
-using G = vector<vector<int>>;
-using M = map<int,int>;
-using P = pair<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int,vector<int>,greater<int>>;
-using V = vector<int>;
+#define REP(i,n) for(int i=0;i<(n);i++)
 using ll = long long;
-using edge = struct { int to; int cost; };
-template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
-template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
 
 int f(int x) {
   if (x == 0) return 0;
-  return f( x % __builtin_popcount(x) ) + 1;
+  return f(x % __builtin_popcount(x))+1;
 }
 
 int main() {
-  IOS;
-  int n; cin >> n;
-  string s; cin >> s;
+  int n; string s;
+  cin >> n >> s;
   vector<int> x(n);
   REP(i,n) x[i] = s[i]-'0';
-  int pc = 0; // popcount
-  REP(i,n) pc += x[i];
-  vector<int> ans(n);
+  // popcount
+  int pc = accumulate(x.begin(),x.end(),0);
 
+  vector<int> ans(n);
+  // ビット反転したときにありえるpopcountは+1か-1のどちらかだけ
   REP(b,2) {
-    int npc = b == 0 ? pc+1 : pc-1;
+    int npc = (b == 0) ? pc+1 : pc-1;
     if (npc <= 0) continue;
 
+    // r0 = 反転していない状態のxから今の popcount で割った余りを10進数で持つ
     int r0 = 0;
-    // 2進数から10進数整数を算出
     REP(i,n) {
       r0 = (r0*2)%npc;
       r0 += x[i];
     }
 
+    // 各桁についてフリップしたときの値を求めていく
     int k = 1;
-    for (int i = n-1; i>=0; i--) {
-      if (x[i] == b) {
+    // 下の桁から見ていく
+    for(int i = n-1; i >= 0; i--) {
+      if (x[i] == b) { // フリップしたとき
         int r = r0;
-        if (b == 0) r = (r+k)%npc;
-        else r = (r-k+npc)%npc;
+        if (b == 0) r = (r+k)%npc; // フリップして1になるとき
+        else r = (r-k+npc)%npc; // フリップして0になるとき. マイナスを考慮する
         ans[i] = f(r)+1;
       }
       k = (k*2)%npc;
     }
   }
 
-  REP(i,n) COUT(ans[i]);
+  for(int v: ans) cout << v << endl;
   return 0;
 }
