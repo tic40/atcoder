d07d35b0d377ad9adb92b0db9b1764f9b1bb5551
tic40 <ccpzjoh@gmail.com>
2022-07-10 23:59:09 +0900

    Update

--- abc/abc147/d/main.cpp
+++ abc/abc147/d/main.cpp
@@ -1,78 +1,24 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace atcoder;
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
+#define REP(i,n) for(int i=0;i<(n);i++)
 using ll = long long;
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
-
-struct mint {
-  ll x; // typedef long long ll;
-  mint(ll x=0):x((x%MOD+MOD)%MOD){}
-  mint operator-() const { return mint(-x);}
-  mint& operator+=(const mint a) {
-    if ((x += a.x) >= MOD) x -= MOD;
-    return *this;
-  }
-  mint& operator-=(const mint a) {
-    if ((x += MOD-a.x) >= MOD) x -= MOD;
-    return *this;
-  }
-  mint& operator*=(const mint a) { (x *= a.x) %= MOD; return *this;}
-  mint operator+(const mint a) const { return mint(*this) += a;}
-  mint operator-(const mint a) const { return mint(*this) -= a;}
-  mint operator*(const mint a) const { return mint(*this) *= a;}
-  mint pow(ll t) const {
-    if (!t) return 1;
-    mint a = pow(t>>1);
-    a *= a;
-    if (t&1) a *= *this;
-    return a;
-  }
-  // for prime MOD
-  mint inv() const { return pow(MOD-2);}
-  mint& operator/=(const mint a) { return *this *= a.inv();}
-  mint operator/(const mint a) const { return mint(*this) /= a;}
-};
-istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
-ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
-
-int n;
-vector<ll> a;
-
-// 愚直
-void solve1() {
-  mint x = 0;
-  REP(i,n) for(int j = i+1; j < n; j++) {
-    x += a[i] ^ a[j];
-  }
-  cout << x << endl;
-  return;
-}
-
-void solve2() {
-  mint x = 1;
-  mint ans = 0;
-  // bitの桁ごとに計算する
-  REP(i,60) {
-    ll zero = 0, one = 0;
-    REP(j,n) {
-      if (a[j] >> i & 1) one++;
-      else zero++;
-    }
-    ans += x * zero * one;
-    x = x*2;
-  }
-  cout << ans << endl;
-  return;
-}
+using mint = modint1000000007;
 
 int main() {
-  cin >> n;
-  a.resize(n);
+  int n; cin >> n;
+  vector<ll> a(n);
   REP(i,n) cin >> a[i];
 
-  // solve1();
-  solve2();
+  mint ans = 0, x = 1;
+  REP(i,60) {
+    int one = 0;
+    REP(j,n) one += (a[j] >> i & 1);
+    ans += x * one * (n-one);
+    x *= 2;
+  }
+
+  cout << ans.val() << endl;
   return 0;
 }
