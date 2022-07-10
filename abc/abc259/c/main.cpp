0d765da73c01034c3a7585daaa098dca0b7c6324
tic40 <ccpzjoh@gmail.com>
2022-07-10 12:00:41 +0900

    Update

--- abc/abc259/c/main.cpp
+++ abc/abc259/c/main.cpp
@@ -1,20 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
-using ll = long long;
 using P = pair<char,int>;
 
 vector<P> run_length(string s) {
   vector<P> vs;
   REP(i,(int)s.size()) {
-    if (i > 0 && vs.back().first == s[i]) vs.back().second++;
+    if (0 < i && vs.back().first == s[i]) vs.back().second++;
     else vs.emplace_back(s[i],1);
   }
   return vs;
 }
 
 int main() {
-  string s,t; cin >> s >> t;
+  string s,t;
+  cin >> s >> t;
 
   auto vs = run_length(s);
   auto vt = run_length(t);
