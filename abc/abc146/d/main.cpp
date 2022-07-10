70d2486e319b27ff36fe9ea0ebeb2fa3face9d8d
tic40 <ccpzjoh@gmail.com>
2022-07-10 18:00:27 +0900

    Update

--- abc/abc146/d/main.cpp
+++ abc/abc146/d/main.cpp
@@ -1,49 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
+#define REP(i,n) for(int i=0;i<(n);i++)
+using P = pair<int,int>;
 
-int n;
-vector<vector<pair<int, int>>> g;
-vector<bool> checked;
-vector<int> ans;
+vector<vector<P>> g;
+vector<int> col;
 
-void dfs(int cur, int pcol) {
-  if (checked[cur]) return;
-
-  int k = 1;
-  checked[cur]=true;
-  for (auto v: g[cur]) {
-    int to = v.first, idx = v.second;
-    if (checked[to]) continue;
-    if (k == pcol) k++;
-    ans[idx] = k;
-    dfs(to, k);
-    k++;
+void dfs(int i, int p, int pcol) {
+  int ncol = 1; // 今使える色
+  for(auto [v, idx]: g[i]) {
+    if (v == p) continue;
+    if (ncol == pcol) ncol++; // 親で使った色と同じであれば++
+    col[idx] = ncol;
+    dfs(v, i, col[idx]);
+    ncol++;
   }
-  return;
-}
-
-void solve() {
-  ans.resize(n-1);
-  checked.resize(n);
-  REP(i,n) dfs(i,0);
-
-  cout << *max_element(ans.begin(), ans.end()) << endl;
-  for(int v: ans) cout << v << endl;
   return;
 }
 
 int main() {
-  cin >> n;
+  int n; cin >> n;
   g.resize(n);
   REP(i,n-1) {
-    int a,b;
-    cin >> a >> b;
+    int a,b; cin >> a >> b;
     a--; b--;
-    g[a].push_back({b,i});
-    g[b].push_back({a,i});
+    g[a].emplace_back(b,i);
+    g[b].emplace_back(a,i);
   }
 
-  solve();
+  col.resize(n-1);
+  dfs(0, -1, -1);
+
+  int mx = *max_element(col.begin(), col.end());
+  cout << mx << endl;
+  for(int v: col) cout << v << endl;
+
   return 0;
 }
