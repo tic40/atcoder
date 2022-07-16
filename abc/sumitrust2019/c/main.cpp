676ba09d85bde6e206510462553bf26f36365742
tic40 <ccpzjoh@gmail.com>
2022-07-16 12:30:14 +0900

    Update

--- abc/sumitrust2019/c/main.cpp
+++ abc/sumitrust2019/c/main.cpp
@@ -1,41 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ALL(x) (x).begin(),(x).end()
-#define COUT(x) cout<<(x)<<"\n"
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define REP(i, n) for(int i=0;i<n;i++)
-#define YES(x) cout<<(x?"YES":"NO")<<"\n"
-#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
-#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
-#define endl "\n"
-using G = vector<vector<int>>;
-using M = map<int,int>;
-using P = pair<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int,vector<int>,greater<int>>;
-using V = vector<int>;
-using ll = long long;
-using edge = struct { int to; int cost; };
-template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
-template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
+#define REP(i,n) for(int i=0;i<(n);i++)
 
 int main() {
-  IOS;
   int x; cin >> x;
-  vector<int> item = {100,101,102,103,104,105};
-  vector<int> dp(100005); // x円の買い物ができるかどうか
-  dp[0] = 1;
+  vector<bool> dp(x+1);
+  dp[0] = true;
 
-  REP(i,x+1) {
-    REP(j,item.size()) {
-      int t = i-item[j];
-      if (0 <= t) dp[i] = max(dp[i], dp[t]);
-    }
+  REP(i,x+1) REP(j,6) {
+    int from = i - (100+j);
+    dp[i] = dp[i] || (from >= 0 && dp[from]);
   }
 
-  COUT(dp[x]);
+  cout << dp[x] << endl;
   return 0;
 }
