ccc0749665f089eb8e39de34909507d1d0f733af
tic40 <ccpzjoh@gmail.com>
2022-07-17 12:09:19 +0900

    Update

--- abc/sumitrust2019/d/main.cpp
+++ abc/sumitrust2019/d/main.cpp
@@ -1,33 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
-using ll = long long;
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
-
-int n;
-string s;
-bool dp[30005][4][1000];
+#define REP(i,n) for(int i=0;i<(n);i++)
 
 int main() {
+  int n; string s;
   cin >> n >> s;
+  vector<vector<int>> m(10);
+  REP(i,n) m[s[i]-'0'].push_back(i);
 
-  dp[0][0][0] = true;
+  auto possible = [&](int now) {
+    int p = 0;
+    REP(_,3) {
+      int x = now%10;
+      now /= 10;
 
-  REP(i,n) REP(j,4) REP(k,1000) {
-    if (!dp[i][j][k]) continue;
-    dp[i+1][j][k] = true;
-    if (j <= 2) {
-      int x = k*10+s[i]-'0';
-      if (0 <= x && x <= 999) {
-        dp[i+1][j+1][k*10+s[i]-'0'] = true;
-      }
+      auto it = lower_bound(m[x].begin(), m[x].end(), p);
+      if (it == m[x].end() || *it < p) return false;
+      p = *it + 1;
     }
-  }
+    return true;
+  };
 
   int ans = 0;
-  REP(k,1000) if (dp[n][3][k]) ans++;
+  REP(i,1000) if (possible(i)) ans++;
+
   cout << ans << endl;
   return 0;
 }
