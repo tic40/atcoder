6324d0dda9cf5cd5da1058db7426c49520d44c28
tic40 <ccpzjoh@gmail.com>
2022-07-18 01:02:30 +0900

    endl

--- template.cpp
+++ template.cpp
@@ -3,6 +3,7 @@
 using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 using ll = long long;
 using P = pair<int,int>;
 const int INF = numeric_limits<int>::max();
