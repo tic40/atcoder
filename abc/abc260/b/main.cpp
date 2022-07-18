0bb3d77e4f8e68f1085653b10536ddf3c7134a1f
tic40 <ccpzjoh@gmail.com>
2022-07-18 10:46:14 +0900

    Update

--- abc/abc260/b/main.cpp
+++ abc/abc260/b/main.cpp
@@ -1,37 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 using P = pair<int,int>;
 
 int main() {
-  int n,x,y,z;
-  cin >> n >> x >> y >> z;
-
+  int n,x,y,z; cin >> n >> x >> y >> z;
   vector<int> a(n),b(n);
   REP(i,n) cin >> a[i];
   REP(i,n) cin >> b[i];
 
-  vector<P> px(n),py(n),pxy(n);
-  REP(i,n) px[i] = {a[i],n-i};
-  REP(i,n) py[i] = {b[i],n-i};
-  REP(i,n) pxy[i] = {a[i]+b[i],n-i};
-
-  sort(px.rbegin(),px.rend());
-  sort(py.rbegin(),py.rend());
-  sort(pxy.rbegin(),pxy.rend());
-  set<int> ans;
-
-  REP(i,x) ans.insert(n-px[i].second);
+  vector<P> ap(n),bp(n),abp(n);
   REP(i,n) {
-    if ((int)ans.size() - x == y) break;
-    ans.insert(n-py[i].second);
+    ap[i] = {a[i],-i};
+    bp[i] = {b[i],-i};
+    abp[i] = {a[i]+b[i],-i};
   }
+  sort(ap.rbegin(),ap.rend());
+  sort(bp.rbegin(),bp.rend());
+  sort(abp.rbegin(),abp.rend());
+
+  set<int> st;
+  REP(i,x) st.insert(-ap[i].second);
 
   REP(i,n) {
-    if ((int)ans.size() - (x+y) == z) break;
-    ans.insert(n-pxy[i].second);
+    if ((int)st.size()-x == y) break;
+    st.insert(-bp[i].second);
+  }
+  REP(i,n) {
+    if ((int)st.size()-(x+y) == z) break;
+    st.insert(-abp[i].second);
   }
 
-  for(int v: ans) cout << v+1 << endl;
+  for(int v: st) cout << v+1 << endl;
   return 0;
 }
