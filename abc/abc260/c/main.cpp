c14cd5f002ebd626aa452061f95351514137f47a
tic40 <ccpzjoh@gmail.com>
2022-07-18 10:49:27 +0900

    Update

--- abc/abc260/c/main.cpp
+++ abc/abc260/c/main.cpp
@@ -1,20 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 using ll = long long;
 
 int main() {
   int n,x,y; cin >> n >> x >> y;
-  vector<ll> r(n+1), b(n+1);
+  vector<ll> r(n+1),b(n+1);
   r[n] = 1;
 
   for(int i = n; i > 1; i--) {
-    // red
     r[i-1] += r[i];
-    b[i] += r[i] * x;
-    // blue
+    b[i] += r[i]*x;
+
     r[i-1] += b[i];
-    b[i-1] += b[i] * y;
+    b[i-1] += b[i]*y;
   }
 
   cout << b[1] << endl;
