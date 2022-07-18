91594ed1888ba0b704eb291d8ea0a9c9f13068f4
tic40 <ccpzjoh@gmail.com>
2022-07-18 12:31:22 +0900

    Update

--- other_contest/typical90/085/main.cpp
+++ other_contest/typical90/085/main.cpp
@@ -1,32 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 using ll = long long;
 
 vector<ll> divisor(ll n) {
-  vector<ll> vec;
-  for(ll i = 1; i*i < n; i++) {
-    if (n % i == 0) {
-      vec.push_back(i);
-      if (i != n / i) vec.push_back(n / i);
+  vector<ll> res;
+  for(ll i = 1; i*i <= n; i++) {
+    if (n%i == 0) {
+      res.push_back(i);
+      if (n/i != i) res.push_back(n/i);
     }
   }
-  return vec;
+  return res;
 }
 
 int main() {
   ll k; cin >> k;
-  vector<ll> d = divisor(k);
+  auto d = divisor(k);
   int n = d.size();
 
-  ll ans = 0;
-  REP(i, d.size()) {
-    for(int j = i; j < d.size(); j++) {
-      ll a = d[i], b = d[j], c = 0;
-      if (k / a < b) continue;
-      if (k % (a*b) != 0) continue;
-      c = k / (a * b);
-      if (b <= c) ans++;
+  int ans = 0;
+  REP(i,n) for(int j = i; j < n; j++) {
+    ll a = d[i];
+    ll b = d[j];
+    ll c = k / a / b;
+    if (a*b*c == k && a <= b && b <= c) {
+      ans++;
     }
   }
 
