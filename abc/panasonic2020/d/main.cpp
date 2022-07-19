ca94b1441dbc08a6b86f98ef7c054e6a245c1cd2
tic40 <ccpzjoh@gmail.com>
2022-07-19 09:56:42 +0900

    Update

--- abc/panasonic2020/d/main.cpp
+++ abc/panasonic2020/d/main.cpp
@@ -4,23 +4,24 @@ using namespace std;
 #define endl '\n'
 
 int n;
-void dfs(string s, char mx) {
+void dfs(string s, int mx) {
   if ((int)s.size() == n) {
     cout << s << endl;
     return;
   }
 
-  // 追加するのに使えるのは'a'～今まで使われた文字の次の文字」
-  // というのを満たす文字列の全列挙
-  for(int i = 0; i <= mx-'a'+1; i++) {
-    char c = char('a'+i);
-    dfs(s+c, max(mx,c));
+ // 追加するのに使えるのは'a'～今まで使われた文字の次の文字
+ // というのを満たす文字列の全列挙
+  for(int i = 0; i <= mx+1; i++) {
+    char c = char('a' + i);
+    dfs(s+c, max(mx, c-'a'));
   }
+
+  return;
 }
 
 int main() {
   cin >> n;
-  dfs("", 'a'-1);
-
+  dfs("", -1);
   return 0;
 }
