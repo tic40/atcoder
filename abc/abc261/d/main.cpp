e314b76a23e072ba122fa912e26ae62f1b63168b
tic40 <ccpzjoh@gmail.com>
2022-07-23 23:13:17 +0900

    Update

--- abc/abc261/d/main.cpp
+++ abc/abc261/d/main.cpp
@@ -19,14 +19,15 @@ int main() {
     vector<ll> p(n+1,-1);
     swap(dp,p);
     REP(cnt,n) {
+      // 裏にする処理
       dp[0] = max(dp[0], p[cnt]);
+      // 表にする処理
       if (p[cnt] != -1) {
         dp[cnt+1] = max(dp[cnt+1], p[cnt]+x[i]+b[cnt+1]);
       }
     }
   }
 
-  ll ans = *max_element(dp.begin(),dp.end());
-  cout << ans << endl;
+  cout << *max_element(dp.begin(),dp.end()) << endl;
   return 0;
 }
