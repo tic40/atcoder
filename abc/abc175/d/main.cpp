855c8533fffca2dd3124c1c4c1ffb091ffbebbe8
tic40 <ccpzjoh@gmail.com>
2022-07-24 20:42:07 +0900

    Update

--- abc/abc175/d/main.cpp
+++ abc/abc175/d/main.cpp
@@ -1,73 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ALL(x) (x).begin(),(x).end()
-#define COUT(x) cout<<(x)<<"\n"
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define REP(i, n) for(int i=0;i<n;i++)
-#define YES(x) cout<<(x?"YES":"NO")<<"\n"
-#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
-#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
-#define endl "\n"
-using G = vector<vector<int>>;
-using M = map<int,int>;
-using P = pair<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int,vector<int>,greater<int>>;
-using V = vector<int>;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 using ll = long long;
-using edge = struct { int to; int cost; };
-template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
-template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
+const ll LINF = numeric_limits<ll>::max();
 
-void solver(int n, int k, vector<int> p, vector<int> c) {
-  REP(i,n) p[i]--; // 0-index
+int main() {
+  int n,k; cin >> n >> k;
+  vector<int> p(n),c(n);
+  REP(i,n) cin >> p[i], p[i]--;
+  REP(i,n) cin >> c[i];
 
   ll ans = -LINF;
-
   REP(i,n) {
-
-
+    // サイクルの合計と移動数を前計算しておく
     ll cycle_sum = 0;
-    int cycle_cnt = 0, v = i;
-    // cycleを計算する
+    int cycle_cnt = 0, now = i;
     while(1) {
       cycle_cnt++;
-      cycle_sum += c[p[v]];
-      v = p[v];
-      if (v == i) break;
+      cycle_sum += c[now];
+      now = p[now];
+      if (now == i) break;
     }
 
+    // シミュレートする
     ll path = 0;
-    int cnt = 0, v = i;
+    int cnt = 0;
     while(1) {
       cnt++;
-      path += c[p[v]];
-
+      path += c[p[now]];
       if (cnt > k) break;
 
       int num = (k - cnt) / cycle_cnt;
-      ll score = path + max(0ll, cycle_sum) * num;
-      chmax(ans, score);
-      v = p[v];
-      if (v == i) break;
+      ll score = path + max(0LL, cycle_sum) * num;
+      ans = max(ans,score);
+      now = p[now];
+      if (now == i) break;
     }
   }
 
-  COUT(ans);
-  return;
-}
-
-int main() {
-  IOS;
-  int n, k; cin >> n >> k;
-  vector<int> p(n),c(n);
-  REP(i,n) cin >> p[i];
-  REP(i,n) cin >> c[i];
-
-  solver(n,k,p,c);
-
+  cout << ans << endl;
   return 0;
 }
