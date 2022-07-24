0f5197cabff1cfdfbee2f59126e6ad0121e8decc
tic40 <ccpzjoh@gmail.com>
2022-07-24 20:01:30 +0900

    Update

--- abc/abc067/d/main.cpp
+++ abc/abc067/d/main.cpp
@@ -1,32 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
-using ll = long long;
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
+const int INF = numeric_limits<int>::max();
 
-int n,a,b;
-vector<int> x;
+vector<vector<int>> g(100001);
+vector<vector<int>> dist(2,vector<int>(100001, INF));
 
-void solve() {
-  ll ans = 0;
-
-  REP(i,n-1) {
-    ll d = x[i+1] - x[i];
-    if (b < a*d) ans+=b;
-    else ans+=a*d;
+void dfs(int i, int p, int id) {
+  for(int v: g[i]) {
+    if (v == p) continue;
+    dist[id][v] = dist[id][i] + 1;
+    dfs(v,i,id);
   }
-
-  cout << ans << endl;
   return;
 }
 
-int main() {
-  cin >> n >> a >> b;
-  x.resize(n);
-  REP(i,n) cin >> x[i];
 
-  solve();
+int main() {
+  int n; cin >> n;
+  REP(i,n-1) {
+    int a,b; cin >> a >> b;
+    a--; b--;
+    g[a].push_back(b);
+    g[b].push_back(a);
+  }
+
+  dist[0][0] = dist[1][n-1] = 0;
+  dfs(0,-1,0);
+  dfs(n-1,-1,1);
+
+  int cnt = 0;
+  REP(i,n) dist[0][i] <= dist[1][i] ? cnt++ : cnt--;
+
+  cout << (cnt > 0 ? "Fennec" : "Snuke") << endl;
   return 0;
 }
