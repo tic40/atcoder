3a974a6403a6daabab3c8cb6ea1efa8d2dbb234c
tic40 <ccpzjoh@gmail.com>
2022-07-25 22:38:34 +0900

    Update

--- abc/abc068/d/main.cpp
+++ abc/abc068/d/main.cpp
@@ -1,48 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ALL(x) (x).begin(),(x).end()
-#define COUT(x) cout<<(x)<<"\n"
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define REP(i, n) for(int i=0;i<n;i++)
-#define YES(x) cout<<(x?"YES":"NO")<<"\n"
-#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
-#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
-#define endl "\n"
-using G = vector<vector<int>>;
-using M = map<int,int>;
-using P = pair<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int,vector<int>,greater<int>>;
-using V = vector<int>;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 using ll = long long;
-using edge = struct { int to; int cost; };
-template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
-template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
-
-const int n = 50;
 
 int main() {
-  IOS;
   ll k; cin >> k;
-  vector<ll> ans(n);
 
-  ll init_num = n-1 + k / n;
-  REP(i,n) ans[i] = init_num;
+  const int n = 50; // n を50に固定して考える
+  vector<ll> ans(n,n);
+  ll d = k % n;
+  REP(i, n-d) ans[i] = 49 - d;
 
-  REP(i,k%n) {
-    ans[i] += n+1;
-    REP(j,n) ans[j]--;
-  }
+  ll add = k / n;
+  REP(i,n) ans[i] += add; // 全ての要素に1回ずつ操作をすると全て均等に-1されるため
 
-  COUT(n);
-  REP(i,n) {
-    if (i > 0) cout << " ";
-    cout << ans[i];
-  }
-  cout << endl;
+  cout << ans.size() << endl;
+  for(ll v: ans) cout << v << " ";
 
   return 0;
 }
