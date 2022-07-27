2f47d38a5e07a64d56d77b6096477235abe7e7d6
tic40 <ccpzjoh@gmail.com>
2022-07-28 00:10:43 +0900

    Update

--- abc/abc080/d/main.cpp
+++ abc/abc080/d/main.cpp
@@ -1,51 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ALL(x) (x).begin(),(x).end()
-#define COUT(x) cout<<(x)<<"\n"
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define REP(i, n) for(int i=0;i<n;i++)
-#define YES(x) cout<<(x?"YES":"NO")<<"\n"
-#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
-#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
-#define endl "\n"
-using G = vector<vector<int>>;
-using M = map<int,int>;
-using P = pair<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int,vector<int>,greater<int>>;
-using V = vector<int>;
-using ll = long long;
-using edge = struct { int to; int cost; };
-template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
-template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
-
-int N,C;
-const int MAX = 100010;
-int cs[30][MAX];
-int ss[MAX];
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 
 int main() {
-  IOS;
-  cin >> N >> C;
-  REP(i,N) {
+  int n,C;
+  cin >> n >> C;
+  vector m(30,vector<int>(1e5+1));
+  REP(i,n) {
     int s,t,c;
     cin >> s >> t >> c;
-    c--; s--;
-    cs[c][s]++;
-    cs[c][t]--;
+    s--; c--;
+    m[c][s]++;
+    m[c][t]--;
   }
 
-  REP(i,C) REP(j,MAX-1) cs[i][j+1] += cs[i][j];
-  REP(i,C) REP(j,MAX) {
-    if (cs[i][j]) ss[j]++;
-  }
+  REP(i,C) REP(j,1e5) m[i][j+1] += m[i][j];
+  REP(i,C) REP(j,1e5) if (m[i][j]) m[i][j] = 1;
 
   int ans = 0;
-  REP(i,MAX) { chmax(ans, ss[i]); }
-  COUT(ans);
+  REP(i,1e5+1) {
+    int cnt = 0;
+    REP(j,C) cnt += m[j][i];
+    ans = max(ans,cnt);
+  }
+  cout << ans << endl;
 
   return 0;
 }
