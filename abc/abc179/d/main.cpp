3655fb81fe6e624c088542835dfc9e1c4fe90130
tic40 <ccpzjoh@gmail.com>
2022-07-31 11:52:33 +0900

    Update

--- abc/abc179/d/main.cpp
+++ abc/abc179/d/main.cpp
@@ -1,32 +1,32 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace atcoder;
 using namespace std;
-#define COUT(x) cout<<(x)<<"\n"
-#define REP(i, n) for(int i=0;i<n;i++)
-#define endl "\n"
-using ll = long long;
-const int MOD = 998244353;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
+using mint = modint998244353;
 
 int main() {
   int n,k; cin >> n >> k;
   vector<int> l(k),r(k);
   REP(i,k) cin >> l[i] >> r[i];
-  vector<ll> dp(n+1), dpsum(n+1);
 
-  dp[1] = 1; // マス1から始まるため dp[1] = 1 にする
-  dpsum[1] = 1; // dpの累積和
+  vector<mint> dp(n+1), dpsum(n+1);
+  dp[1] = 1;
+  dpsum[1] = 1;
 
-  for (int i = 2; i <= n; i++) {
+  // もらうDP
+  for(int i = 2; i <= n; i++) {
     REP(j,k) {
-      int li = max(i-r[j], 1);
-      int ri = i-l[j];
-      if (ri < 0) continue; // 右端が範囲外
+      int li = max(1, i - r[j]);
+      int ri = i - l[j];
+      if (ri < 0) continue;
 
-      dp[i] += dpsum[ri]-dpsum[li-1];
-      dp[i] = dp[i] < 0 ? dp[i]+MOD : dp[i]%MOD; // MOD
+      dp[i] += dpsum[ri] - dpsum[li-1];
     }
-    dpsum[i] = (dpsum[i-1]+dp[i]) % MOD;
+    dpsum[i] = dpsum[i-1] + dp[i];
   }
 
-  COUT(dp[n]);
+  cout << dp[n].val() << endl;
   return 0;
 }
