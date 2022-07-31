0a808eb710fa928f7d9ab9f59c3e4940326aea03
tic40 <ccpzjoh@gmail.com>
2022-08-01 08:44:09 +0900

    Update

--- abc/abc262/c/main.cpp
+++ abc/abc262/c/main.cpp
@@ -1,31 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
-#define endl '\n'
 using ll = long long;
+#define endl '\n'
 
 int main() {
   int n; cin >> n;
   vector<int> a(n);
-  REP(i,n) {
-    cin >> a[i];
-    a[i]--;
-  }
+  REP(i,n) { cin >> a[i]; a[i]--; }
 
-  // a[i] = i の数
   vector<int> m(n+1);
-  REP(i,n) {
-    if (a[i] == i) m[i]++;
-    m[i+1] = m[i];
-  }
+  REP(i,n) if (a[i] == i) m[i]++;
+  REP(i,n) m[i+1] += m[i];
 
   ll ans = 0;
   REP(i,n) {
     if (a[i] < i) continue;
     if (a[i] == i) {
       ans += m[n] - m[i];
-    } else {
-      if (a[a[i]] == i) ans++;
+    }
+    if (a[i] > i) {
+      if (a[ a[i] ] == i) ans++;
     }
   }
 
