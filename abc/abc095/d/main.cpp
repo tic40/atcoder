d399d10aa4136030cfa1fdc6b0a60c167e250771
tic40 <ccpzjoh@gmail.com>
2022-08-02 00:07:41 +0900

    Update

--- abc/abc095/d/main.cpp
+++ abc/abc095/d/main.cpp
@@ -1,62 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ALL(x) (x).begin(),(x).end()
-#define COUT(x) cout<<(x)<<"\n"
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define REP(i,n) for(int i=0;i<n;i++)
-#define YES(x) cout<<(x?"YES":"NO")<<"\n"
-#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
-#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
-#define endl "\n"
-using G = vector<vector<int>>;
-using M = map<int,int>;
-using P = pair<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int,vector<int>,greater<int>>;
-using V = vector<int>;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 using ll = long long;
-using edge = struct { int to; int cost; };
-template<class T>bool chmax(T &a,const T &b) {if(a<b){a=b; return 1;} return 0;}
-template<class T>bool chmin(T &a,const T &b) {if(b<a){a=b; return 1;} return 0;}
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
-
-ll n,c;
+template<class T> void chmax(T& a, T b) { if (a < b) a = b; }
 
 int main() {
-  IOS;
+  int n; ll c;
   cin >> n >> c;
   vector<ll> x(n),v(n);
   REP(i,n) cin >> x[i] >> v[i];
 
-  ll ans = 0;
-  vector<pair<ll,ll>> s(n+1); // 累積和 <oa, 2oa>
+  vector<ll> m1(n+1),m2(n+1); // m1は片道, m2は往復
   REP(i,n) {
-    s[i+1].first = s[i].first + v[i] - x[i];
-    s[i+1].second = s[i].second + v[i] - x[i]*2;
+    // 累積和を取る: m[i+1] = m1[i] + v[i] - (x[i] - x[i-1])
+    m1[i+1] = m1[i] + v[i] - x[i];
+    m2[i+1] = m2[i] + v[i] - x[i]*2;
     if (0 < i) {
-      s[i+1].first += x[i-1];
-      s[i+1].second += x[i-1]*2;
+      m1[i+1] += x[i-1];
+      m2[i+1] += x[i-1]*2;
     }
-    chmax(ans,s[i+1].first);
   }
 
-  vector<pair<ll,ll>> dp(n+1); // dp[i] = iまでの最大値. <oa, 2oa>
+  ll ans = *max_element(m1.begin(), m1.end());
+  vector<ll> dp1(n+1), dp2(n+1); // dp[i] := iまでの最大値. dp1は片道, dp2は往復
   REP(i,n) {
-    dp[i+1] = {
-      max(dp[i].first,s[i+1].first),
-      max(dp[i].second,s[i+1].second)
-    };
+    dp1[i+1] = max(dp1[i], m1[i+1]);
+    dp2[i+1] = max(dp2[i], m2[i+1]);
   }
 
   ll sum = 0;
   for (int i = n-1; 0 <= i; i--) {
     sum += v[i];
-    chmax(ans, sum - (c-x[i]) + dp[i].second);
-    chmax(ans, sum - (c-x[i])*2 + dp[i].first);
+    // 時計回りに進んで、その後に反時計回り
+    chmax(ans, sum - (c-x[i]) + dp2[i]);
+    // 反時計回りに進んで、その後に時計回り
+    chmax(ans, sum - (c-x[i])*2 + dp1[i]);
   }
 
-  COUT(ans);
+  cout << ans << endl;
   return 0;
 }
