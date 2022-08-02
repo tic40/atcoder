c498620cddde516cb092d7d885cc769edde1bd1a
tic40 <ccpzjoh@gmail.com>
2022-08-02 23:37:52 +0900

    Update

--- abc/abc262/d/main.cpp
+++ abc/abc262/d/main.cpp
@@ -12,17 +12,17 @@ int main() {
   vector<int> a(n);
   REP(i,n) cin >> a[i];
 
-  // m個選ぶ
   mint ans = 0;
+  // m個選ぶ
   for(int m = 1; m <= n; m++) {
-    vector dp(n+2, vector(n+2, vector<mint>(n+2)));
-    // dp[i][j][k] := iからj項選ぶ
+    // dp[i][j][k] := i項の中からj項を選んで、mで割ったあまりがkの総数
+    vector dp(n+5, vector(n+5, vector<mint>(n+5)));
     dp[0][0][0] = 1;
     REP(i,n) REP(j,m+1) REP(k,m) {
       // 選ばない
       dp[i+1][j][k] += dp[i][j][k];
       // 選ぶ
-      dp[i+1][j+1][(k + a[i]) % m] += dp[i][j][k];
+      dp[i+1][j+1][ (k+a[i]) % m ] += dp[i][j][k];
     }
     ans += dp[n][m][0];
   }
