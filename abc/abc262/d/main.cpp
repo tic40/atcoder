a47b131eca0995f294947f0d6a1e425a40c579b3
tic40 <ccpzjoh@gmail.com>
2022-08-03 09:12:33 +0900

    Update

--- abc/abc262/d/main.cpp
+++ abc/abc262/d/main.cpp
@@ -4,7 +4,6 @@ using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
 #define endl '\n'
-using ll = long long;
 using mint = modint998244353;
 
 int main() {
@@ -13,16 +12,12 @@ int main() {
   REP(i,n) cin >> a[i];
 
   mint ans = 0;
-  // m個選ぶ
   for(int m = 1; m <= n; m++) {
-    // dp[i][j][k] := i項の中からj項を選んで、mで割ったあまりがkの総数
-    vector dp(n+5, vector(n+5, vector<mint>(n+5)));
+    vector dp(n+1, vector(m+2, vector<mint>(m)));
     dp[0][0][0] = 1;
     REP(i,n) REP(j,m+1) REP(k,m) {
-      // 選ばない
       dp[i+1][j][k] += dp[i][j][k];
-      // 選ぶ
-      dp[i+1][j+1][ (k+a[i]) % m ] += dp[i][j][k];
+      dp[i+1][j+1][ (k+a[i])%m ] += dp[i][j][k];
     }
     ans += dp[n][m][0];
   }
