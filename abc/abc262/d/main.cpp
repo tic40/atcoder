f4fef4e130595abdc9826b7fc21733b3e5aa7f18
tic40 <ccpzjoh@gmail.com>
2022-08-03 09:13:52 +0900

    Update

--- abc/abc262/d/main.cpp
+++ abc/abc262/d/main.cpp
@@ -12,11 +12,15 @@ int main() {
   REP(i,n) cin >> a[i];
 
   mint ans = 0;
+  // m個選ぶときのDPをそれぞれ計算する
   for(int m = 1; m <= n; m++) {
     vector dp(n+1, vector(m+2, vector<mint>(m)));
     dp[0][0][0] = 1;
+    // dp[i][j][k] := i項の中からj項選んで合計をmで割った余りがkになるものの総数
     REP(i,n) REP(j,m+1) REP(k,m) {
+      // 選ぶ
       dp[i+1][j][k] += dp[i][j][k];
+      // 選ばない
       dp[i+1][j+1][ (k+a[i])%m ] += dp[i][j][k];
     }
     ans += dp[n][m][0];
