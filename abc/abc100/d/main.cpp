d86de16d9f499679612547779822a31ca211b833
tic40 <ccpzjoh@gmail.com>
2022-08-07 22:50:53 +0900

    Update

--- abc/abc100/d/main.cpp
+++ abc/abc100/d/main.cpp
@@ -1,26 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 using ll = long long;
+const ll LINF = numeric_limits<ll>::max();
 
 int main() {
-  int n,m;
-  cin >> n >> m;
-  ll x[n],y[n],z[n];
+  int n,m; cin >> n >> m;
+  vector<ll> x(n),y(n),z(n);
   REP(i,n) cin >> x[i] >> y[i] >> z[i];
 
-  ll sum[1<<3][n],ans = 0;
-  for (int bit = 0; bit < (1<<3); bit++) {
+  ll ans = -LINF;
+  REP(bit, 1<<3) {
+    vector<ll> sum(n);
     REP(i,n) {
-      sum[bit][i] = 0;
-      sum[bit][i] += (bit >> 0 & 1) ? x[i] : -x[i];
-      sum[bit][i] += (bit >> 1 & 1) ? y[i] : -y[i];
-      sum[bit][i] += (bit >> 2 & 1) ? z[i] : -z[i];
+      sum[i] += (bit >> 2 & 1) ? x[i] : -x[i];
+      sum[i] += (bit >> 1 & 1) ? y[i] : -y[i];
+      sum[i] += (bit & 1) ? z[i] : -z[i];
     }
-    sort(sum[bit], sum[bit]+n, greater<ll>());
-    ll tmp = 0;
-    REP(i,m) tmp += sum[bit][i];
-    if (ans < tmp) ans = tmp;
+    sort(sum.rbegin(),sum.rend());
+    ans = max(ans, accumulate(sum.begin(), sum.begin()+m, 0LL));
   }
 
   cout << ans << endl;
