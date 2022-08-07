f7ee45bbe3cce816ee3e95feefb9777e8308a697
tic40 <ccpzjoh@gmail.com>
2022-08-07 21:56:35 +0900

    Update

--- abc/abc099/d/main.cpp
+++ abc/abc099/d/main.cpp
@@ -1,15 +1,7 @@
 #include <bits/stdc++.h>
-#include <atcoder/all>
-using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
 #define endl '\n'
-using ll = long long;
-using P = pair<int,int>;
-const int INF = numeric_limits<int>::max();
-const ll LINF = numeric_limits<ll>::max();
-const int MOD = 1e9+7;
-// using mint = modint998244353; // modint1000000007;
 
 int main() {
   int n,C;
@@ -28,7 +20,7 @@ int main() {
     cost[ (i+j)%3 ][x] += d[ c[i][j] ][x];
   }
 
-  int ans = INF;
+  int ans = 1e9;
   REP(i,C) REP(j,C) REP(k,C) {
     if (i==j || j==k || k==i) continue;
     ans = min(ans, cost[0][i]+cost[1][j]+cost[2][k]);
