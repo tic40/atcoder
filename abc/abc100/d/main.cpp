ba47cbd0a1ca8ca9581b25c6427a080d9a2b66a4
tic40 <ccpzjoh@gmail.com>
2022-08-07 22:57:11 +0900

    Update

--- abc/abc100/d/main.cpp
+++ abc/abc100/d/main.cpp
@@ -10,16 +10,16 @@ int main() {
   vector<ll> x(n),y(n),z(n);
   REP(i,n) cin >> x[i] >> y[i] >> z[i];
 
-  ll ans = -LINF;
+  ll ans = 0;
   REP(bit, 1<<3) {
     vector<ll> sum(n);
     REP(i,n) {
-      sum[i] += (bit >> 2 & 1) ? x[i] : -x[i];
-      sum[i] += (bit >> 1 & 1) ? y[i] : -y[i];
+      sum[i] += (bit>>2 & 1) ? x[i] : -x[i];
+      sum[i] += (bit>>1 & 1) ? y[i] : -y[i];
       sum[i] += (bit & 1) ? z[i] : -z[i];
     }
     sort(sum.rbegin(),sum.rend());
-    ans = max(ans, accumulate(sum.begin(), sum.begin()+m, 0LL));
+    ans = max(ans,accumulate(sum.begin(),sum.begin()+m,0LL));
   }
 
   cout << ans << endl;
