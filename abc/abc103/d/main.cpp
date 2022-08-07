8c48547468b68f00c3f98bd07b20ef9b942177a3
tic40 <ccpzjoh@gmail.com>
2022-08-07 23:20:45 +0900

    Update

--- abc/abc103/d/main.cpp
+++ abc/abc103/d/main.cpp
@@ -1,47 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ALL(x) (x).begin(),(x).end()
-#define COUT(x) cout<<(x)<<"\n"
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define REP(i,n) for(int i=0;i<n;i++)
-#define YES(x) cout<<(x?"YES":"NO")<<"\n"
-#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
-#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
-#define endl "\n"
-using G = vector<vector<int>>;
-using M = map<int,int>;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 using P = pair<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int,vector<int>,greater<int>>;
-using V = vector<int>;
-using ll = long long;
-using edge = struct { int to; int cost; };
-template<class T>bool chmax(T &a,const T &b) {if(a<b){a=b; return 1;} return 0;}
-template<class T>bool chmin(T &a,const T &b) {if(b<a){a=b; return 1;} return 0;}
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
 
 int main() {
-  IOS;
-  int n,m,ans=0,last=-1;
-  cin >> n >> m;
-  vector<pair<int, int>> p(m);
+  int n,m; cin >> n >> m;
+  vector<P> p(m);
   REP(i,m) {
     int a,b; cin >> a >> b;
-    p[i] = { a-1, b-1 };
+    a--; b--;
+    p[i] = {b,a};
   }
+  sort(p.begin(),p.end());
 
-  // 右端が左にあるものから順にソートする
-  sort(p.begin(), p.end(), [](auto &first, auto &second) {
-    return first.second < second.second;
-  });
-
+  int ans = 0, last = -1;
   REP(i,m) {
-    int l = p[i].first, r = p[i].second;
-    if (last <= l) ans++, last = r;
+    if (p[i].second < last) continue;
+    last = p[i].first;
+    ans++;
   }
-
   cout << ans << endl;
   return 0;
 }
