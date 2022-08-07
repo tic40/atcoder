5e2a0abf7e095bc768607578384954be24866105
tic40 <ccpzjoh@gmail.com>
2022-08-08 00:41:43 +0900

    Update

--- abc/abc180/e/main.cpp
+++ abc/abc180/e/main.cpp
@@ -1,21 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
-template<class T>bool chmin(T &a,const T &b) {if(b<a){a=b; return 1;} return 0;}
-const int INF = 1e9;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
+const int INF = numeric_limits<int>::max();
+template<class T> void chmin(T& a, T b) { if (a > b) a = b; }
 
 int main() {
   int n; cin >> n;
-  int x[n],y[n],z[n];
+  vector<int> x(n),y(n),z(n);
   REP(i,n) cin >> x[i] >> y[i] >> z[i];
 
-  // dp定義: [訪れた都市集合][今いる都市] = 最小コスト
-  // 訪れた都市集合はbitで表現する
-  // INF(十分大きい値)で初期化する
+  // dp[bit][j] := [訪れた都市集合][今いる都市] = 最小コスト
   vector<vector<int>> dp(1<<n,vector<int>(n,INF));
 
   // 都市iから都市jへのコストを事前計算
-  int cost[n][n];
+  vector cost(n,vector<int>(n));
   REP(i,n) REP(j,n) {
     int c1 = abs(x[j]-x[i]);
     int c2 = abs(y[j]-y[i]);
@@ -24,23 +23,12 @@ int main() {
   }
 
   // 開始点0から各都市への移動
-  REP(i,n) {
-    // 0から0への移動はスキップ
-    if (i==0) continue;
-    dp[1<<i][i] = cost[0][i];
-  }
+  for(int i =1; i < n; i++) dp[1<<i][i] = cost[0][i];
 
-  REP(i,1<<n) {
-    REP(j,n) {
-      // j都市のフラグが立っていない場合
-      // 未訪問のj都市からは移動できないのでスキップ
-      if (~i>>j&1) continue;
-      REP(k,n) {
-        // k都市のフラグが立っている場合
-        // 訪問済みなのでスキップ
-        if (i>>k&1) continue;
-        chmin(dp[i|1<<k][k], dp[i][j]+cost[j][k]);
-      }
+  // bitの状態のときにiからjへの移動
+  REP(bit,1<<n) REP(i,n) REP(j,n) {
+    if ((bit >> i & 1) && (bit >> j & 1) == 0) {
+      chmin(dp[bit | 1<<j][j], dp[bit][i] + cost[i][j]);
     }
   }
 
