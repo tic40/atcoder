9f2e2c5c128ff1e51ccef04383fe3c11a795fdc8
tic40 <ccpzjoh@gmail.com>
2022-08-10 19:58:02 +0900

    Update

--- abc/abc104/d/main.cpp
+++ abc/abc104/d/main.cpp
@@ -10,17 +10,25 @@ int main() {
   string s; cin >> s;
   int n = s.size();
 
-  // m[i][j] := jがそれぞれの状態の総数. 1: A, 2: AB, 3: ABC
+  // "", A, AB, ABC それぞれの状態の総数
   vector<mint> dp(4);
   dp[0] = 1;
   REP(i,n) {
     vector<mint> p(4);
     swap(dp,p);
+
+    // ABCの要素を選ばない
     REP(j,4) dp[j] += s[i] == '?' ? p[j]*3 : p[j];
 
-    if (s[i] == 'A' || s[i] == '?') dp[1] += p[0];
-    if (s[i] == 'B' || s[i] == '?') dp[2] += p[1];
-    if (s[i] == 'C' || s[i] == '?') dp[3] += p[2];
+    // 選ぶ
+    if (s[i] == 'A') dp[1] += p[0];
+    if (s[i] == 'B') dp[2] += p[1];
+    if (s[i] == 'C') dp[3] += p[2];
+    if (s[i] == '?') {
+      dp[1] += p[0];
+      dp[2] += p[1];
+      dp[3] += p[2];
+    }
   }
 
   cout << dp[3].val() << endl;
