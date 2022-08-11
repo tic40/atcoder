6007729b4f5819160709c4d9171d9cb9069e0368
tic40 <ccpzjoh@gmail.com>
2022-08-11 18:26:45 +0900

    Update

--- abc/abc182/e/main.cpp
+++ abc/abc182/e/main.cpp
@@ -1,74 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ALL(x) (x).begin(),(x).end()
-#define COUT(x) cout<<(x)<<"\n"
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define REP(i,n) for(int i=0;i<n;i++)
-#define YES(x) cout<<(x?"YES":"NO")<<"\n"
-#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
-#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
-#define endl "\n"
-using G = vector<vector<int>>;
-using M = map<int,int>;
-using P = pair<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int,vector<int>,greater<int>>;
-using V = vector<int>;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 using ll = long long;
-using edge = struct { int to; int cost; };
-template<class T>bool chmax(T &a,const T &b) {if(a<b){a=b; return 1;} return 0;}
-template<class T>bool chmin(T &a,const T &b) {if(b<a){a=b; return 1;} return 0;}
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
+using P = pair<int,int>;
 
-
-const int MX = 1500;
-const pair<int, int> direction[4] = {
-  { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 }
-};
-
-int h,w,n,m;
-
-bool light[MX][MX];
-bool wall[MX][MX];
-bool ok[MX][MX];
-
-bool visited[MX][MX];
-bool memo[MX][MX];
-
-bool f(int d, int i, int j) {
-  if (i < 0 || j < 0 || h <= i || w <= j) return false;
-  if (wall[i][j]) return false;
-  if (light[i][j]) return true;
-  if (visited[i][j]) return memo[i][j];
-
-  visited[i][j] = true;
-  int ni = i + direction[d].first;
-  int nj = j + direction[d].second;
-
-  return memo[i][j] = f(d, ni, nj);
-}
+const vector<P> dir = { {1,0}, {-1,0}, {0,1}, {0,-1} };
 
 int main() {
-  IOS;
+  int h,w,n,m;
   cin >> h >> w >> n >> m;
+  vector<int> a(n),b(n);
+  vector block(h, vector<bool>(w));
   REP(i,n) {
-    int a,b; cin >> a >> b;
-    a--; b--; light[a][b] = 1;
+    cin >> a[i] >> b[i];
+    a[i]--; b[i]--;
   }
   REP(i,m) {
     int c,d; cin >> c >> d;
-    c--; d--; wall[c][d] = 1;
+    c--; d--;
+    block[c][d] = true;
   }
 
-  REP(d,4) {
-    REP(i,h) REP(j,w) visited[i][j] = 0;
-    REP(i,h) REP(j,w) if(f(d,i,j)) ok[i][j] = 1;
+  vector light(h, vector<int>(w));
+  // 4方向をそれぞれ試す
+  REP(k,4) {
+    auto [dx,dy] = dir[k];
+    REP(i,n) {
+      int nx = a[i], ny = b[i];
+      while(1) {
+        if (nx < 0 || h <= nx || ny < 0 || w <= ny) break;
+        if (block[nx][ny]) break;
+        if (light[nx][ny] == k+1) break;
+        light[nx][ny] = k+1;
+        nx += dx; ny += dy;
+      }
+    }
   }
 
   int ans = 0;
-  REP(i,h) REP(j,w) ans += ok[i][j];
-  COUT(ans);
+  REP(i,h) REP(j,w) if (light[i][j]) ans++;
+  cout << ans << endl;
   return 0;
 }
