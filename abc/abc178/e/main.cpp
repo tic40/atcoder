4c0a2c2c20a77363646e58ab1448dd0f63370a5f
tic40 <ccpzjoh@gmail.com>
2022-08-15 20:40:17 +0900

    Update

--- abc/abc178/e/main.cpp
+++ abc/abc178/e/main.cpp
@@ -1,43 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ALL(x) (x).begin(),(x).end()
-#define COUT(x) cout<<(x)<<"\n"
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define REP(i, n) for(int i=0;i<n;i++)
-#define YES(x) cout<<(x?"YES":"NO")<<"\n"
-#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
-#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
-#define endl "\n"
-using G = vector<vector<int>>;
-using M = map<int,int>;
-using P = pair<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int,vector<int>,greater<int>>;
-using V = vector<int>;
-using ll = long long;
-using edge = struct { int to; int cost; };
-template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
-template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
-const int INF = 1e9;
-const ll LINF = 1e18;
-const int MOD = 1e9+7;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 
 int main() {
-  IOS;
   int n; cin >> n;
-  int x[n],y[n];
+  vector<int> x(n),y(n);
   REP(i,n) cin >> x[i] >> y[i];
-  ll sum[n],sub[n];
 
-  REP(i,n) {
-    sum[i] = x[i] + y[i];
-    sub[i] = x[i] - y[i];
-  }
+  vector<int> sum(n), sub(n);
+  REP(i,n) sum[i] = x[i]+y[i];
+  REP(i,n) sub[i] = x[i]-y[i];
 
-  sort(sum, sum+n);
-  sort(sub, sub+n);
-  ll ans = max(sum[n-1] - sum[0], sub[n-1] - sub[0]);
+  sort(sum.begin(),sum.end());
+  sort(sub.begin(),sub.end());
 
-  COUT(ans);
+  int ans = max(sum.back() - sum[0], sub.back()-sub[0]);
+  cout << ans << endl;
+
   return 0;
 }
