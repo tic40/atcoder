7d240817f35a856f9641c5eb62618458b49176cf
tic40 <ccpzjoh@gmail.com>
2022-08-19 12:16:28 +0900

    Update

--- abc/abc184/d/main.cpp
+++ abc/abc184/d/main.cpp
@@ -7,18 +7,17 @@ const int x = 100;
 
 int main() {
   int a,b,c; cin >> a >> b >> c;
-
+  // dp[i][j][k] := 金i枚,銀j枚,銅k枚ある状態からの期待値
   vector dp(x+1, vector(x+1, vector<double>(x+1)));
 
   for(int i = x-1; i >= 0; i--) {
     for(int j = x-1; j >= 0; j--) {
       for(int k = x-1; k >= 0; k--) {
         if(i+j+k == 0) continue;
-        double now = 0;
-        now += dp[i+1][j][k] * i;
-        now += dp[i][j+1][k] * j;
-        now += dp[i][j][k+1] * k;
-        dp[i][j][k] = now / (i+j+k) + 1;
+        // i+j+k の中から一枚取ることを考える
+        dp[i][j][k] += (dp[i+1][j][k]+1) * i / (i+j+k);
+        dp[i][j][k] += (dp[i][j+1][k]+1) * j / (i+j+k);
+        dp[i][j][k] += (dp[i][j][k+1]+1) * k / (i+j+k);
       }
     }
   }
