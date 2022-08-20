0daf9daf2a23018eab01943a2a2489c2b84560d2
tic40 <ccpzjoh@gmail.com>
2022-08-20 23:12:50 +0900

    Update c

--- abc/abc104/c/main.cpp
+++ abc/abc104/c/main.cpp
@@ -1,52 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i, n) for(int i = 0; i < n; i++)
-#define COUT(x) cout<<(x)<<endl
-#define dump(x)  cout << #x << " = " << (x) << endl;
-using ll = long long;
-using P = pair<int,int>;
-using Graph = vector<vector<int>>;
-using M = map<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int, vector<int>, greater<int>>;
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 
 int main() {
-  int d,g;
-  cin >> d >> g;
-  int p[d],c[d];
+  int d,g; cin >> d >> g;
+  vector<int> p(d),c(d);
   REP(i,d) cin >> p[i] >> c[i];
 
-  int ans = INF;
-  for (int bit = 0; bit < (1 << d); bit++) {
-    int cnt = 0, sum = 0;
+  int ans = 1e9;
+  REP(bit, 1<<d) {
+    int sum = 0, cnt = 0, li = -1;
     REP(i,d) {
       if (bit >> i & 1) {
         sum += 100 * (i+1) * p[i] + c[i];
         cnt += p[i];
+      } else {
+        li = i;
       }
     }
 
-    if (g <= sum) {
+    if (sum >= g) {
       ans = min(ans, cnt);
     } else {
-      int mxidx;
-      for (int i = d-1; 0 <= i; i--) {
-        if (bit >> i & 1) continue;
-        mxidx = i;
-        break;
-      }
-
-      REP(j,p[mxidx]) {
-        sum += 100 * (mxidx+1);
-        cnt++;
-        if (g <= sum) ans = min(ans, cnt);
-      }
+      int t = 100 * (li+1);
+      int k = ( (g-sum) + t - 1) / t;
+      if (k <= p[li]) ans = min(ans, cnt + k);
     }
   }
 
-  COUT(ans);
+  cout << ans << endl;
   return 0;
 }
