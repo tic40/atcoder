e9bc42befca4a987d368c2729fd1136408d2ac75
tic40 <ccpzjoh@gmail.com>
2022-08-26 00:06:43 +0900

    Update

--- abc/abc186/e/main.cpp
+++ abc/abc186/e/main.cpp
@@ -4,6 +4,8 @@ using namespace std;
 #define endl '\n'
 using ll = long long;
 
+// 拡張ユークリッドの互除法
+// {g,x,y}: ax + by = g
 tuple<ll,ll,ll> extgcd(ll a, ll b) {
   if (b == 0) return {a,1,0};
   ll g,x,y;
@@ -16,14 +18,16 @@ int main() {
   REP(i,t) {
     ll n,s,k; cin >> n >> s >> k;
     ll g,x,y;
+    // s + k*x ≡ 0 mod n
+    // k*x ≡ -s mod n
+    // k*x + n*y = g に変形できる
+    // g, x, y を求める
     tie(g,x,y) = extgcd(k,n);
     if (s%g != 0) {
       cout << -1 << endl;
       continue;
     }
-    n /= g;
-    s /= g;
-    k /= g;
+    n /= g; s /= g; k /= g;
     ll ans = ((x * -s) % n + n) % n;
     cout << ans << endl;
   }
