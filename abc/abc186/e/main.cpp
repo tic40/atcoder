1b0187ba29a68a6fe1e63e0249afe0b546264c84
tic40 <ccpzjoh@gmail.com>
2022-08-25 22:04:06 +0900

    Update

--- abc/abc186/e/main.cpp
+++ abc/abc186/e/main.cpp
@@ -1,100 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ALL(x) (x).begin(),(x).end()
-#define COUT(x) cout<<(x)<<"\n"
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define REP(i,n) for(int i=0;i<n;i++)
-#define YES(x) cout<<(x?"YES":"NO")<<"\n"
-#define Yes(x) cout<<(x?"Yes":"No")<<"\n"
-#define dump(x) cout<<#x<<" = "<<(x)<<"\n"
-#define endl "\n"
-using G = vector<vector<int>>;
-using M = map<int,int>;
-using P = pair<int,int>;
-using PQ = priority_queue<int>;
-using PQG = priority_queue<int,vector<int>,greater<int>>;
-using V = vector<int>;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 using ll = long long;
-using edge = struct { int to; int cost; };
-template<class T>bool chmax(T &a,const T &b) {if(a<b){a=b; return 1;} return 0;}
-template<class T>bool chmin(T &a,const T &b) {if(b<a){a=b; return 1;} return 0;}
-const int INF = 1e9;
-const ll LINF = 1e18;
 
-int mod;
-struct mint {
-  ll x; // typedef long long ll;
-  mint(ll x=0):x((x%mod+mod)%mod){}
-  mint operator-() const { return mint(-x);}
-  mint& operator+=(const mint a) {
-    if ((x += a.x) >= mod) x -= mod;
-    return *this;
-  }
-  mint& operator-=(const mint a) {
-    if ((x += mod-a.x) >= mod) x -= mod;
-    return *this;
-  }
-  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
-  mint operator+(const mint a) const { return mint(*this) += a;}
-  mint operator-(const mint a) const { return mint(*this) -= a;}
-  mint operator*(const mint a) const { return mint(*this) *= a;}
-  mint pow(ll t) const {
-    if (!t) return 1;
-    mint a = pow(t>>1);
-    a *= a;
-    if (t&1) a *= *this;
-    return a;
-  }
-  // for prime mod
-  mint inv() const { return pow(mod-2);}
-  mint& operator/=(const mint a) { return *this *= a.inv();}
-  mint operator/(const mint a) const { return mint(*this) /= a;}
-};
-istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
-ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
-
-ll gcd(ll a, ll b) { return b ? gcd(b, a%b) : a; }
-
-// mod. m での a の逆元 a^{-1} を計算する
-ll modinv(ll a, ll m) {
-  ll b = m, u = 1, v = 0;
-  while (b) {
-    ll t = a / b;
-    a -= t * b; swap(a, b);
-    u -= t * v; swap(u, v);
-  }
-  u %= m;
-  if (u < 0) u += m;
-  return u;
-}
-
-void solve(ll n,ll s,ll k) {
-  // S + K*x ⇔ 0 (mod N)
-  // K*x ⇔ -S (mod N)
-  ll g = gcd(k,n);
-  if (s%g) { // Sがgの倍数でない場合
-    COUT(-1);
-    return;
-  }
-
-  k /= g; s /= g; n /= g;
-  ll v = modinv(k,n);
-  mod = n;
-  mint ans = mint(-s*v);
-  COUT(ans);
-
-  return;
+tuple<ll,ll,ll> extgcd(ll a, ll b) {
+  if (b == 0) return {a,1,0};
+  ll g,x,y;
+  tie(g,x,y) = extgcd(b, a%b);
+  return { g, y, x-a/b*y };
 }
 
 int main() {
-  IOS;
   int t; cin >> t;
-
   REP(i,t) {
-    ll n,s,k;
-    cin >> n >> s >> k;
-    solve(n, s, k);
+    ll n,s,k; cin >> n >> s >> k;
+    ll g,x,y;
+    tie(g,x,y) = extgcd(k,n);
+    if (s%g != 0) {
+      cout << -1 << endl;
+      continue;
+    }
+    n /= g;
+    s /= g;
+    k /= g;
+    ll ans = ((x * -s) % n + n) % n;
+    cout << ans << endl;
   }
-
   return 0;
 }
