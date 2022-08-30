acfa64bdc19e5abec9ed797a296cd64499aaebb6
tic40 <ccpzjoh@gmail.com>
2022-08-30 11:19:04 +0900

    Update

--- abc/abc266/e/main.cpp
+++ abc/abc266/e/main.cpp
@@ -6,16 +6,13 @@ using namespace std;
 int main() {
   int n; cin >> n;
 
-  double t = 3.5;
+  double ans = 3.5;
   for(int i = 1; i < n; i++) {
     double now = 0;
-    for(int j = 1; j <= 6; j++) {
-      if ((double)j < t) now += t;
-      else now += (double)j;
-    }
-    t = now / 6.0;
+    for(int j = 1; j <= 6; j++) now += max(double(j),ans);
+    ans = now / 6.0;
   }
 
-  printf("%0.10f\n", t);
+  printf("%0.10f\n", ans);
   return 0;
 }
