a8fc59cf325e47c1ac47e95ce0ae0e4349e074c7
tic40 <ccpzjoh@gmail.com>
2022-08-30 11:02:17 +0900

    Update

--- abc/abc266/d/main.cpp
+++ abc/abc266/d/main.cpp
@@ -1,47 +1,41 @@
 #include <bits/stdc++.h>
-#include <atcoder/all>
-using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
 #define endl '\n'
 using ll = long long;
+const int INF = numeric_limits<int>::max();
 
 int main() {
   int n; cin >> n;
-  vector<int> t(n),x(n),a(n);
-  REP(i,n) cin >> t[i] >> x[i] >> a[i];
+  vector<int> x(1e5+1),a(1e5+1);
+  REP(i,n) {
+    int t,_x,_a;
+    cin >> t >> _x >> _a;
+    x[t] = _x; a[t] = _a;
+  }
 
-  vector<ll> dp(5,-1); // dp[i] := 座標i地点にいるときの最大値
+  // dp[i] := 座標i地点にいるときの最大値
+  vector<ll> dp(5,-INF);
   dp[0] = 0;
-  int now = 0;
   REP(i,1e5+1) {
-    vector<ll> p(5,-1);
+    vector<ll> p(5,-INF);
     p[0] = 0;
     swap(dp,p);
 
-    int tx = -1;
-    int ta = 0;
-    if (t[now] == i) {
-      tx = x[now];
-      ta = a[now];
-      now++;
-    }
-
     REP(j,5) {
-      if (p[j] == -1) continue;
-
       // 移動しない
       dp[j] = max(dp[j], p[j]);
-      if (tx == j) dp[j] = max(dp[j], p[j]+ta);
+      if (x[i] == j) dp[j] = max(dp[j], p[j]+a[i]);
 
-      // -1
+      // -1移動
       if (j > 0) {
-        if (tx == j) dp[j-1] = max(dp[j-1], p[j]+ta);
+        if (x[i] == j) dp[j-1] = max(dp[j-1], p[j]+a[i]);
         else dp[j-1] = max(dp[j-1], p[j]);
       }
-      // +1
+
+      // +1移動
       if (j < 4) {
-        if (tx == j) dp[j+1] = max(dp[j+1], p[j]+ta);
+        if (x[i] == j) dp[j+1] = max(dp[j+1], p[j]+a[i]);
         else dp[j+1] = max(dp[j+1], p[j]);
       }
     }
