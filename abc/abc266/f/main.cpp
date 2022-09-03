274f6626b2282d7005414416158d73abe56982dc
tic40 <ccpzjoh@gmail.com>
2022-09-03 15:21:04 +0900

    Update

--- abc/abc266/f/main.cpp
+++ abc/abc266/f/main.cpp
@@ -6,9 +6,9 @@ using namespace std;
 #define endl '\n'
 
 vector<vector<int>> g(2e5);
-vector<bool> visited(2e5), on(2e5);
+vector<bool> visited(2e5),on(2e5);
 
-// dfsでサイクルを検出する
+// サイクルを検出するdfs
 int dfs(int v, int p) {
   if (visited[v]) return v;
   visited[v] = true;
@@ -18,9 +18,10 @@ int dfs(int v, int p) {
     int r = dfs(u,v);
     if (r == -1) continue;
     on[v] = true;
-    if (v == r) return -1;
-    return r;
+
+    return v == r ? -1 : r;
   }
+  // サイクルでなければ -1 を返す
   return -1;
 }
 
@@ -28,25 +29,25 @@ int main() {
   int n; cin >> n;
   REP(i,n) {
     int u,v; cin >> u >> v;
-    u--, v--;
+    u--; v--;
     g[u].push_back(v);
     g[v].push_back(u);
   }
 
   dfs(0,-1);
-  dsu t(n);
+  dsu uf(n);
 
   REP(i,n) for(int j: g[i]) {
+    // サイクル同士以外を連結する
     if (on[i] && on[j]) continue;
-    t.merge(i,j);
+    uf.merge(i,j);
   }
 
   int q; cin >> q;
-  REP(_,q) {
-    int a,b; cin >> a >> b;
-    a--; b--;
-    cout << (t.same(a,b) ? "Yes" : "No") << endl;
+  REP(i,q) {
+    int x,y; cin >> x >> y;
+    x--; y--;
+    cout << (uf.same(x,y) ? "Yes" : "No") << endl;
   }
-
   return 0;
 }
