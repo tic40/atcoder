cf0bd44f55bbe764dce26962cbfa8d97066e3702
tic40 <ccpzjoh@gmail.com>
2022-09-04 01:32:53 +0900

    Update

--- abc/abc151/e/main.cpp
+++ abc/abc151/e/main.cpp
@@ -1,24 +1,18 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace atcoder;
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 using ll = long long;
 const int MOD = 1e9+7;
-const double PI = 3.141592653589793;
+using mint = modint1000000007;
 
-const int MAX = 100005;
-
-ll gcd(ll a, ll b) { return b?gcd(b,a%b):a; }
-ll lcm(ll a, ll b) { return a/gcd(a,b)*b; }
-long long extGCD(ll a, ll b, ll &x, ll &y) {
-  if (b == 0) { x = 1; y = 0; return a; }
-  ll d = extGCD(b, a%b, y, x);
-  y -= a/b * x;
-  return d;
-}
-
-// 二項係数
+const int MAX = 1e5+1;
 ll fac[MAX], finv[MAX], inv[MAX];
-void comInit() { // テーブルを作る前処理
+
+// combinationテーブルを作る
+void com_init() {
   fac[0] = fac[1] = 1;
   finv[0] = finv[1] = 1;
   inv[1] = 1;
@@ -29,69 +23,29 @@ void comInit() { // テーブルを作る前処理
   }
   return;
 }
-ll com(int n, int k) { // 二項係数計算
+
+// 二項係数nCr
+ll com(int n, int k) {
   if (n < k) return 0;
   if (n < 0 || k < 0) return 0;
   return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
 }
 
-struct mint {
-  ll x;
-  mint(ll x=0):x((x%MOD+MOD)%MOD){}
-  mint operator-() const { return mint(-x);}
-  mint& operator+=(const mint a) {
-    if ((x += a.x) >= MOD) x -= MOD;
-    return *this;
-  }
-  mint& operator-=(const mint a) {
-    if ((x += MOD-a.x) >= MOD) x -= MOD;
-    return *this;
-  }
-  mint& operator*=(const mint a) { (x *= a.x) %= MOD; return *this;}
-  mint operator+(const mint a) const { return mint(*this) += a;}
-  mint operator-(const mint a) const { return mint(*this) -= a;}
-  mint operator*(const mint a) const { return mint(*this) *= a;}
-  mint pow(ll t) const {
-    if (!t) return 1;
-    mint a = pow(t>>1);
-    a *= a;
-    if (t&1) a *= *this;
-    return a;
-  }
-  // for prime MOD
-  mint inv() const { return pow(MOD-2);}
-  mint& operator/=(const mint a) { return *this *= a.inv();}
-  mint operator/(const mint a) const { return mint(*this) /= a;}
-};
-istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
-ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
-
-int n,k;
-vector<int> a;
-
-void solve() {
-  comInit();
-
-  auto f = [&]() {
-    mint sum = 0;
-    for (int i=k-1; i<=n; i++) sum += a[i] * com(i, k-1);
-    return sum;
-  };
-
-  sort(a.begin(), a.end());
-  mint sumMax = f();
-  reverse(a.begin(), a.end());
-  mint sumMin = f();
-
-  cout << sumMax - sumMin << endl;
-  return;
-}
-
 int main() {
-  cin >> n >> k;
-  a.resize(n);
+  int n,k; cin >> n >> k;
+  vector<int> a(n);
   REP(i,n) cin >> a[i];
 
-  solve();
+  com_init();
+  mint sum_max = 0, sum_min = 0;
+
+  sort(a.begin(),a.end());
+  for(int i = k-1; i <= n; i++) sum_max += com(i,k-1) * a[i];
+  reverse(a.begin(),a.end());
+  for(int i = k-1; i <= n; i++) sum_min += com(i,k-1) * a[i];
+
+  mint ans = sum_max - sum_min;
+  cout << ans.val() << endl;
+
   return 0;
 }
