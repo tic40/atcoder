cb8439de98f31ecc0e7740666987338ec790ecf1
tic40 <ccpzjoh@gmail.com>
2022-09-05 10:37:12 +0900

    Update

--- abc/abc190/d/main.cpp
+++ abc/abc190/d/main.cpp
@@ -1,21 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 using ll = long long;
-#define REP(i,n) for(int i=0;i<n;i++)
+
+vector<ll> divisor(ll x) {
+  vector<ll> res;
+  for(ll i = 1; i*i <= x; i++) {
+    if (x % i == 0) {
+      res.push_back(i);
+      if (x / i != i) res.push_back(x/i);
+    }
+  }
+  return res;
+}
 
 int main() {
-  ll n;
-  cin >> n;
+  ll n; cin >> n;
 
-  ll n2 = n*2, ans = 0;
-  ll sq = sqrt(n2);
-  // 約数を平方根までチェックする
-  for (ll x = 1; x <= sq; x++) {
-    if (n2 % x) continue; // 約数ではない
+  // (a+b) * (b-a+1) / 2 = n
+  // (a+b) * (b-a+1) = 2*n
+  // a+b, b-a+1 は 2nの約数
 
-    ll y = n2/x;
-    bool ok = (x+y-1) % 2 == 0 && (x-y+1) % 2 == 0;
-    if (ok) ans += x==y ? 1 : 2; // x==yの場合は1通り
+  int ans = 0;
+  auto d = divisor(n*2);
+  for(auto x: d) {
+    ll y = 2 * n / x;
+    // a+b = x
+    // a = x - b
+    // a = x - y - a + 1
+    // 2a = x - y + 1
+    // a = (x-y+1)/2
+
+    // b-a+1 = y
+    // b = y+a-1
+    // b = y + x - b - 1
+    // b = (x+y-1)/2
+    if (x % 2 == y % 2) continue;
+    ans++;
   }
 
   cout << ans << endl;
