965fd5dd0f0bf1494d6c335773fe641978e85c84
tic40 <ccpzjoh@gmail.com>
2022-09-06 10:44:46 +0900

    Update

--- abc/abc191/c/main.cpp
+++ abc/abc191/c/main.cpp
@@ -1,20 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 
 int main() {
-  int h,w;
-  cin >> h >> w;
-  char s[h][w];
-  REP(i,h) REP(j,w) { cin >> s[i][j]; }
+  int h,w; cin >> h >> w;
+  vector<string> s(h);
+  REP(i,h) cin >> s[i];
 
   int ans = 0;
   REP(i,h-1) REP(j,w-1) {
     int cnt = 0;
-    REP(k,2) REP(l,2) {
-      if (s[k+i][l+j] == '#') cnt++;
-    }
-    if (cnt%2) ans++;
+    REP(k,2) REP(l,2) if (s[i+k][j+l] == '#') cnt++;
+    if (cnt == 1 || cnt == 3) ans++;
   }
 
   cout << ans << endl;
