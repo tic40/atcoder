887217093e0dff5518e53e1d4a3c3d4dbd4edace
tic40 <ccpzjoh@gmail.com>
2022-09-08 13:38:44 +0900

    Update

--- abc/abc117/d/main.cpp
+++ abc/abc117/d/main.cpp
@@ -1,65 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 using ll = long long;
-template<class T>bool chmax(T &a,const T &b) {if(a<b){a=b; return 1;} return 0;}
-template<class T>bool chmin(T &a,const T &b) {if(b<a){a=b; return 1;} return 0;}
+template<class T> void chmax(T& a, T b) { if (a < b) a = b; }
 
-int n;
-ll k;
-vector<ll> a;
+int main() {
+  int n; ll k;
+  cin >> n >> k;
+  vector<ll> a(n);
+  REP(i,n) cin >> a[i];
 
-// [i][smaller] := 上からi桁目まで決めたとき、
-// j=0: ちょうど一致する
-// j=1: よりも小さくなっている
-// についての最大値
-ll dp[51][2];
+  // dp[i][j] := 上からi桁目までを決めたとき、
+  // j=0: k未満であることが確定
+  // j=1: 今のところkと一致している
+  // ときの最大値
+  vector<ll> dp(2,-1);
+  dp[1] = 0;
 
-void solve() {
-  memset(dp, -1, sizeof(dp)); // DP 配列を-1で初期化
+  REP(i,41) {
+    vector<ll> p(2,-1);
+    swap(dp,p);
 
-  dp[0][0] = 0;
-  REP(d, 50) {
-    ll mask = 1LL << (50 - d - 1);
-    // a で元々 d 桁目にビットが立っているものの個数
-    int num = 0;
-    REP(i, n) if (a[i] & mask) num++;
+    ll mask = 1LL << (40 - i);
+    int bitcnt = 0;
+    REP(j,n) if (a[j] & mask) bitcnt++;
 
-    // x の d 桁目を 0, 1 にしたときのコスト
-    ll cost0 = mask * num;
-    ll cost1 = mask * (n-num);
+    ll cost0 = mask * bitcnt;
+    ll cost1 = mask * (n - bitcnt);
 
-    // smaller -> smaller
-    if (dp[d][1] != -1) {
-      // 0 でも 1 でも大きい方をとってよい
-      chmax(dp[d+1][1], dp[d][1] + max(cost0, cost1));
+    if (p[0] != -1) {
+      chmax(dp[0], p[0] + max(cost0, cost1));
     }
 
-    // exact -> smaller
-    if (dp[d][0] != -1) {
-      // k の d 桁目が 1 だったら、x の d 桁目は 0 にする
+    if (p[1] != -1) {
       if (k & mask) {
-        chmax(dp[d+1][1], dp[d][0] + cost0);
+        chmax(dp[0], p[1] + cost0);
+        chmax(dp[1], p[1] + cost1);
+      } else {
+        chmax(dp[1], p[1] + cost0);
       }
-    }
-
-    // exact -> exact(kにぴったりあわせる)
-    if (dp[d][0] != -1) {
-      if (k & mask) chmax(dp[d+1][0], dp[d][0] + cost1);
-      else chmax(dp[d+1][0], dp[d][0] + cost0);
     }
   }
 
-  cout << max(dp[50][0], dp[50][1]) << endl;
-
-  return;
-}
-
-int main() {
-  cin >> n >> k;
-  a.resize(n);
-  REP(i,n) cin >> a[i];
-
-  solve();
+  cout << max(dp[0],dp[1]) << endl;
   return 0;
 }
