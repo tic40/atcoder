0da788bfafb695ef8d7e88cb755b2d4ab5fd6df2
tic40 <ccpzjoh@gmail.com>
2022-09-08 14:06:17 +0900

    Update

--- abc/abc195/c/main.cpp
+++ abc/abc195/c/main.cpp
@@ -1,16 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 using ll = long long;
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
 
 int main() {
-  ll n, ans = 0; cin >> n;
-  for(ll x: { 1e3, 1e6, 1e9, 1e12, 1e15 }) {
-    if (x <= n) ans += n-(x-1);
+  ll n; cin >> n;
+
+  ll x = 1000, ans = 0;
+  while(x <= n) {
+    ans += n-(x-1);
+    x *= 1000;
   }
+
   cout << ans << endl;
   return 0;
 }
