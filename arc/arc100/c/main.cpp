26f9b476111c50d3a1029866c2f042c532aa9aa9
tic40 <ccpzjoh@gmail.com>
2022-09-08 17:30:50 +0900

    Update

--- arc/arc100/c/main.cpp
+++ arc/arc100/c/main.cpp
@@ -1,21 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 using ll = long long;
 
 int main() {
   int n; cin >> n;
-  vector<int> a(n),b(n);
+  vector<int> a(n);
   REP(i,n) cin >> a[i];
-  REP(i,n) b[i] = a[i]-(i+1);
+  vector<int> b(n);
+  REP(i,n) b[i] = a[i] - (i+1);
 
   sort(b.begin(),b.end());
-  // 中央値が最小となる
-  ll x = b[n/2];
+  int x = b[n/2];
 
   ll ans = 0;
   REP(i,n) ans += abs(x-b[i]);
-
   cout << ans << endl;
   return 0;
 }
