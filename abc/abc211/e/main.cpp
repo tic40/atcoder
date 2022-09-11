34c8eb9d60325f6d513c522677336688706b3525
tic40 <ccpzjoh@gmail.com>
2022-09-11 18:10:44 +0900

    Update

--- abc/abc211/e/main.cpp
+++ abc/abc211/e/main.cpp
@@ -1,48 +1,45 @@
 #include <bits/stdc++.h>
-#include <atcoder/all>
-using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
 #define endl '\n'
 using P = pair<int,int>;
 
-const vector<P> moves = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-int n,k,ans = 0;
+const vector<P> moves = { {1,0}, {-1,0}, {0,1}, {0,-1} };
+int n,k;
 
-void dfs(vector<string> s, int cnt) {
-  if (cnt == k) { ans++; return; }
+int dfs(vector<string> s, int cnt) {
+  if (cnt == k) return 1;
 
   REP(i,n) REP(j,n) {
     if (s[i][j] != '.') continue;
 
-    bool ok = false;
-    if (cnt > 0) {
+    bool ok = (cnt == 0);
+    if (!ok) {
       for(auto [x,y]: moves) {
         int ni = i+x, nj = j+y;
-        if (ni < 0 || n <= ni || nj < 0 || n <= nj) continue;
+        if (ni < 0 || nj < 0 || ni >= n || nj >= n) continue;
         if (s[ni][nj] == '@') ok = true;
       }
-    } else {
-      ok = true;
     }
 
-    if (!ok) continue;
-    s[i][j] = '@'; // 今の場所を赤マスに
-    dfs(s,cnt+1);
-    s[i][j] = '#'; // 見終わったので今の場所を黒マスに
-    dfs(s,cnt);
-    return;
+    if (ok) {
+      int res = 0;
+      s[i][j] = '@';
+      res += dfs(s,cnt+1);
+      s[i][j] = '#';
+      res += dfs(s,cnt);
+      return res;
+    }
   }
-  return;
+
+  return 0;
 }
 
 int main() {
   cin >> n >> k;
-  vector<string> s(8);
+  vector<string> s(n);
   REP(i,n) cin >> s[i];
 
-  dfs(s,0);
-
-  cout << ans << endl;
+  cout << dfs(s,0) << endl;
   return 0;
 }
