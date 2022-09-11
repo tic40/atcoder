455712963bf9813c2d41ed7b6cd1024c219c1094
tic40 <ccpzjoh@gmail.com>
2022-09-11 22:01:18 +0900

    Update

--- abc/abc216/e/main.cpp
+++ abc/abc216/e/main.cpp
@@ -1,49 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 using ll = long long;
 
-int n,k;
-vector<ll> a;
+int main() {
+  int n,k; cin >> n >> k;
+  vector<ll> a(n);
+  REP(i,n) cin >> a[i];
 
-ll binary_search() {
-  ll ok = 2*1e9; // k回以下乗れるときの数
-  ll ng = 0; // k回以下乗れないときの数
-
-  while (abs(ok-ng)>1) {
-    ll mid = (ok+ng)/2;
-    ll cnt = 0; // 選んだ数
-    REP(i,n) cnt += max(0LL, a[i]-mid+1);
+  // 二分探索で楽しさの値を探す
+  ll ok = 2e9, ng = 0;
+  while(ok - ng > 1) {
+    ll mid = (ok+ng) / 2;
+    ll cnt = 0;
+    REP(i,n) cnt += max(0LL, a[i] - mid + 1);
 
     if (cnt <= k) ok = mid;
     else ng = mid;
   }
-  return ok;
-}
 
-void solve() {
-  sort(a.begin(), a.end());
-  ll p = binary_search();
-
-  ll ans = 0;
-  int cnt = 0;
+  ll p = ok;
+  ll ans = 0, total = 0;
   REP(i,n) {
-    if (p <= a[i]) {
-      ans += (a[i] + p) * (0, a[i]-p+1) / 2;
-      cnt += a[i] - p + 1;
-    }
+    if (p > a[i]) continue;
+    int num = a[i] - p + 1;
+    ans += (a[i] + p) * num / 2;
+    total += num;
   }
-  if (0 < k-cnt) ans += (p-1) * (k-cnt);
 
+  // 半端な分を計算する
+  ans += (p - 1) * max(0LL, k - total);
   cout << ans << endl;
-  return;
-}
-
-int main() {
-  cin >> n >> k;
-  a.resize(n);
-  REP(i,n) cin >> a[i];
-
-  solve();
   return 0;
 }
