f3c0859d0126f8fafeb8fa9b5a5ec35da16bb570
tic40 <ccpzjoh@gmail.com>
2022-09-11 12:21:47 +0900

    Update

--- abc/abc201/d/main.cpp
+++ abc/abc201/d/main.cpp
@@ -1,39 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
-const int INF = 1e9;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
+const int INF = numeric_limits<int>::max();
 
 int h,w;
-vector<string> a;
+vector s(2000, vector<int>(2000));
+vector memo(2000, vector<int>(2000,-INF));
 
-int solve() {
-  int s[h][w];
-  REP(i,h) REP(j,w) s[i][j] = a[i][j] == '+' ? 1 : -1;
+int f(int i, int j) {
+  if (i == h-1 && j == w-1) return 0;
+  if (memo[i][j] != -INF) return memo[i][j];
 
-  // dp[i][j] := i,j地点でのtakahashiのスコア - aokiのスコアの最良スコア
-  int dp[h][w];
-  REP(i,h) REP(j,w) dp[i][j] = -INF; // dp初期値
-  dp[h-1][w-1] = 0; // 右下は0
-
-  // 右下から順番にみていく
-  for(int i = h-1; 0 <= i; i--) {
-    for(int j = w-1; 0 <= j; j--) {
-      if (i < h-1) dp[i][j] = max(dp[i][j], s[i+1][j] - dp[i+1][j]); // 下
-      if (j < w-1) dp[i][j] = max(dp[i][j], s[i][j+1] - dp[i][j+1]); // 右
-    }
-  }
-  return dp[0][0];
+  // 次は相手のターンなので、 -f(i+1,j) する
+  if (i+1 < h) memo[i][j] = max(memo[i][j], s[i+1][j] - f(i+1,j));
+  if (j+1 < w) memo[i][j] = max(memo[i][j], s[i][j+1] - f(i,j+1));
+  return memo[i][j];
 }
 
 int main() {
   cin >> h >> w;
-  a.resize(h);
+  vector<string> a(h);
   REP(i,h) cin >> a[i];
+  REP(i,h) REP(j,w) s[i][j] = a[i][j] == '+' ? 1 : -1;
 
-  int score = solve();
+  int score = f(0,0);
   if (0 < score) cout << "Takahashi" << endl;
-  else if (score < 0) cout << "Aoki" << endl;
-  else cout << "Draw" << endl;
-
+  else if (score == 0) cout << "Draw" << endl;
+  else cout << "Aoki" << endl;
   return 0;
 }
