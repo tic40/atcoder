8d23aa66d475b52b9407a5f37e23a9d2ec5cbe2a
tic40 <ccpzjoh@gmail.com>
2022-09-11 19:51:37 +0900

    Update

--- abc/abc213/d/main.cpp
+++ abc/abc213/d/main.cpp
@@ -1,23 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 
 int n;
-vector<vector<int>> g;
+vector<vector<int>> g(2e5);
+vector<int> ans;
 
-void dfs(int cur, int prev) {
-  cout << cur+1 << " ";
-  for(int v: g[cur]) {
-    if (prev != v) {
-      dfs(v,cur);
-      cout << cur+1 << " ";
+void dfs(int i, int p) {
+  ans.push_back(i);
+  for(int v: g[i]) {
+    if (v != p) {
+      dfs(v,i);
+      ans.push_back(i);
     }
   }
+  return;
 }
 
 int main() {
   cin >> n;
-  g.resize(n);
   REP(i,n-1) {
     int a,b; cin >> a >> b;
     a--; b--;
@@ -25,8 +27,11 @@ int main() {
     g[b].push_back(a);
   }
 
-  for(auto& v: g) sort(v.begin(), v.end());
+  for(auto& v: g) sort(v.begin(),v.end());
+
   dfs(0,-1);
+
+  for(int v: ans) cout << v+1 << " ";
   cout << endl;
   return 0;
 }
