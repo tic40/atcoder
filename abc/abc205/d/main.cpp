406d5232982ef979334327463f5edf5b0e939838
tic40 <ccpzjoh@gmail.com>
2022-09-11 16:22:15 +0900

    Update

--- abc/abc205/d/main.cpp
+++ abc/abc205/d/main.cpp
@@ -1,37 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 using ll = long long;
 
-int n,q;
-vector<ll> a,k;
+int main() {
+  int n,q; cin >> n >> q;
+  vector<ll> a(n);
+  REP(i,n) cin >> a[i];
 
-void solve() {
+  // s[i] = a[i]以下の良い整数の総数
   vector<ll> s(n);
-  REP(i,n) {
-    if (i == 0) s[i] = a[i]-1;
-    else s[i] = s[i-1] + a[i]-a[i-1]-1;
-  }
+  s[0] = a[0] - 1;
+  for(int i = 1; i < n; i++) s[i] = s[i-1] + (a[i] - a[i-1] - 1);
 
   REP(i,q) {
-    auto it = lower_bound(s.begin(), s.end(), k[i]);
-    int idx = it-s.begin();
-    if (idx == n) {
-      cout << a[n-1] + (k[i] - s[n-1]) << endl;
+    ll k; cin >> k;
+    auto it = lower_bound(s.begin(), s.end(), k);
+    if (it == s.end()) {
+      cout << a.back() + (k - s.back()) << endl;
     } else {
-      cout << a[idx] - (s[idx] - k[i] + 1) << endl;
+      int idx = it - s.begin();
+      cout << a[idx] - (s[idx] - k + 1) << endl;
     }
   }
-  return;
-}
-
-int main() {
-  cin >> n >> q;
-  a.resize(n);
-  k.resize(q);
-  REP(i,n) cin >> a[i];
-  REP(i,q) cin >> k[i];
-
-  solve();
   return 0;
 }
