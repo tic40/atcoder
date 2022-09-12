9f3bdcdd815435d5b2710532be027c80dc7439df
tic40 <ccpzjoh@gmail.com>
2022-09-12 15:17:44 +0900

    Update

--- abc/abc257/c/main.cpp
+++ abc/abc257/c/main.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 
 int main() {
   int n; string s;
@@ -8,17 +9,18 @@ int main() {
   vector<int> w(n);
   REP(i,n) cin >> w[i];
 
-  map<int,vector<int>> mp;
+  map<int, vector<int>> mp;
   REP(i,n) mp[w[i]].push_back(i);
 
-  int now = 0;
-  for(char c: s) if (c == '1') now++;
-  int ans = now;
+  int x = 0;
+  for(char c: s) if (c == '1') x++;
+  int ans = x;
 
   for(auto [_,v]: mp) {
-    for(auto i: v) s[i] == '1' ? now-- : now++;
-    ans = max(ans,now);
+    for(int i: v) s[i] == '1' ? x-- : x++;
+    ans = max(ans,x);
   }
+
   cout << ans << endl;
   return 0;
 }
