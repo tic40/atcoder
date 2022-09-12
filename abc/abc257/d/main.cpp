d2039b6a0a3c865b740ad9fd886a500d45287931
tic40 <ccpzjoh@gmail.com>
2022-09-12 20:55:49 +0900

    Update

--- abc/abc257/d/main.cpp
+++ abc/abc257/d/main.cpp
@@ -10,25 +10,24 @@ int main() {
   REP(i,n) cin >> x[i] >> y[i] >> p[i];
 
   ll ok = 4e9, ng = 0;
-  while(ok - ng  > 1) {
+  while(ok - ng > 1) {
     ll mid = (ok+ng) / 2;
 
-    // dp[i][j] := iからjへの移動が可能なら1、不可能なら0
     vector dp(n, vector<int>(n));
-
     REP(i,n) REP(j,n) {
       ll d = (ll)abs(x[i]-x[j]) + abs(y[i]-y[j]);
-      if (mid*p[i] >= d) dp[i][j] = 1;
+      if (mid * p[i] >= d) dp[i][j] = 1;
     }
+
     REP(k,n) REP(i,n) REP(j,n) {
       dp[i][j] |= dp[i][k] & dp[k][j];
     }
 
     auto possible = [&]() {
       REP(i,n) {
-        bool res = true;
-        REP(j,n) if (dp[i][j] == 0) res = false;
-        if (res) return true;
+        bool ok = true;
+        REP(j,n) if (!dp[i][j]) ok = false;
+        if (ok) return true;
       }
       return false;
     };
