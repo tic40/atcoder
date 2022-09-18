353cf73f1bd9d5d7be9859c6647e9689a3d33188
tic40 <ccpzjoh@gmail.com>
2022-09-18 17:10:18 +0900

    Update

--- abc/abc269/e/main.cpp
+++ abc/abc269/e/main.cpp
@@ -5,17 +5,18 @@ using namespace std;
 
 int main() {
   int n; cin >> n;
-
   int x,y;
+
   REP(ti,2) {
     int ok = n, ng = 0;
     while(ok-ng>1) {
       int mid = (ok+ng)/2;
       if (ti == 0) printf("? %d %d %d %d\n",1,mid,1,n);
       else printf("? %d %d %d %d\n",1,n,1,mid);
+      fflush(stdout);
       int res; cin >> res;
-      if (res < mid) ok = mid;
-      else ng = mid;
+      if (res == mid) ng = mid;
+      else ok = mid;
     }
     x = ok; swap(x,y);
   }
