f7edcabb27c13391ef910096dc1bca7ceb99dc0c
tic40 <ccpzjoh@gmail.com>
2022-09-18 17:05:52 +0900

    Update

--- abc/abc269/d/main.cpp
+++ abc/abc269/d/main.cpp
@@ -4,28 +4,23 @@ using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
 #define endl '\n'
-using ll = long long;
 using P = pair<int,int>;
 
-const vector<P> moves = { {-1, -1}, {-1, 0}, {0, -1}, {0, 1}, {1, 0}, {1, 1} };
+const vector<P> moves = {{1,0}, {0,1}, {1,1}};
 
 int main() {
   int n; cin >> n;
   vector<int> x(n),y(n);
-  map<P,int> mi;
+  map<P,int> mp;
   REP(i,n) {
     cin >> x[i] >> y[i];
-    mi[{x[i], y[i]}] = i;
+    mp[{x[i],y[i]}] = i;
   }
 
   dsu uf(n);
-  map<P,bool> mp;
-  REP(i,n) mp[{x[i],y[i]}] = true;
-
   REP(i,n) for(auto [dx,dy]: moves) {
-    int nx = x[i]+dx;
-    int ny = y[i]+dy;
-    if (mp[{nx,ny}]) uf.merge(i, mi[{nx,ny}]);
+    int nx = x[i]+dx, ny = y[i]+dy;
+    if (mp.count({nx,ny})) uf.merge(i, mp[{nx,ny}]);
   }
 
   auto g = uf.groups();
