8037dfcf7d2de3ff40d4ca35380f815e48bd86e2
tic40 <ccpzjoh@gmail.com>
2022-09-18 16:52:38 +0900

    Update

--- abc/abc269/c/main.cpp
+++ abc/abc269/c/main.cpp
@@ -4,36 +4,19 @@ using namespace std;
 #define endl '\n'
 using ll = long long;
 
-string f(ll n) {
-  string ans = "";
-  while(n>=1) {
-    if (n % 2 == 0) ans = "0" + ans;
-    if (n % 2 == 1) ans = "1" + ans;
-    n/=2;
-  }
-  return ans;
-}
-
-string b = "";
+ll n;
 vector<ll> ans;
+void dfs(int i, ll x) {
+  if (i == 60) { ans.push_back(x); return; }
 
-void dfs(int i, ll now) {
-  if (i == (int)b.size()) {
-    ans.push_back(now);
-    return;
-  }
-
-  dfs(i+1, now);
-  if (b[i] == '1') dfs(i+1, now + powl(2,i));
-  return;
+  dfs(i+1,x);
+  if (n >> i & 1) dfs(i+1, x + powl(2,i));
 }
 
 int main() {
-  ll n; cin >> n;
-  b = f(n);
-  reverse(b.begin(),b.end());
-
+  cin >> n;
   dfs(0,0);
+
   sort(ans.begin(),ans.end());
   for(auto v: ans) cout << v << endl;
   return 0;
