9428f958051a25dd6cb625d9d3826ceb59d42503
tic40 <ccpzjoh@gmail.com>
2022-09-24 23:45:17 +0900

    Update

--- abc/abc270/d/main.cpp
+++ abc/abc270/d/main.cpp
@@ -6,15 +6,14 @@ const int INF = numeric_limits<int>::max();
 
 int n,k;
 vector<int> a(100);
-// memo[i][j] := 残りの石の数i, 手番j のときの最適値
-vector memo(10005, vector<int> (2));
+vector memo(10001, vector<int>(2));
 
 int f(int x, int t) {
-  if (memo[x][t] != -INF && memo[x][t] != INF) return memo[x][t];
-  if (x <= 0) return memo[x][t] = 0;
+  if (abs(memo[x][t]) != INF) return memo[x][t];
+  if (x == 0) return memo[x][t] = 0;
 
   REP(i,k) {
-    if (x-a[i] < 0) break;
+    if (x - a[i] < 0) break;
     if (t == 1) {
       memo[x][t] = max(memo[x][t], a[i] + f(x-a[i], 0));
     } else {
@@ -28,11 +27,11 @@ int f(int x, int t) {
 int main() {
   cin >> n >> k;
   REP(i,k) cin >> a[i];
-  REP(i,10001) {
+
+  REP(i,n+1) {
     memo[i][0] = INF;
     memo[i][1] = -INF;
   }
-
   cout << f(n,1) << endl;
   return 0;
 }
