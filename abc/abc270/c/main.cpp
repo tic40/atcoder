3ce410f4bfd610b5eb831afcdc444cab93e8d0f6
tic40 <ccpzjoh@gmail.com>
2022-09-25 10:48:14 +0900

    Update

--- abc/abc270/c/main.cpp
+++ abc/abc270/c/main.cpp
@@ -1,32 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
-#define endl '\n'
 
 int n,x,y;
-vector<vector<int>> g;
+vector<vector<int>> g(2e5);
 vector<int> ans;
 
 bool dfs(int i, int p) {
-  if (i == y) {
-    ans.push_back(i);
-    return true;
-  }
+  if (i == y) { ans.push_back(i); return true; }
 
-  int ok = false;
   for(int v: g[i]) {
     if (v == p) continue;
-    if (dfs(v,i)) { ok = true; break; }
+    if (dfs(v,i)) { ans.push_back(i); return true; }
   }
-
-  if (ok) ans.push_back(i);
-  return ok;
+  return false;
 }
 
 int main() {
   cin >> n >> x >> y;
   x--; y--;
-  g.resize(n);
   REP(i,n-1) {
     int u,v; cin >> u >> v;
     u--; v--;
--- abc/abc270/d/main.cpp
+++ abc/abc270/d/main.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
-#define endl '\n'
 const int INF = numeric_limits<int>::max();
 
 int n,k;
 vector<int> a(100);
+// memo[i][j] := 山の残りの石の数がi、手番がj(0:高橋,1:青木)のときの高橋が取り除ける最大の石の数
 vector memo(10001, vector<int>(2));
 
 int f(int x, int t) {
@@ -14,10 +14,11 @@ int f(int x, int t) {
 
   REP(i,k) {
     if (x - a[i] < 0) break;
-    if (t == 1) {
-      memo[x][t] = max(memo[x][t], a[i] + f(x-a[i], 0));
+
+    if (t == 0) {
+      memo[x][t] = max(memo[x][t], a[i] + f(x-a[i],1));
     } else {
-      memo[x][t] = min(memo[x][t], f(x-a[i], 1));
+      memo[x][t] = min(memo[x][t], f(x-a[i],0));
     }
   }
 
@@ -26,12 +27,12 @@ int f(int x, int t) {
 
 int main() {
   cin >> n >> k;
-  REP(i,k) cin >> a[i];
-
+  REP(i,n) cin >> a[i];
   REP(i,n+1) {
-    memo[i][0] = INF;
-    memo[i][1] = -INF;
+    memo[i][0] = -INF;
+    memo[i][1] = INF;
   }
-  cout << f(n,1) << endl;
+
+  cout << f(n,0) << endl;
   return 0;
 }
