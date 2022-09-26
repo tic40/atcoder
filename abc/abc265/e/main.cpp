b0458465c670b8b13bbd1a3ff3123bab2e75db69
tic40 <ccpzjoh@gmail.com>
2022-09-26 14:18:25 +0900

    Update

--- abc/abc265/e/main.cpp
+++ abc/abc265/e/main.cpp
@@ -1,44 +1,39 @@
 #include <bits/stdc++.h>
 #include <atcoder/all>
-using namespace atcoder;
 using namespace std;
+using namespace atcoder;
 #define REP(i,n) for(int i=0;i<(n);i++)
 #define endl '\n'
 using ll = long long;
 using P = pair<ll,ll>;
-using mint = modint998244353; // modint1000000007;
+using mint = modint998244353;
 
 int main() {
   int n,m; cin >> n >> m;
-  int a,b,c,d,e,f; cin >> a >> b >> c >> d >> e >> f;
-
+  int a,b,c,d,e,f;
+  cin >> a >> b >> c >> d >> e >> f;
   set<P> st;
   REP(i,m) {
     int x,y; cin >> x >> y;
-    st.emplace(x,y);
+    st.insert({x,y});
   }
 
-  map<P,mint> mp;
-  mp[{0,0}] = 1;
+  map<P,mint> dp;
+  dp[{0,0}] = 1;
+
   REP(i,n) {
     map<P,mint> p;
-    swap(mp,p);
-    for(auto [k,v]: p) {
-      auto [nx,ny] = k;
-      if (st.count({nx+a, ny+b}) == 0) {
-        mp[{nx+a,ny+b}] += v;
-      }
-      if (st.count({nx+c, ny+d}) == 0) {
-        mp[{nx+c,ny+d}] += v;
-      }
-      if (st.count({nx+e, ny+f}) == 0) {
-        mp[{nx+e,ny+f}] += v;
-      }
+    swap(dp,p);
+    for(auto [t,v]: p) {
+      auto [x,y] = t;
+      if (st.count({x+a, y+b}) == 0) dp[{x+a, y+b}] += v;
+      if (st.count({x+c, y+d}) == 0) dp[{x+c, y+d}] += v;
+      if (st.count({x+e, y+f}) == 0) dp[{x+e, y+f}] += v;
     }
   }
 
   mint ans = 0;
-  for(auto [k,v]: mp) ans += v;
+  for(auto [_,v]: dp) ans += v;
   cout << ans.val() << endl;
   return 0;
 }
