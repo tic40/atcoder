47a955479d2fcdfa252e5078be03628744747cc3
tic40 <ccpzjoh@gmail.com>
2022-10-01 20:02:41 +0900

    Update

--- other_contest/typical90/002/main.cpp
+++ other_contest/typical90/002/main.cpp
@@ -1,32 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 
 int n;
-set<string> st;
-
-bool possible(string s) {
-  int cnt = 0;
-  for (char c: s) {
-    c == '(' ? cnt++ : cnt--;
-    if (cnt < 0) break;
-  }
-  return cnt == 0;
-}
-
-void dfs(string s = "") {
-  if (s.size() == n) {
-    if (possible(s)) st.insert(s);
+void f(int i, int l, string s) {
+  if (l < 0) return;
+  if (i == n) {
+    if (l == 0) cout << s << endl;
     return;
   }
 
-  dfs(s+"("); dfs(s+")");
+  f(i+1, l+1, s+'(');
+  f(i+1, l-1, s+')');
+
+  return;
 }
 
 int main() {
   cin >> n;
-  dfs();
-  for(string v: st) cout << v << endl;
-
+  f(1,1,"(");
   return 0;
 }
