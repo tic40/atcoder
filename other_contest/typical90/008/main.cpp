e0f55e8934df52bac366530476a476434e2fa7fd
tic40 <ccpzjoh@gmail.com>
2022-10-02 15:03:47 +0900

    Update

--- other_contest/typical90/008/main.cpp
+++ other_contest/typical90/008/main.cpp
@@ -1,31 +1,28 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace atcoder;
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
-const int MOD = 1e9+7;
-
-// dp[pos][atcoderの何文字目まで] = 通り数
-int dp[100005][8];
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
+using mint = modint1000000007;
 
 int main() {
-  int n; cin >> n;
-  string s; cin >> s;
+  int n; string s;
+  cin >> n >> s;
+  const string t = "atcoder";
 
-  dp[0][0]=1;
+  vector<mint> dp(8);
+  dp[0] = 1;
   REP(i,n) {
-    char cur = s[i];
-    REP(j,8) {
-      dp[i+1][j] += dp[i][j];
-			if (cur=='a' && j==0) dp[i+1][j+1] += dp[i][j];
-			if (cur=='t' && j==1) dp[i+1][j+1] += dp[i][j];
-			if (cur=='c' && j==2) dp[i+1][j+1] += dp[i][j];
-			if (cur=='o' && j==3) dp[i+1][j+1] += dp[i][j];
-			if (cur=='d' && j==4) dp[i+1][j+1] += dp[i][j];
-			if (cur=='e' && j==5) dp[i+1][j+1] += dp[i][j];
-			if (cur=='r' && j==6) dp[i+1][j+1] += dp[i][j];
+    vector<mint> p(8);
+    p[0] = 1;
+    swap(dp,p);
+    REP(j,7) {
+      dp[j+1] = p[j+1];
+      if (s[i] == t[j]) dp[j+1] += p[j];
     }
-    REP(j,8) dp[i+1][j]%=MOD;
   }
 
-  cout << dp[n][7] << endl;
+  cout << dp[7].val() << endl;
   return 0;
 }
