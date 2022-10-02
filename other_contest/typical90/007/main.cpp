40e7c73a0abd603de46808b6a60e4fd990b93e57
tic40 <ccpzjoh@gmail.com>
2022-10-02 14:31:00 +0900

    Update

--- other_contest/typical90/007/main.cpp
+++ other_contest/typical90/007/main.cpp
@@ -1,32 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
-using ll = long long;
-
-int n,q;
-vector<ll> a,b;
-
-void solve() {
-  sort(a.begin(), a.end());
-
-  REP(i,q) {
-    auto it = lower_bound(a.begin(), a.end(), b[i]);
-    int idx = it-a.begin();
-    ll ans = min( abs(b[i]-a[idx]), abs(b[i]-a[max(0, idx-1)]) );
-    cout << ans << endl;
-  }
-
-  return;
-}
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
+const int INF = numeric_limits<int>::max();
 
 int main() {
-  cin >> n;
-  a.resize(n);
+  int n; cin >> n;
+  vector<int> a(n);
   REP(i,n) cin >> a[i];
-  cin >> q;
-  b.resize(q);
-  REP(i,q) cin >> b[i];
+  sort(a.begin(),a.end());
 
-  solve();
+  int q; cin >> q;
+  REP(_,q) {
+    int b; cin >> b;
+    auto it = lower_bound(a.begin(),a.end(),b);
+    int idx = it - a.begin();
+    int ans = min( abs(a[min(n-1,idx)] - b), abs(a[max(0,idx-1)] - b) );
+    cout << ans << endl;
+  }
   return 0;
 }
