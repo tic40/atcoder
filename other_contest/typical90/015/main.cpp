403a5695ef034f9d16aeebb3b3f15eb7424f09cf
tic40 <ccpzjoh@gmail.com>
2022-10-04 09:44:17 +0900

    Update

--- other_contest/typical90/015/main.cpp
+++ other_contest/typical90/015/main.cpp
@@ -9,7 +9,7 @@ using mint = modint1000000007;
 
 const int MAX = 200005;
 
-long long fac[MAX], finv[MAX], inv[MAX];
+ll fac[MAX], finv[MAX], inv[MAX];
 void comInit() { // テーブルを作る前処理
   fac[0] = fac[1] = 1;
   finv[0] = finv[1] = 1;
@@ -22,25 +22,21 @@ void comInit() { // テーブルを作る前処理
   return;
 }
 
-long long com(int n, int k) {
+ll com(int n, int k) {
   if (n < k) return 0;
   if (n < 0 || k < 0) return 0;
   return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
 }
 
-int n;
 int main() {
-  cin >> n;
+  int n; cin >> n;
   comInit();
 
   for(int k = 1; k <= n; k++) {
     mint sum = 0;
     for(int i = 1; i <= n/k+1; i++) {
       // 1..nからk以上の差になるものをi個選ぶ場合
-
-      /*
-        https://twitter.com/e869120/status/1382827276673306624/photo/1
-      */
+      // k-1個の選ばないボールを挿入することを考える
       sum += com(n-(k-1)*(i-1), i);
     }
     cout << sum.val() << endl;
