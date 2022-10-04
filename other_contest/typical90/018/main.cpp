ea4dfd548dbd2f66405b1f7aafc6aa1b3913f839
tic40 <ccpzjoh@gmail.com>
2022-10-04 22:26:41 +0900

    Update

--- other_contest/typical90/018/main.cpp
+++ other_contest/typical90/018/main.cpp
@@ -1,33 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
-using ll = long long;
-const double pi = 3.141592653589793;
-
-double t,l,x,y,q;
-vector<double> e;
-
-void solve() {
-  REP(i,q) {
-    // 観覧車のx,y,z座標
-    double deg = 360 * e[i] / t * pi / 180.0;
-    double nx = 0;
-    double ny = -l/2.0*sin(deg);
-    double nz = l/2.0 - l/2.0*cos(deg);
-
-    double a = sqrt(x*x + (ny-y)*(ny-y));
-    double b = nz;
-    double ans = atan2(b,a) * 180 / pi;
-    cout << fixed << setprecision(10) << ans << endl;
-  }
-  return;
-}
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 
 int main() {
-  cin >> t >> l >> x >> y >> q;
-  e.resize(q);
-  REP(i,q) cin >> e[i];
+  double t,l,x,y; cin >> t >> l >> x >> y;
+  int q; cin >> q;
+  REP(_,q) {
+    double e; cin >> e;
 
-  solve();
+    // 現在の観覧車の角度
+    double deg = 360.0 * e / t;
+    double rad = deg * M_PI / 180.0;
+
+    double ny = -l / 2.0 * sin(rad);
+    double nz = l / 2.0 - l / 2.0 * cos(rad);
+
+    // 高橋像までの水平距離
+    double a = sqrt(x*x + (ny-y)*(ny-y));
+    double b = nz;
+    double ans = atan2(b,a) * 180.0 / M_PI;
+    printf("%.10lf\n", ans);
+  }
+
   return 0;
 }
