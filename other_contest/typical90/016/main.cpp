5187a6e5cb0e403969ba3d6148f3f33eaa4baccf
tic40 <ccpzjoh@gmail.com>
2022-10-04 19:23:20 +0900

    Update

--- other_contest/typical90/016/main.cpp
+++ other_contest/typical90/016/main.cpp
@@ -1,20 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 using ll = long long;
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
 
 int main() {
-  ll n,a,b,c;
-  cin >> n >> a >> b >> c;
+  int n; cin >> n;
+  ll a,b,c; cin >> a >> b >> c;
 
-  int ans = 10000;
-  REP(i,10000) REP(j,10000-i) {
-    ll d = n - (a*i+b*j);
-    int r = i+j+d/c;
-    if (d%c == 0 && 0 <= d) ans = min(ans, r);
+  int ans = 1e4;
+  REP(i,1e4) REP(j,1e4-i) {
+    ll now = n - a*i - b*j;
+    if (now >= 0 && now % c == 0) ans = min(ans, (int)(i+j+now/c));
   }
 
   cout << ans << endl;
