a7b25b1782f34d1fe230762c602d1f1da9fd8897
tic40 <ccpzjoh@gmail.com>
2022-10-05 22:28:56 +0900

    Update

--- other_contest/typical90/019/main.cpp
+++ other_contest/typical90/019/main.cpp
@@ -1,12 +1,8 @@
 #include <bits/stdc++.h>
-#include <atcoder/all>
-using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<(int)(n);i++)
 const int INF = 1e9;
-
-// dp[l][r] = (l,l+1..., r番目の人だけが抜けるのに必要なコスト)
-int dp[405][405];
+template<class T> void chmin(T& a, T b) { if (a > b) a = b; }
 
 int main() {
   int n; cin >> n;
@@ -14,18 +10,17 @@ int main() {
   vector<int> a(n);
   REP(i,n) cin >> a[i];
 
-  // 初期化
-  REP(i,n) {
-    REP(j,n) dp[i][j] = INF;
-		if (i+1 < n) dp[i][i + 1] = abs(a[i]-a[i + 1]);
-	}
+	// dp[l][r] := l,l+1..., r番目の人だけが抜けるのに必要な最小のコスト
+	vector dp(n,vector<int>(n,INF));
+	REP(i,n-1) dp[i][i+1] = abs(a[i]-a[i+1]);
 
-  // dp
 	for (int i = 3; i < n; i += 2) {
     REP(j,n-i) {
 			int cl = j, cr = j + i;
-			for (int k = cl; k <= cr - 1; k++) dp[cl][cr] = min(dp[cl][cr], dp[cl][k] + dp[k + 1][cr]);
-			dp[cl][cr] = min(dp[cl][cr], dp[cl + 1][cr - 1] + abs(a[cl] - a[cr]));
+			for (int k = cl; k <= cr-1; k++) {
+				chmin(dp[cl][cr], dp[cl][k] + dp[k+1][cr]);
+			}
+			chmin(dp[cl][cr], dp[cl+1][cr-1] + abs(a[cl] - a[cr]));
 		}
 	}
 
