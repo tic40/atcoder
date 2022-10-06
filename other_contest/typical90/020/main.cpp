e9664b13d7fb5997315731467e9e8badfc2b607d
tic40 <ccpzjoh@gmail.com>
2022-10-06 10:15:49 +0900

    Update

--- other_contest/typical90/020/main.cpp
+++ other_contest/typical90/020/main.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 using ll = long long;
 
 int main() {
-  ll a,b,c;
-  cin >> a >> b >> c;
+  ll a,b,c; cin >> a >> b >> c;
 
-  ll r = 1;
-  REP(i,b) r *= c;
-  a < r ? cout << "Yes" : cout << "No";
+  // log2(a) < b*log2(c) は、
+  // log2(a) < log2(c^b) と置き換えられる
+  cout << (a < powl(c,b) ? "Yes" : "No") << endl;
   return 0;
 }
