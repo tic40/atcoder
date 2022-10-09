2c80e02be8f5f426a616971fa543da7c502ad8b4
tic40 <ccpzjoh@gmail.com>
2022-10-09 22:15:37 +0900

    Update

--- other_contest/typical90/026/main.cpp
+++ other_contest/typical90/026/main.cpp
@@ -1,50 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 
-int n;
-vector<vector<int>> g;
+vector<int> dist(1e5);
+vector<vector<int>> g(1e5);
 
-void solve() {
-  vector<int> col(n, -1);
-  queue<int> q;
-  q.push(0);
-  col[0]=0;
-
-  vector<int> cnt(2);
-  while(q.size()) {
-    int v = q.front(); q.pop();
-
-    for(int u: g[v]) {
-      if (col[u] != -1) continue;
-      q.push(u);
-      col[u] = col[v]^1;
-    }
+void dfs(int i, int p) {
+  for(int v: g[i]) {
+    if (v == p) continue;
+    dist[v] = dist[i]+1;
+    dfs(v,i);
   }
-
-  vector<int> col1,col2;
-  REP(i,n) {
-    if (col[i] == 0) col1.push_back(i);
-    else col2.push_back(i);
-  }
-
-  vector<int> ans = col1.size() <= col2.size() ? col2 : col1;
-  REP(i,n/2) cout << ans[i]+1 << endl;
-
   return;
 }
 
 int main() {
-  cin >> n;
-  g.resize(n);
+  int n; cin >> n;
   REP(i,n-1) {
-    int a,b;
-    cin >> a >> b;
+    int a,b; cin >> a >> b;
     a--; b--;
     g[a].push_back(b);
     g[b].push_back(a);
   }
 
-  solve();
+  dfs(0,-1);
+  vector<int> odd,even;
+  REP(i,n) {
+    if (dist[i]%2) odd.push_back(i+1);
+    else even.push_back(i+1);
+  }
+
+  vector<int> &ans = ((int)odd.size() >= n/2) ? odd : even;
+  REP(i,n/2) cout << ans[i] << " ";
   return 0;
 }
