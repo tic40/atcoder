ddd97604bdbfb71894605a76b85d67194c413ef0
tic40 <ccpzjoh@gmail.com>
2022-10-09 13:29:28 +0900

    Update

--- other_contest/typical90/021/main.cpp
+++ other_contest/typical90/021/main.cpp
@@ -1,10 +1,12 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 using ll = long long;
 
 int n,m;
-vector<vector<int>> g,gr;
+vector<vector<int>> g(1e5),gr(1e5);
 vector<int> dir;
 vector<bool> used;
 ll cnt = 0;
@@ -28,7 +30,15 @@ void dfs2(int cur) {
 }
 
 // 強連結成分分解
-void solve() {
+void solve1() {
+  REP(i,m) {
+    int a,b;
+    cin >> a >> b;
+    a--; b--;
+    g[a].push_back(b);
+    gr[b].push_back(a);
+  }
+
   used.resize(n, false);
   REP(i,n) dfs1(i);
 
@@ -45,19 +55,24 @@ void solve() {
   return;
 }
 
-int main() {
-  cin >> n >> m;
-  g.resize(n);
-  gr.resize(n);
-
+// ac libraryを使う場合
+void solve2() {
+  scc_graph sg(n);
   REP(i,m) {
     int a,b;
     cin >> a >> b;
     a--; b--;
-    g[a].push_back(b);
-    gr[b].push_back(a);
+    sg.add_edge(a,b);
   }
+  auto scc = sg.scc();
+  ll ans = 0;
+  for(auto v: scc) ans += v.size() * (v.size()-1) / 2;
+  cout << ans << endl;
+}
 
-  solve();
+int main() {
+  cin >> n >> m;
+  // solve1();
+  solve2();
   return 0;
 }
