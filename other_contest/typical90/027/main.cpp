f0b8c411e73093c2a2c0d3043420438994aa53b7
tic40 <ccpzjoh@gmail.com>
2022-10-09 22:21:06 +0900

    Update

--- other_contest/typical90/027/main.cpp
+++ other_contest/typical90/027/main.cpp
@@ -1,18 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
-using ll = long long;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 
 int main() {
   int n; cin >> n;
-
-  map<string,int> mp;
+  unordered_set<string> st;
   REP(i,n) {
     string s; cin >> s;
-    if (mp[s] == 1) continue;
-    mp[s] = 1;
+    if (st.count(s)) continue;
+    st.insert(s);
     cout << i+1 << endl;
   }
-
   return 0;
 }
