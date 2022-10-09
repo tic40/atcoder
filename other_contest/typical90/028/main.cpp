279e92c8abfe46a47a3821c7ccb6ff735d2cf347
tic40 <ccpzjoh@gmail.com>
2022-10-09 23:04:47 +0900

    Update

--- other_contest/typical90/028/main.cpp
+++ other_contest/typical90/028/main.cpp
@@ -1,13 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
-using ll = long long;
-
-// 2次元累積和
-int s[1005][1005];
+#define endl '\n'
 
 int main() {
   int n; cin >> n;
+  vector s(1001, vector<int>(1001)); // 2次元累積和
   REP(i,n) {
     int lx,ly,rx,ry;
     cin >> lx >> ly >> rx >> ry;
@@ -20,11 +18,8 @@ int main() {
   REP(i,1000) REP(j,1000) s[i+1][j] += s[i][j];
   REP(i,1000) REP(j,1000) s[i][j+1] += s[i][j];
 
-  vector<int> ans(1000005);
-  REP(i,1000) REP(j,1000) {
-    if (1 <= s[i][j]) ans[s[i][j]]++;
-  }
-
-	for (int i = 1; i <= n; i++) cout << ans[i] << endl;
+  vector<int> ans(n+1);
+  REP(i,1000) REP(j,1000) ans[s[i][j]]++;
+  for (int i = 1; i <= n; i++) cout << ans[i] << endl;
   return 0;
 }
