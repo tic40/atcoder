70b12dd1728f6e7a3eab09d4e2771000625f7934
tic40 <ccpzjoh@gmail.com>
2022-10-11 23:52:53 +0900

    Update

--- other_contest/typical90/034/main.cpp
+++ other_contest/typical90/034/main.cpp
@@ -1,42 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 
-int n,k;
-vector<int> a;
+int main() {
+  int n,k; cin >> n >> k;
+  vector<int> a(n);
+  REP(i,n) cin >> a[i];
 
-void solve() {
-  int ans = 0;
-
-  map<int, int> m;
-  int r = 0;
-  int tot = 0; // 種類の合計
-  // しゃくとり
+  map<int,int> mp;
+  // sum: 種類の合計数
+  int r = 0, sum = 0, ans = 0;
   REP(l,n) {
     while(r < n) {
-      if (m[a[r]] == 0) {
-        if (k <= tot) break;
-        tot++;
+      if (mp[a[r]] == 0) {
+        if (sum == k) break;
+        sum++;
       }
-      m[a[r]]++;
+      mp[a[r]]++;
       r++;
     }
-    ans = max(ans, r-l);
-
-    if (r == l) r++;
-    m[a[l]]--;
-    if (m[a[l]] == 0) tot--;
+    ans = max(ans,r-l);
+    mp[a[l]]--;
+    if (mp[a[l]] == 0) sum--;
   }
 
   cout << ans << endl;
-  return;
-}
-
-int main() {
-  cin >> n >> k;
-  a.resize(n);
-  REP(i,n) cin >> a[i];
-
-  solve();
   return 0;
 }
