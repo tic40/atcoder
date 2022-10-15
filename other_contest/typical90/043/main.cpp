83b9d10d7e9ff9c7411bf756ad22835b5a165bdd
tic40 <ccpzjoh@gmail.com>
2022-10-15 15:42:35 +0900

    Update

--- other_contest/typical90/043/main.cpp
+++ other_contest/typical90/043/main.cpp
@@ -1,58 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
-const int INF = 1e9;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
+using P = pair<int,int>;
+using T = tuple<int,int,int,int>;
+const int INF = numeric_limits<int>::max();
 
-int h,w;
-int sy,sx,gy,gx;
-vector<string> s;
-vector<pair<int, int>> moves = { {1,0},{0,1},{-1,0},{0,-1} };
-
-int dist[1005][1005][4];
-struct State {
-  int y, x, dir;
-};
-
-void bfs() {
-  REP(i,h) REP(j,w) REP(k,4) dist[i][j][k] = INF;
-  queue<State> q;
-
-  REP(i,4) {
-    dist[sy][sx][i] = 0;
-    q.push({ sy, sx, i });
-  }
-
-  while(q.size()) {
-    auto state = q.front(); q.pop();
-    REP(i,4) {
-      auto move = moves[i];
-      int ny = state.y + move.first;
-      int nx = state.x + move.second;
-      int cost = dist[state.y][state.x][state.dir] + (state.dir != i ? 1 : 0);
-      if (ny < 0 || h <= ny || nx < 0 || w <= nx) continue;
-      if (s[ny][nx] == '#') continue;
-      if (dist[ny][nx][i] <= cost) continue;
-
-      dist[ny][nx][i] = cost;
-      q.push({ ny, nx, i });
-    }
-  }
-  return;
-}
+const vector<P> moves = {{1,0},{0,1},{-1,0},{0,-1}};
 
 int main() {
-  cin >> h >> w;
-  cin >> sy >> sx;
-  cin >> gy >> gx;
-  sy--,sx--,gy--,gx--;
-
-  s.resize(h);
+  int h,w; cin >> h >> w;
+  int rs,cs,rt,ct; cin >> rs >> cs >> rt >> ct;
+  rs--; cs--; rt--; ct--;
+  vector<string> s(h);
   REP(i,h) cin >> s[i];
 
-  bfs();
+  priority_queue<T, vector<T>, greater<T>> q;
+  vector dist(h, vector(w, vector<int>(4,INF)));
+  auto push = [&](int a, int b, int dir, int cost) {
+    if (a < 0 || b < 0 || a >= h || b >= w) return;
+    if (s[a][b] == '#') return;
+    if (dist[a][b][dir] <= cost) return;
+    q.emplace(cost, a, b, dir);
+    dist[a][b][dir] = cost;
+  };
+  REP(i,4) push(rs,cs,i,0);
+
+  while(q.size()) {
+    auto [cost,x,y,dir] = q.top(); q.pop();
+    REP(i,4) {
+      auto [dx,dy] = moves[i];
+      int nc = i == dir ? cost : cost+1;
+      push(x+dx, y+dy, i, nc);
+    }
+  }
 
   int ans = INF;
-  REP(i,4) ans = min(ans, dist[gy][gx][i]);
+  REP(i,4) ans = min(ans, dist[rt][ct][i]);
   cout << ans << endl;
   return 0;
 }
