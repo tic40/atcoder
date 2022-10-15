e27169b4bd39b75c0a6c94423de68e76dcf4835e
tic40 <ccpzjoh@gmail.com>
2022-10-15 14:18:09 +0900

    Update

--- other_contest/typical90/038/main.cpp
+++ other_contest/typical90/038/main.cpp
@@ -1,21 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 using ll = long long;
 
-ll a,b;
-
-ll gcd(ll a, ll b) { return b?gcd(b,a%b):a; }
-ll lcm(ll a, ll b) { return a/gcd(a,b)*b; }
-
 int main() {
-  cin >> a >> b;
-  ll mx = 1e18;
-  ll ans = lcm(a,b);
-
-  ll r = a / gcd(a,b);
-  if (r > mx/b) cout << "Large" << endl;
-  else cout << r * b << endl;
-
+  ll a,b; cin >> a >> b;
+  ll d = gcd(a,b);
+  if (a/d > (ll)1e18/b) cout << "Large" << endl;
+  else cout << a/d*b << endl;
   return 0;
 }
