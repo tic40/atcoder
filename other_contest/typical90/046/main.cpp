be90867bc9d0ee08019fce2b44a007afc8cdc41d
tic40 <ccpzjoh@gmail.com>
2022-10-15 20:42:20 +0900

    Update

--- other_contest/typical90/046/main.cpp
+++ other_contest/typical90/046/main.cpp
@@ -1,37 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 using ll = long long;
 
-int n;
-vector<int> a,b,c;
-
-ll solve() {
-  ll ans = 0;
-  map<int, ll> ma,mb,mc;
-  REP(i,n) {
-    ma[ a[i]%46 ]++;
-    mb[ b[i]%46 ]++;
-    mc[ c[i]%46 ]++;
-  }
-
-  for(auto x: ma) for(auto y: mb) for(auto z: mc) {
-    int tot = x.first + y.first + z.first;
-    if (tot % 46 == 0) ans += x.second * y.second * z.second;
-  }
-
-  return ans;
-}
-
 int main() {
-  cin >> n;
-  a.resize(n);
-  b.resize(n);
-  c.resize(n);
-  REP(i,n) cin >> a[i];
-  REP(i,n) cin >> b[i];
-  REP(i,n) cin >> c[i];
-  cout << solve() << endl;
+  int n; cin >> n;
+  auto in = [&]() {
+    vector<int> res(46);
+    REP(i,n) { int x; cin >> x; res[x%46]++; }
+    return res;
+  };
 
+  auto a = in();
+  auto b = in();
+  auto c = in();
+  ll ans = 0;
+  REP(i,46) REP(j,46) REP(k,46) {
+    if ((i+j+k)%46) continue;
+    ans += (ll)a[i] * b[j] * c[k];
+  }
+
+  cout << ans << endl;
   return 0;
 }
