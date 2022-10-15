f3713b66de031d0ca5e04920959fe0fa558e84a5
tic40 <ccpzjoh@gmail.com>
2022-10-15 14:17:52 +0900

    Update

--- other_contest/typical90/039/main.cpp
+++ other_contest/typical90/039/main.cpp
@@ -1,46 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 using ll = long long;
 
-int n;
-vector<int> a,b,dp;
-vector<vector<int>> g;
+vector<vector<int>> g(1e5);
+vector<int> dp(1e5);
 
-void dfs(int cur, int pre) {
-  dp[cur] = 1;
-  for(int nv: g[cur]) {
-    if (nv == pre) continue;
-    dfs(nv, cur);
-    dp[cur] += dp[nv];
+int dfs(int i, int p) {
+  int now = 1;
+  for(int v: g[i]) {
+    if (v == p) continue;
+    now += dfs(v,i);
   }
-  return;
-}
-
-ll solve() {
-  dfs(0, -1);
-  ll ans = 0;
-  REP(i,n-1) {
-    ll r = min(dp[a[i]], dp[b[i]]);
-    ans += r * (n-r);
-  }
-  return ans;
+  return dp[i] = now;
 }
 
 int main() {
-  cin >> n;
-  a.resize(n);
-  b.resize(n);
-  g.resize(n);
-  dp.resize(n);
-
+  int n; cin >> n;
+  vector<int> a(n),b(n);
   REP(i,n-1) {
     cin >> a[i] >> b[i];
-    a[i]--, b[i]--;
+    a[i]--; b[i]--;
     g[a[i]].push_back(b[i]);
     g[b[i]].push_back(a[i]);
   }
 
-  cout << solve() << endl;
+  dfs(0,-1);
+  ll ans = 0;
+  // 辺ごとに通る数を合算する
+  REP(i,n-1) {
+    // 貢献度が小さい方が部下にあたる頂点
+    ll r = min(dp[a[i]], dp[b[i]]);
+    ans += r * (n-r);
+  }
+  cout << ans << endl;
   return 0;
 }
