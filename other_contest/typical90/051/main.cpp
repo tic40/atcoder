f988ce391b0ed299de0d8c17ef44b56c6497fff8
tic40 <ccpzjoh@gmail.com>
2022-10-17 10:21:09 +0900

    Update

--- other_contest/typical90/051/main.cpp
+++ other_contest/typical90/051/main.cpp
@@ -7,28 +7,33 @@ using ll = long long;
 int main() {
   int n,k; ll p;
   cin >> n >> k >> p;
-  vector<ll> a(n);
-  REP(i,n) cin >> a[i];
+  vector<ll> a,b;
+  REP(i,n) {
+    ll x; cin >> x;
+    if (i%2 == 0) a.push_back(x);
+    else b.push_back(x);
+  }
 
-  auto f = [&](vector<vector<ll>> &g, int sz, int base) {
+  auto f = [&](vector<vector<ll>> &g, vector<ll> x) {
+    int sz = x.size();
     REP(bit, 1 << sz) {
       ll now = 0;
       int popcnt = 0;
       REP(i,sz) {
-        if (bit >> i & 1) { now += a[i+base]; popcnt++; }
+        if (bit >> i & 1) { now += x[i]; popcnt++; }
       }
-      g[popcnt].push_back(now);
+      if (popcnt <= k) g[popcnt].push_back(now);
     }
     for(auto &v: g) sort(v.begin(),v.end());
   };
 
-  vector<vector<ll>> ga(n+1), gb(n+1);
-  f(ga,n/2,0);
-  f(gb,n-n/2,n/2);
+  vector<vector<ll>> ga(k+1), gb(k+1);
+  f(ga,a);
+  f(gb,b);
 
   ll ans = 0;
   REP(i,k+1) for(auto v: ga[i]) {
-    auto it = lower_bound(gb[k-i].begin(), gb[k-i].end(), p-v+1);
+    auto it = upper_bound(gb[k-i].begin(), gb[k-i].end(), p-v);
     int idx = it - gb[k-i].begin();
     ans += idx;
   }
