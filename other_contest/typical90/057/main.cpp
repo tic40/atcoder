1c0570d48cc399a9674d2be24b04dbca1f2aeb60
tic40 <ccpzjoh@gmail.com>
2022-10-18 23:29:27 +0900

    Update

--- other_contest/typical90/057/main.cpp
+++ other_contest/typical90/057/main.cpp
@@ -1,65 +1,54 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
-using ll = long long;
-const int MOD = 998244353;
+#define endl '\n'
+using mint = modint998244353;
 
-int n,m;
-int a[300][300];
-vector<int> s(300);
+int main() {
+  int n,m; cin >> n >> m;
+  vector a(n, vector<int>(m));
+  REP(i,n) {
+    int t; cin >> t;
+    REP(j,t) {
+      int x; cin >> x;
+      x--;
+      a[i][x] = 1;
+    }
+  }
+  vector<int> s(m);
+  REP(i,m) cin >> s[i];
 
-void solve() {
   int pos = 0;
   REP(j,m) {
     bool found = false;
     for(int i = pos; i < n; i++) {
       if (a[i][j]) {
         found = true;
-        swap(a[pos],a[i]);
+        swap(a[i],a[pos]);
         break;
       }
     }
 
-    if (found) {
-      for(int i = pos+1; i < n; i++) {
-        if (a[i][j]) {
-          REP(k,m) {
-            a[i][k] ^= a[pos][k];
-          }
-        }
-      }
-      if (s[j]) {
-        REP(k,m) {
-          s[k] ^= a[pos][k];
-        }
-      }
-      pos++;
+    if (!found) continue;
+
+    for(int i = pos+1; i < n; i++) {
+      if (!a[i][j]) continue;
+      for(int k = j; k < m; k++) a[i][k] ^= a[pos][k];
     }
-  }
-
-  REP(i,m) {
-    if (s[i]) { cout << 0 << endl; return; }
-  }
-
-  ll ans = 1;
-  for(int i = pos; i < n; i++) ans = ans * 2 % MOD;
-  cout << ans << endl;
-
-  return;
-}
-
-int main() {
-  cin >> n >> m;
-  REP(i,n) {
-    int t; cin >> t;
-    REP(j,t) {
-      int x; cin >> x;
-      x--;
-      a[i][x] = true;
+    if (s[j]) {
+      for(int k = j; k < m; k++) s[k] ^= a[pos][k];
     }
+    pos++;
   }
-  REP(i,m) cin >> s[i];
 
-  solve();
+  if (accumulate(s.begin(),s.end(),0) > 0) {
+    cout << 0 << endl;
+  } else {
+    mint ans = 1;
+    for(int i = pos; i < n; i++) ans *= 2;
+    cout << ans.val() << endl;
+  }
   return 0;
 }
