5431e9c1f5115ba81647e52ab1ae843e1a9bb5de
tic40 <ccpzjoh@gmail.com>
2022-10-20 00:13:04 +0900

    Update

--- other_contest/typical90/061/main.cpp
+++ other_contest/typical90/061/main.cpp
@@ -1,26 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
-using ll = long long;
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 
 int main() {
   int q; cin >> q;
-  int t,x;
-  deque<int> d;
-
-  REP(i,q) {
+  deque<int> dq;
+  REP(_,q) {
+    int t,x;
     cin >> t >> x;
     if (t == 1) {
-      d.push_front(x);
+      dq.push_front(x);
     } else if (t == 2) {
-      d.push_back(x);
+      dq.push_back(x);
     } else {
-      cout << d[x-1] << endl;
+      cout << dq.at(x-1) << endl;
     }
   }
-
   return 0;
 }
