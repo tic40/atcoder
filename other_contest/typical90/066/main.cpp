7861126b8dedb270a74c1ab8df47025a61105aab
tic40 <ccpzjoh@gmail.com>
2022-10-23 16:15:42 +0900

    Update

--- other_contest/typical90/066/main.cpp
+++ other_contest/typical90/066/main.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 
 int main() {
   int n; cin >> n;
@@ -8,16 +9,15 @@ int main() {
   REP(i,n) cin >> l[i] >> r[i];
 
   double ans = 0;
-  REP(i,n) {
-    for(int j = i+1; j < n; j++) {
-      for(int k = l[i]; k <= r[i]; k++) {
-        int cnt = 0;
-        for(int m = l[j]; m <= r[j]; m++) if (m < k) cnt++;
-        int all = (r[i]-l[i]+1) * (r[j]-l[j]+1);
-        ans += (double)cnt / all;
-      }
+  REP(i,n) for(int j = i+1; j < n; j++) {
+    for(int k = l[i]; k <= r[i]; k++) {
+      int cnt = 0;
+      for(int m = l[j]; m <= r[j]; m++) if (m < k) cnt++;
+      int all = (r[i]-l[i]+1) * (r[j]-l[j]+1);
+      ans += (double)cnt/all;
     }
   }
-  cout << fixed << setprecision(10) << ans << endl;
+
+  printf("%.10f\n",ans);
   return 0;
 }
