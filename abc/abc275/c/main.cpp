e03c6c5971e84c81076c8f527142e7dd83647580
tic40 <ccpzjoh@gmail.com>
2022-10-30 17:14:17 +0900

    Update

--- abc/abc275/c/main.cpp
+++ abc/abc275/c/main.cpp
@@ -2,44 +2,31 @@
 using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
 #define endl '\n'
-using ll = long long;
 using P = pair<int,int>;
 
-bool check(vector<int>& x, vector<int>& y) {
-	vector<int> v;
-	for (int i = 0; i < 4; i++)
-		for (int j = i + 1; j < 4; j++) {
-			int dx = x[i] - x[j], dy = y[i] - y[j];
-			v.push_back(dx * dx + dy * dy);
-		}
-	sort(v.begin(), v.end());
-	int l = v[0];
-	if (l == 0) return false;
-	return v[0]==l && v[1]==l && v[2]==l && v[3]==l && v[4]==l*2 && v[5]==l*2;
-}
-
 int main() {
-  vector<string> s(9);
-  REP(i,9) cin >> s[i];
-
-  vector<P> pa;
-  REP(i,9) REP(j,9) if (s[i][j] == '#') pa.emplace_back(i,j);
-
-  int sz = pa.size();
-  int ans = 0;
-  REP(i,sz) {
-    for(int j = i+1; j < sz; j++) {
-      for(int k = j+1; k < sz; k++) {
-        for(int l = k+1; l < sz; l++) {
-          // i,j,k,lが正方形かどうか
-          vector<int> x = {pa[i].first, pa[j].first, pa[k].first, pa[l].first};
-          vector<int> y = {pa[i].second, pa[j].second, pa[k].second, pa[l].second};
-          if (check(x,y)) ans++;
-        }
-      }
+  const int n = 9;
+  vector<string> s(n);
+  REP(i,n) cin >> s[i];
+  set<P> st;
+  vector<P> ps;
+  REP(i,n) REP(j,n) {
+    if (s[i][j] == '#') {
+      st.emplace(i,j);
+      ps.emplace_back(i,j);
     }
   }
 
-  cout << ans << endl;
+  int ans = 0;
+  for(auto a: ps) for(auto b: ps) {
+    if (a == b) continue;
+    int di = b.first - a.first;
+    int dj = b.second - a.second;
+    P c(b.first - dj, b.second + di);
+    P d(c.first - di, c.second - dj);
+    if (st.count(c) && st.count(d)) ans++;
+  }
+
+  cout << ans/4 << endl;
   return 0;
 }
