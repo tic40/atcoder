1664eef26a798da30b480f8c34084377e7b353a8
tic40 <ccpzjoh@gmail.com>
2022-11-02 08:53:40 +0900

    Update

--- other_contest/typical90/073/main.cpp
+++ other_contest/typical90/073/main.cpp
@@ -1,58 +1,48 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace atcoder;
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
-using ll = long long;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 const int MOD = 1e9+7;
+using mint = modint1000000007;
 
-int n;
-vector<char> c;
-vector<int> a,b;
-vector<vector<int>> g;
+vector<char> c(1e5);
+vector<vector<int>> g(1e5);
+// dp[i][j(今の連結成分にどれがあるか)] := 頂点iの部分木を考えたとき何通りか
+// jは0: 'a' しかない, 1: 'b' しかない, 2: 'a','b' 両方ある
+vector dp(1e5, vector<mint>(3));
 
-// dp[pos][今の連結成分にどれがあるか] = 頂点posの部分木を考えたとき何通りか
-// 2つ目については、0: 'a' しかない, 1: 'b' しかない, 2: 'a','b' 両方ある
-int dp[100005][3];
+void dfs(int i, int p) {
+  // val1: a,b両方含まない数,
+  // val2: 全体の数
+  // val2 - val1 = a,b両方含む数
+  mint val1 = 1, val2 = 1;
 
-void dfs(int pos, int pre) {
-  // val1: a のみまたは b のみのケース
-  // val2: 全体のケース
-  ll val1 = 1, val2 = 1;
+  for(int v: g[i]) {
+    if (v == p) continue;
+    dfs(v,i);
 
-  for(int i: g[pos]) {
-    if (i == pre) continue;
-
-    dfs(i, pos);
-
-    if (c[pos] == 'a') {
-      // 削除しないとき dp[i][0]
-      // 削除するとき dp[i][2]
-      val1 *= (dp[i][0] + dp[i][2]);
+    if (c[i] == 'a') {
+      // 削除しないとき: dp[i][0]
+      // 削除するとき: dp[i][2]
+      val1 *= (dp[v][0] + dp[v][2]);
     } else {
-			val1 *= (dp[i][1] + dp[i][2]);
+      val1 *= (dp[v][1] + dp[v][2]);
     }
-    // 2*dp[i][2] しているのは削除するときとしないとき
- 		val2 *= (dp[i][0] + dp[i][1] + (ll)2 * dp[i][2]);
-
-    val1 %= MOD;
-    val2 %= MOD;
+    // dp[i][2]*2 しているのは削除するときとしないとき
+    val2 *= (dp[v][0] + dp[v][1] + dp[v][2]*2);
   }
 
-  if (c[pos] == 'a') {
-		dp[pos][0] = val1;
-  } else {
-		dp[pos][1] = val1;
-  }
-	dp[pos][2] = (val2 - val1 + MOD) % MOD;
+  if (c[i] == 'a') dp[i][0] = val1;
+  else dp[i][1] = val1;
 
+  dp[i][2] = val2 - val1;
   return;
 }
 
-
 int main() {
-  cin >> n;
-  c.resize(n);
-  g.resize(n);
-
+  int n; cin >> n;
   REP(i,n) cin >> c[i];
   REP(i,n-1) {
     int a,b; cin >> a >> b;
@@ -61,7 +51,8 @@ int main() {
     g[b].push_back(a);
   }
 
-  dfs(1, -1);
-  cout << dp[1][2] << endl;
+  dfs(1,-1);
+  cout << dp[1][2].val() << endl;
+
   return 0;
 }
