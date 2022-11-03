d27238833fb52fdf38633afc4910fc1ec26a8498
tic40 <ccpzjoh@gmail.com>
2022-11-03 11:00:53 +0900

    Update

--- other_contest/typical90/078/main.cpp
+++ other_contest/typical90/078/main.cpp
@@ -1,21 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 
 int main() {
-  int n,m;
-  cin >> n >> m;
-  vector<int> s(n);
-  int a,b;
+  int n,m; cin >> n >> m;
+  vector<int> cnt(n);
   REP(i,m) {
-    cin >> a >> b;
+    int a,b; cin >> a >> b;
     a--; b--;
-    a < b ? s[b]++ : s[a]++;
+    if (a > b) cnt[a]++;
+    else cnt[b]++;
   }
-
   int ans = 0;
-  REP(i,n) if (s[i] == 1) ans++;
-
+  REP(i,n) if (cnt[i] == 1) ans++;
   cout << ans << endl;
+
   return 0;
 }
