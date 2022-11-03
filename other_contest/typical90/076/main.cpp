c2e693787e87366519bcec73d4c37359c9a9a466
tic40 <ccpzjoh@gmail.com>
2022-11-03 10:49:49 +0900

    Update

--- other_contest/typical90/076/main.cpp
+++ other_contest/typical90/076/main.cpp
@@ -1,50 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 using ll = long long;
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
-
-int n;
-vector<int> a;
-
-void solve() {
-  ll sum = 0;
-  REP(i,n) sum += a[i];
-  if (sum % 10 > 0) {
-    cout << "No" << endl;
-    return;
-  }
-
-  ll p = sum / 10;
-  sum = 0;
-  int r = 0;
-
-  REP(l,n*2) {
-    while (r < n*2) {
-      if (sum == p) {
-        cout << "Yes" << endl;
-        return;
-      }
-      if (p < sum) break;
-
-      sum += a[r%n];
-      r++;
-    }
-    if (r == l) r++;
-    sum -= a[l%n];
-  }
-
-  cout << "No" << endl;
-  return;
-}
 
 int main() {
-  cin >> n;
-  a.resize(n);
+  int n; cin >> n;
+  vector<int> a(n);
   REP(i,n) cin >> a[i];
+  ll sum = accumulate(a.begin(),a.end(),0LL);
+  if (sum % 10) { cout << "No" << endl; return 0; }
+  ll t = sum/10;
 
-  solve();
+  int r = 0;
+  ll now = 0;
+  bool ok = false;
+  REP(l,n) {
+    while(r < n*2) {
+      if (t <= now) break;
+      now += a[r%n];
+      r++;
+    }
+    if (t == now) { ok = true; break; }
+    if (r == l) r++;
+    now -= a[l];
+  }
+
+  cout << (ok ? "Yes" : "No") << endl;
   return 0;
 }
