62e5280f8b8662eb33f88023445f042e43c7f664
tic40 <ccpzjoh@gmail.com>
2022-11-05 19:37:04 +0900

    Update

--- other_contest/typical90/081/main.cpp
+++ other_contest/typical90/081/main.cpp
@@ -1,31 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
 
-int m[5005][5005];
 int main() {
   int n,k; cin >> n >> k;
+  vector m(5001, vector<int>(5001));
   REP(i,n) {
     int a,b; cin >> a >> b;
     m[a][b]++;
   }
 
-  // 二次元累積和
-  REP(i,5000) REP(j,5000) m[i][j+1] += m[i][j];
-  REP(i,5000) REP(j,5000) m[i+1][j] += m[i][j];
-
-  int ans = 0;
-  REP(i,5001) REP(j,5001) {
-    int lx = max(0,j-k-1);
-    int ty = max(0,i-k-1);
-
-    int cnt = m[i][j];
-    cnt -= m[ty][j];
-    cnt -= m[i][lx];
-    cnt += m[ty][lx];
-    ans = max(ans,cnt);
+  REP(i,5000) REP(j,5000) {
+    m[i+1][j+1] += m[i+1][j];
+    m[i+1][j+1] += m[i][j+1];
+    m[i+1][j+1] -= m[i][j];
   }
 
+  int ans = 0;
+  REP(i,5001-k) REP(j,5001-k) {
+    int ri = min(5000, i+k+1);
+    int rj = min(5000, j+k+1);
+    int now = m[ri][rj];
+    now -= m[ri][j];
+    now -= m[i][rj];
+    now += m[i][j];
+    ans = max(ans,now);
+  }
   cout << ans << endl;
   return 0;
 }
