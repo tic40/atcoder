db77bcc4217729b8ea3bf1009a94f89007ce89f5
tic40 <ccpzjoh@gmail.com>
2022-11-13 21:47:20 +0900

    Update

--- other_contest/typical90/085/main.cpp
+++ other_contest/typical90/085/main.cpp
@@ -7,9 +7,9 @@ using ll = long long;
 vector<ll> divisor(ll n) {
   vector<ll> res;
   for(ll i = 1; i*i <= n; i++) {
-    if (n%i == 0) {
+    if (n % i == 0) {
       res.push_back(i);
-      if (n/i != i) res.push_back(n/i);
+      if (i != n/i) res.push_back(n/i);
     }
   }
   return res;
@@ -18,18 +18,13 @@ vector<ll> divisor(ll n) {
 int main() {
   ll k; cin >> k;
   auto d = divisor(k);
-  int n = d.size();
-
-  int ans = 0;
-  REP(i,n) for(int j = i; j < n; j++) {
-    ll a = d[i];
-    ll b = d[j];
-    ll c = k / a / b;
-    if (a*b*c == k && a <= b && b <= c) {
-      ans++;
+  ll ans = 0;
+  for(auto a: d) for(auto b: d) {
+    if ((k/a) % b == 0) {
+      ll c = (k / a) / b;
+      if (a <= b && b <= c) ans++;
     }
   }
-
   cout << ans << endl;
   return 0;
 }
