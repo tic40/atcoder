443f943b492b3f9154c179d4c3181724e005d6cd
tic40 <ccpzjoh@gmail.com>
2022-11-14 09:40:22 +0900

    Update

--- abc/abc277/d/main.cpp
+++ abc/abc277/d/main.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
 #define endl '\n'
@@ -8,32 +10,19 @@ int main() {
   int n,m; cin >> n >> m;
   vector<int> a(n);
   REP(i,n) cin >> a[i];
-  ll tot = accumulate(a.begin(),a.end(),0LL);
   sort(a.begin(),a.end());
-  a.insert(a.end(),a.begin(),a.end());
-  int n2 = a.size();
 
-  vector<ll> s(n2+1);
-  REP(i,n2) s[i+1] = s[i]+a[i];
-
-  vector<bool> b(n2+1); // +1して末尾に番兵を置く
-  REP(i,n2-1) {
-    if (a[i+1] == a[i] || a[i+1] == (a[i]+1)%m) b[i+1] = true;
+  dsu uf(n);
+  REP(i,n) {
+    int j = (i+1)%n;
+    if (a[i] == a[j] || (a[i]+1)%m == a[j]) uf.merge(i,j);
   }
 
-  int p = -1;
-  ll ans = tot - a.back();
-  REP(i,n2+1) {
-    if (b[i]) {
-      if (p == -1) p = i-1;
-    } else {
-      if (p == -1) continue;
-      ll now = s[i] - s[p];
-      ans = min(ans, max(0LL,tot-now));
-      p = -1;
-    }
-  }
+  vector<ll> s(n);
+  REP(i,n) s[uf.leader(i)] += a[i];
 
-  cout << ans << endl;
+  ll mx = *max_element(s.begin(),s.end());
+  ll tot = accumulate(a.begin(),a.end(),0LL);
+  cout << tot-mx << endl;
   return 0;
 }
