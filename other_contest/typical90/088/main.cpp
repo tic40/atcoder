9d23503d16acd2c30c1079f437af0d29a7246690
tic40 <ccpzjoh@gmail.com>
2022-11-18 09:15:58 +0900

    Update

--- other_contest/typical90/088/main.cpp
+++ other_contest/typical90/088/main.cpp
@@ -1,32 +1,38 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
 #define endl '\n'
-using G = vector<vector<int>>;
 
 int n,q;
-vector<int> a(100),c(10000),vec;
-G g(10000);
-vector<G> ans(10000);
-bool flag = false;
+bool found = false;
+vector<vector<int>> g(88);
+vector<vector<int>> memo(8889);
+vector<int> a(88), cnt(88);
+vector<int> vec;
 
 void dfs(int pos, int sum) {
-  if (flag) return;
+  if (found) return; // 答えが見つかっていたら打ち切る
   if (pos == n) {
-    ans[sum].push_back(vec);
-    // 答えが見つかったらdfsを終了させる
-    if ((int)ans[sum].size() == 2) flag = true;
+    if (memo[sum].size()) {
+      found = true;
+      for(auto ans: { memo[sum], vec }) {
+        cout << ans.size() << endl;
+        for(auto v: ans) cout << v+1 << " ";
+        cout << endl;
+      }
+    }
+    memo[sum] = vec;
     return;
   }
 
-  // 選ばない
   dfs(pos+1, sum);
-  // 選ぶ
-  if (c[pos] == 0) {
+  if (cnt[pos] == 0) {
     vec.push_back(pos);
-    for(int i: g[pos]) c[i]++;
+    for(int i: g[pos]) cnt[i]++;
     dfs(pos+1, sum+a[pos]);
-    for(int i: g[pos]) c[i]--;
+    for(int i: g[pos]) cnt[i]--;
     vec.pop_back();
   }
   return;
@@ -35,23 +41,12 @@ void dfs(int pos, int sum) {
 int main() {
   cin >> n >> q;
   REP(i,n) cin >> a[i];
-  REP(i,q) {
+  REP(_,q) {
     int x,y; cin >> x >> y;
     x--; y--;
-    g[x].push_back(y);
+    g[x].push_back(y); // x[i] < y[i] の制約があるので双方向じゃなくて良い
   }
 
   dfs(0,0);
-
-  REP(i,10000) {
-    if (ans[i].size() < 2) continue;
-
-    REP(j,2) {
-      cout << ans[i][j].size() << endl;
-      for(int v: ans[i][j]) cout << v+1 << " ";
-      cout << endl;
-    }
-    return 0;
-  }
   return 0;
 }
