c08f37cf56663812929186c94bf9ec50f14692c3
tic40 <ccpzjoh@gmail.com>
2022-11-22 22:43:34 +0900

    Update

--- abc/abc278/e/main.cpp
+++ abc/abc278/e/main.cpp
@@ -4,38 +4,40 @@ using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
 #define endl '\n'
-using ll = long long;
-using P = pair<int,int>;
 
 int main() {
   int H,W,n,h,w;
   cin >> H >> W >> n >> h >> w;
   vector a(H,vector<int>(W));
-  // 各数字の出現数合計
-  vector<int> cnt(n+1), now(n+1);
+  vector<int> cnt(n+1);
+  int tot = 0;
   REP(i,H) REP(j,W) {
     cin >> a[i][j];
+    if (cnt[a[i][j]] == 0) tot++;
     cnt[a[i][j]]++;
   }
-  int tot = 0;
-  REP(i,n+1) if (cnt[i] > 0) tot++;
 
   REP(i,H-h+1) {
-    REP(ii,n+1) now[ii] = 0;
-    REP(ii,h) REP(jj,w) now[a[i+ii][jj]]++;
+    auto ncnt = cnt;
+    int now = tot;
 
+    auto add = [&](int x, int y){
+      if (ncnt[a[x][y]] == 0) now++;
+      ncnt[a[x][y]]++;
+    };
+    auto del = [&](int x, int y){
+      ncnt[a[x][y]]--;
+      if (ncnt[a[x][y]] == 0) now--;
+    };
+
+    REP(si,h) REP(sj,w-1) del(i+si,sj);
     REP(j,W-w+1) {
-      int ans = tot;
-      if (j != 0) {
-        REP(ii,h) {
-          now[a[i+ii][j-1]]--;
-          now[a[i+ii][j+w-1]]++;
-        }
-      }
-      for(int k = 1; k <= n; k++) if (cnt[k] > 0 && now[k] == cnt[k]) ans--;
-      cout << ans << " ";
+      REP(si,h) del(i+si,j+w-1);
+      cout << now << " ";
+      REP(si,h) add(i+si,j);
     }
     cout << endl;
   }
+
   return 0;
 }
