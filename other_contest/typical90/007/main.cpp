acd51a7bbdd6acaf642980e369ddd78fbb520a8b
tic40 <ccpzjoh@gmail.com>
2022-12-04 19:36:35 +0900

    Update

--- other_contest/typical90/007/main.cpp
+++ other_contest/typical90/007/main.cpp
@@ -2,20 +2,18 @@
 using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
 #define endl '\n'
-const int INF = numeric_limits<int>::max();
 
 int main() {
   int n; cin >> n;
   vector<int> a(n);
   REP(i,n) cin >> a[i];
   sort(a.begin(),a.end());
-
   int q; cin >> q;
+
   REP(_,q) {
     int b; cin >> b;
-    auto it = lower_bound(a.begin(),a.end(),b);
-    int idx = it - a.begin();
-    int ans = min( abs(a[min(n-1,idx)] - b), abs(a[max(0,idx-1)] - b) );
+    int idx = lower_bound(a.begin(),a.end(),b) - a.begin();
+    int ans = min( abs(a[idx]-b), abs(a[max(0,idx-1)]-b) );
     cout << ans << endl;
   }
   return 0;
