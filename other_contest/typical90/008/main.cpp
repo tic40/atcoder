522709bf9b66162e4baa211a511027aba96e422b
tic40 <ccpzjoh@gmail.com>
2022-12-04 19:50:39 +0900

    Update

--- other_contest/typical90/008/main.cpp
+++ other_contest/typical90/008/main.cpp
@@ -6,20 +6,20 @@ using namespace std;
 #define endl '\n'
 using mint = modint1000000007;
 
+const string atc = " atcoder";
+
 int main() {
   int n; string s;
   cin >> n >> s;
-  const string t = "atcoder";
 
   vector<mint> dp(8);
   dp[0] = 1;
   REP(i,n) {
     vector<mint> p(8);
-    p[0] = 1;
     swap(dp,p);
-    REP(j,7) {
-      dp[j+1] = p[j+1];
-      if (s[i] == t[j]) dp[j+1] += p[j];
+    REP(j,8) {
+      dp[j] += p[j];
+      if (s[i] == atc[j]) dp[j] += p[j-1];
     }
   }
 
