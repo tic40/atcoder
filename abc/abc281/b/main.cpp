5fe23c33a65c577b80c071ec8f2b194f4293ecd2
tic40 <ccpzjoh@gmail.com>
2022-12-11 18:48:52 +0900

    Update

--- abc/abc281/b/main.cpp
+++ abc/abc281/b/main.cpp
@@ -1,26 +1,13 @@
 #include <bits/stdc++.h>
-#include <atcoder/all>
-using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 
 int main() {
   string s; cin >> s;
-  int n = s.size();
+  regex r("^[A-Z][1-9][0-9]{5}[A-Z]$");
+  bool ok = regex_match(s,r);
+  cout << (ok ? "Yes" : "No") << endl;
 
-  if (n == 8) {
-    if (s[0] >= 'A' && s[0] <= 'Z') {
-      if (s.back() >= 'A' && s.back() <= 'Z') {
-        int x = stoi( s.substr(1,6) );
-        if (x >= 100000 && x <= 999999) {
-          cout << "Yes" << endl;
-          return 0;
-        }
-      }
-    }
-  }
-
-  cout << "No" << endl;
   return 0;
 }
