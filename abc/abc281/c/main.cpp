175fb3a348a753b0996c18f26484bdafb17ad818
tic40 <ccpzjoh@gmail.com>
2022-12-11 18:53:12 +0900

    Update

--- abc/abc281/c/main.cpp
+++ abc/abc281/c/main.cpp
@@ -1,25 +1,21 @@
 #include <bits/stdc++.h>
-#include <atcoder/all>
-using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 using ll = long long;
 
 int main() {
-  ll n,t; cin >> n >> t;
-  vector<ll> a(n);
+  int n; ll t;
+  cin >> n >> t;
+  vector<int> a(n);
   REP(i,n) cin >> a[i];
-  ll tot = accumulate(a.begin(),a.end(),0LL);
 
-  ll r = t / tot; // 何周するか
-  t -= tot * r;
-
+  t %= accumulate(a.begin(),a.end(),0LL);
   REP(i,n) {
     t -= a[i];
     if (t <= 0) {
       cout << i+1 << " " << a[i]+t << endl;
-      return 0;
+      break;
     }
   }
 
