ede280235bbf91d7a2e94781a0dd7500acc70f8a
tic40 <ccpzjoh@gmail.com>
2023-01-03 12:21:12 +0900

    Update

--- other_contest/typical90/042/main.cpp
+++ other_contest/typical90/042/main.cpp
@@ -10,11 +10,12 @@ int main() {
   int k; cin >> k;
   if (k % 9) { cout << 0 << endl; return 0; }
 
+  // dp[i] := iにできる数字の総数
   vector<mint> dp(k+1);
   dp[0] = 1;
   REP(i,k+1) for(int j = 1; j <= 9; j++) {
-    if (i-j < 0 || i-j > k) continue;
-    if (dp[i-j] == 0) continue;
+    // もらうDP
+    if (i-j < 0) continue;
     dp[i] += dp[i-j];
   }
 
