f0deccb29b7c9166d1f738c66961812d05e7ca33
tic40 <ccpzjoh@gmail.com>
2023-01-16 22:11:59 +0900

    Update

--- other_contest/typical90/051/main.cpp
+++ other_contest/typical90/051/main.cpp
@@ -1,42 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 using ll = long long;
 
 int main() {
-  int n,k; ll p;
-  cin >> n >> k >> p;
+  int n,k; cin >> n >> k;
+  ll p; cin >> p;
   vector<ll> a,b;
   REP(i,n) {
-    ll x; cin >> x;
-    if (i%2 == 0) a.push_back(x);
-    else b.push_back(x);
+    ll v; cin >> v;
+    i%2==0 ? a.push_back(v) : b.push_back(v);
   }
 
-  auto f = [&](vector<vector<ll>> &g, vector<ll> x) {
+  auto f = [&](vector<ll> x) {
+    vector<vector<ll>> g(k+1);
     int sz = x.size();
-    REP(bit, 1 << sz) {
+    REP(bit,1<<sz) {
+      int popcnt = __builtin_popcount(bit);
+      if (popcnt > k) continue;
+
       ll now = 0;
-      int popcnt = 0;
-      REP(i,sz) {
-        if (bit >> i & 1) { now += x[i]; popcnt++; }
-      }
-      if (popcnt <= k) g[popcnt].push_back(now);
+      REP(i,sz) if (bit >> i & 1) now += x[i];
+      g[popcnt].push_back(now);
     }
     for(auto &v: g) sort(v.begin(),v.end());
+    return g;
   };
 
-  vector<vector<ll>> ga(k+1), gb(k+1);
-  f(ga,a);
-  f(gb,b);
+  auto ga = f(a);
+  auto gb = f(b);
 
   ll ans = 0;
   REP(i,k+1) for(auto v: ga[i]) {
-    auto it = upper_bound(gb[k-i].begin(), gb[k-i].end(), p-v);
-    int idx = it - gb[k-i].begin();
-    ans += idx;
+    auto it = upper_bound(gb[k-i].begin(), gb[k-i].end(),p-v);
+    ans += it - gb[k-i].begin();
   }
+
   cout << ans << endl;
   return 0;
 }
