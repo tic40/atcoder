49b3cab3dbcc3429c5139633b8e04271a7449d80
tic40 <ccpzjoh@gmail.com>
2023-01-29 09:47:35 +0900

    Update

--- abc/abc287/c/main.cpp
+++ abc/abc287/c/main.cpp
@@ -17,10 +17,13 @@ int main() {
     uf.merge(u,v);
   }
 
-  vector<int> cnt(4);
-  REP(i,n) cnt[min(3,deg[i])]++;
+  int one = 0, two = 0;
+  REP(i,n) {
+    if (deg[i] == 1) one++;
+    if (deg[i] == 2) two++;
+  }
 
-  bool ok = uf.size(0) == n && cnt[1] == 2 && cnt[2] == n-2;
+  bool ok = uf.size(0) == n && one == 2 && two == n-2;
   cout << (ok ? "Yes" : "No") << endl;
   return 0;
 }
