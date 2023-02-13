fa9aa57b3241b30b0d6551bead786e1ba7ebce56
tic40 <ccpzjoh@gmail.com>
2023-02-13 20:08:49 +0900

    Add a29, b29

---
+++ other_contest/tessoku_book/b29/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace atcoder;
+using namespace std;
+#define REP(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
+using ll = long long;
+using mint = modint1000000007;
+
+mint f(ll base, ll n) {
+  if (n == 0) return 1;
+  mint x = f(base, n/2);
+  x *= x;
+  if (n % 2 == 1) x *= base;
+
+  return x;
+}
+
+int main() {
+  ll a,b; cin >> a >> b;
+  cout << f(a,b).val() << endl;
+  return 0;
+}
