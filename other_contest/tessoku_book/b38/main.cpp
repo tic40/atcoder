391c489f768ca9c3f7aca52cb8a23ec31f35ff4d
tic40 <ccpzjoh@gmail.com>
2023-02-18 22:52:26 +0900

    Update

--- other_contest/tessoku_book/b38/main.cpp
+++ other_contest/tessoku_book/b38/main.cpp
@@ -4,25 +4,13 @@ using namespace std;
 #define endl '\n'
 
 int main() {
-  int n; string s;
-  cin >> n >> s;
+  int n; string s; cin >> n >> s;
   vector<int> ret1(n),ret2(n);
 
-  int streak1 = 1;
   ret1[0] = 1;
-  REP(i,n-1) {
-    if (s[i] == 'A') streak1++;
-    else streak1 = 1;
-    ret1[i+1] = streak1;
-  }
-
-  int streak2 = 1;
+  REP(i,n-1) ret1[i+1] = s[i]=='A' ? ret1[i]+1 : 1;
   ret2[n-1] = 1;
-  for(int i = n-2; i >= 0; i--) {
-    if (s[i] == 'B') streak2++;
-    else streak2 = 1;
-    ret2[i] = streak2;
-  }
+  for(int i = n-2; i >= 0; i--) ret2[i] = s[i]=='B' ? ret2[i+1]+1 : 1;
 
   int ans = 0;
   REP(i,n) ans += max(ret1[i],ret2[i]);
