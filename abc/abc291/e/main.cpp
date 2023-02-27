3d2e0a4800788b7104cab5abbca8fff17a0e1f91
tic40 <ccpzjoh@gmail.com>
2023-02-27 09:21:52 +0900

    Update

--- abc/abc291/e/main.cpp
+++ abc/abc291/e/main.cpp
@@ -4,46 +4,35 @@ using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
-using P = pair<int,int>;
 
 int main() {
   int n,m; cin >> n >> m;
-  set<P> st;
+  vector<vector<int>> g(n);
+  vector<int> deg(n);
   REP(i,m) {
     int x,y; cin >> x >> y;
     x--; y--;
-    st.emplace(x,y);
-  }
-
-  vector<vector<int>> g(n);
-  vector<int> deg(n);
-  for(auto [x,y]: st) {
     g[x].push_back(y);
     deg[y]++;
   }
 
   queue<int> q;
   REP(i,n) if (deg[i] == 0) q.push(i);
-  vector<int> ans;
+  vector<int> ans(n);
 
-  while(q.size() == 1) {
+  int now = 1;
+  while(q.size()) {
+    if (q.size() > 1) { cout << "No" << endl; return 0; }
     int v = q.front(); q.pop();
-    ans.push_back(v);
+    ans[v] = now;
+    now++;
     for(int nv: g[v]) {
       deg[nv]--;
       if (deg[nv] == 0) q.push(nv);
     }
   }
 
-  if ((int)ans.size() == n) {
-    vector<int> a(n);
-    REP(i,n) { a[ans[i]] = i+1; }
-    cout << "Yes" << endl;
-    REP(i,n) cout << a[i] << " ";
-    cout << endl;
-  } else {
-    cout << "No" << endl;
-  }
-
+  cout << "Yes" << endl;
+  for(int v: ans) cout << v << " ";
   return 0;
 }
