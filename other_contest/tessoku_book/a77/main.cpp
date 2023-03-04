37d0103bbdaf2837070b64c3019c63b4c2475a36
tic40 <ccpzjoh@gmail.com>
2023-03-04 11:02:03 +0900

    Add a77

---
+++ other_contest/tessoku_book/a77/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
+
+int main() {
+  int n,l,k; cin >> n >> l >> k;
+  vector<int> a(n);
+  REP(i,n) cin >> a[i];
+
+  int ok = 1, ng = l;
+  while(abs(ok-ng) > 1) {
+    int mid = (ok+ng)/2;
+    int cnt = 0, last = 0;
+    REP(i,n) {
+      int now = a[i] - last;
+      if (now >= mid && l - a[i] >= mid) {
+        cnt++;
+        last = a[i];
+      }
+    }
+
+    if (cnt >= k) ok = mid;
+    else ng = mid;
+  }
+
+  cout << ok << endl;
+  return 0;
+}
