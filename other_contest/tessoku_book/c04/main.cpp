feb31c899e67dd249d1da816081d4553eead659e
tic40 <ccpzjoh@gmail.com>
2023-03-04 11:23:22 +0900

    Add c04

---
+++ other_contest/tessoku_book/c04/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
+using ll = long long;
+
+int main() {
+  ll n; cin >> n;
+  vector<ll> ans;
+  for(ll i = 1; i*i <= n; i++) {
+    if (n % i == 0) {
+      ans.push_back(i);
+      if (i != n/i) ans.push_back(n/i);
+    }
+  }
+
+  sort(ans.begin(),ans.end());
+  for(auto v: ans) cout << v << endl;
+  return 0;
+}
