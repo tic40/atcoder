3720b991075038bfb754c314b698903a2ada22bc
tic40 <ccpzjoh@gmail.com>
2023-03-04 11:22:42 +0900

    Add c02

---
+++ other_contest/tessoku_book/c02/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
+
+int main() {
+  int n; cin >> n;
+  vector<int> a(n);
+  REP(i,n) cin >> a[i];
+  sort(a.rbegin(),a.rend());
+  cout << a[0]+a[1] << endl;
+  return 0;
+}
