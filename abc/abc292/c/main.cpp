c56d135d052a2c0191ce66bc86d98577fd8acaba
tic40 <ccpzjoh@gmail.com>
2023-03-05 09:32:16 +0900

    Update

--- abc/abc292/c/main.cpp
+++ abc/abc292/c/main.cpp
@@ -4,28 +4,18 @@ using namespace std;
 #define endl '\n'
 using ll = long long;
 
-vector<ll> memo(2e5+1,-1);
-ll divisor_num(ll n) {
-  if (memo[n] != -1) return memo[n];
-  ll res = 0;
-  for (int i = 1; i*i <= n; i++) {
-    if (n % i == 0) {
-      res++;
-      res += i != n/i;
-    }
-  }
-  return memo[n] = res;
-}
-
 int main() {
   int n; cin >> n;
-  ll ans = 0;
-  for(ll ab = 0; ab <= n; ab++) {
-    ll cd = n - ab;
-    auto add = divisor_num(ab);
-    auto cdd = divisor_num(cd);
-    ans += add*cdd;
+
+  vector<int> f(n+1);
+  for(int a = 1; a <= n; a++) {
+    for(int b = 1; a*b <= n; b++) {
+      f[a*b]++;
+    }
   }
+
+  ll ans = 0;
+  for(int i = 1; i < n; i++) ans += f[i]*f[n-i];
   cout << ans << endl;
   return 0;
 }
