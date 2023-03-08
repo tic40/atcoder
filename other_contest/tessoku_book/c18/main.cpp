188a55e48f5d4498a12908f9f0993cf4de73b8b6
tic40 <ccpzjoh@gmail.com>
2023-03-08 09:14:43 +0900

    Add c18

---
+++ other_contest/tessoku_book/c18/main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define endl '\n'
+const int INF = 1e9;
+template<class T> void chmin(T& a, T b) { a = min(a,b); }
+
+int main() {
+  int n; cin >> n;
+  n*=2;
+  vector<int> a(n);
+  REP(i,n) cin >> a[i];
+
+  vector dp(n+1, vector<int>(n,INF));
+  REP(i,n-1) dp[i][i+1] = abs(a[i+1]-a[i]);
+
+  for(int i = 3; i < n; i+=2) REP(j,n-i) {
+    int l = j, r = j+i;
+    chmin(dp[l][r], abs(a[l]-a[r]) + dp[l+1][r-1]);
+    for(int k = l; k < r; k++) chmin(dp[l][r], dp[l][k]+dp[k+1][r]);
+  }
+
+  cout << dp[0][n-1] << endl;
+  return 0;
+}
