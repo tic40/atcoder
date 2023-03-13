736e006fc8c80ce54ab02a1970571ab9b4d71d73
tic40 <ccpzjoh@gmail.com>
2023-03-13 09:51:59 +0900

    Update

--- abc/abc293/e/main.cpp
+++ abc/abc293/e/main.cpp
@@ -1,30 +1,42 @@
 #include <bits/stdc++.h>
-#include <atcoder/all>
-using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 using ll = long long;
-using P = pair<int,int>;
-const int INF = numeric_limits<int>::max();
-const ll LINF = numeric_limits<ll>::max();
-const int MOD = 1e9+7;
-using mint = modint;
+int MOD;
+
+vector<vector<ll>> mat_mul(vector<vector<ll>> a, vector<vector<ll>> b) {
+  int n = a.size();
+  vector res(n,vector<ll>(n));
+  REP(i,n) REP(j,n) REP(k,n) {
+    res[i][j] += a[i][k] * b[k][j];
+    res[i][j] %= MOD; // MOD取る場合
+  }
+  return res;
+}
+
+/*
+  a: 行列
+  x: 何乗するか
+*/
+vector<vector<ll>> mat_pow(vector<vector<ll>> a, ll x) {
+  if (x == 0) {
+    int n = a.size();
+    vector res(n,vector<ll>(n));
+    REP(i,n) res[i][i] = 1;
+    return res;
+  }
+
+  auto b = mat_pow(a,x/2);
+  auto res = mat_mul(b,b);
+
+  return x % 2 == 1 ? mat_mul(res,a) : res;
+}
 
 int main() {
-  int a,m; ll x;
-  cin >> a >> x >> m;
-  mint::set_mod(m);
-
-  auto f = [&](auto f, ll x) -> pair<mint,mint> {
-    if (x == 0) return { 0, 1 };
-    auto [s,p] = f(f,x/2);
-    s += s*p;
-    p *= p;
-    if (x % 2 == 1) { s = s*a+1; p *= a; }
-    return { s, p };
-  };
-
-  mint ans = f(f,x).first;
-  cout << ans.val() << endl;
+  int a; ll x;
+  cin >> a >> x >> MOD;
+  vector<vector<ll>> f = { {a,1}, {0,1} };
+  vector<vector<ll>> g = mat_pow(f,x);
+  cout << g[0][1] << endl;
 }
