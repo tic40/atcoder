5ad526a9336f49e53388cbb39376835bfdadaebe
tic40 <ccpzjoh@gmail.com>
2023-03-16 09:16:47 +0900

    Update

--- other_contest/typical90/056/main.cpp
+++ other_contest/typical90/056/main.cpp
@@ -1,8 +1,6 @@
 #include <bits/stdc++.h>
-#include <atcoder/all>
-using namespace atcoder;
 using namespace std;
-#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 
 int main() {
@@ -13,24 +11,19 @@ int main() {
   vector dp(n+1,vector<int>(s+1));
   dp[0][0] = 1;
   REP(i,n) REP(j,s+1) {
-    if (j - a[i] >= 0) dp[i+1][j] |= dp[i][j-a[i]];
-    if (j - b[i] >= 0) dp[i+1][j] |= dp[i][j-b[i]];
+    if (j-a[i] >= 0) dp[i+1][j] |= dp[i][j-a[i]];
+    if (j-b[i] >= 0) dp[i+1][j] |= dp[i][j-b[i]];
   }
 
-  if (dp[n][s] == 0) {
-    cout << "Impossible" << endl;
-    return 0;
-  }
+  if (dp[n][s] == 0) { cout << "Impossible" << endl; return 0; }
 
   int now = s;
   string ans = "";
   for(int i = n-1; i >= 0; i--) {
-    if (now-a[i] >= 0 && dp[i][now-a[i]] == 1) {
-      now -= a[i]; ans = "A" + ans;
-    } else {
-      now -= b[i]; ans = "B" + ans;
-    }
+    if (now - a[i] >= 0 && dp[i][now-a[i]]) { ans = 'A' + ans; now -= a[i]; }
+    else { ans = 'B' + ans; now -= b[i]; }
   }
+
   cout << ans << endl;
   return 0;
 }
