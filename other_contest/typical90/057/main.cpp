804c5425391613f965708d10427ea42ccfc1187b
tic40 <ccpzjoh@gmail.com>
2023-03-19 11:59:55 +0900

    Update

--- other_contest/typical90/057/main.cpp
+++ other_contest/typical90/057/main.cpp
@@ -2,13 +2,13 @@
 #include <atcoder/all>
 using namespace atcoder;
 using namespace std;
-#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 using mint = modint998244353;
 
 int main() {
   int n,m; cin >> n >> m;
-  vector a(n, vector<int>(m));
+  vector a(n,vector<int>(m));
   REP(i,n) {
     int t; cin >> t;
     REP(j,t) {
@@ -21,34 +21,32 @@ int main() {
   REP(i,m) cin >> s[i];
 
   int pos = 0;
-  REP(j,m) {
+  REP(i,m) {
     bool found = false;
-    for(int i = pos; i < n; i++) {
-      if (a[i][j]) {
-        found = true;
-        swap(a[i],a[pos]);
-        break;
-      }
+    for(int j = pos; j < n; j++) {
+      if (!a[j][i]) continue;
+      swap(a[j],a[pos]);
+      found = true;
+      break;
     }
 
     if (!found) continue;
 
-    for(int i = pos+1; i < n; i++) {
-      if (!a[i][j]) continue;
-      for(int k = j; k < m; k++) a[i][k] ^= a[pos][k];
+    for(int j = pos+1; j < n; j++) {
+      if (!a[j][i]) continue;
+      REP(k,m) a[j][k] ^= a[pos][k];
     }
-    if (s[j]) {
-      for(int k = j; k < m; k++) s[k] ^= a[pos][k];
-    }
+
+    if (s[i]) REP(j,m) s[j] ^= a[pos][j];
     pos++;
   }
 
   if (accumulate(s.begin(),s.end(),0) > 0) {
-    cout << 0 << endl;
-  } else {
-    mint ans = 1;
-    for(int i = pos; i < n; i++) ans *= 2;
-    cout << ans.val() << endl;
+    cout << 0 << endl; return 0;
   }
+
+  mint ans = 1;
+  REP(i,n-pos) ans *= 2;
+  cout << ans.val() << endl;
   return 0;
 }
