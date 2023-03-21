47916c890b95b37432b20a014f1a834b67a42f20
tic40 <ccpzjoh@gmail.com>
2023-03-21 10:21:32 +0900

    Update

--- other_contest/typical90/061/main.cpp
+++ other_contest/typical90/061/main.cpp
@@ -1,21 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 
 int main() {
   int q; cin >> q;
   deque<int> dq;
   REP(_,q) {
-    int t,x;
-    cin >> t >> x;
-    if (t == 1) {
-      dq.push_front(x);
-    } else if (t == 2) {
-      dq.push_back(x);
-    } else {
-      cout << dq.at(x-1) << endl;
-    }
+    int t,x; cin >> t >> x;
+    if (t == 1) dq.push_front(x);
+    if (t == 2) dq.push_back(x);
+    if (t == 3) { x--; cout << dq.at(x) << endl; }
   }
   return 0;
 }
