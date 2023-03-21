94326ca071e56d682ba19ac4303046ff63b572e0
tic40 <ccpzjoh@gmail.com>
2023-03-21 19:53:02 +0900

    Update

--- other_contest/typical90/063/main.cpp
+++ other_contest/typical90/063/main.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 
 int main() {
@@ -8,24 +8,24 @@ int main() {
   vector p(h,vector<int>(w));
   REP(i,h) REP(j,w) cin >> p[i][j];
 
-  int ans = 0;
-  REP(hbit,1<<h) {
+  auto f = [&](vector<int>& v) {
     map<int,int> mp;
-    vector<int> d;
-    REP(i,h) if (hbit>>i & 1) d.push_back(i);
-
     REP(j,w) {
-      int now = -1;
       bool ok = true;
-      for(int i: d) {
-        if (now == -1) now = p[i][j];
-        if (now != p[i][j]) ok = false;
-      }
-      if (ok) mp[now] += d.size();
+      int now = p[v[0]][j];
+      for(int i: v) if (now != p[i][j]) { ok = false; break; }
+      if (ok) mp[now] += v.size();
     }
-    for(auto [k,v]: mp) ans = max(ans,v);
-  }
+    return mp;
+  };
 
+  int ans = 0;
+  for(int bit = 1; bit < 1<<h; bit++) {
+    vector<int> v;
+    REP(j,h) if (bit >> j & 1) v.push_back(j);
+    auto mp = f(v);
+    for(auto [_,cnt]: mp) ans = max(ans,cnt);
+  }
   cout << ans << endl;
   return 0;
 }
