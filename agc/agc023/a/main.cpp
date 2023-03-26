7aa7662971d16ca6bdf21f71a0a7021d6c2d3e2b
tic40 <ccpzjoh@gmail.com>
2023-03-26 21:34:09 +0900

    Update

--- agc/agc023/a/main.cpp
+++ agc/agc023/a/main.cpp
@@ -1,33 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
 using ll = long long;
-const int INF = 1e9;
-const int MOD = 1e9+7;
-const ll LINF = 1e18;
-
-int n;
-vector<int> a;
-
-void solve() {
-  vector<ll> s(n+1);
-  map<ll, int> m;
-  REP(i,n) s[i+1] = s[i] + a[i];
-  REP(i,n+1) m[s[i]]++;
-
-  ll sum = 0;
-  REP(i,n+1) sum += max(0, m[s[i]]-1);
-
-  ll ans = sum/2;
-  cout << ans << endl;
-  return;
-}
 
 int main() {
-  cin >> n;
-  a.resize(n);
+  int n; cin >> n;
+  vector<int> a(n);
   REP(i,n) cin >> a[i];
+  vector<ll> s(n+1);
+  REP(i,n) s[i+1] = s[i] + a[i];
 
-  solve();
+  map<ll,int> mp;
+  REP(i,n+1) mp[s[i]]++;
+  ll ans = 0;
+  for(auto [_,cnt]: mp) ans += (ll)cnt * (cnt-1) / 2;
+  cout << ans << endl;
   return 0;
 }
