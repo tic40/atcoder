5e8130526bd7a2460c5817ce0aa99031183ee7cc
tic40 <ccpzjoh@gmail.com>
2023-03-27 22:41:59 +0900

    Update

--- other_contest/typical90/072/main.cpp
+++ other_contest/typical90/072/main.cpp
@@ -1,44 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 using P = pair<int,int>;
-const int INF = numeric_limits<int>::max();
 
-const vector<P> moves = { {1,0}, {-1,0}, {0,1}, {0,-1} };
-int h,w;
+const vector<P> d = { {0,1}, {0,-1}, {1,0}, {-1,0} };
+int h,w,si,sj;
 vector<string> c(16);
 vector dist(16,vector<int>(16));
-int gx, gy;
-int ans = -1;
 
-void dfs(int i, int j) {
-  int k = dist[i][j];
-  if (k > 0 && i == gx && j == gy) {
-    if (k >= 3) ans = max(ans,k);
-    return;
+int dfs(int i, int j) {
+  if (i == si && j == sj && dist[i][j]) {
+    return dist[i][j] >= 3 ? dist[i][j] : -1;
   }
 
-  for(auto [di,dj]: moves) {
-    int ni = i+di, nj = j+dj;
+  int res = -1;
+  for(auto [di,dj]: d) {
+    int ni = di+i;
+    int nj = dj+j;
     if (ni < 0 || nj < 0 || ni >= h || nj >= w) continue;
     if (c[ni][nj] == '#') continue;
-    if (dist[ni][nj] > 0) continue;
-    dist[ni][nj] = k+1;
-    dfs(ni,nj);
+    if (dist[ni][nj]) continue;
+    dist[ni][nj] = dist[i][j]+1;
+    res = max(res, dfs(ni,nj));
     dist[ni][nj] = 0;
   }
-  return;
+  return res;
 }
 
 int main() {
   cin >> h >> w;
   REP(i,h) cin >> c[i];
+
+  int ans = -1;
   REP(i,h) REP(j,w) {
     if (c[i][j] == '#') continue;
-    dist = vector(h,vector<int>(w,0));
-    gx = i, gy = j;
-    dfs(i,j);
+    si = i; sj = j;
+    dist = vector (h,vector<int>(w));
+    ans = max(ans, dfs(i,j));
   }
   cout << ans << endl;
   return 0;
