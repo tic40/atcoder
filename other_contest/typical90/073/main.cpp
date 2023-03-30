3529f2b9cbc4a09ae1bb11c7c508ec2f6896ba12
tic40 <ccpzjoh@gmail.com>
2023-03-30 09:56:32 +0900

    Update

--- other_contest/typical90/073/main.cpp
+++ other_contest/typical90/073/main.cpp
@@ -2,51 +2,15 @@
 #include <atcoder/all>
 using namespace atcoder;
 using namespace std;
-#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 using mint = modint1000000007;
 
-vector<char> c(1e5);
-vector<vector<int>> g(1e5);
-// dp[i][j(今の連結成分にどれがあるか)] := 頂点iの部分木を考えたとき何通りか
-// j := 0: 'a' しかない, 1: 'b' しかない, 2: 'a','b' 両方ある
-vector dp(1e5, vector<mint>(3));
-
-void dfs(int i, int p) {
-  // val1: a or b 片方だけ含む数
-  // val2: 全体の数
-  // val2 - val1 = a,b 両方含む数
-  mint val1 = 1, val2 = 1;
-
-  for(int v: g[i]) {
-    if (v == p) continue;
-    dfs(v,i);
-
-    if (c[i] == 'a') {
-      // 今いる場所が a のとき
-      // dp[v][0]: 切らない
-      // dp[v][2]: 切る
-      val1 *= (dp[v][0] + dp[v][2]);
-    } else {
-      val1 *= (dp[v][1] + dp[v][2]);
-    }
-    // 全体の数
-    // dp[v][0]: 切らない
-    // dp[v][1]: 切らない
-    // dp[v][2]*2: 切らない分と切る分
-    val2 *= (dp[v][0] + dp[v][1] + dp[v][2]*2);
-  }
-
-  if (c[i] == 'a') dp[i][0] = val1;
-  else dp[i][1] = val1;
-
-  dp[i][2] = val2 - val1;
-  return;
-}
-
 int main() {
   int n; cin >> n;
+  vector<char> c(n);
   REP(i,n) cin >> c[i];
+  vector g(n, vector<int>());
   REP(i,n-1) {
     int a,b; cin >> a >> b;
     a--; b--;
@@ -54,8 +18,32 @@ int main() {
     g[b].push_back(a);
   }
 
-  dfs(0,-1);
-  cout << dp[0][2].val() << endl;
+  // dp[i][j] := i頂点のとき
+  // jが 0: aのみ, 1: bのみ, 2: a,b両方 が何通りあるか
+  vector dp(n,vector<mint>(3));
 
+  auto dfs = [&](int i, int p, auto self) -> void {
+    // val1: a,b どちらか一方の数
+    // val2: 全体の数
+    // a,b 両方含む数: val2 - val1
+    mint val1 = 1, val2 = 1;
+    for(int v: g[i]) {
+      if (v == p) continue;
+      self(v,i,self);
+
+      // 例えば今の頂点が a のとき
+      // a だけの数は、v 頂点が aだけの場合と a,b 両方含むものを切ったとき
+      val1 *= dp[v][c[i] == 'a' ? 0 : 1] + dp[v][2];
+      // 全体の数は a だけ + b だけ + a,b 両方含む + a,b 両方含むを切ったとき
+      val2 *= dp[v][0] + dp[v][1] + dp[v][2] * 2;
+    }
+    dp[i][c[i] == 'a' ? 0 : 1] = val1;
+    dp[i][2] = val2 - val1;
+
+    return;
+  };
+
+  dfs(0,-1,dfs);
+  cout << dp[0][2].val() << endl;
   return 0;
 }
