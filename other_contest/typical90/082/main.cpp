f5d7f8296cc0f6cd4e8c76f5b0e1d1727eb3f32c
tic40 <ccpzjoh@gmail.com>
2023-04-07 12:51:30 +0900

    Update

--- other_contest/typical90/082/main.cpp
+++ other_contest/typical90/082/main.cpp
@@ -2,7 +2,7 @@
 #include <atcoder/all>
 using namespace atcoder;
 using namespace std;
-#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 using ll = unsigned long long;
 using mint = modint1000000007;
@@ -11,15 +11,14 @@ int main() {
   ll l,r; cin >> l >> r;
 
   mint ans = 0;
-  ll now = 10;
-  for(int i = 1; i <= 19; i++) {
-    ll ml = max(l,now/10);
-    ll mr = min(r,now-1);
-    if (ml <= mr) {
-      mint n = mr-ml+1;
-      ans += n*(ml+mr)/2*i;
+  ll x = 1;
+  REP(i,19) {
+    if (l < x*10 && x <= r) {
+      ll nl = max(l,x);
+      ll nr = min(r, x*10-1);
+      ans += (mint)(nl+nr)*(nr-nl+1)/2*(i+1);
     }
-    now *= 10;
+    x *= 10;
   }
   cout << ans.val() << endl;
   return 0;
