c1f96778b2671584a8d1c56c057c6f6ab5b9a6ae
tic40 <ccpzjoh@gmail.com>
2023-04-07 12:15:48 +0900

    Update

--- other_contest/typical90/081/main.cpp
+++ other_contest/typical90/081/main.cpp
@@ -1,30 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 
 int main() {
   int n,k; cin >> n >> k;
-  vector m(5001, vector<int>(5001));
+  vector s(5001,vector<int>(5001));
   REP(i,n) {
     int a,b; cin >> a >> b;
-    m[a][b]++;
+    s[a][b]++;
   }
 
-  REP(i,5000) REP(j,5000) {
-    m[i+1][j+1] += m[i+1][j];
-    m[i+1][j+1] += m[i][j+1];
-    m[i+1][j+1] -= m[i][j];
-  }
+  REP(i,5000) REP(j,5000) s[i+1][j] += s[i][j];
+  REP(i,5000) REP(j,5000) s[i][j+1] += s[i][j];
 
   int ans = 0;
-  REP(i,5001-k) REP(j,5001-k) {
-    int ri = min(5000, i+k+1);
-    int rj = min(5000, j+k+1);
-    int now = m[ri][rj];
-    now -= m[ri][j];
-    now -= m[i][rj];
-    now += m[i][j];
+  REP(i,5000) REP(j,5000) {
+    if (i+k+1 > 5000 || j+k+1 > 5000) break;
+
+    int now = s[i+k+1][j+k+1];
+    now -= s[i+k+1][j];
+    now -= s[i][j+k+1];
+    now += s[i][j];
     ans = max(ans,now);
   }
   cout << ans << endl;
