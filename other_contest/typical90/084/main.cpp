53363081f918a2284bd99f6c45bc4fc83c74f129
tic40 <ccpzjoh@gmail.com>
2023-04-17 22:06:01 +0900

    Update

--- other_contest/typical90/084/main.cpp
+++ other_contest/typical90/084/main.cpp
@@ -1,20 +1,18 @@
 #include <bits/stdc++.h>
-#include <atcoder/all>
-using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 using ll = long long;
 
 int main() {
-  int n; string s; cin >> n >> s;
+  int n; string s;
+  cin >> n >> s;
   vector<int> a(n);
   REP(i,n) a[i] = s[i] == 'o' ? 0 : 1;
   vector<int> cnt(2);
 
-  // しゃくとり法
-  int r = 0;
   ll ans = 0;
+  int r = 0;
   REP(l,n) {
     while(r < n) {
       if (cnt[0] && cnt[1]) break;
@@ -22,9 +20,10 @@ int main() {
       r++;
     }
     if (cnt[0] && cnt[1]) ans += n-r+1;
-    if (r == l) r++;
+    if (l == r) r++;
     cnt[a[l]]--;
   }
+
   cout << ans << endl;
   return 0;
 }
