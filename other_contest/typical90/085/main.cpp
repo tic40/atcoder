cb2ce436af8de62185304582cd978b52faa97473
tic40 <ccpzjoh@gmail.com>
2023-04-17 22:30:54 +0900

    Update

--- other_contest/typical90/085/main.cpp
+++ other_contest/typical90/085/main.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 using ll = long long;
 
@@ -18,17 +18,14 @@ vector<ll> divisor(ll n) {
 int main() {
   ll k; cin >> k;
   auto d = divisor(k);
+  int n = d.size();
   sort(d.begin(),d.end());
-  int sz = d.size();
-
-  ll ans = 0;
-  for(int i = 0; i < sz; i++) for(int j = i; j < sz; j++) {
-    ll a = d[i], b = d[j];
-    if ((k/a) % b == 0) {
-      ll c = (k / a) / b;
-      if (a <= b && b <= c) ans++;
-    }
+  int ans = 0;
+  REP(i,n) for(int j = i; j < n; j++) {
+    ll c = k / d[i] / d[j];
+    if (d[j] <= c && d[i]*d[j]*c == k) ans++;
   }
+
   cout << ans << endl;
   return 0;
 }
