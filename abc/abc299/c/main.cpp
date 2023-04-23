f216ed7aac18f7f1d5b64b8e2d79afd0e7f92f6c
tic40 <ccpzjoh@gmail.com>
2023-04-23 21:11:51 +0900

    Update

--- abc/abc299/c/main.cpp
+++ abc/abc299/c/main.cpp
@@ -2,30 +2,20 @@
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
-using P = pair<char,int>;
 
 int main() {
   int n; string s;
   cin >> n >> s;
 
-  vector<P> pa;
-  REP(i,n) {
-    if (i > 0 && pa.back().first == s[i]) pa.back().second++;
-    else pa.push_back({s[i],1});
+  int ans = 0, cnt = 0;
+  bool ok = false;
+  for(auto c: s) {
+    if (c == '-') { cnt = 0; ok = true; }
+    else cnt++;
+    ans = max(ans,cnt);
   }
 
-  int ans = 0, sz = pa.size();
-  auto f = [&]() {
-    REP(i,sz) {
-      auto [c,cnt] = pa[i];
-      if (c == '-' && i+1 < sz) ans = max(ans,pa[i+1].second);
-    }
-  };
-
-  f();
-  reverse(pa.begin(),pa.end());
-  f();
-
-  cout << (ans == 0 ? -1 : ans) << endl;
+  if (ans == 0 || !ok) ans = -1;
+  cout << ans << endl;
   return 0;
 }
