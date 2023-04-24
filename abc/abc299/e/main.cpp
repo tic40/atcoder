91cc1f84cefaa404f3ce2528029d488120e425c2
tic40 <ccpzjoh@gmail.com>
2023-04-24 09:07:27 +0900

    Update

--- abc/abc299/e/main.cpp
+++ abc/abc299/e/main.cpp
@@ -13,44 +13,46 @@ int main() {
     g[u].push_back(v);
     g[v].push_back(u);
   }
-
-  vector dist(n, vector<int>(n,INF));
-  REP(i,n) {
-    queue<int> q;
-    q.push(i);
-    dist[i][i] = 0;
-    while(q.size()) {
-      int now = q.front(); q.pop();
-      int nd = dist[i][now]+1;
-      for(int v: g[now]) {
-        if (dist[i][v] <= nd) continue;
-        dist[i][v] = nd;
-        q.push(v);
-      }
-    }
-  }
-
   int k; cin >> k;
   vector<int> p(k),d(k);
   REP(i,k) { cin >> p[i] >> d[i]; p[i]--; }
 
-  // 0: 白, 1: 黒
+  auto bfs = [&](int i) {
+    vector<int> dist(n,INF);
+    dist[i] = 0;
+    queue<int> q;
+    q.push(i);
+    while(q.size()) {
+      int now = q.front(); q.pop();
+      int nd = dist[now]+1;
+      for(int v: g[now]) {
+        if (dist[v] <= nd) continue;
+        dist[v] = nd;
+        q.push(v);
+      }
+    }
+    return dist;
+  };
+
+  vector dist(n,vector<int>(n));
+  REP(i,n) dist[i] = bfs(i);
+
   vector<int> col(n,1);
   REP(i,k) REP(j,n) {
-    if (dist[ p[i] ][j] < d[i]) col[j] = 0;
+    if (dist[p[i]][j] < d[i]) col[j] = 0;
   }
 
   bool ok = true;
   REP(i,k) {
-    int mind = INF;
-    REP(j,n) if (col[j] == 1) mind = min(mind, dist[ p[i] ][j]);
-    if (mind != d[i]) ok = false;
+    int min_dist = INF;
+    REP(j,n) if (col[j]) min_dist = min(min_dist,dist[p[i]][j]);
+    if (min_dist != d[i]) ok = false;
   }
 
   if (!ok) cout << "No" << endl;
   else {
     cout << "Yes" << endl;
-    REP(i,n) cout << col[i];
+    for(int v: col) cout << v;
   }
   return 0;
 }
