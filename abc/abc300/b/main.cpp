e46416a7cf3cb1f11fee5e3bc91dd40f01eb5815
tic40 <ccpzjoh@gmail.com>
2023-04-30 17:30:36 +0900

    Update

--- abc/abc300/b/main.cpp
+++ abc/abc300/b/main.cpp
@@ -5,20 +5,22 @@ using namespace std;
 
 int main() {
   int h,w; cin >> h >> w;
-  vector<string> a(h), b(h);
+  vector<string> a(h),b(h);
   REP(i,h) cin >> a[i];
   REP(i,h) cin >> b[i];
 
-  bool ok = false;
-  REP(i,h) {
-    if (a == b) ok = true;
-    REP(k,h-1) swap(a[k],a[k+1 >= h ? 0 : k+1]);
-    REP(j,w) {
-      REP(k,h) rotate(a[k].rbegin(), a[k].rbegin()+1, a[k].rend());
-      if (a == b) ok = true;
+  // s: 縦方向shift数, t: 横方向shit数
+  auto f = [&](int s, int t) {
+    REP(i,h) REP(j,w) {
+      int ni = (i+s) % h;
+      int nj = (j+t) % w;
+      if (a[ni][nj] != b[i][j]) return false;
     }
-  }
+    return true;
+  };
 
+  int ok = 0;
+  REP(i,h) REP(j,w) ok |= f(i,j);
   cout << (ok ? "Yes" : "No") << endl;
   return 0;
 }
