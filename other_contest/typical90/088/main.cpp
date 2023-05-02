e1acd8812f430c0cdb635279e3f6ea220d9d95c8
tic40 <ccpzjoh@gmail.com>
2023-05-02 22:44:41 +0900

    Update

--- other_contest/typical90/088/main.cpp
+++ other_contest/typical90/088/main.cpp
@@ -1,52 +1,50 @@
 #include <bits/stdc++.h>
-#include <atcoder/all>
-using namespace atcoder;
 using namespace std;
-#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 
-int n,q;
-bool found = false;
-vector<vector<int>> g(88);
-vector<vector<int>> memo(8889);
-vector<int> a(88), cnt(88);
-vector<int> vec;
-
-void dfs(int pos, int sum) {
-  if (found) return; // 答えが見つかっていたら打ち切る
-  if (pos == n) {
-    if (memo[sum].size()) {
-      found = true;
-      for(auto ans: { memo[sum], vec }) {
-        cout << ans.size() << endl;
-        for(auto v: ans) cout << v+1 << " ";
-        cout << endl;
-      }
-    }
-    memo[sum] = vec;
-    return;
-  }
-
-  dfs(pos+1, sum);
-  if (cnt[pos] == 0) {
-    vec.push_back(pos);
-    for(int i: g[pos]) cnt[i]++;
-    dfs(pos+1, sum+a[pos]);
-    for(int i: g[pos]) cnt[i]--;
-    vec.pop_back();
-  }
-  return;
-}
-
 int main() {
-  cin >> n >> q;
+  int n,q; cin >> n >> q;
+  vector<int> a(n);
   REP(i,n) cin >> a[i];
+  vector g(n, vector<int>());
   REP(_,q) {
     int x,y; cin >> x >> y;
     x--; y--;
-    g[x].push_back(y); // x[i] < y[i] の制約があるので双方向じゃなくて良い
+    g[x].push_back(y); // x < y のため一方向で良い
   }
 
-  dfs(0,0);
+  vector mem(8889, vector<int>());
+  vector<int> vec, cnt(n);
+
+  auto output_ans = [](vector<int> v1, vector<int> v2) {
+    for(auto vec: {v1,v2}) {
+      cout << vec.size() << endl;
+      for(auto v: vec) cout << v << " ";
+      cout << endl;
+    }
+  };
+
+  auto dfs = [&](auto self, int i, int now) {
+    if (i == n) {
+      if (mem[now].size()) {
+        output_ans(mem[now], vec);
+        exit(0);
+      }
+      mem[now] = vec;
+      return;
+    }
+
+    self(self, i+1, now);
+    if (cnt[i] == 0) {
+      vec.push_back(i+1);
+      for(int v: g[i]) cnt[v]++;
+      self(self, i+1, now+a[i]);
+      vec.pop_back();
+      for(int v: g[i]) cnt[v]--;
+    }
+  };
+
+  dfs(dfs,0,0);
   return 0;
 }
