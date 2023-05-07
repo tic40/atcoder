952a1494ac03240ccd696089cb970901c5f6de1a
tic40 <ccpzjoh@gmail.com>
2023-05-07 17:00:48 +0900

    Update

--- other_contest/typical90/011/main.cpp
+++ other_contest/typical90/011/main.cpp
@@ -1,41 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 using ll = long long;
-template<class T> void chmax(T& a, T b) { if (a < b) a = b; }
-
-struct Work {
-  int d,c,s;
-  Work(int d, int c, int s) : d(d),c(c),s(s) {}
-  bool operator<(const Work& a) const { return d < a.d; }
-};
 
 int main() {
   int n; cin >> n;
-  vector<Work> w;
+  vector<tuple<int,int,int>> t(n);
   REP(i,n) {
     int d,c,s; cin >> d >> c >> s;
-    w.emplace_back(d,c,s);
+    t[i] = { d, c, s };
   }
-  sort(w.begin(),w.end());
-  int mxd = w.back().d;
 
-  // dp[i] := i日目までで最大の報酬
-  vector<ll> dp(mxd+1, -1);
+  sort(t.begin(),t.end());
+  // dp[i] := i 日目の終わりに貰っている報酬の最大値
+  vector<ll> dp(5001,-1);
   dp[0] = 0;
   REP(i,n) {
-    vector<ll> p(mxd+1, -1);
+    vector<ll> p(5001);
+    auto [d,c,s] = t[i];
     swap(dp,p);
-    REP(j,w[i].d+1) {
-      dp[j] = p[j];
-      if (j - w[i].c < 0) continue;
-      if (p[j - w[i].c] == -1) continue;
-      chmax(dp[j], p[j-w[i].c] + w[i].s);
+    REP(j,5001) {
+      dp[j] = max(dp[j],p[j]);
+      if (p[j] != -1 && j+c <= d) dp[j+c] = max(dp[j+c],p[j] + s);
     }
   }
 
-  ll ans = *max_element(dp.begin(),dp.end());
-  cout << ans << endl;
+  cout << *max_element(dp.begin(),dp.end()) << endl;
   return 0;
 }
