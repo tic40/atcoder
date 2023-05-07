1374bfc6b6f76a93424dd221ac885fa65881afd3
tic40 <ccpzjoh@gmail.com>
2023-05-07 11:53:23 +0900

    Update

--- other_contest/typical90/010/main.cpp
+++ other_contest/typical90/010/main.cpp
@@ -5,23 +5,20 @@ using namespace std;
 
 int main() {
   int n; cin >> n;
-  vector vp(2,vector<int>(n));
+  vector a(2,vector<int>(n));
+  vector m(2,vector<int>(n+1));
   REP(i,n) {
     int c,p; cin >> c >> p;
     c--;
-    vp[c][i] = p;
-  }
-  vector m(2,vector<int>(n+1));
-  REP(i,n) {
-    m[0][i+1] = m[0][i] + vp[0][i];
-    m[1][i+1] = m[1][i] + vp[1][i];
+    a[c][i] = p;
   }
 
+  REP(i,2) REP(j,n) m[i][j+1] = m[i][j] + a[i][j];
   int q; cin >> q;
   REP(_,q) {
     int l,r; cin >> l >> r;
     l--;
-    printf("%d %d\n", m[0][r] - m[0][l], m[1][r] - m[1][l]);
+    printf("%d %d\n", m[0][r]-m[0][l], m[1][r]-m[1][l]);
   }
   return 0;
 }
