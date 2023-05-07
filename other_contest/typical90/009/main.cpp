8b008f7df5a24fe6c900fe4bc420b9ad161b0b13
tic40 <ccpzjoh@gmail.com>
2023-05-07 11:35:57 +0900

    Update

--- other_contest/typical90/009/main.cpp
+++ other_contest/typical90/009/main.cpp
@@ -11,57 +11,44 @@ struct Point {
   }
 };
 
-int n;
-vector<Point> vp;
-
-// 座標(x,y) の偏角を返す
-double getangle(double x, double y) {
-  return atan2(y,x) * 180.0 / M_PI;
-}
-
-// [偏角a]-[原点]-[偏角b] のなす角度を求める
-// 例えば a = 240°、b = 30°のとき、求める角度は 150°
-double getangle2(double a, double b) {
-  double x = abs(a-b);
-  return x >= 180 ? 360 - x : x;
-}
-
-double solve(int x) {
-  vector<double> vs;
-  REP(i,n) {
-    if (i == x) continue;
-    auto now = vp[x] - vp[i];
-    vs.push_back(getangle(now.px, now.py));
-  }
-  sort(vs.begin(),vs.end());
-
-  double res = 0;
-  int nvs = vs.size();
-  for(double v: vs) {
-    double target = v+180;
-    if (target >= 360) target -= 360;
-    int idx = lower_bound(vs.begin(),vs.end(),target) - vs.begin();
-    int idx1 = idx % nvs;
-    int idx2 = ((idx - 1) % nvs + nvs) % nvs;
-
-    res = max({
-      res,
-      getangle2(v,vs[idx1]),
-      getangle2(v,vs[idx2])
-    });
-  }
-  return res;
+double getangle(double x, double y) { return atan2(y,x) * 180.0 / M_PI; }
+double getangle2(double x, double y) {
+  double res = abs(x-y);
+  return res > 180.0 ? 360 - res : res;
 }
 
 int main() {
-  cin >> n;
+  int n; cin >> n;
+  vector<Point> vp;
   REP(i,n) {
     double x,y; cin >> x >> y;
     vp.emplace_back(x,y);
   }
 
+  auto solve = [&](int x) {
+    // vp[x] を原点に移動したときの各点の偏角
+    vector<double> va;
+    REP(i,n) {
+      if (i == x) continue;
+      auto now = vp[i] - vp[x];
+      va.push_back(getangle(now.px, now.py));
+    }
+    sort(va.begin(),va.end());
+    double res = 0;
+    int sz = n-1;
+    for(auto angle: va) {
+      double target = angle + 180;
+      if (target >= 360) angle -= 360;
+      int idx = lower_bound(va.begin(),va.end(),target) - va.begin();
+      int idx1 = idx % sz;
+      int idx2 = ((idx1 - 1) % sz + sz) % sz;
+      res = max({ res, getangle2(angle, va[idx1]), getangle2(angle, va[idx2]) });
+    }
+    return res;
+  };
+
   double ans = 0;
-  REP(i,n) ans = max(ans, solve(i));
+  REP(i,n) ans = max(ans, solve(i)); // 真ん中を決め打つ
   printf("%.10f",ans);
   return 0;
 }
