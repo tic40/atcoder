44d9714025ac85f716536590a8e808dc44741612
tic40 <ccpzjoh@gmail.com>
2023-05-09 09:49:15 +0900

    Update

--- other_contest/typical90/014/main.cpp
+++ other_contest/typical90/014/main.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 using ll = long long;
 
@@ -9,11 +9,11 @@ int main() {
   vector<int> a(n),b(n);
   REP(i,n) cin >> a[i];
   REP(i,n) cin >> b[i];
-
   sort(a.begin(),a.end());
   sort(b.begin(),b.end());
+
   ll ans = 0;
-  REP(i,n) ans += abs(a[i] - b[i]);
+  REP(i,n) ans += abs(b[i]-a[i]);
   cout << ans << endl;
   return 0;
 }
