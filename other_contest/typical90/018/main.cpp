d205c7c9c98c7746ea20ca11cb8e8b2c29836e61
tic40 <ccpzjoh@gmail.com>
2023-05-13 11:12:27 +0900

    Update

--- other_contest/typical90/018/main.cpp
+++ other_contest/typical90/018/main.cpp
@@ -8,17 +8,16 @@ int main() {
   int q; cin >> q;
   REP(_,q) {
     double e; cin >> e;
-
     // 現在の観覧車の角度
     double deg = 360.0 * e / t;
     double rad = deg * M_PI / 180.0;
-
+    // 観覧車の座標 y,z
     double ny = -l / 2.0 * sin(rad);
     double nz = l / 2.0 - l / 2.0 * cos(rad);
-
-    // 高橋像までの水平距離
+    // 高橋像までの水平、垂直距離
     double a = sqrt(x*x + (ny-y)*(ny-y));
     double b = nz;
+    // 俯角を求める
     double ans = atan2(b,a) * 180.0 / M_PI;
     printf("%.10lf\n", ans);
   }
