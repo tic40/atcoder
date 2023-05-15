26c66872d4c19b5473468921d2ee5e05ef7895dd
tic40 <ccpzjoh@gmail.com>
2023-05-15 22:02:42 +0900

    Update

--- other_contest/typical90/022/main.cpp
+++ other_contest/typical90/022/main.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 using ll = long long;
 
 int main() {
   ll a,b,c; cin >> a >> b >> c;
-  ll x = gcd(a,gcd(b,c));
-  ll ans = (a+b+c)/x - 3;
+  ll d = gcd(gcd(a,b),c);
+  ll ans = (a+b+c) / d - 3LL;
   cout << ans << endl;
   return 0;
 }
