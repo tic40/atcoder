88c0d4ea08fa8554d327ea48d80d5c9e2ff90f77
tic40 <ccpzjoh@gmail.com>
2023-05-19 09:44:20 +0900

    Update

--- other_contest/typical90/033/main.cpp
+++ other_contest/typical90/033/main.cpp
@@ -3,11 +3,9 @@ using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 
 int main() {
-  int h,w;
-  cin >> h >> w;
-
+  int h,w; cin >> h >> w;
   // コーナーケース
-  if (h==1 || w==1) cout << h*w << endl;
+  if (h == 1 || w == 1) cout << h*w << endl;
   else cout << ((h+1)/2) * ((w+1)/2) << endl;
   return 0;
 }
