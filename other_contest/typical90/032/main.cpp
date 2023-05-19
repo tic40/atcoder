cd03342f8ef2809e49cecf18ec278485fd4323fd
tic40 <ccpzjoh@gmail.com>
2023-05-19 09:28:23 +0900

    Update

--- other_contest/typical90/032/main.cpp
+++ other_contest/typical90/032/main.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 const int INF = numeric_limits<int>::max();
 
@@ -9,25 +9,28 @@ int main() {
   vector a(n,vector<int>(n));
   REP(i,n) REP(j,n) cin >> a[i][j];
   int m; cin >> m;
-  vector g(n,vector<int>(n));
-  REP(i,m) {
+  vector g(n,set<int>());
+  REP(_,m) {
     int x,y; cin >> x >> y;
     x--; y--;
-    g[x][y] = g[y][x] = true;
+    g[x].insert(y);
+    g[y].insert(x);
   }
 
-  int ans = INF;
-  vector<int> b(n);
-  iota(b.begin(),b.end(), 0);
-  do {
-    int now = a[b[0]][0];
-    for(int i = 1; i < n; i++) {
-      if (g[b[i-1]][b[i]]) { now = INF; break; }
-      now += a[b[i]][i];
-    }
-    ans = min(ans,now);
-  } while (next_permutation(b.begin(), b.end()));
+  auto dfs = [&](auto self, int p, int total, int s) {
+    int now = __builtin_popcount(s);
+    if (now == n) return total;
 
+    int ans = INF;
+    REP(i,n) {
+      if ((s >> i & 1)) continue;
+      if (g[i].count(p)) continue;
+      ans = min(ans, self(self,i,total+a[i][now], (s|1<<i)));
+    }
+    return ans;
+  };
+
+  int ans = dfs(dfs,-1,0,0);
   cout << (ans == INF ? -1 : ans) << endl;
   return 0;
 }
