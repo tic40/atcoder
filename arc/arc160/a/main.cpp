8e002b1128f64a7b3cacd4f5a112e66bb66669b5
tic40 <ccpzjoh@gmail.com>
2023-05-25 08:34:38 +0900

    Update

--- arc/arc160/a/main.cpp
+++ arc/arc160/a/main.cpp
@@ -5,33 +5,34 @@ using namespace std;
 
 int main() {
   int n,k; cin >> n >> k;
+  k--;
   vector<int> a(n);
   REP(i,n) cin >> a[i];
 
-  int t = 1, b = n*(n+1)/2;
+  int snum = 0, lnum = 0;
+  int tot = n*(n+1)/2 - 1;
   REP(i,n) {
-    vector<int> small, large;
+    vector<int> small,large;
     for(int j = i+1; j < n; j++) {
       a[j] < a[i] ? small.push_back(a[j]) : large.push_back(a[j]);
     }
     int x = -1;
-    if (k - t < int(small.size())) {
+    if (k-snum < int(small.size())) {
       sort(small.begin(),small.end());
-      x = small[k-t];
+      x = small[k-snum];
     }
-    if (b - k < int(large.size())) {
-      sort(large.begin(),large.end());
-      reverse(large.begin(),large.end());
-      x = large[b-k];
+    if (tot-k-lnum < int(large.size())) {
+      sort(large.rbegin(),large.rend());
+      x = large[tot-lnum-k];
     }
     if (x != -1) {
-      int j = i;
-      while (a[j] != x) j++;
-      reverse(a.begin()+i, a.begin()+j+1);
+      int l = i, r = i;
+      while (a[r] != x) r++;
+      reverse(a.begin()+l, a.begin()+r+1);
       break;
     }
-    t += small.size();
-    b -= large.size();
+    snum += small.size();
+    lnum += large.size();
   }
   REP(i,n) cout << a[i] << " ";
   return 0;
