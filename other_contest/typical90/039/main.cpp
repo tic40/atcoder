5f663e5083a10db13b3be4a29d236e7c97eeb72f
tic40 <ccpzjoh@gmail.com>
2023-05-28 16:07:58 +0900

    Update

--- other_contest/typical90/039/main.cpp
+++ other_contest/typical90/039/main.cpp
@@ -1,39 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 using ll = long long;
 
-vector<vector<int>> g(1e5);
-vector<int> dp(1e5);
-
-int dfs(int i, int p) {
-  int now = 1;
-  for(int v: g[i]) {
-    if (v == p) continue;
-    now += dfs(v,i);
-  }
-  return dp[i] = now;
-}
-
 int main() {
   int n; cin >> n;
-  vector<int> a(n),b(n);
+  vector g(n,vector<int>());
   REP(i,n-1) {
-    cin >> a[i] >> b[i];
-    a[i]--; b[i]--;
-    g[a[i]].push_back(b[i]);
-    g[b[i]].push_back(a[i]);
+    int u,v; cin >> u >> v;
+    u--; v--;
+    g[u].push_back(v);
+    g[v].push_back(u);
   }
 
-  dfs(0,-1);
-  ll ans = 0;
-  // 辺ごとに通る数を合算する
-  REP(i,n-1) {
-    // 貢献度が小さい方が部下にあたる頂点
-    ll r = min(dp[a[i]], dp[b[i]]);
-    ans += r * (n-r);
-  }
-  cout << ans << endl;
+  // sz[i] := 頂点 i が配下に持つ頂点の個数
+  vector<ll> sz(n);
+  auto dfs = [&](auto self, int i, int p) -> ll {
+    ll ans = 0;
+    int now = 1;
+    for(auto v: g[i]) {
+      if (v == p) continue;
+      ans += self(self,v,i);
+      now += sz[v];
+      ans += sz[v] * (n-sz[v]);
+    }
+    sz[i] = now;
+    return ans;
+  };
+
+  cout << dfs(dfs,0,-1) << endl;
   return 0;
 }
