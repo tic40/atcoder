5c8164ef143b2d6f72a17ff04a0a455add7b3170
tic40 <ccpzjoh@gmail.com>
2023-05-28 21:23:55 +0900

    Update

--- other_contest/typical90/045/main.cpp
+++ other_contest/typical90/045/main.cpp
@@ -4,6 +4,8 @@ using namespace std;
 #define endl '\n'
 using ll = long long;
 const ll LINF = numeric_limits<ll>::max();
+template<class T> void chmax(T& a, T b) { a = max(a,b); }
+template<class T> void chmin(T& a, T b) { a = min(a,b); }
 
 int main() {
   int n,k; cin >> n >> k;
@@ -11,29 +13,27 @@ int main() {
   REP(i,n) cin >> x[i] >> y[i];
 
   vector dist(n,vector<ll>(n));
-  REP(i,n) REP(j,n) dist[i][j] = powl(x[i]-x[j],2) + powl(y[i]-y[j],2);
+  REP(i,n) REP(j,n) dist[i][j] = powl(x[i]-x[j],2) + powl(y[i] - y[j],2);
 
   // s[bit] := 集合がbitのときの2点間の最大値
   vector<ll> s(1<<n);
   REP(bit,1<<n) REP(i,n) REP(j,n) {
-    if (bit >> i & 1 && bit >> j & 1) {
-      s[bit] = max(s[bit], dist[i][j]);
-    }
+    if ((bit>>i&1) && (bit>>j&1)) chmax(s[bit], dist[i][j]);
   }
 
   // dp[i][j]: [何個のグループ数に分けたときか][すでに選んだ点の集合(bit)]
-  vector<ll> dp(1<<n, LINF);
+  vector<ll> dp(1<<n,LINF);
   dp[0] = 0;
   REP(i,k) {
-    vector<ll> p(1<<n, LINF);
+    vector<ll> p(1<<n,LINF);
     swap(dp,p);
-    REP(bit,1<<n) {
-      for(int j = bit; j > 0; j = (j-1) & bit) {
-        dp[bit] = min(dp[bit], max(p[bit-j], s[j]));
+    REP(bit, 1<<n) {
+      // bit の 部分集合
+      for(int pbit = bit; pbit > 0; pbit = (pbit-1) & bit) {
+        chmin(dp[bit], max(p[bit - pbit], s[pbit]));
       }
     }
   }
-
   cout << dp[(1<<n)-1] << endl;
   return 0;
 }
