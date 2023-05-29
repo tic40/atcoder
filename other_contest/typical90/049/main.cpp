084b47616c50977fb42b6a19613603abef9819e0
tic40 <ccpzjoh@gmail.com>
2023-05-29 22:13:08 +0900

    Update

--- other_contest/typical90/049/main.cpp
+++ other_contest/typical90/049/main.cpp
@@ -2,37 +2,35 @@
 #include <atcoder/all>
 using namespace atcoder;
 using namespace std;
-#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
 using ll = long long;
 
 struct Edge {
-  int c; int l; int r;
+  int c,l,r;
   bool operator<(const Edge& o) const { return c < o.c; }
+  Edge(int c, int l, int r): c(c),l(l),r(r) {}
 };
 
 int main() {
   int n,m; cin >> n >> m;
-  vector<Edge> edges(m);
+  vector<Edge> es;
   REP(i,m) {
     int c,l,r; cin >> c >> l >> r;
     l--; r--;
-    edges[i] = {c,l,r};
+    es.emplace_back(c,l,r);
   }
 
-
-  ll ans = 0;
-  dsu uf(n+1); // N+1頂点
-
   // クラスカル
-  // costの昇順にソート
-  sort(edges.begin(), edges.end());
-  for(auto [c,l,r]: edges) {
+  sort(es.begin(),es.end());
+  dsu uf(n+1);
+  ll ans = 0;
+  for(auto [c,l,r]: es) {
     if (uf.same(l,r+1)) continue;
     uf.merge(l,r+1);
     ans += c;
   }
 
-  if (uf.size(0) != n+1) ans = -1;
-  cout << ans << endl;
+  cout << (uf.size(0) != n+1 ? -1 : ans) << endl;
   return 0;
 }
