09cf09d3b25329972b414c98bf1b85c0052e2e13
tic40 <ccpzjoh@gmail.com>
2023-05-31 09:09:58 +0900

    Update

--- other_contest/typical90/054/main.cpp
+++ other_contest/typical90/054/main.cpp
@@ -6,32 +6,33 @@ const int INF = numeric_limits<int>::max();
 
 int main() {
   int n,m; cin >> n >> m;
-  vector<vector<int>> g(n+m);
+  vector g(n+m,vector<int>());
   REP(i,m) {
     int k; cin >> k;
     REP(j,k) {
-      int x; cin >> x;
-      x--;
-      g[x].push_back(n+i);
-      g[n+i].push_back(x);
+      int r; cin >> r; r--;
+      g[n+i].push_back(r);
+      g[r].push_back(n+i);
     }
   }
 
-  vector<int> dist(n+m,INF);
-  queue<int> q;
-  auto push = [&](int i, int nd) {
-    if (dist[i] <= nd) return;
-    q.push(i);
-    dist[i] = nd;
+  auto bfs = [&]() {
+    vector<int> dist(n+m,INF);
+    queue<int> q;
+    q.push(0);
+    dist[0] = 0;
+    while(q.size()) {
+      int v = q.front(); q.pop();
+      for(auto nv: g[v]) {
+        if (dist[nv] != INF) continue;
+        q.push(nv);
+        dist[nv] = dist[v]+1;
+      }
+    }
+    return dist;
   };
 
-  push(0,0);
-  while(q.size()) {
-    int now = q.front(); q.pop();
-    int nd = dist[now]+1;
-    for(auto v: g[now]) push(v,nd);
-  }
-
+  auto dist = bfs();
   REP(i,n) cout << (dist[i] == INF ? -1 : dist[i]/2) << endl;
   return 0;
 }
