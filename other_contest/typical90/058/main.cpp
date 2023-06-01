75148582f30d0ff9c6cf0b3692b3f5d4a3442bbf
tic40 <ccpzjoh@gmail.com>
2023-06-02 08:27:17 +0900

    Update

--- other_contest/typical90/058/main.cpp
+++ other_contest/typical90/058/main.cpp
@@ -2,32 +2,27 @@
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
-using ll = long long;
 const int MOD = 1e5;
+using ll = long long;
 
 int main() {
-  int n; ll k;
-  cin >> n >> k;
+  int n; ll k; cin >> n >> k;
 
-  int logK = 1;
-  while ((1LL << logK) <= k) logK++;
-
-  vector doubling(logK, vector<int>(1e5));
+  // doubling
+  vector dp(61, vector<int>(1e5));
   REP(i,1e5) {
     string s = to_string(i);
     int sum = 0;
-    for(char c: s) sum += c-'0';
-    doubling[0][i] = (i + sum) % MOD;
+    for(auto c: s) sum += (c - '0');
+    dp[0][i] = (i+sum) % MOD;
   }
 
-  REP(i,logK-1) REP(j,1e5) {
-    doubling[i+1][j] = doubling[i][doubling[i][j]];
+  REP(i,60) REP(j,1e5) {
+    dp[i+1][j] = dp[i][dp[i][j]];
   }
 
-  int ans = n;
-  REP(i,logK) {
-    if ((k >> i) & 1) ans = doubling[i][ans];
-  }
-  cout << ans << endl;
+  int now = n;
+  REP(i,61) if ((k >> i) & 1) now = dp[i][now];
+  cout << now << endl;
   return 0;
 }
