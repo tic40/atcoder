810eead0165d75740f2e26e025ee625e61a071bf
tic40 <ccpzjoh@gmail.com>
2023-06-13 10:22:46 +0900

    Update

--- abc/abc304/f/main.cpp
+++ abc/abc304/f/main.cpp
@@ -9,28 +9,27 @@ using mint = modint998244353;
 int main() {
   int n; string s; cin >> n >> s;
 
-  mint ans;
-  // b[i] := 周期 i の総数
+  mint ans = 0;
+
   vector<mint> b(n+1);
-  // ds[i] := 重複分の周期が i になる j の集合
   vector ds(n+1,vector<int>());
+  // m は n でない n の約数。よって m < n かつ n % m == 0
   for(int m = 1; m < n; m++) {
     if (n%m) continue;
     mint a = 1;
     REP(i,m) {
       int x = 2;
-      for(int j = i; j < n; j+=m) if (s[j] == '.') x = 1;
+      for(int j = i; j < n; j+=m) {
+        if(s[j] == '.') x = 1;
+      }
       a *= x;
     }
-
-    for(int d: ds[m]) a -= b[d]; // 重複分を取り除く
+    for(int d: ds[m]) a -= b[d];
     ans += a;
     b[m] = a;
 
-    // 重複分を記録する
     for(int i = m*2; i < n; i+=m) ds[i].push_back(m);
   }
-
   cout << ans.val() << endl;
   return 0;
 }
