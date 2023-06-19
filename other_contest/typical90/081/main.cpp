92b266da16b245a278fdb5afda9e75175594d362
tic40 <ccpzjoh@gmail.com>
2023-06-19 23:53:58 +0900

    Update

--- other_contest/typical90/081/main.cpp
+++ other_contest/typical90/081/main.cpp
@@ -6,22 +6,18 @@ using namespace std;
 int main() {
   int n,k; cin >> n >> k;
   vector s(5001,vector<int>(5001));
-  REP(i,n) {
-    int a,b; cin >> a >> b;
-    s[a][b]++;
-  }
-
+  REP(i,n) { int a,b; cin >> a >> b; s[a][b]++; }
   REP(i,5000) REP(j,5000) s[i+1][j] += s[i][j];
   REP(i,5000) REP(j,5000) s[i][j+1] += s[i][j];
 
   int ans = 0;
   REP(i,5000) REP(j,5000) {
-    if (i+k+1 > 5000 || j+k+1 > 5000) break;
-
-    int now = s[i+k+1][j+k+1];
-    now -= s[i+k+1][j];
-    now -= s[i][j+k+1];
-    now += s[i][j];
+    int ri = min(5000,i+k+1);
+    int rj = min(5000,j+k+1);
+    int now = s[i][j];
+    now -= s[ri][j];
+    now -= s[i][rj];
+    now += s[ri][rj];
     ans = max(ans,now);
   }
   cout << ans << endl;
