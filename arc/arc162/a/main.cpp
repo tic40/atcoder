3e46a5ebf24490e755435f1d4c810211796c1ceb
tic40 <ccpzjoh@gmail.com>
2023-06-27 00:36:08 +0900

    Solve a

---
+++ arc/arc162/a/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
+const int INF = numeric_limits<int>::max();
+
+int main() {
+  int t; cin >> t;
+  REP(_,t) {
+    int n; cin >> n;
+    vector<int> p(n);
+    REP(i,n) cin >> p[i];
+    int mn = INF, ans = 0;
+    for(int i = n; i--;) {
+      if (p[i] <= mn) { ans++; mn = p[i]; }
+    }
+    cout << ans << endl;
+  }
+  return 0;
+}
