029f7674651f901c95bc72bb04faacbcd821ad10
tic40 <ccpzjoh@gmail.com>
2023-06-30 09:47:20 +0900

    Update

--- other_contest/typical90/003/main.cpp
+++ other_contest/typical90/003/main.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<(n);i++)
-const int INF = numeric_limits<int>::max();
+#define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 
 int main() {
@@ -14,26 +13,19 @@ int main() {
     g[b].push_back(a);
   }
 
-  auto bfs = [&](int i) {
-    vector<int> dist(n,INF);
-    queue<int> q;
-    q.push(i);
-    dist[i] = 0;
-    while(q.size()) {
-      int now = q.front(); q.pop();
-      for(int v: g[now]) {
-        if (dist[v] != INF) continue;
-        dist[v] = dist[now]+1;
-        q.push(v);
-      }
+  vector<int> dist(n);
+  auto dfs = [&](auto self, int i, int p) -> void {
+    for(auto v: g[i]) {
+      if (v == p) continue;
+      dist[v] = dist[i]+1;
+      self(self,v,i);
     }
-    return dist;
   };
-
-  auto dist1 = bfs(0);
-  int idx = max_element(dist1.begin(),dist1.end()) - dist1.begin();
-  auto dist2 = bfs(idx);
-  int ans = *max_element(dist2.begin(),dist2.end()) + 1;
+  dfs(dfs,0,-1);
+  auto idx = max_element(dist.begin(),dist.end()) - dist.begin();
+  dist = vector<int>(n);
+  dfs(dfs,idx,-1);
+  int ans = *max_element(dist.begin(),dist.end()) + 1;
   cout << ans << endl;
 
   return 0;
