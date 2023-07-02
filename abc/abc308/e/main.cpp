6943d2f5f7cdbd91581d8d3328baf84571628775
tic40 <ccpzjoh@gmail.com>
2023-07-02 09:10:46 +0900

    Update

--- abc/abc308/e/main.cpp
+++ abc/abc308/e/main.cpp
@@ -11,18 +11,21 @@ int main() {
   REP(i,n) cin >> a[i];
   string s; cin >> s;
 
+  auto mex = [](int x, int y, int z) {
+    vector<bool> mex(3);
+    mex[x] = mex[y] = mex[z] = true;
+    REP(i,3) if (!mex[i]) return i;
+    return 3;
+  };
+
   vector<int> m(3);
-  map<P,ll> e;
+  vector e(3,vector<ll>(3));
   ll ans = 0;
   REP(i,n) {
     if (s[i] == 'M') m[a[i]]++;
-    if (s[i] == 'E') REP(j,3) e[{j,a[i]}] += m[j];
+    if (s[i] == 'E') REP(j,3) e[j][a[i]] += m[j];
     if (s[i] == 'X') {
-      for(auto [k,v]: e) {
-        vector<bool> mex(4);
-        mex[k.first] = mex[k.second] = mex[a[i]] = true;
-        REP(j,4) if (!mex[j]) { ans += (ll)j * v; break; }
-      }
+      REP(j,3) REP(k,3) ans += mex(j,k,a[i]) * e[j][k];
     }
   }
   cout << ans << endl;
