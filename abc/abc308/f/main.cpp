97d8d4e06ecb81a39f4567a2314f7dfbae365957
tic40 <ccpzjoh@gmail.com>
2023-07-02 09:14:17 +0900

    Update

--- abc/abc308/f/main.cpp
+++ abc/abc308/f/main.cpp
@@ -3,20 +3,19 @@ using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 using ll = long long;
-using P = pair<ll,ll>;
+using P = pair<int,int>;
 
 int main() {
   int n,m; cin >> n >> m;
-  vector<ll> p(n),l(m),d(m);
-  REP(i,n) cin >> p[i];
+  vector<int> p(n),l(m),d(m);
+  multiset<int> st;
+  REP(i,n) { cin >> p[i]; st.insert(p[i]); }
   REP(i,m) cin >> l[i];
   REP(i,m) cin >> d[i];
 
   vector<P> pa(m);
   REP(i,m) pa[i] = {d[i],l[i]};
   sort(pa.rbegin(),pa.rend());
-  multiset<ll> st;
-  REP(i,n) st.insert(p[i]);
 
   ll ans = accumulate(p.begin(),p.end(),0LL);
   REP(i,m) {
@@ -26,7 +25,6 @@ int main() {
     ans -= nd;
     st.erase(it);
   }
-
   cout << ans << endl;
   return 0;
 }
