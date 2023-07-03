d3daaeea2d6da51196baed8e374d3caca3567bbd
tic40 <ccpzjoh@gmail.com>
2023-07-03 20:54:31 +0900

    Solve

--- other_contest/typical90/005/main.cpp
+++ other_contest/typical90/005/main.cpp
@@ -1,10 +1,12 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace atcoder;
 using namespace std;
-#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 using ll = long long;
-const int MOD = 1e9+7;
-const int LOG = 62; // Nの対数
+using mint = modint1000000007;
+using mintb = modint;
 
 int main() {
   ll n; int b,k;
@@ -12,35 +14,33 @@ int main() {
   vector<int> c(k);
   REP(i,k) cin >> c[i];
 
-  // dp[i] と dp[j] を掛け合わせて dp[i+j] を得る処理
-  // tj: 10^j を b で割ったあまり
-  auto mul = [&](const vector<ll>& dpi, const vector<ll>& dpj, ll tj) {
-    vector<ll> res(b);
-    REP(p,b) REP(q,b) {
-      int now = (p * tj + q) % b;
-      res[now] += dpi[p] * dpj[q];
-      res[now] %= MOD;
+  mintb::set_mod(b);
+  vector<mintb> power10(64);
+  vector dp(64,vector<mint>(1009));
+  vector ans(64,vector<mint>(1009));
+
+  REP(i,63) power10[i] = (mintb(10)).pow(1LL << i);
+
+  // dp[0][i] を求める
+  REP(i,k) dp[0][c[i] % b]++;
+
+  REP(i,62) REP(j,b) REP(k,b) {
+    int nex = (ll)(j * power10[i].val() + k) % b;
+    dp[i+1][nex] += dp[i][j] * dp[i][k];
+  }
+
+  ans[0][0] = 1;
+  REP(i,62) {
+    if ((n & (1LL << i)) != 0LL) {
+      REP(j,b) REP(k,b) {
+        int nex = (ll)(j * power10[i].val() + k) % b;
+        ans[i+1][nex] += ans[i][j] * dp[i][k];
+      }
+    } else {
+      REP(j,b) ans[i+1][j] = ans[i][j];
     }
-    return res;
-  };
+  }
 
-  vector<ll> ten(LOG, 10); // ten[i]: 10^(2^i) を b で割ったあまり
-  for (int i = 1; i < LOG; ++i) ten[i] = (ten[i-1] * ten[i-1])%b;
-
-  // dp[2^i][r] を doubl[i][r] で書くことにする
-  vector doubl(LOG, vector<ll>(b));
-  // 初期化 (doubl[0] := dp[1])
-  REP(i,k) doubl[0][c[i] % b] += 1;
-
-  // ダブリング
-  for (int i = 1; i < LOG; ++i) doubl[i] = mul(doubl[i-1],doubl[i-1],ten[i-1]);
-
-  // ダブリングした結果をもとに答えを求める
-  vector<ll> ans(b);
-  ans[0] = 1;
-  // n を二の冪乗の積で表すときに、2^i を含むかどうか
-  REP(i,LOG) if (n & (1LL << i)) ans = mul(ans, doubl[i], ten[i]);
-
-  cout << ans[0] << endl;
+  cout << ans[62][0].val() << endl;
   return 0;
 }
