7e97616636956c5761097d6372c3eb166b1c39fe
tic40 <ccpzjoh@gmail.com>
2023-07-03 21:49:00 +0900

    Update

--- other_contest/typical90/005/main.cpp
+++ other_contest/typical90/005/main.cpp
@@ -9,26 +9,26 @@ using mint = modint1000000007;
 using mintb = modint;
 
 int main() {
-  ll n; int b,k;
-  cin >> n >> b >> k;
+  ll n; int b,k; cin >> n >> b >> k;
   vector<int> c(k);
   REP(i,k) cin >> c[i];
 
   mintb::set_mod(b);
   vector<mintb> power10(64);
-  vector dp(64,vector<mint>(1009));
-  vector ans(64,vector<mint>(1009));
+  vector dp(64,vector<mint>(1009)), ans(64,vector<mint>(1009));
 
-  // 前計算
+  // 前計算: 10^(1<<i) mod を求めておく
   REP(i,63) power10[i] = (mintb(10)).pow(1LL << i);
   // dp[0][i] を求める
-  REP(i,k) dp[0][c[i] % b]++;
+  REP(i,k) dp[0][c[i]%b]++;
 
+  // dp[1][i], dp[2][i], ..., dp[2^n][i] を求める
   REP(i,62) REP(j,b) REP(k,b) {
     int nex = (ll)(j * power10[i].val() + k) % b;
     dp[i+1][nex] += dp[i][j] * dp[i][k];
   }
 
+  // 繰り返し二乗法により dp[N][i] を求める
   ans[0][0] = 1;
   REP(i,62) {
     if ((n & (1LL << i)) != 0LL) {
