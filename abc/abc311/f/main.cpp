5d9453f2d30efa1ae7efdc8f0ee8c55fd760640c
tic40 <ccpzjoh@gmail.com>
2023-07-31 22:12:01 +0900

    Update

--- abc/abc311/f/main.cpp
+++ abc/abc311/f/main.cpp
@@ -18,18 +18,39 @@ int main() {
     if (i+1 < n && j+1 < m) s[i+1][j+1] = '#';
   }
 
-  // dp[i][j] = i 行目まで塗り方を決めて、
-  // 美しいグリッドでありマス (i,j) を白く、マス (i,j+1) を黒く塗るようなものの個数
-  vector dp(n+2,vector<mint>(m+1));
-  dp[0][m] = 1;
-
-  for(int j = m; j >= 0; j--) REP(i,n+2) {
-    if (i+1 < n+2) dp[i+1][j] += dp[i][j];
-    if (i-1 >= 0 && j-1 >= 0) {
-      if (i == 1 || s[i-2][j-1] == '.') dp[i-1][j-1] += dp[i][j];
+  // 解説放送解 https://www.youtube.com/watch?v=OA4q9-u8ecg
+  auto solve1 = [&]() {
+    vector dp(n+2,vector<mint>(m+1));
+    dp[0][m]=1;
+    for(int j = m; j >= 0; j--) REP(i,n+2) {
+      if (i+1 < n+2) dp[i+1][j] += dp[i][j];
+      if (i-1 >= 0 && j-1 >= 0) {
+        if (i == 1 || s[i-2][j-1] == '.') dp[i-1][j-1] += dp[i][j];
+      }
     }
-  }
+    cout << dp[n+1][0].val() << endl;
+  };
 
-  cout << dp[n+1][0].val() << endl;
+  // 解 https://atcoder.jp/contests/abc311/editorial/6822
+  auto solve2 = [&]() {
+    vector<mint> dp(m+1);
+    dp[m]=1;
+    for(int c = -m+1; c <= n-1; c++) {
+      for(int j = m-1; j >= 0; j--) {
+        dp[j]+=dp[j+1];
+      }
+      REP(j,m) {
+        int i = j+c;
+        if (i < 0) dp[j] = 0;
+        else if (i >= n || s[i][j] == '#') dp[j+1] = 0;
+      }
+    }
+    mint ans = 0;
+    for(auto v: dp) ans += v;
+    cout << ans.val() << endl;
+  };
+
+  // solve1();
+  solve2();
   return 0;
 }
