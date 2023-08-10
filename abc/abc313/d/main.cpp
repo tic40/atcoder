26f98293ad57f8da931513d8ec0d2013bc53588f
tic40 <ccpzjoh@gmail.com>
2023-08-10 10:06:25 +0900

    Update

--- abc/abc313/d/main.cpp
+++ abc/abc313/d/main.cpp
@@ -5,40 +5,39 @@ using namespace std;
 
 int main() {
   int n,k; cin >> n >> k;
-  vector<int> a(n);
+  vector<int> ans(n);
 
-  auto f = [&](vector<int> x) {
-    REP(i,k) x[i]++;
+  auto ask = [&](vector<int> &x) {
     cout << "?";
-    REP(i,k) cout << " " << x[i];
+    REP(i,k) cout << " " << x[i]+1;
     cout << endl;
-    int res;
-    cin >> res;
+    int res; cin >> res;
     return res;
   };
 
-  vector<int> b(k+1);
-  int t = 0;
+  vector<int> b(k+1); // b[i] := i を含まない k 個の xor 和の値
+  int t = 0; // k+1 個それぞれの xor 和を合計した値
   REP(i,k+1) {
     vector<int> x;
     REP(j,k+1) if (i != j) x.push_back(j);
-    b[i] = f(x);
-    // t += b[i]; t %= 2;
+    b[i] = ask(x);
+    // ここは t += b[i]; t %= 2; でもいいが、mod 2 なので xor を取っても同じ
     t ^= b[i];
   }
-  REP(i,k+1) a[i] = b[i]^t;
+  REP(i,k+1) ans[i] = b[i]^t;
 
-  t = 0;
-  REP(i,k-1) t ^= a[i];
+  t = 0; // ans[0],ans[1]... ans[k-2] の xor 和
+  REP(i,k-1) t ^= ans[i];
+  vector<int> x;
+  REP(j,k-1) x.push_back(j);
   for(int i = k+1; i < n; i++) {
-    vector<int> x;
-    REP(j,k-1) x.push_back(j);
     x.push_back(i);
-    a[i] = t ^ f(x);
+    ans[i] = t ^ ask(x);
+    x.pop_back();
   }
 
   cout << "!";
-  REP(i,n) cout << " " << a[i];
+  for(auto v: ans) cout << " " << v;
   cout << endl;
   return 0;
 }
