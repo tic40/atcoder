ea1352e50a74744d58f107a66a5cfb81aee2ab75
tic40 <ccpzjoh@gmail.com>
2023-08-15 23:06:01 +0900

    Update

--- abc/abc275/e/main.cpp
+++ abc/abc275/e/main.cpp
@@ -2,31 +2,26 @@
 #include <atcoder/all>
 using namespace atcoder;
 using namespace std;
-#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 using mint = modint998244353;
 
 int main() {
   int n,m,k; cin >> n >> m >> k;
+  mint p = mint(1) / m;
 
-  mint m_inv = mint(1) / m;
-  // dp[i][j] := i回振ってjへたどり着ける確率
-  vector<mint> dp(n+1);
-  dp[0] = 1;
+  vector dp(k+1,vector<mint>(n+1));
+  dp[0][0] = 1;
   REP(i,k) {
-    vector<mint> p(n+1);
-    swap(dp,p);
-    dp[n] = p[n];
-    for(int j = 1; j <= m; j++) {
-      REP(l,n) {
-        if (p[l] == 0) continue;
-        int nl = l+j;
-        if (nl > n) nl = n-(nl-n);
-        dp[nl] += p[l] * m_inv;
+    REP(j,n) {
+      if (dp[i][j] == 0) continue;
+      for(int l = 1; l <= m; l++) {
+        int nj = j+l <= n ? j+l : n-(j+l-n);
+        dp[i+1][nj] += dp[i][j] * p;
       }
     }
+    dp[i+1][n] += dp[i][n];
   }
-
-  cout << dp[n].val() << endl;
+  cout << dp[k][n].val() << endl;
   return 0;
 }
