d336704a8bda06a64ca675e9a6298a6e1ae03387
tic40 <ccpzjoh@gmail.com>
2023-08-15 22:38:42 +0900

    Update

--- abc/abc280/e/main.cpp
+++ abc/abc280/e/main.cpp
@@ -2,22 +2,26 @@
 #include <atcoder/all>
 using namespace atcoder;
 using namespace std;
-#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 using mint = modint998244353;
 
 int main() {
-  int n, p; cin >> n >> p;
-  mint x1 = (mint)p / 100;
-  mint x2 = (mint)1 - x1;
+  int n, _p;
+  cin >> n >> _p;
+  mint p = (mint)_p / 100;
+  mint q = (mint)1 - p;
 
-  // dp[i] := 体力がi以下になるときの期待値
-  vector<mint> dp(n+2);
-  for(int i = n-1; i >= 0; i--) {
-    dp[i] += (dp[i+2] + 1) * x1;
-    dp[i] += (dp[i+1] + 1) * x2;
+  vector<mint> dp(n+1);
+  dp[0] = 0;
+  for(int i = 1; i <= n; i++) {
+    dp[i] = 1;
+    // 1 減らす p
+    dp[i] += dp[max(0,i-2)] * p;
+    // 2 減らす 1-p
+    dp[i] += dp[i-1] * q;
   }
 
-  cout << dp[0].val() << endl;
+  cout << dp[n].val() << endl;
   return 0;
 }
