a0bb52d967f5c2c6f538d47448ec41ca062f40b0
tic40 <ccpzjoh@gmail.com>
2023-08-21 09:32:07 +0900

    Update

--- abc/abc315/c/main.cpp
+++ abc/abc315/c/main.cpp
@@ -3,24 +3,23 @@ using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 using ll = long long;
+template<class T> void chmax(T& a, T b) { a = max(a,b); }
 
 int main() {
   int n; cin >> n;
   vector<ll> f(n),s(n);
-  REP(i,n) cin >> f[i] >> s[i];
+  REP(i,n) { cin >> f[i] >> s[i]; f[i]--; }
+  vector g(n,vector<ll>());
+  REP(i,n) g[f[i]].push_back(s[i]);
+  for(auto& v: g) sort(v.rbegin(),v.rend());
 
   ll ans = 0;
-  map<int,vector<ll>> mp;
-  REP(i,n) mp[f[i]].push_back(s[i]);
-  for(auto &[k,v]: mp) sort(v.rbegin(),v.rend());
-  for(auto [k,v]: mp) {
-    if (v.size() > 1) ans = max(ans,v[0]+v[1]/2);
-  }
-  vector<ll> mx;
-  for(auto [k,v]: mp) mx.push_back(v[0]);
-  if (mx.size() > 1) {
-    sort(mx.rbegin(),mx.rend());
-    ans = max(ans,mx[0]+mx[1]);
+  for(auto v: g) if (v.size() > 1) chmax(ans,v[0]+v[1]/2);
+  vector<ll> x;
+  for(auto v: g) if (v.size()) x.push_back(v[0]);
+  if (x.size() > 1) {
+    sort(x.rbegin(),x.rend());
+    chmax(ans,x[0]+x[1]);
   }
 
   cout << ans << endl;
