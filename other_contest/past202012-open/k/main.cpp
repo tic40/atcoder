be795f6cd10b44489ddc515dd671da216819384a
tic40 <ccpzjoh@gmail.com>
2023-08-27 16:27:47 +0900

    Update

--- other_contest/past202012-open/k/main.cpp
+++ other_contest/past202012-open/k/main.cpp
@@ -1,12 +1,22 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
+using ll = long long;
 using P = pair<int,int>;
 const int INF = numeric_limits<int>::max();
+const ll LINF = numeric_limits<ll>::max();
+const int MOD = 1e9+7;
+// using mint = modint998244353; // modint1000000007;
 
 const vector<P> moves = {
-  {0, 1}, {0, -1}, {1, 0}, {-1, 0}, {0, 0}
+  {0,0},
+  {1,0},
+  {-1,0},
+  {0,1},
+  {0,-1}
 };
 
 int main() {
@@ -15,25 +25,27 @@ int main() {
 
   vector<double> dp(1<<16,INF);
   dp[0] = 0;
-  for(int bit = 1; bit < 1<<16; bit++) {
+  REP(bit,1<<16) {
     REP(i,4) REP(j,4) {
       int cnt = 0;
       double tot = 5;
       for(auto [dx,dy]: moves) {
-        int ni = i+dx;
-        int nj = j+dy;
-        if (ni < 0 || nj < 0 || ni >= 4 || nj >= 4) continue;
-        int now = 1 << (ni*4+nj);
-        if ((bit & now) == 0) continue;
+        int nx = i+dx;
+        int ny = j+dy;
+        if (nx < 0 || ny < 0 || nx >= 4 || ny >= 4) continue;
+        int mask = 1 << (nx*4+ny);
+        if ((bit & mask) == 0) continue;
+        tot += dp[bit ^ mask];
         cnt++;
-        tot += dp[bit ^ now];
       }
-      if (0 < cnt) dp[bit] = min(dp[bit], tot / cnt);
+      if (cnt) dp[bit] = min(dp[bit], tot / cnt);
     }
   }
 
-  int msk = 0;
-  REP(i,4) REP(j,4) if (s[i][j] == '#') msk |= 1 << (i*4+j);
-  printf("%.10f\n",dp[msk]);
+  int x = 0;
+  REP(i,4) REP(j,4) {
+    if (s[i][j] == '#') x |= 1 << (i*4+j);
+  }
+  printf("%.10f\n",dp[x]);
   return 0;
 }
