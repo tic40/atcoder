be874bbc119a192d31d63ab00fc14974df72cb79
tic40 <ccpzjoh@gmail.com>
2023-08-27 15:28:10 +0900

    Update

--- abc/abc317/e/main.cpp
+++ abc/abc317/e/main.cpp
@@ -1,10 +1,7 @@
 #include <bits/stdc++.h>
-#include <atcoder/all>
-using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
-using ll = long long;
 using P = pair<int,int>;
 const int INF = numeric_limits<int>::max();
 
@@ -13,71 +10,49 @@ int main() {
   vector<string> a(h);
   REP(i,h) cin >> a[i];
 
-  P s = {0,0};
-  P g = {0,0};
+  auto update = [&](int i, int j, int di, int dj) -> void {
+    while(1) {
+      i += di; j += dj;
+      if (i < 0 || j < 0 || i >= h || j >= w) return;
+      char t = a[i][j];
+      bool ok = t == '.' || t == '_';
+      if (!ok) return;
+      a[i][j] = '_';
+    }
+  };
+
+  P s,g;
   REP(i,h) REP(j,w) {
     char t = a[i][j];
     if (t == 'S') s = {i,j};
     if (t == 'G') g = {i,j};
-    if (t == '^') {
-      REP(k,h) {
-        int ni = i-k-1;
-        if (ni < 0) break;
-        if (a[ni][j] != '.' && a[ni][j] != 'x') break;
-        a[ni][j] = 'x';
-      }
-    }
-    if (t == '>') {
-      REP(k,w) {
-        int nj = j+k+1;
-        if (nj >= w) break;
-        if (a[i][nj] != '.' && a[i][nj] != 'x') break;
-        a[i][nj] = 'x';
-      }
-    }
-    if (t == '<') {
-      REP(k,w) {
-        int nj = j-k-1;
-        if (nj < 0) break;
-        if (a[i][nj] != '.' && a[i][nj] != 'x') break;
-        a[i][nj] = 'x';
-      }
-    }
-    if (t == 'v') {
-      REP(k,h) {
-        int ni = i+k+1;
-        if (ni >= h) break;
-        if (a[ni][j] != '.' && a[ni][j] != 'x') break;
-        a[ni][j] = 'x';
-      }
+    if (t == '^') update(i,j,-1,0);
+    if (t == 'v') update(i,j,1,0);
+    if (t == '>') update(i,j,0,1);
+    if (t == '<') update(i,j,0,-1);
+  }
+
+  const vector<P> moves = { {1,0}, {-1,0}, {0,1}, {0,-1} };
+  vector dist(h,vector<int>(w,INF));
+  queue<P> q;
+  q.emplace(s);
+  dist[s.first][s.second] = 0;
+  while(q.size()) {
+    auto [i,j] = q.front();
+    q.pop();
+    for(auto [di,dj]: moves) {
+      int ni = i+di, nj = j+dj;
+      if (ni < 0 || nj < 0 || ni >= h || nj >= w) continue;
+      char t = a[ni][nj];
+      bool ok = t == '.' || t == 'G';
+      if (!ok) continue;
+      int ndist = dist[i][j]+1;
+      if (dist[ni][nj] <= ndist) continue;
+      dist[ni][nj] = ndist;
+      q.emplace(ni,nj);
     }
   }
 
-  const vector<pair<int, int>> moves = { {1,0},{0,1},{-1,0},{0,-1} };
-
-  auto bfs = [&]() {
-    vector dist(h,vector<int>(w, INF));
-    queue<P> q;
-    q.push(s);
-    dist[s.first][s.second] = 0;
-    while(q.size()) {
-      auto [x,y] = q.front();
-      q.pop();
-      for(auto [dx,dy]: moves) {
-        int nx = x+dx;
-        int ny = y+dy;
-        if (nx < 0 || ny < 0 || nx >= h || ny >= w) continue;
-        if (a[nx][ny] != '.' && a[nx][ny] != 'G') continue;
-        int ndist = dist[x][y]+1;
-        if (dist[nx][ny] <= ndist) continue;
-        dist[nx][ny] = ndist;
-        q.emplace(nx,ny);
-      }
-    }
-    return dist;
-  };
-
-  auto dist = bfs();
   int ans = dist[g.first][g.second];
   if (ans == INF) ans = -1;
   cout << ans << endl;
