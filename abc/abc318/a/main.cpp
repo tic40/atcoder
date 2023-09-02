138cb500cbdfbdd313d72066b4132b3318ecee8e
tic40 <ccpzjoh@gmail.com>
2023-09-02 23:19:43 +0900

    Update

--- abc/abc318/a/main.cpp
+++ abc/abc318/a/main.cpp
@@ -1,27 +1,11 @@
 #include <bits/stdc++.h>
-#include <atcoder/all>
-using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
-using ll = long long;
-using P = pair<int,int>;
-const int INF = numeric_limits<int>::max();
-const ll LINF = numeric_limits<ll>::max();
-const int MOD = 1e9+7;
-// using mint = modint998244353; // modint1000000007;
 
 int main() {
-  int n,m,p;
-  cin >> n >> m >> p;
-  int ans = 0;
-  for(int i = 1; i <= n; i++) {
-    if (i == m) {
-      ans++;
-      continue;
-    }
-    if (i > m && ((i - m)) % p == 0) ans++;
-  }
+  int n,m,p; cin >> n >> m >> p;
+  int ans = (n+(p-m))/p;
   cout << ans << endl;
   return 0;
 }
--- abc/abc318/c/main.cpp
+++ abc/abc318/c/main.cpp
@@ -1,15 +1,9 @@
 #include <bits/stdc++.h>
-#include <atcoder/all>
-using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 using ll = long long;
-using P = pair<int,int>;
-const int INF = numeric_limits<int>::max();
 const ll LINF = numeric_limits<ll>::max();
-const int MOD = 1e9+7;
-// using mint = modint998244353; // modint1000000007;
 
 int main() {
   ll n,d,p; cin >> n >> d >> p;
@@ -21,12 +15,9 @@ int main() {
   REP(i,n) s[i+1] = s[i]+f[i];
 
   ll ans = LINF;
-  REP(i,n+1) {
-    ll now = i * p;
-    ll m = i * d;
-    now += s[n] - s[min(n,m)];
+  for(int i = 0; i <= n/d+1; i++) {
+    ll now = i * p + s[n] - s[min(n,i*d)];
     ans = min(ans,now);
-    if (m >= n) break;
   }
 
   cout << ans << endl;
