5a48f303a864a30283bfe492ba00836b71f9abab
tic40 <ccpzjoh@gmail.com>
2023-09-03 12:07:05 +0900

    Update

--- abc/abc318/e/main.cpp
+++ abc/abc318/e/main.cpp
@@ -5,9 +5,10 @@ using namespace std;
 using ll = long long;
 
 ll choose(int n, int a) {
+  if (n < a) return 0;
   ll x = 1, y = 1;
   for(ll i = 0; i < a; i++) {
-    x *= n-i; y *= i+1;
+    x *= n-i; y *= a-i;
   }
   return x / y;
 }
@@ -24,16 +25,16 @@ int main() {
   // step1. x?x のケースを数え上げる
   REP(k,n) {
     // Σ(Σ(k-i-1))
-    // Σ(Σ(k)) - Σ(Σ(i)) - Σ(Σ(1))
-    // Σ(Σ(k-1)) - Σ(Σ(i))
-    // Σ(k-1)Σ(1) - Σ(Σ(i))
+    // ΣΣ(k) - ΣΣ(i) - ΣΣ(1)
+    // ΣΣ(k-1) - ΣΣ(i)
+    // Σ(k-1)Σ(1) - ΣΣ(i)
     // Σ(1) := i の個数, Σ(i) := i の合計
     ans += (ll)(k-1)*cnt[a[k]] - sum[a[k]];
     cnt[a[k]]++;
     sum[a[k]] += k;
   }
   // step2. xxx のケースを引く
-  REP(i,n) if (cnt[i+1] >= 3) ans -= choose(cnt[i+1],3);
+  REP(i,n+1) if (cnt[i] >= 3) ans -= choose(cnt[i],3);
 
   cout << ans << endl;
   return 0;
