8f12cac3646ec5fff95440c3f34ef230c4a97925
tic40 <ccpzjoh@gmail.com>
2023-09-22 10:04:33 +0900

    Update

--- arc/arc165/b/main.cpp
+++ arc/arc165/b/main.cpp
@@ -8,32 +8,36 @@ int main() {
   vector<int> p(n);
   REP(i,n) cin >> p[i];
 
-  vector<int> s(n); // p[i] < p[i+1] が成り立つ個数の累積和
+  vector<int> s(n); // s[i] := p[i-1] < p[i] が成り立つ個数の累積和
   REP(i,n-1) s[i+1] = s[i] + (p[i+1] > p[i] ? 1 : 0);
-  vector<int> sm(n,1e9); // p[n-k]以降の累積min
+  vector<int> sm(n,1e9); // sm[i] := p[n-k]...p[i] の累積 min
   for(int i = n-k; i < n; i++) sm[i] = min(sm[i-1], p[i]);
 
-  auto output = [&]() { for(auto v: p) cout << v << " "; };
+  // l-r 間をソートして答えを出力
+  auto ans = [&](int l = 0, int r = 0) {
+    sort(p.begin()+l,p.begin()+r);
+    for(auto v: p) cout << v << " ";
+    exit(0);
+  };
 
-  REP(l,n-k+1) {
+  REP(l,n) {
     int r = l+k-1;
+    if (r >= n) break;
     // p[l] < p[l+1] < p[l+2] < ... < p[r] が成り立つ
-    if (s[r] - s[l] == k-1) { output(); return 0; }
+    if (s[r] - s[l] == k-1) ans();
   }
 
-  for(int l = max(0,n-k*2+1); l < n-k; l++) {
+  REP(l,n) {
     int r = l+k-1;
+    if (r >= n) break;
+    if (r < n-k) continue;
     // 条件1: p[l] < p[l+1] < ... < p[n-k]
     bool ok = (s[n-k] - s[l]) == n-k-l;
     // 条件2: p[n-k], p[n-k+1],..., p[r] の最小値は p[n-k-1] より大きい
     ok = ok && p[n-k-1] < sm[r];
-    if (ok) {
-      sort(p.begin()+l, p.begin()+l+k);
-      output(); return 0;
-    }
+    if (ok) ans(l,r+1);
   }
 
-  sort(p.end()-k,p.end());
-  output();
+  ans(n-k,n);
   return 0;
 }
