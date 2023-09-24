82e4decf77b8c805f5669f6e621791c8cfc80d07
tic40 <ccpzjoh@gmail.com>
2023-09-24 21:39:54 +0900

    Update

--- abc/abc321/e/main.cpp
+++ abc/abc321/e/main.cpp
@@ -9,15 +9,13 @@ void solve() {
 
   // 頂点 v から d 降りたところの頂点を求める
   auto f = [&](ll v, ll d) -> ll {
-    if (v > n) return 0;
     ll l = v, r = v;
     REP(i,d) {
       l *= 2;
       r = r*2+1;
       if (l > n) return 0;
     }
-    r = min(r,n);
-    return r - l + 1;
+    return min(n,r) - l + 1;
   };
 
   // x から k 個降りたところの個数
@@ -29,7 +27,7 @@ void solve() {
     ans += f(x^1, k-2);
     k--; x /= 2;
   }
-  if (k == 1 && x != 1) ans++;
+  if (k == 1 && x > 1) ans++;
   cout << ans << endl;
 }
 
