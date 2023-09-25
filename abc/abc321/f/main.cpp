f37ecd7419823299d4ddb92e97523b3b4e26d57f
tic40 <ccpzjoh@gmail.com>
2023-09-25 09:52:43 +0900

    Solve f

---
+++ abc/abc321/f/main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace atcoder;
+using namespace std;
+#define REP(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
+using mint = modint998244353;
+
+int main() {
+  int q,k; cin >> q >> k;
+
+  vector<mint> dp(k+1);
+  dp[0]=1;
+
+  REP(_,q) {
+    char c; int x;
+    cin >> c >> x;
+    if (c == '+') {
+      for(int i = k; i >= x; i--) {
+        dp[i] += dp[i-x];
+      }
+    } else {
+      for(int i = x; i <= k; i++) {
+        dp[i] -= dp[i-x];
+      }
+    }
+    cout << dp[k].val() << endl;
+  }
+  return 0;
+}
