2880b3d9c3234f4e753494fac470ca106a01f25f
tic40 <ccpzjoh@gmail.com>
2023-10-04 08:49:48 +0900

    Update

--- abc/abc322/e/main.cpp
+++ abc/abc322/e/main.cpp
@@ -14,28 +14,27 @@ int main() {
     REP(j,k) cin >> a[i][j];
   }
 
-  int sn = pow(10,k);
+  int base = p+1; // base 進数
+  int sn = pow(base,k);
   // dp[i][j] := i 個までみたとき、パラメータ集合が j のときの最小コスト
   vector dp(n+1,vector<ll>(sn, LINF));
   dp[0][0] = 0;
   REP(i,n) REP(s,sn) {
     if (dp[i][s] == LINF) continue;
-
     // 選ばない
     dp[i+1][s] = min(dp[i+1][s],dp[i][s]);
-
     // 選ぶ
     int x = 1, nex = s;
     REP(j,k) {
-      int now = nex / x % 10; // 現在のパラメータ値
+      int now = s / x % base; // 現在のパラメータ値
       nex += (min(p, now + a[i][j]) - now) * x;
-      x *= 10;
+      x *= base;
     }
     dp[i+1][nex] = min(dp[i+1][nex],dp[i][s] + c[i]);
   }
 
   int x = 1, t = 0;
-  REP(i,k) { t += p * x; x *= 10; }
+  REP(i,k) { t += p * x; x *= base; }
   if (dp[n][t] == LINF) dp[n][t] = -1;
   cout << dp[n][t] << endl;
   return 0;
