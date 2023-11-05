e0ba28b0936de6a81bf17aed57ccb5655f323a5f
tic40 <ccpzjoh@gmail.com>
2023-11-05 22:45:22 +0900

    Update

--- abc/abc327/d/main.cpp
+++ abc/abc327/d/main.cpp
@@ -8,30 +8,30 @@ int main() {
   vector<int> a(m),b(m);
   REP(i,m) { cin >> a[i]; a[i]--; }
   REP(i,m) { cin >> b[i]; b[i]--; }
-
-  REP(i,m) if (a[i] == b[i]) { cout << "No" << endl; return 0; }
-  vector<vector<int>> g(n);
+  vector g(n,vector<int>());
   REP(i,m) {
     g[a[i]].push_back(b[i]);
     g[b[i]].push_back(a[i]);
   }
 
   vector<int> col(n,-1);
-  auto dfs = [&](auto self, int i) -> void {
-    int nx = col[i] ^ 1;
+  auto dfs = [&](auto self, int i) -> bool {
+    bool res = true;
+    int nc = col[i] ^ 1;
     for(auto v: g[i]) {
-      if (col[v] == -1 ) {
-        col[v] = nx;
-        self(self,v);
+      if (col[v] == -1) {
+        col[v] = nc;
+        res = res && self(self,v);
       }
-      if (col[v] != nx) { cout << "No" << endl; exit(0); }
+      if (col[v] == col[i]) return false;
     }
+    return res;
   };
 
   REP(i,n) {
     if (col[i] != -1) continue;
     col[i] = 0;
-    dfs(dfs,i);
+    if (!dfs(dfs,i)) { cout << "No" << endl; return 0; }
   }
   cout << "Yes" << endl;
   return 0;
