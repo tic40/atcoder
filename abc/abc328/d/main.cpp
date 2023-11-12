edea3431170f554e76dd8f20faa924ca9f731e8a
tic40 <ccpzjoh@gmail.com>
2023-11-12 22:47:58 +0900

    Update

--- abc/abc328/d/main.cpp
+++ abc/abc328/d/main.cpp
@@ -7,13 +7,9 @@ int main() {
   string s; cin >> s;
   string ans = "";
   REP(i,(int)s.size()) {
-    if (s[i] == 'C') {
-      if ((int)ans.size() >= 2 && ans.back() == 'B' && ans[(int)ans.size()-2] == 'A') {
-        ans.pop_back();
-        ans.pop_back();
-      } else {
-        ans += s[i];
-      }
+    if (s[i] == 'C' && (int)ans.size() >= 2 && ans.substr(ans.size() - 2) == "AB") {
+      ans.pop_back();
+      ans.pop_back();
     } else {
       ans += s[i];
     }
