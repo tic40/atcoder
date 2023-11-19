6dc838f397f095905ec2ee46adf2750bbd3fdd4d
tic40 <ccpzjoh@gmail.com>
2023-11-20 00:07:53 +0900

    Update

--- abc/abc329/e/main.cpp
+++ abc/abc329/e/main.cpp
@@ -6,26 +6,26 @@ using namespace std;
 int main() {
   int n,m; cin >> n >> m;
   string s,t; cin >> s >> t;
-  string tg = string(m,'#');
 
+  vector<bool> checked(n);
   queue<int> q;
   auto f = [&](int i) -> void {
-    if (i < 0 || i+m-1 >= n) return;
-    if (s.substr(i,m) == tg) return;
+    if (i < 0 || i+m > n) return;
+    if (checked[i]) return;
 
     bool ok = true;
     REP(j,m) ok = ok && (s[i+j] == t[j] || s[i+j] == '#');
-    if (ok) {
-      REP(j,m) s[i+j] = '#';
-      q.push(i);
-    }
+    if (!ok) return;
+
+    checked[i] = true;
+    REP(j,m) s[i+j] = '#';
+    q.push(i);
   };
 
   REP(i,n-m+1) f(i);
   while(q.size()) {
     auto v = q.front(); q.pop();
-    for(int i = v-m+1; i < v; i++) f(i);
-    for(int i = v+1; i < v+m; i++) f(i);
+    for(int i = v-m+1; i < v+m; i++) f(i);
   }
 
   cout << (s == string(n,'#') ? "Yes" : "No") << endl;
