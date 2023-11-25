6229e7d87e29d42129f1060e672690f9961dceb5
tic40 <ccpzjoh@gmail.com>
2023-11-25 23:11:55 +0900

    Update

--- abc/abc330/e/main.cpp
+++ abc/abc330/e/main.cpp
@@ -1,6 +1,4 @@
 #include <bits/stdc++.h>
-#include <atcoder/all>
-using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
@@ -9,23 +7,22 @@ int main() {
   int n,q; cin >> n >> q;
   vector<int> a(n);
   REP(i,n) cin >> a[i];
-
-  vector<int> bk(n+1);
-  REP(i,n) if (a[i] <= n) bk[a[i]]++;
+  vector<int> cnt(n+1);
+  REP(i,n) if (a[i] <= n) cnt[a[i]]++;
   set<int> st;
-  REP(i,n) if (bk[i] == 0) st.insert(i);
+  REP(i,n+1) if (cnt[i] == 0) st.insert(i);
 
   REP(_,q) {
     int i,x; cin >> i >> x;
     i--;
     if (a[i] <= n) {
-      bk[a[i]]--;
-      if (bk[a[i]] == 0) st.insert(a[i]);
+      cnt[a[i]]--;
+      if (cnt[a[i]] == 0) st.insert(a[i]);
     }
     a[i] = x;
     if (a[i] <= n) {
-      bk[a[i]]++;
-      if (bk[a[i]] == 1) st.erase(a[i]);
+      cnt[a[i]]++;
+      if (cnt[a[i]] == 1) st.erase(a[i]);
     }
     cout << *st.begin() << endl;
   }
