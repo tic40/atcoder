96cc9b8c978a797f028c8793cfa8289ed8aa63a8
tic40 <ccpzjoh@gmail.com>
2023-11-27 21:14:55 +0900

    Update

--- abc/abc330/e/main.cpp
+++ abc/abc330/e/main.cpp
@@ -3,28 +3,54 @@ using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 
+struct Mex {
+  private:
+    int _n; // あり得る mex 値の上限 0 - _n
+    vector<int> cnt; // 要素 0 - _n のカウンタ
+    set<int> st; // mex 管理用
+  public:
+    Mex(int n): _n(n), cnt(n+1) {
+      for(int i = 0; i <= n; i++) st.insert(i);
+    }
+
+  // 要素を追加
+  void add(long long x) {
+    if (x >= _n) return;
+    cnt[x]++;
+    if (cnt[x] == 1) st.erase(x);
+  }
+
+  // 要素を削除
+  void del(long long x) {
+    if (x >= _n) return;
+    cnt[x]--;
+    if (cnt[x] == 0) st.insert(x);
+  }
+
+  int get() {
+    return *st.begin();
+  }
+
+  void debug() {
+    for(auto v: st) cout << v << endl;
+  }
+};
+
 int main() {
   int n,q; cin >> n >> q;
   vector<int> a(n);
   REP(i,n) cin >> a[i];
-  vector<int> cnt(n+1);
-  REP(i,n) if (a[i] <= n) cnt[a[i]]++;
-  set<int> st;
-  REP(i,n+1) if (cnt[i] == 0) st.insert(i);
+
+  Mex mex(n);
+  REP(i,n) mex.add(a[i]);
 
   REP(_,q) {
     int i,x; cin >> i >> x;
     i--;
-    if (a[i] <= n) {
-      cnt[a[i]]--;
-      if (cnt[a[i]] == 0) st.insert(a[i]);
-    }
+    mex.del(a[i]);
+    mex.add(x);
     a[i] = x;
-    if (a[i] <= n) {
-      cnt[a[i]]++;
-      if (cnt[a[i]] == 1) st.erase(a[i]);
-    }
-    cout << *st.begin() << endl;
+    cout << mex.get() << endl;
   }
   return 0;
 }
