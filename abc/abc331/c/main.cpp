2985abcc47e21a0be14e71570b450e9683a8c9cd
tic40 <ccpzjoh@gmail.com>
2023-12-02 23:34:11 +0900

    Update

--- abc/abc331/c/main.cpp
+++ abc/abc331/c/main.cpp
@@ -1,15 +1,9 @@
 #include <bits/stdc++.h>
-#include <atcoder/all>
-using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 using ll = long long;
 using P = pair<int,int>;
-const int INF = numeric_limits<int>::max();
-const ll LINF = numeric_limits<ll>::max();
-const int MOD = 1e9+7;
-// using mint = modint998244353; // modint1000000007;
 
 int main() {
   int n; cin >> n;
@@ -21,12 +15,9 @@ int main() {
   REP(i,n) s[i+1] = s[i]+b[i];
 
   REP(i,n) {
-    auto it = upper_bound(b.begin(),b.end(),a[i]);
-    int idx = it - b.begin();
+    int idx = upper_bound(b.begin(),b.end(),a[i]) - b.begin();
     ll ans = s[n] - s[idx];
     cout << ans << " ";
   }
-  cout << endl;
-
   return 0;
 }
