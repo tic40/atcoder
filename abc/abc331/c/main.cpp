df61792c1fe8f88e0a5650584b09c4d0a5c46ce2
tic40 <ccpzjoh@gmail.com>
2023-12-03 22:33:54 +0900

    Update

--- abc/abc331/c/main.cpp
+++ abc/abc331/c/main.cpp
@@ -9,15 +9,37 @@ int main() {
   int n; cin >> n;
   vector<int> a(n);
   REP(i,n) cin >> a[i];
-  auto b = a;
-  sort(b.begin(),b.end());
-  vector<ll> s(n+1);
-  REP(i,n) s[i+1] = s[i]+b[i];
 
-  REP(i,n) {
-    int idx = upper_bound(b.begin(),b.end(),a[i]) - b.begin();
-    ll ans = s[n] - s[idx];
-    cout << ans << " ";
-  }
+  // sort 解
+  auto solve1 = [&]() {
+    auto b = a;
+    sort(b.begin(),b.end());
+    vector<ll> s(n+1);
+    REP(i,n) s[i+1] = s[i]+b[i];
+
+    REP(i,n) {
+      int idx = upper_bound(b.begin(),b.end(),a[i]) - b.begin();
+      ll ans = s[n] - s[idx];
+      cout << ans << " ";
+    }
+  };
+
+  // 解説放送解
+  auto solve2 = [&]() {
+    const int M = 1000005;
+    vector is(M,vector<int>());
+    REP(i,n) is[a[i]].push_back(i);
+
+    vector<ll> ans(n);
+    ll now = 0;
+    for(int x = M-1; x >= 1; x--) {
+      for(int i: is[x]) ans[i] = now;
+      now += (ll)x * is[x].size();
+    }
+
+    for(auto v: ans) cout << v << " ";
+  };
+
+  solve2();
   return 0;
 }
