df4fba56bb843662d647a28b2e50222bc2c63a7f
tic40 <ccpzjoh@gmail.com>
2024-01-14 23:18:44 +0900

    Update

--- abc/abc336/b/main.cpp
+++ abc/abc336/b/main.cpp
@@ -3,15 +3,10 @@ using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 
-
 int main() {
   int n; cin >> n;
   int ans = 0;
-  while(n > 0) {
-    if (n % 2) break;
-    ans++;
-    n /= 2;
-  }
+  while((n&1) == 0) { ans++; n = n >>1; }
   cout << ans << endl;
   return 0;
 }
