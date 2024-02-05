f0734e0433bdb66612c1ba9b73dece17494acaaa
tic40 <ccpzjoh@gmail.com>
2024-02-05 09:41:21 +0900

    Update

--- abc/abc339/e/main.cpp
+++ abc/abc339/e/main.cpp
@@ -5,8 +5,13 @@ using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 
-int op(int a, int b) { return max(a,b); }
-int e() { return 0; }
+using S = ll;
+using F = ll;
+S op(S a, S b){ return max(a, b); }
+S e(){ return 0; }
+S mapping(F f, S x){ return f+x; }
+F composition(F f, F g){ return f+g; }
+F id(){ return 0; }
 
 int main() {
   int n,d; cin >> n >> d;
@@ -15,16 +20,18 @@ int main() {
 
   const int M = 500005;
   // dp[i][j] := i 個目までみて最後に選んだのが値 j のときの max
-  segtree<int,op,e> dp(M);
+  // segtree で解ける。練習で lazy segtree を使ってみたただけ
+  // segtree<int,op,e> seg(M);
+  lazy_segtree<S, op, e, F, mapping, composition, id> seg(M);
 
   REP(i,n) {
     int l = max(0, a[i]-d);
     int r = min(M, a[i]+d+1);
-    int now = dp.prod(l,r) + 1;
-    dp.set(a[i],now);
+    int now = seg.prod(l,r) + 1;
+    seg.set(a[i],now);
   }
 
-  int ans = dp.prod(0,M);
+  int ans = seg.prod(0,M);
   cout << ans << endl;
   return 0;
 }
