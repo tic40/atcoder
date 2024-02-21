cab84a282dbb78e9989792f01ace140a3a09ede5
tic40 <ccpzjoh@gmail.com>
2024-02-21 09:11:55 +0900

    Update

--- abc/abc340/f/main.cpp
+++ abc/abc340/f/main.cpp
@@ -4,6 +4,9 @@ using namespace std;
 #define endl '\n'
 using ll = long long;
 
+// 拡張ユークリッドの互除法
+// 返り値: a と b の最大公約数
+// ax + by = gcd(a,b) を満たす (x,y) が格納される
 ll extgcd(ll a, ll b, ll& x, ll& y) {
   if (b == 0) { x = 1; y = 0; return a; }
   auto d = extgcd(b,a-b*(a/b),y,x); // extgcd(b,a%b,y,x);
@@ -14,12 +17,15 @@ ll extgcd(ll a, ll b, ll& x, ll& y) {
 int main() {
   ll x,y; cin >> x >> y;
   ll b,a;
-  auto g = extgcd(x,y,b,a);
-  if (2%g) { cout << -1 << endl; return 0; }
+  // ベクトルの外積より |ax - by| = 2 となる a,b を求めたい
+  // 拡張ユークリッドの互除法で ax + by = gcd(a,b) を求める
+  // gcd(a,b) が 3 以上の場合は |ax - by| も gcd(a,b) 以上の倍数となるため
+  // 答えは存在しない
+  auto g = abs(extgcd(x,y,b,a));
+  if (g > 2) { cout << -1 << endl; return 0; }
 
   a = -a;
-  a *= 2/g;
-  b *= 2/g;
+  if (g == 1) { a *= 2; b *= 2; }
   cout << a << " " << b << endl;
   return 0;
 }
