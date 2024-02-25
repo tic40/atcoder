974cf0cf4ae38cda2287c4954cbdc75602dbc0af
tic40 <ccpzjoh@gmail.com>
2024-02-25 20:45:49 +0900

    Add bookmarklet

---
+++ scripts/bookmarkletSampleDownloader.js
@@ -0,0 +1,19 @@
+javascript:(function() {
+  if (new URL(window.location.href).host !== 'atcoder.jp') { alert('Error: only works on atcoder.jp'); return; }
+  let num = 1;
+  const messages = [];
+  document.querySelectorAll("[id^='pre-sample']").forEach(v => {
+    const prevTextContent = v.previousSibling.previousSibling.textContent;
+    if (!RegExp('^Sample Input [0-9]+').test(prevTextContent)) return;
+
+    const blob = new Blob([v.textContent], { type: 'application/octet-stream' });
+    const a = document.createElement('a');
+    a.href = window.URL.createObjectURL(blob);
+    const filename = `test${num++}`;
+    a.download = filename;
+    a.click();
+    messages.push(`downloaded: ${filename}\n${v.textContent}`);
+  });
+  if (messages.length) console.info(messages.join('\n'));
+  else alert('Error: no sample inputs');
+})();
