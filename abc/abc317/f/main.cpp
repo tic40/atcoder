4ae9e004136dfa4598a0e972d6c4d23c15aa523e
tic40 <ccpzjoh@gmail.com>
2024-02-26 09:37:55 +0900

    Update

--- abc/abc317/f/main.cpp
+++ abc/abc317/f/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 using ll = long long;
-using mint = modint998244353; // modint1000000007;
+using mint = modint998244353;
 using T = tuple<int,bool,bool>;
 using VT = vector<T>;
 
@@ -16,26 +16,33 @@ int main() {
   REP(i,m) cin >> a[i];
 
   n++;
-  vector<int> ds;
+  vector<int> ds; // ds[i] := 各桁の bit
   while(n) { ds.push_back(n&1); n >>= 1; }
   reverse(ds.begin(),ds.end());
 
+  // 桁DP
+  // dp[i][VT] := i 桁目までみたときに状態が VT のときの組み合わせの数
+  // VT := [md,lt,z]
+  //   md: 割った余り
+  //   lt: N未満になることが確定したかどうかflag
+  //   z: 0 or 1
   map<VT,mint> dp;
   dp[VT(3)] = 1;
-
   for(int digit: ds) {
     map<VT,mint> pre;
     swap(dp,pre);
     for(auto [s,num]: pre) {
       REP(b,1<<m) {
+        // b の bit に 1 が奇数個あるときは xor で 0 にならないのでスキップ
         if(__builtin_parity(b)) continue;
         VT ns;
         bool ok = true;
         REP(i,m) {
           auto [md, lt, z] = s[i];
-          int nb = b>>i & 1;
+          int nb = b>>i & 1; // 今の桁
           md = (md*2+nb)%a[i];
           z |= nb;
+          // less than が確定していない かつ 今の桁の値が digit より大きい
           if (!lt && nb > digit) ok = false;
           lt |= nb < digit;
           ns.emplace_back(md,lt,z);
