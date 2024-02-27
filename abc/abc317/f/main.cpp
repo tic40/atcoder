3b5974d552039ab3a4eb5d98aea205c14930bd97
tic40 <ccpzjoh@gmail.com>
2024-02-27 09:46:53 +0900

    Update

--- abc/abc317/f/main.cpp
+++ abc/abc317/f/main.cpp
@@ -21,26 +21,29 @@ int main() {
   reverse(ds.begin(),ds.end());
 
   // 桁DP
-  // dp[i][VT] := i 桁目までみたときに状態が VT のときの組み合わせの数
+  // dp[i][VT] := 各xを i 桁目までみたときに状態が VT のときの組み合わせの数
   // VT := [md,lt,z]
   //   md: 割った余り
   //   lt: N未満になることが確定したかどうかflag
-  //   z: 0 or 1
+  //   z: x が 0 でないかどうか(1 <= x <= n より). 0 or 1
+  // 多次元配列をさけるために map を使っている
   map<VT,mint> dp;
   dp[VT(3)] = 1;
   for(int digit: ds) {
     map<VT,mint> pre;
     swap(dp,pre);
     for(auto [s,num]: pre) {
+      // b: bit で A1,A2,A3 の今の2進数の桁の値が 1 or 0 を表す
       REP(b,1<<m) {
         // b の bit に 1 が奇数個あるときは xor で 0 にならないのでスキップ
-        if(__builtin_parity(b)) continue;
+        if (__builtin_parity(b)) continue;
         VT ns;
         bool ok = true;
+        // A1,A2,A3 それぞれの処理
         REP(i,m) {
           auto [md, lt, z] = s[i];
-          int nb = b>>i & 1; // 今の桁
-          md = (md*2+nb)%a[i];
+          int nb = b>>i & 1; // x[i] の今の桁の値
+          md = (md*2+nb)%a[i]; // x[i] を a[i] で割った余りの数
           z |= nb;
           // less than が確定していない かつ 今の桁の値が digit より大きい
           if (!lt && nb > digit) ok = false;
@@ -52,6 +55,7 @@ int main() {
     }
   }
 
+  // 余りが 0, N未満フラグが 1, z が 1
   mint ans = dp[VT(3,{0,1,1})];
   cout << ans.val() << endl;
   return 0;
