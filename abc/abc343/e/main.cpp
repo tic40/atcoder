304171fe029ac1a410ee50aa3abcc82bb122bf2a
tic40 <ccpzjoh@gmail.com>
2024-03-03 11:14:27 +0900

    Update

--- abc/abc343/e/main.cpp
+++ abc/abc343/e/main.cpp
@@ -4,42 +4,35 @@ using namespace std;
 #define endl '\n'
 
 const int L = 7;
-// 3つの直方体が重なっている部分の面積を返す
-int calcV3(int x1, int y1, int z1, int x2, int y2, int z2, int x3, int y3, int z3) {
-  int minX = max({x1, x2, x3});
-  int maxX = min({x1 + L, x2 + L, x3 + L});
-  int minY = max({y1, y2, y3});
-  int maxY = min({y1 + L, y2 + L, y3 + L});
-  int minZ = max({z1, z2, z3});
-  int maxZ = min({z1 + L, z2 + L, z3 + L});
-  if (maxX - minX < 0 || maxY - minY < 0 || maxZ - minZ < 0) return 0;
-  int v = (maxX - minX) * (maxY - minY) * (maxZ - minZ);
-  return max(0,v);
-}
+struct P {
+  int x,y,z;
+  P(int x, int y, int z): x(x),y(y),z(z) {}
+};
 
-// 2つの直方体が重なっている部分の面積を返す
-int calcV2(int x1, int y1, int z1, int x2, int y2, int z2) {
-  int minX = max(x1, x2);
-  int maxX = min(x1 + L, x2 + L);
-  int minY = max(y1, y2);
-  int maxY = min(y1 + L, y2 + L);
-  int minZ = max(z1, z2);
-  int maxZ = min(z1 + L, z2 + L);
-  if (maxX - minX < 0 || maxY - minY < 0 || maxZ - minZ < 0) return 0;
-  int v = (maxX - minX) * (maxY - minY) * (maxZ - minZ);
-  return max(0,v);
+// 3つの直方体が重なっている部分の面積を返す
+int calcOverRapped(P p1, P p2, P p3) {
+  int minX = max({p1.x, p2.x, p3.x});
+  int maxX = min({p1.x, p2.x, p3.x}) + L;
+  int minY = max({p1.y, p2.y, p3.y});
+  int maxY = min({p1.y, p2.y, p3.y}) + L;
+  int minZ = max({p1.z, p2.z, p3.z});
+  int maxZ = min({p1.z, p2.z, p3.z}) + L;
+  return max(0,(maxX - minX)) * max(0,(maxY - minY)) * max(0,(maxZ - minZ));
 }
 
 int main() {
   vector<int> v(3);
   REP(i,3) cin >> v[i];
 
-  auto f = [&](int x2, int y2, int z2, int x3, int y3, int z3) {
-    int x1 = 0, y1 = 0, z1 = 0;
+  auto f = [&](int x1, int y1, int z1, int x2, int y2, int z2) {
+    auto p1 = P(0,0,0); // p1 は原点に固定
+    auto p2 = P(x1,y1,z1);
+    auto p3 = P(x2,y2,z2);
+
     vector<int> nv(3);
-    nv[2] = calcV3(x1,y1,z1, x2,y2,z2, x3,y3,z3);
+    nv[2] = calcOverRapped(p1,p2,p3);
     // 1-2, 1-3, 2-3 それぞれの組み合わせで重なっている部分の面積の合計 - 3つ重なっている部分の面積
-    nv[1] = calcV2(x1,y1,z1, x2,y2,z2) + calcV2(x1,y1,z1, x3,y3,z3) + calcV2(x2,y2,z2, x3,y3,z3) - nv[2]*3;
+    nv[1] = calcOverRapped(p1,p1,p2) +  calcOverRapped(p1,p1,p3) + calcOverRapped(p2,p2,p3) - nv[2]*3;
     // 3つの合計面積 - 重なっている部分の面積
     nv[0] = (7*7*7*3) - (nv[1]*2 + nv[2]*3);
     return v == nv;
