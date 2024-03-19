935b962fb0cbeda90baca62fd1a262062fa44cf3
tic40 <ccpzjoh@gmail.com>
2024-03-19 23:50:05 +0900

    Update

--- daily_training/20240319_3/h/main.cpp
+++ daily_training/20240319_3/h/main.cpp
@@ -1,30 +1,19 @@
 #include <bits/stdc++.h>
-#include <atcoder/all>
-using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 using ll = long long;
-using P = pair<int,int>;
-const int INF = numeric_limits<int>::max();
-const ll LINF = numeric_limits<ll>::max();
-const int MOD = 1e9+7;
-// using mint = modint998244353; // modint1000000007;
 
 int main() {
   int n,k; cin >> n >> k;
   vector<int> a(n);
   REP(i,n) cin >> a[i];
-  set<ll> st;
-  auto push = [&](ll x) {
-    REP(i,n) st.insert(x+a[i]);
-  };
-  push(0);
+  set<ll> st = {0};
 
-  REP(i,k-1) {
+  REP(i,k) {
     ll x = *st.begin();
     st.erase(x);
-    push(x);
+    REP(i,n) st.insert(x+a[i]);
   }
   ll ans = *st.begin();
   cout << ans << endl;
