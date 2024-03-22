a5562abe527735078381594c4d51a123baca4dff
tic40 <ccpzjoh@gmail.com>
2024-03-22 22:26:46 +0900

    Update

--- abc/abc339/e/main.cpp
+++ abc/abc339/e/main.cpp
@@ -4,34 +4,28 @@ using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
+const int INF = numeric_limits<int>::max();
 
-using S = ll;
-using F = ll;
-S op(S a, S b){ return max(a, b); }
-S e(){ return 0; }
-S mapping(F f, S x){ return f+x; }
-F composition(F f, F g){ return f+g; }
-F id(){ return 0; }
+using S = int;
+using F = int;
+S op (S a, S b) { return max(a,b); }
+S e() { return 0; }
+S mapping(F f, S x) { return f+x; }
+F composition(F f, F g) { return f+g; }
+F id() { return 0; }
 
 int main() {
   int n,d; cin >> n >> d;
   vector<int> a(n);
   REP(i,n) cin >> a[i];
 
-  const int M = 500005;
-  // dp[i][j] := i 個目までみて最後に選んだのが値 j のときの max
-  // segtree で解ける。練習で lazy segtree を使ってみたただけ
-  // segtree<int,op,e> seg(M);
-  lazy_segtree<S, op, e, F, mapping, composition, id> seg(M);
-
+  lazy_segtree<S, op, e, F, mapping, composition, id> seg(500005);
   REP(i,n) {
-    int l = max(0, a[i]-d);
-    int r = min(M, a[i]+d+1);
-    int now = seg.prod(l,r) + 1;
-    seg.set(a[i],now);
+    int l = max(0,a[i]-d);
+    int r = min(500001, a[i]+d);
+    int mx = seg.prod(l,r+1)+1;
+    seg.set(a[i],mx);
   }
-
-  int ans = seg.prod(0,M);
-  cout << ans << endl;
+  cout << seg.all_prod() << endl;
   return 0;
 }
