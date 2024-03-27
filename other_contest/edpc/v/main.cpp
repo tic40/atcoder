86bda38829efd9c3b9a343479abf6afdb895a093
tic40 <ccpzjoh@gmail.com>
2024-03-27 22:42:10 +0900

    Solve

--- other_contest/edpc/v/main.cpp
+++ other_contest/edpc/v/main.cpp
@@ -4,12 +4,7 @@ using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
-using ll = long long;
-using P = pair<int,int>;
-const int INF = numeric_limits<int>::max();
-const ll LINF = numeric_limits<ll>::max();
-const int MOD = 1e9+7;
-// using mint = modint998244353; // modint1000000007;
+using mint = modint;
 
 int main() {
   int n,m; cin >> n >> m;
@@ -21,5 +16,40 @@ int main() {
     g[b].push_back(a);
   }
 
+  mint::set_mod(m);
+  vector<mint> dp(n+1), ans(n+1);
+
+  auto dfs1 = [&](auto self, int now, int p) -> void {
+    dp[now] = 1;
+    for(auto to: g[now]) {
+      if (to == p) continue;
+      self(self,to,now);
+      dp[now] *= dp[to] + 1;
+    }
+  };
+
+  auto dfs2 = [&](auto self, int now, int p) -> void {
+    ans[now] = 1;
+    for(auto to: g[now]) ans[now] *= dp[to] + 1;
+
+    int n = g[now].size();
+    vector<mint> l(n),r(n); // left, right
+    REP(i,n) l[i] = r[i] = dp[g[now][i]] + 1;
+    for(int i = 1; i < n; i++) l[i] *= l[i-1];
+    for(int i = n-2; i >= 0; i--) r[i] *= r[i+1];
+
+    REP(i,n) {
+      if (g[now][i] == p) continue;
+      dp[now] = 1;
+      if (i > 0) dp[now] *= l[i-1];
+      if (i+1 < n) dp[now] *= r[i+1];
+      self(self,g[now][i],now);
+    }
+  };
+
+  dfs1(dfs1,0,-1);
+  dfs2(dfs2,0,-1);
+
+  REP(i,n) cout << ans[i].val() << endl;
   return 0;
 }
