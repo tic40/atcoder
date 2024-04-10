6661135ed64d1b0fa183ae99bddf52ad1b992468
tic40 <ccpzjoh@gmail.com>
2024-04-10 09:18:32 +0900

    Update

--- abc/abc348/e/main.cpp
+++ abc/abc348/e/main.cpp
@@ -16,39 +16,50 @@ int main() {
   vector<int> c(n);
   REP(i,n) cin >> c[i];
 
-  // c の合計
-  ll tot = accumulate(c.begin(),c.end(),0LL);
-  // x: 木の重心となる頂点
-  int x =  -1;
-  {
-    auto dfs = [&](auto self, int i, int p = -1) -> ll {
-      ll res = c[i];
-      ll mx = 0;
+  // 解説放送解: 木の重心から解く
+  auto solve1 = [&]() {
+    // c の合計
+    ll tot = accumulate(c.begin(),c.end(),0LL);
+    // x: 木の重心となる頂点
+    int x =  -1;
+    {
+      auto dfs = [&](auto self, int i, int p = -1) -> ll {
+        ll res = c[i];
+        ll mx = 0;
+        for(auto v: g[i]) {
+          if (v == p) continue;
+          ll now = self(self,v,i);
+          mx = max(mx,now);
+          res += now;
+        }
+        // tot - res は親の部分木分
+        mx = max(mx,tot-res);
+        // どの部分木も 1/2 * tot 以下である
+        if (mx * 2 <= tot) x = i;
+        return res;
+      };
+      dfs(dfs,0);
+    }
+
+    ll ans = 0;
+    auto dfs = [&](auto self, int i, int dist = 0, int p = -1) -> void {
+      ans += (ll)dist * c[i];
       for(auto v: g[i]) {
         if (v == p) continue;
-        ll now = self(self,v,i);
-        mx = max(mx,now);
-        res += now;
+        self(self,v,dist+1,i);
       }
-      // tot - res は親の部分木分
-      mx = max(mx,tot-res);
-      // どの部分木も 1/2 * tot 以下である
-      if (mx * 2 <= tot) x = i;
-      return res;
     };
-    dfs(dfs,0);
-  }
 
-  ll ans = 0;
-  auto dfs = [&](auto self, int i, int dist = 0, int p = -1) -> void {
-    ans += (ll)dist * c[i];
-    for(auto v: g[i]) {
-      if (v == p) continue;
-      self(self,v,dist+1,i);
-    }
+    dfs(dfs,x);
+    cout << ans << endl;
   };
 
-  dfs(dfs,x);
-  cout << ans << endl;
+  // 全方位木DP
+  auto solve2 = [&]() {
+
+  };
+
+  solve1(); // 木の重心
+  // solve2(); // 全方位木DP
   return 0;
 }
