355defa217ef4b7caf271ff89b02c017e387779c
tic40 <ccpzjoh@gmail.com>
2024-04-29 21:20:11 +0900

    Update

--- abc/abc351/e/main.cpp
+++ abc/abc351/e/main.cpp
@@ -1,29 +1,22 @@
 #include <bits/stdc++.h>
-#include <atcoder/all>
-using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 using ll = long long;
-using P = pair<int,int>;
-const int INF = numeric_limits<int>::max();
-const ll LINF = numeric_limits<ll>::max();
-const int MOD = 1e9+7;
-// using mint = modint998244353; // modint1000000007;
 
 int main() {
   int n; cin >> n;
-  vector<vector<int>> xs(2), ys(2);
+  vector xs(2,vector<int>());
+  vector ys(2,vector<int>());
   REP(i,n) {
     int x,y; cin >> x >> y;
-    // x,y を 45 度回転する
-    int nx = x+y;
-    int ny = x-y;
+    // { x,y } 座標を 45 度回転する
+    int nx = x+y, ny = x-y;
     xs[nx%2].push_back(nx);
     ys[nx%2].push_back(ny);
   }
 
-  auto f = [&](vector<int> xs) {
+  auto f = [&](vector<int>& xs) {
     int n = xs.size();
     sort(xs.begin(),xs.end());
     ll res = 0;
