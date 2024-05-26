4672ddb6ecf7c928d672ad4fd9af80c454dddbdd
tic40 <ccpzjoh@gmail.com>
2024-05-26 10:44:07 +0900

    Update

--- abc/abc355/d/main.cpp
+++ abc/abc355/d/main.cpp
@@ -3,32 +3,26 @@ using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 using ll = long long;
-
-struct Event {
-  // type := 0: in, 1: out
-  int time, type;
-  bool operator<(const Event& other) const {
-    return time == other.time ? type < other.type : time < other.time;
-  }
-};
+using P = pair<int,int>;
 
 int main() {
   int n; cin >> n;
-  vector<Event> events;
+  vector<P> ev;
   REP(i,n) {
-    int l, r; cin >> l >> r;
-    events.emplace_back(l, 0);
-    events.emplace_back(r, 1);
+    int l,r; cin >> l >> r;
+    ev.emplace_back(l,0);
+    ev.emplace_back(r,1);
   }
-  sort(events.begin(), events.end());
-
+  sort(ev.begin(),ev.end());
   ll ans = 0;
   int now = 0;
-  for (auto [_,t] : events) {
-    if (t == 0) ans += now++;
-    else now--;
+  for(auto [k,v]: ev) {
+    if (v == 0) {
+      ans += now++;
+    } else {
+      now--;
+    }
   }
-
   cout << ans << endl;
   return 0;
 }
