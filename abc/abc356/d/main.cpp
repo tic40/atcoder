310c08ea5706c86bf21628cb9892283b8fa2e208
tic40 <ccpzjoh@gmail.com>
2024-06-02 00:08:04 +0900

    Update

--- abc/abc356/d/main.cpp
+++ abc/abc356/d/main.cpp
@@ -7,18 +7,21 @@ const int MOD = 998244353;
 
 int main() {
   ll n,m; cin >> n >> m;
-  vector<ll> bit_counts(60);
-  REP(bit,60) {
-    ll mask = 1LL << bit;
+  vector<ll> cnt(60); // cnt[i] := bit の i 桁目の総数 % MOD
+  // i := bit の i 桁目
+  REP(i,60) {
+    ll mask = 1LL << i;
     if (m & mask) {
       ll period = mask << 1;
       ll full_periods = (n + 1) / period;
-      bit_counts[bit] = full_periods * mask + max(0LL, (n + 1) % period - mask);
+      cnt[i] = full_periods * mask;
+      cnt[i] += max(0LL, (n + 1) % period - mask);
+      cnt[i] %= MOD;
     }
   }
 
   ll ans = 0;
-  REP(bit,60) ans = (ans + bit_counts[bit]) % MOD;
+  REP(bit,60) { ans += cnt[bit]; ans %= MOD; }
   cout << ans << endl;
   return 0;
 }
