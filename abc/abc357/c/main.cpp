8223cca42cfe87bf229cf0e103f40cc125062b3f
tic40 <ccpzjoh@gmail.com>
2024-06-10 09:27:12 +0900

    Update

--- abc/abc357/c/main.cpp
+++ abc/abc357/c/main.cpp
@@ -3,25 +3,26 @@ using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 
-void f(int n, int x, int y, vector<vector<char>>& ans) {
-  if (n == 0) return;
-  int size = pow(3,n-1);
-  REP(i,size) REP(j,size) ans[x+i+size][y+j+size] = '.';
-
-  REP(i,3) REP(j,3) if (i != 1 || j != 1) {
-    f(n-1, x+i*size, y+j*size, ans);
-  }
-}
-
 int main() {
-    int n; cin >> n;
-    int size = pow(3,n);
-    vector ans(size, vector<char>(size,'#'));
-    f(n,0,0,ans);
+  int n; cin >> n;
 
-    REP(i,size) {
-      REP(j,size) cout << ans[i][j];
-      cout << endl;
+  auto f = [&](auto self, int n) -> vector<string> {
+    vector<string> res;
+    if (n == 0) {
+      res.push_back("#");
+      return res;
     }
-    return 0;
+    auto p = self(self,n-1);
+    res.assign(p.size()*3, string(p.size()*3,'.'));
+    REP(i,3) REP(j,3) {
+      if (i == 1 && j == 1) continue;
+      REP(x,(int)p.size()) REP(y,(int)p.size()) {
+        res[i*p.size()+x][j*p.size()+y] = p[x][y];
+      }
+    }
+    return res;
+  };
+
+  for(auto v: f(f,n)) cout << v << endl;
+  return 0;
 }
