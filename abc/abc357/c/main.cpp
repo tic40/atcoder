e8015a26d3f72b99697931c2969dce810a233834
tic40 <ccpzjoh@gmail.com>
2024-06-11 09:29:01 +0900

    Update

--- abc/abc357/c/main.cpp
+++ abc/abc357/c/main.cpp
@@ -3,26 +3,22 @@ using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 
+vector<string> f(int n) {
+  if (n == 0) return { "#" };
+  auto p = f(n-1);
+  int psize = p.size();
+  vector<string> res(psize*3,string(psize*3,'.'));
+
+  REP(i,3) REP(j,3) if (i != 1 || j != 1) {
+    REP(x,psize) REP(y,psize) {
+      res[i*psize+x][j*psize+y] = p[x][y];
+    }
+  }
+  return res;
+}
+
 int main() {
   int n; cin >> n;
-
-  auto f = [&](auto self, int n) -> vector<string> {
-    vector<string> res;
-    if (n == 0) {
-      res.push_back("#");
-      return res;
-    }
-    auto p = self(self,n-1);
-    res.assign(p.size()*3, string(p.size()*3,'.'));
-    REP(i,3) REP(j,3) {
-      if (i == 1 && j == 1) continue;
-      REP(x,(int)p.size()) REP(y,(int)p.size()) {
-        res[i*p.size()+x][j*p.size()+y] = p[x][y];
-      }
-    }
-    return res;
-  };
-
-  for(auto v: f(f,n)) cout << v << endl;
+  for(auto s: f(n)) cout << s << endl;
   return 0;
 }
