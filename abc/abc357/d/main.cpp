4266b8866e242e1e531749d9996d7727312ccbdd
tic40 <ccpzjoh@gmail.com>
2024-06-16 21:36:08 +0900

    Update

--- abc/abc357/d/main.cpp
+++ abc/abc357/d/main.cpp
@@ -9,13 +9,12 @@ using mint = modint998244353; // modint1000000007;
 
 int main() {
   ll n; cin >> n;
-  mint a = 1;
-  {
-    ll x = n;
-    while(x) { a *= 10; x /= 10; }
-  }
-
-  mint s = n * (a.pow(n) - 1) / (a - 1);
+  int d = to_string(n).size();
+  // a := 等比数列として考えたときの比
+  mint r = mint(10).pow(d);
+  // 等比数列の和の公式
+  // s := N * ( 等比数列の和 )
+  mint s = n * (r.pow(n) - 1) / (r - 1);
   cout << s.val() << endl;
   return 0;
 }
