16a74d0a76d26889fbf1a4618f512d26381424d3
tic40 <ccpzjoh@gmail.com>
2024-06-16 13:55:38 +0900

    Update

--- abc/abc358/d/main.cpp
+++ abc/abc358/d/main.cpp
@@ -3,7 +3,6 @@ using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 using ll = long long;
-using P = pair<int,int>;
 
 int main() {
   int n,m; cin >> n >> m;
@@ -17,11 +16,6 @@ int main() {
   ll ans = 0;
   int p = 0;
   REP(i,m) {
-    if (a[p] >= b[i]) {
-      ans += a[p];
-      p = p+1;
-      continue;
-    }
     auto it = lower_bound(a.begin()+p, a.end(),b[i]);
     if (it == a.end()) { cout << -1 << endl; return 0; }
 
