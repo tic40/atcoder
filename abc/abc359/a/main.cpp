f7543d25ca518d65a37c5c8a17a7c0cd02ba22dd
tic40 <ccpzjoh@gmail.com>
2024-06-22 22:42:20 +0900

    Add abc359

---
+++ abc/abc359/a/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace atcoder;
+using namespace std;
+#define REP(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
+using ll = long long;
+using P = pair<int,int>;
+const int INF = numeric_limits<int>::max();
+const ll LINF = numeric_limits<ll>::max();
+const int MOD = 1e9+7;
+// using mint = modint998244353; // modint1000000007;
+
+int main() {
+  int n; cin >> n;
+  int ans = 0;
+  REP(i,n) {
+    string s; cin >> s;
+    if (s == "Takahashi") ans++;
+  }
+  cout << ans << endl;
+  return 0;
+}
