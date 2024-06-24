154275ba97adef71a58f9ac86abd2ca6fcffa96b
tic40 <ccpzjoh@gmail.com>
2024-06-24 23:54:50 +0900

    Update

--- other_contest/edpc/a/main.cpp
+++ other_contest/edpc/a/main.cpp
@@ -1,17 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define REP(i,n) for(int i=0;i<(n);i++)
+template<class T> void chmin(T& a, T b) { a = min(a,b); }
 
 int main() {
   int n; cin >> n;
-  vector<int> h(n+2);
+  vector<int> h(n);
   REP(i,n) cin >> h[i];
 
   vector<int> dp(n+2, 1e9);
   dp[0] = 0;
   REP(i,n) {
-    dp[i+1] = min(dp[i+1], dp[i] + abs(h[i]-h[i+1]));
-    dp[i+2] = min(dp[i+2], dp[i] + abs(h[i]-h[i+2]));
+    chmin(dp[i+1], dp[i] + abs(h[i+1]-h[i]));
+    chmin(dp[i+2], dp[i] + abs(h[i+2]-h[i]));
   }
 
   cout << dp[n-1] << endl;
