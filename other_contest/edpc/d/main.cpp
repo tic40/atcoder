094b5ea083f392db15a2b342d5cd0a416288814f
tic40 <ccpzjoh@gmail.com>
2024-06-25 07:31:14 +0900

    Update

--- other_contest/edpc/d/main.cpp
+++ other_contest/edpc/d/main.cpp
@@ -1,26 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
 using ll = long long;
+template<class T> void chmax(T& a, T b) { a = max(a,b); }
 
 int main() {
   int n,W; cin >> n >> W;
-  vector<int>  w(n),v(n);
+  vector<int> w(n),v(n);
   REP(i,n) cin >> w[i] >> v[i];
 
-  vector<ll> dp(W+1); // [重さ] = 最大の価値
-  REP(i,n) {
-    vector<ll> p(W+1);
-    REP(now,W+1) {
-      p[now] = max(p[now], dp[now]);
-      if (W < now + w[i]) continue;
-      p[ now + w[i] ] = max( p[ now + w[i] ], dp[now] + v[i] );
-    }
-    swap(dp,p);
+  // dp[i][j] := i 個までで重さ j のときの最大の価値
+  vector dp(n+1, vector<ll>(W+1));
+
+  REP(i,n) REP(j,W+1) {
+    chmax(dp[i+1][j],dp[i][j]);
+    int nj = j + w[i];
+    if (nj <= W) chmax(dp[i+1][nj],dp[i][j] + v[i]);
   }
 
-  ll ans = 0;
-  REP(i,W+1) ans = max(ans, dp[i]);
+  ll ans = *max_element(dp[n].begin(),dp[n].end());
   cout << ans << endl;
   return 0;
 }
