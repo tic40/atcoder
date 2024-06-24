f9dc8ee64c308c75b5adefb9eecc60b788e9e1c3
tic40 <ccpzjoh@gmail.com>
2024-06-25 00:03:33 +0900

    Update

--- other_contest/edpc/b/main.cpp
+++ other_contest/edpc/b/main.cpp
@@ -1,18 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<(n);i++)
-const int INF = 1e9;
+#define REP(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
+const int INF = numeric_limits<int>::max();
+template<class T> void chmin(T& a, T b) { a = min(a,b); }
 
 int main() {
   int n,k; cin >> n >> k;
   vector<int> h(n);
   REP(i,n) cin >> h[i];
 
-  vector<int> dp(n, INF);
+  vector<int> dp(n,INF);
   dp[0] = 0;
-  REP(i,n) for (int j = 1; j <= k; j++) {
-    if (i-j < 0) break;
-    dp[i] = min(dp[i], dp[i-j] + abs(h[i]-h[i-j]));
+  REP(i,n) {
+    for(int j = 1; j <= k; j++) {
+      int nj = i+j;
+      if (nj >= n) break;
+      chmin(dp[nj], dp[i] + abs(h[nj] - h[i]));
+    }
   }
 
   cout << dp[n-1] << endl;
