ff8778ac49ad5f69eac2768d7a992b9f38ce5400
tic40 <ccpzjoh@gmail.com>
2024-06-30 00:39:54 +0900

    Solve

---
+++ abc/abc359/d/main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace atcoder;
+using namespace std;
+#define REP(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
+using mint = modint998244353; // modint1000000007;
+
+auto is_palindrome(string s) {
+  string t = s;
+  reverse(t.begin(),t.end());
+  return t == s;
+}
+
+int main() {
+  int n,k; cin >> n >> k;
+  string s; cin >> s;
+  // dp[i][t] := i まで決めて直前 k-1 文字が t である
+  map<string,mint> dp;
+  dp[""] = 1;
+  REP(i,n) {
+    map<string,mint> old;
+    swap(dp,old);
+    for(auto [t,num]: old) {
+      for(char c = 'A'; c <= 'B'; c++) {
+        if (s[i] != '?' && s[i] != c) continue;
+        string nt = t + c;
+        if ((int)nt.size() == k && is_palindrome(nt)) continue;
+        if ((int)nt.size() == k) nt.erase(nt.begin());
+
+        dp[nt] += num;
+      }
+    }
+  }
+
+  mint ans = 0;
+  for(auto [t,num]: dp) ans += num;
+  cout << ans.val() << endl;
+  return 0;
+}
