efc9166a513a34468ef54d6e759d282404ed4b37
tic40 <ccpzjoh@gmail.com>
2024-07-02 23:11:58 +0900

    Update

--- abc/abc360/e/main.cpp
+++ abc/abc360/e/main.cpp
@@ -8,12 +8,14 @@ using mint = modint998244353; // modint1000000007;
 
 int main() {
   int n,k; cin >> n >> k;
-  mint p = mint(2) / n / n; // p = 2/n^2
-  mint x = 1;
-  REP(ki,k) {
-    x = x * (1-p * (n-1)) + (mint(1) - x) * p;
+  mint p = mint(2) * (n-1) / n / n;
+  mint q = mint(2) / n / n;
+  vector<mint> dp(k+1); // dp[i] := i 回後に黒いボールが最も左にある確率
+  dp[0] = 1;
+  REP(i,k) {
+    dp[i+1] = (mint(1)-p) * dp[i] + q * (mint(1) - dp[i]);
   }
-  mint ans = x + (mint(1) - x) * (mint(2+n) / 2);
+  mint ans = dp[k] + (mint(1) - dp[k]) * (mint(2+n) / 2);
   cout << ans.val() << endl;
   return 0;
 }
