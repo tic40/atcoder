33bdf5b9596e595ddbcd0f01d9e117cfe714a7ca
tic40 <ccpzjoh@gmail.com>
2024-07-02 10:09:13 +0900

    Update

--- abc/abc360/c/main.cpp
+++ abc/abc360/c/main.cpp
@@ -2,28 +2,21 @@
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
-using P = pair<int,int>;
 using ll = long long;
 
 int main() {
   int n; cin >> n;
   vector<int> a(n),w(n);
-  REP(i,n) { cin >> a[i]; a[i]--; }
+  REP(i,n) cin >> a[i];
   REP(i,n) cin >> w[i];
 
-  vector<P> pa;
-  vector<int> box(n);
-  REP(i,n) {
-    pa.emplace_back(w[i],a[i]);
-    box[a[i]]++;
-  }
-  sort(pa.begin(),pa.end());
-
   ll ans = 0;
-  for(auto [k,v]: pa) if (box[v] > 1) {
-    ans += k;
-    box[v]--;
+  vector<int> mx(1e5+1);
+  REP(i,n) {
+    ans += min(mx[a[i]],w[i]);
+    mx[a[i]] = max(mx[a[i]],w[i]);
   }
+
   cout << ans << endl;
   return 0;
 }
