994a6db06ccf3060880319f1be6809c50f2c85ce
tic40 <ccpzjoh@gmail.com>
2024-07-09 00:24:35 +0900

    Update

--- other_contest/edpc/o/main.cpp
+++ other_contest/edpc/o/main.cpp
@@ -1,27 +1,27 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace atcoder;
 using namespace std;
-#define REP(i,n) for(int i=0;i<(n);i++)
-using ll = long long;
-const int MOD = 1e9+7;
+#define REP(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
+using mint = modint1000000007;
 
 int main() {
   int n; cin >> n;
-  vector<vector<int>> a(n,vector<int>(n));
+  vector a(n,vector<int>(n));
   REP(i,n) REP(j,n) cin >> a[i][j];
 
-  vector<ll> dp((1<<n)+1);
-  dp[0]=1;
-  REP(bit, (1<<n)-1) {
-    int i = __builtin_popcount(bit);
-    REP(j,n) {
-      if ( (bit >> j & 1) == 0 && a[i][j]) {
-        int nbit = bit | (1<<j);
-        dp[nbit] += dp[bit];
-        dp[nbit] %= MOD;
-      }
+  // dp[bit] := 女性の bit 集合が男性とペアになっているときの組み合わせ数
+  vector<mint> dp(1<<n);
+  dp[0] = 1;
+  REP(bit,(1<<n)-1) REP(i,n) {
+    // popcount 番目の男性が i 番目の女性とペアになる遷移
+    if ((bit >> i & 1) == 0 && a[__builtin_popcount(bit)][i]) {
+      int nbit = bit | (1 << i);
+      dp[bit | (1<<i)] += dp[bit];
     }
   }
 
-  cout << dp[(1<<n)-1] << endl;
+  cout << dp[(1<<n)-1].val() << endl;
   return 0;
 }
