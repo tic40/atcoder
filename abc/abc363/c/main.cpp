414fbaf49857165322707434fd7e4270c0a20f88
tic40 <ccpzjoh@gmail.com>
2024-07-22 09:27:23 +0900

    Update

--- abc/abc363/c/main.cpp
+++ abc/abc363/c/main.cpp
@@ -7,27 +7,19 @@ int main() {
   int n,k; cin >> n >> k;
   string s; cin >> s;
 
-  vector<int> b(n);
-  set<string> st;
-  iota(b.begin(),b.end(),0);
-  do {
-    string t = "";
-    REP(i,n) t += s[b[i]];
-    st.insert(t);
-  } while (next_permutation(b.begin(), b.end()));
-
   int ans = 0;
-  for(auto v: st) {
+  sort(s.begin(),s.end());
+  do {
     auto is_palindrome = [&](int i) {
-      REP(j,k/2) if (v[i+j] != v[i+k-1-j]) return false;
+      REP(j,k/2) if (s[i+j] != s[i+k-1-j]) return false;
       return true;
     };
 
     bool ok = true;
     REP(i,n-k+1) if (is_palindrome(i)) { ok = false; break; }
     if (ok) ans++;
-  }
-  cout << ans << endl;
+  } while (next_permutation(s.begin(), s.end()));
 
+  cout << ans << endl;
   return 0;
 }
