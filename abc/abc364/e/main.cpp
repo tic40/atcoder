fe45a9900edd8f1fc24607c1dd3d447f4041f4e1
tic40 <ccpzjoh@gmail.com>
2024-08-01 10:35:41 +0900

    Update

--- abc/abc364/e/main.cpp
+++ abc/abc364/e/main.cpp
@@ -10,25 +10,24 @@ int main() {
   vector<int> a(n),b(n);
   REP(i,n) cin >> a[i] >> b[i];
 
-  // dp[i][j] :=  甘さが i で、個数が j のときのしょっぱさの最小値
+  // dp[i][j][k] :=  i 個までみたとき、甘さが i、個数が j のときのしょっぱさの最小値
   // 甘さ x しょっぱさ = 個数で DP テーブルを持つと n * x * y = 8e9 となり大きすぎる
   // 甘さ x 個数 = しょっぱさで DP テーブルを持つと n * x * n = 80^2 * 1000 = 6e5 で済む
-  vector dp(x+1,vector<int>(n+1,INF));
-  dp[0][0] = 0;
+  vector dp(n+1,vector(x+1,vector<int>(n+1,INF)));
+  dp[0][0][0] = 0;
 
   REP(i,n) {
-    vector old(x+1,vector<int>(n+1,INF));
-    swap(dp,old);
     REP(j,x+1) REP(k,n+1) {
-      int now = old[j][k];
+      int now = dp[i][j][k];
       if (now == INF) continue;
-      chmin(dp[j][k],old[j][k]);
-      if (j+a[i] <= x) chmin(dp[j+a[i]][k+1],now+b[i]);
+      chmin(dp[i+1][j][k],dp[i][j][k]);
+      if (j+a[i] <= x) chmin(dp[i+1][j+a[i]][k+1],now+b[i]);
     }
   }
 
   int ans = 0;
-  REP(j,x+1) REP(k,n+1) if (dp[j][k] <= y) ans = max(ans,k);
+  REP(j,x+1) REP(k,n+1) if (dp[n][j][k] <= y) ans = max(ans,k);
+  // 全部食べてなければ、x も y 越えてないのでもう一つ食べられる
   if (ans < n) ans++;
   cout << ans << endl;
   return 0;
