5d6bef6fcf4dd94f7c6fa81fba464c9c91697456
tic40 <ccpzjoh@gmail.com>
2024-08-12 11:16:36 +0900

    Update

--- other_contest/tessoku_book/b13/main.cpp
+++ other_contest/tessoku_book/b13/main.cpp
@@ -9,16 +9,14 @@ int main() {
   vector<int> a(n);
   REP(i,n) cin >> a[i];
 
-  ll ans = 0, sum = 0;
-  int r = 0;
-  REP(l,n) {
-    while(r < n && sum+a[r] <= k) {
-      sum += a[r];
-      r++;
-    }
-    ans += r-l;
-    if (r == l) r++;
-    else sum -= a[l];
+  int j = 0;
+  ll now = 0;
+  int ans = 0;
+  REP(i,n) {
+    while(j < n && now+a[j] <= k) { now += a[j]; j++; }
+    ans += j-i;
+    if (j == i) j++;
+    else now -= a[i];
   }
   cout << ans << endl;
   return 0;
