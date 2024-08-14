644109c7edc1761dbb8e1680d2c23d71066a9ec1
tic40 <ccpzjoh@gmail.com>
2024-08-14 23:23:31 +0900

    Update

--- daily_training/20240808_2/i/main.cpp
+++ daily_training/20240808_2/i/main.cpp
@@ -5,34 +5,27 @@ using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 
-const int m = 10;
+const int M = 10;
 
 int main() {
   int n,q; cin >> n >> q;
-  // uf[i] := 辺の重みがすべて i 以上の dsu
-  vector<dsu> uf(m+1,dsu(n));
-  // num[i] :=　辺の重みが i 以上の連結になっている辺の数
-  vector<int> num(m+1);
+  vector<dsu> uf(M+1,dsu(n));
+  vector<int> cnt(M+1);
 
-  auto add = [&](int u, int v, int w) -> void {
-    for(int i = w; i <= m; i++) if (!uf[i].same(u,v)) {
-      num[i]++;
-      uf[i].merge(u,v);
+  auto add = [&]() -> void {
+    int a,b,c; cin >> a >> b >> c;
+    a--; b--;
+    for(int i = c; i <= M; i++) if (!uf[i].same(a,b))  {
+      cnt[i]++;
+      uf[i].merge(a,b);
     }
   };
 
-  REP(i,n-1) {
-    int a,b,c; cin >> a >> b >> c; a--; b--;
-    add(a,b,c);
-  }
-
+  REP(i,n-1) add();
   REP(_,q) {
-    int u,v,w; cin >> u >> v >> w; u--; v--;
-    add(u,v,w);
-
-    // 最小全域木に含まれる辺の重みの和
+    add();
     int ans = 0;
-    for(int i = 1; i <= m; i++) ans += i * (num[i] - num[i-1]);
+    for(int i = 1; i <= M; i++) ans += (cnt[i] - cnt[i-1]) * i;
     cout << ans << endl;
   }
   return 0;
