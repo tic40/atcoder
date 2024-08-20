4074d451a692331f1525ffca875eb539fa58d817
tic40 <ccpzjoh@gmail.com>
2024-08-20 23:41:31 +0900

    Solve

---
+++ abc/abc367/f/main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
+using ll = long long;
+using ull = unsigned long long;
+
+mt19937_64 rng(58); // 64bit 乱数生成
+const ull mod = (1LL<<61) - 1; // 素数
+
+int main() {
+  int n,q; cin >> n >> q;
+  vector<int> a(n), b(n);
+  REP(i,n) cin >> a[i];
+  REP(i,n) cin >> b[i];
+
+  const int MX = 2e5+5;
+  vector<ull> h(MX);
+  REP(i,MX) h[i] = rng() % mod;
+
+  vector<ull> sa(n+1), sb(n+1);
+  REP(i,n) sa[i+1] = (sa[i]+h[a[i]]) % mod;
+  REP(i,n) sb[i+1] = (sb[i]+h[b[i]]) % mod;
+
+  REP (_,q) {
+    int la, ra, lb, rb;
+    cin >> la >> ra >> lb >> rb;
+    la--; lb--;
+    bool ok = (sa[ra] - sa[la] + mod) % mod == (sb[rb] - sb[lb] + mod) % mod;
+    cout << (ok ? "Yes" : "No") << endl;
+  }
+
+  return 0;
+}
