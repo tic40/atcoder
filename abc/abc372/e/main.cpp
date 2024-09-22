911c8afb39f0af0198412bc460f16ef8030da20f
tic40 <ccpzjoh@gmail.com>
2024-09-22 18:41:43 +0900

    Update

--- abc/abc372/e/main.cpp
+++ abc/abc372/e/main.cpp
@@ -5,40 +5,38 @@ using namespace std;
 
 struct UnionFind {
   vector<int> d;
-  vector<set<int>> member; // 頂点番号を管理
-  UnionFind(int n=0): d(n,-1), member(n) {
-    REP(i,n) member[i].insert(i);
+  vector<set<int>> st;
+  UnionFind(int n = 0): d(n,-1), st(n) {
+    REP(i,n) st[i].insert(i);
   }
   int root(int x) {
     return d[x] < 0 ? x : d[x] = root(d[x]);
   }
-  bool unite(int x, int y) {
+  int unite(int x, int y) {
     x = root(x); y = root(y);
-    if (x == y) return false;
+    if (x == y) return 0;
     if (d[x] > d[y]) swap(x,y);
     d[x] += d[y];
     d[y] = x;
 
-    member[x].insert(member[y].begin(),member[y].end());
-    while(member[x].size() > 10) member[x].erase(member[x].begin());
-    return true;
+    st[x].insert(st[y].begin(),st[y].end());
+    while(st[x].size() > 10) st[x].erase(st[x].begin());
+    return 1;
   }
-  int kth_largest(int v, int k) {
-    v = root(v);
-    if ((int)member[v].size() < k) return -1;
-    auto it = member[v].rbegin();
-    advance(it,k-1); // k 番目
-    return *it + 1;
+  int kth_largest(int u, int k) {
+    u = root(u);
+    if ((int)st[u].size() < k) return -1;
+    auto it = st[u].rbegin();
+    advance(it,k-1);
+    return *it+1;
   }
-  bool same(int x, int y) { return root(x) == root(y); }
-  int size(int x) { return -d[root(x)]; }
 };
 
 int main() {
-  int n, q; cin >> n >> q;
+  int n,q; cin >> n >> q;
   UnionFind uf(n);
   REP(_,q) {
-    int t, u, v; cin >> t >> u >> v;
+    int t,u,v; cin >> t >> u >> v;
     u--; v--;
     if (t == 1) uf.unite(u,v);
     if (t == 2) cout << uf.kth_largest(u,v+1) << endl;
