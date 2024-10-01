f3669587dbfcf601b04a9f8dcf22e25b385eec27
tic40 <ccpzjoh@gmail.com>
2024-10-01 10:11:27 +0900

    Update

--- abc/abc373/d/main.cpp
+++ abc/abc373/d/main.cpp
@@ -5,10 +5,9 @@ using namespace std;
 using ll = long long;
 using P = pair<int,int>;
 
-struct Edge { int to, cost; };
 int main() {
   int n,m; cin >> n >> m;
-  vector g(n,vector<Edge>());
+  vector g(n,vector<P>());
   REP(i,m) {
     int u,v,w; cin >> u >> v >> w;
     u--; v--;
@@ -16,19 +15,16 @@ int main() {
     g[v].emplace_back(u,-w);
   }
 
+  vector<ll> ans(n);
   vector<int> visited(n);
-  vector<ll> ans(n,0);
-  auto dfs = [&](auto self, int i) -> void {
+  auto dfs = [&](auto self, int i) {
     if (visited[i]) return;
     visited[i] = true;
-
-    for(auto [to, cost]: g[i]) {
-      if (visited[to]) continue;
-      ans[to] = ans[i] + cost; // ans[to] - ans[i] = cost;
+    for(auto [to,cost]: g[i]) if (!visited[to]) {
+      ans[to] = cost + ans[i];
       self(self,to);
     }
   };
-
   REP(i,n) dfs(dfs,i);
   for(auto v: ans) cout << v << " ";
   return 0;
