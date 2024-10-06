b26a4087e45d737fc51f4bee6457a0a275946d65
tic40 <ccpzjoh@gmail.com>
2024-10-06 21:24:50 +0900

    Update

--- abc/abc374/d/main.cpp
+++ abc/abc374/d/main.cpp
@@ -2,14 +2,12 @@
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
-using ll = long long;
 using P = pair<int,int>;
 
 int main() {
   int n,s,t; cin >> n >> s >> t;
-  vector<int> a(n),b(n),c(n),d(n);
-  REP(i,n) cin >> a[i] >> b[i] >> c[i] >> d[i];
-  auto dist = [](int x, int y) -> double { return sqrt(x*x + y*y); };
+  vector<P> a(n),b(n);
+  REP(i,n) cin >> a[i].first >> a[i].second >> b[i].first >> b[i].second;
 
   vector<int> id(n);
   iota(id.begin(),id.end(),0);
@@ -20,21 +18,11 @@ int main() {
       int x = 0, y = 0;
       REP(i,n) {
         int idx = id[i];
-        // (a,b) -> (c,d)
-        if (bit >> i & 1) {
-          auto d1 = dist(a[idx] - x, b[idx] - y);
-          tot += d1 / s;
-          auto d2 = dist(c[idx] - a[idx], d[idx] - b[idx]);
-          tot += d2 / t;
-          x = c[idx], y = d[idx];
-        // (c,d) -> (a,b)
-        } else {
-          auto d1 = dist(c[idx] - x, d[idx] - y);
-          tot += d1 / s;
-          auto d2 = dist(c[idx] - a[idx], d[idx] - b[idx]);
-          tot += d2 / t;
-          x = a[idx], y = b[idx];
-        }
+        if (bit >> i & 1) swap(a[idx],b[idx]);
+        tot += hypot(a[idx].first - x, a[idx].second - y) / s;
+        tot += hypot(b[idx].first - a[idx].first, b[idx].second - a[idx].second) / t;
+        x = b[idx].first, y = b[idx].second;
+        if (bit >> i & 1) swap(a[idx],b[idx]);
       }
       ans = min(ans,tot);
     }
