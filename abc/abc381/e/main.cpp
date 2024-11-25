6ad93d7ef0610c05049b435e8a7069c163514435
tic40 <ccpzjoh@gmail.com>
2024-11-25 23:17:59 +0900

    Update

--- abc/abc381/e/main.cpp
+++ abc/abc381/e/main.cpp
@@ -3,43 +3,39 @@ using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 using ll = long long;
-const int INF = numeric_limits<int>::max();
+const int INF = 1e9;
 
 int main() {
   int n,q; cin >> n >> q;
   string s; cin >> s;
 
-  vector is(3,vector<int>());
-  REP(i,n) {
-    int now = s[i] == '/' ? 0 : s[i]-'0';
-    is[now].push_back(i);
-  }
+  map<char,vector<int>> mp;
+  REP(i,n) mp[s[i]].push_back(i);
 
-  // c: 文字種, i: 位置, x: 連続する文字数
-  auto getNext = [&](int c, int i, int x) {
-    if (x == 0) return i;
-    auto& nis = is[c];
-    int j = lower_bound(nis.begin(),nis.end(),i) - nis.begin();
-    j += x-1;
-    return j < (int)nis.size() ? nis[j]+1 : INF;
+  auto getNext = [&](char c, int i, int k) {
+    if (k == 0) return i;
+    auto& vec = mp[c];
+    int j = lower_bound(vec.begin(),vec.end(),i) - vec.begin();
+    j += k-1;
+    return j < (int)vec.size() ? vec[j] : INF;
   };
 
   REP(qi,q) {
     int l,r; cin >> l >> r;
-    l--; // 半開区間
+    l--; r--;
 
-    auto judge = [&](int k) {
+    auto f = [&](int k) {
       int i = l;
-      i = getNext(1,i,k);
-      i = getNext(0,i,1); // slash
-      i = getNext(2,i,k);
+      i = getNext('1',i,k);
+      i = getNext('/',i+1,1);
+      i = getNext('2',i+1,k);
       return i <= r;
     };
 
     int ok = -1, ng = n;
     while(ng-ok>1) {
       int mid = (ok+ng)/2;
-      if (judge(mid)) ok = mid;
+      if (f(mid)) ok = mid;
       else ng = mid;
     }
     cout << (ok == -1 ? 0 : ok*2+1) << endl;
