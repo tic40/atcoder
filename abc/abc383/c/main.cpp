c2bfefcb760137805094e44167f2965a282e0082
tic40 <ccpzjoh@gmail.com>
2024-12-11 23:17:20 +0900

    Update

--- abc/abc383/c/main.cpp
+++ abc/abc383/c/main.cpp
@@ -2,39 +2,39 @@
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
-using ll = long long;
-using T = tuple<int,int,int>;
-const vector<pair<int, int>> moves = { {-1,0},{0,1},{1,0},{0,-1} };
+using P = pair<int,int>;
+const vector<int> di = {-1,0,1,0};
+const vector<int> dj = {0,1,0,-1};
 
 int main() {
   int h,w,d; cin >> h >> w >> d;
   vector<string> s(h);
   REP(i,h) cin >> s[i];
 
-  vector dist(h,vector<int>(w,-1));
-  priority_queue<T, vector<T>, less<T>> q;
+  vector visited(h,vector<int>(w,-1));
+  queue<P> q;
   REP(i,h) REP(j,w) if (s[i][j] == 'H') {
-    q.emplace(d,i,j);
-    dist[i][j] = d;
+    q.emplace(i,j);
+    visited[i][j] = d;
   }
 
   while(q.size()) {
-    auto [nd,i,j] = q.top(); q.pop();
-    if (dist[i][j] > nd) continue;
-    if (nd == 0) continue;
-    for(auto [di,dj]: moves) {
-      int ni = i+di;
-      int nj = j+dj;
+    auto [x,y] = q.front(); q.pop();
+    int nd = visited[x][y];
+    REP(i,4) {
+      int ni = x+di[i];
+      int nj = y+dj[i];
       if (ni < 0 || nj < 0 || ni >= h || nj >= w) continue;
-      if (s[ni][nj] != '.') continue;
-      if (dist[ni][nj] >= nd-1) continue;
-      dist[ni][nj] = nd-1;
-      q.emplace(nd-1,ni,nj);
+      if (s[ni][nj] == '#') continue;
+      if (nd-1 > visited[ni][nj]) {
+        visited[ni][nj] = nd-1;
+        q.emplace(ni,nj);
+      }
     }
   }
 
   int ans = 0;
-  REP(i,h) REP(j,w) if (dist[i][j] != -1) ans++;
+  REP(i,h) REP(j,w) if (visited[i][j] >= 0) ans++;
   cout << ans << endl;
   return 0;
 }
