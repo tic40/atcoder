d03864ae9923b56c31d2bcdc45253b481abf65b5
tic40 <ccpzjoh@gmail.com>
2024-12-11 22:56:08 +0900

    Update

--- abc/abc383/b/main.cpp
+++ abc/abc383/b/main.cpp
@@ -8,34 +8,20 @@ int main() {
   vector<string> s(h);
   REP(i,h) cin >> s[i];
 
-  vector g(h,vector<int>(w));
-
-  auto f = [&](int x, int y) -> void {
-    for(int i = x-d; i <= x+d; i++) {
-      for(int j = y-d; j <= y+d; j++) {
-        if (i < 0 || j < 0 || i >= w || j >= h) continue;
-        int dist = abs(x-i) + abs(y-j);
-        if (dist > d) continue;
-        if (s[j][i] == '#') continue;
-        g[j][i] = 1;
-      }
-    }
-  };
-
   int ans = 0;
-  REP(i,h*w) REP(j,h*w) if (i != j) {
-    int x1 = i % w;
-    int y1 = i / w;
-    int x2 = j % w;
-    int y2 = j / w;
-
-    if (s[y1][x1] == '#' || s[y2][x2] == '#') continue;
-    g = vector(h,vector<int>(w));
-    f(x1,y1);
-    f(x2,y2);
-    int now = 0;
-    REP(i,h) REP(j,w) now += g[i][j];
-    ans = max(ans,now);
+  REP(i1,h) REP(j1,w) REP(i2,h) REP(j2,w) if (s[i1][j1] == '.' && s[i2][j2] == '.') {
+    auto t = s;
+    auto f = [&](int ni, int nj) {
+      int res = 0;
+      for(int i = max(0,ni-d); i <= min(h-1,i+d); i++) {
+        for(int j = max(0,nj-d); j <= min(w-1,j+d); j++) {
+          int nd = abs(i-ni) + abs(j-nj);
+          if (nd <= d && t[i][j] == '.') { res++; t[i][j] = '#'; }
+        }
+      }
+      return res;
+    };
+    ans = max(ans, f(i1,j1)+f(i2,j2));
   }
   cout << ans << endl;
   return 0;
