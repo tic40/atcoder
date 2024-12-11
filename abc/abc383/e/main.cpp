550e9c867b1c94393ca1af8baeae056abaae18e1
tic40 <ccpzjoh@gmail.com>
2024-12-12 08:59:40 +0900

    Update

--- abc/abc383/e/main.cpp
+++ abc/abc383/e/main.cpp
@@ -5,14 +5,12 @@ using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 using ll = long long;
-using T = tuple<int,int,int>;
 
 int main() {
   int n,m,k; cin >> n >> m >> k;
-  vector<T> edges;
+  vector<tuple<int,int,int>> edges;
   REP(i,m) {
-    int u,v,w; cin >> u >> v >> w;
-    u--; v--;
+    int u,v,w; cin >> u >> v >> w; u--; v--;
     edges.emplace_back(w,u,v);
   }
   sort(edges.begin(),edges.end());
@@ -26,17 +24,17 @@ int main() {
   for(auto [w,u,v]: edges) if (!uf.same(u,v)) {
     u = uf.leader(u);
     v = uf.leader(v);
-    if (a[v]) swap(u,v); // u が a, v が b になるように swap
+    if (!a[v] || !b[u]) swap(u,v); // u が a, v が b になるように swap
 
     // x = マッチングできる数。これは両者の min の数だけ
-    int x = min(a[u],b[v]);
-    ans += (ll)w * x;
-    a[u] -= x; b[v] -= x;
-    int suma = a[u]+a[v];
-    int sumb = b[u]+b[v];
+    int add = min(a[u],b[v]);
+    ans += (ll)w * add;
+    a[u] -= add; b[v] -= add;
+    int sa = a[u]+a[v];
+    int sb = b[u]+b[v];
     uf.merge(u,v);
-    a[uf.leader(u)] = suma;
-    b[uf.leader(u)] = sumb;
+    a[uf.leader(u)] = sa;
+    b[uf.leader(u)] = sb;
   }
   cout << ans << endl;
   return 0;
