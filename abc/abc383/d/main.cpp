3c7289855775c76c773c5c9c69023be109ae3687
tic40 <ccpzjoh@gmail.com>
2024-12-11 23:38:57 +0900

    Update

--- abc/abc383/d/main.cpp
+++ abc/abc383/d/main.cpp
@@ -6,33 +6,27 @@ using ll = long long;
 
 int main() {
   ll n; cin >> n;
-
   int n2 = sqrt(n);
   vector<int> is_prime(n2+1,1);
-  vector<int> primes;
   for(int i = 2; i <= n2; i++) {
-    if (!is_prime[i]) continue;
-    primes.push_back(i);
-    for(int j = i*2; j <= n2; j+=i) is_prime[j] = 0;
+    for(int j=i*2; j <= n2; j+=i) is_prime[j] = 0;
   }
+  vector<int> p;
+  for(int i = 2; i <= n2; i++) if (is_prime[i]) p.push_back(i);
 
-  int sz = primes.size();
   int ans = 0;
-  for(auto v: primes) {
-    ll now = v;
-    int cnt = 1;
+  for(auto v: p) {
+    int cnt = 1; ll now = v;
     while(cnt < 8 && now*v <= n) { now *= v; cnt++; }
     if (cnt < 8) break;
     ans++;
   }
 
-  REP(i,sz) {
-    ll a = (ll)primes[i] * primes[i];
-    for(int j = i+1; j < sz; j++) {
-      ll b = (ll)primes[j] * primes[j];
-      if (a > n/b) break;
-      ans++;
-    }
+  REP(i,(int)p.size()) for(int j = i+1; j < (int)p.size(); j++) {
+    ll a = (ll)p[i]*p[i];
+    ll b = (ll)p[j]*p[j];
+    if (a > n/b) break;
+    ans++;
   }
   cout << ans << endl;
   return 0;
