b83cbb3d09635deb8aa46eee435e8ee9f5ee44ae
tic40 <ccpzjoh@gmail.com>
2024-12-15 15:28:54 +0900

    Update

--- abc/abc384/d/main.cpp
+++ abc/abc384/d/main.cpp
@@ -6,20 +6,17 @@ using ll = long long;
 
 int main() {
   int n; ll s; cin >> n >> s;
-  vector<ll> a(n);
+  vector<int> a(n);
   REP(i,n) cin >> a[i];
-
-  ll tot = accumulate(a.begin(),a.end(),0LL);
-  REP(i,n) a.push_back(a[i]);
   int n2 = n*2;
   vector<ll> sum(n2+1);
-  REP(i,n2) sum[i+1] = sum[i]+a[i];
+  REP(i,n2) sum[i+1] = sum[i] + a[i%n];
 
-  s %= tot;
+  s = s % accumulate(a.begin(),a.end(),0LL);
   int ok = 0;
   REP(i,n) {
-    ll k = s+sum[i];
-    ok |= (k == *lower_bound(sum.begin(),sum.end(),k));
+    ll k = sum[i]+ s;
+    ok |= k == *lower_bound(sum.begin(),sum.end(),k);
   }
   cout << (ok ? "Yes" : "No") << endl;
   return 0;
