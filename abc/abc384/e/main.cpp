f470d71261ea080344323447960f34366a22a703
tic40 <ccpzjoh@gmail.com>
2024-12-15 15:40:53 +0900

    Update

--- abc/abc384/e/main.cpp
+++ abc/abc384/e/main.cpp
@@ -4,38 +4,38 @@ using namespace std;
 #define endl '\n'
 using ll = long long;
 using P = pair<int,int>;
-
+using T = pair<ll,pair<int,int>>;
 const vector<int> di = {-1,0,1,0};
 const vector<int> dj = {0,1,0,-1};
 
 int main() {
-  int h,w; ll x; cin >> h >> w >> x;
+  int h,w,x; cin >> h >> w >> x;
   int p,q; cin >> p >> q; p--; q--;
   vector s(h,vector<ll>(w));
   REP(i,h) REP(j,w) cin >> s[i][j];
 
-  priority_queue<pair<ll,P>, vector<pair<ll,P>>, greater<pair<ll,P>>> pq;
+  priority_queue<T, vector<T>, greater<T>> pq;
   vector visited(h,vector<int>(w));
-  ll power = 0;
 
-  auto add = [&](int i, int j) {
+  ll power = 0;
+  auto push = [&](int i, int j) -> void {
     visited[i][j] = 1;
     power += s[i][j];
     REP(k,4) {
       int ni = i+di[k], nj = j+dj[k];
       if (ni < 0 || nj < 0 || ni >= h || nj >= w) continue;
       if (visited[ni][nj]) continue;
-      pq.emplace(s[ni][nj],P{ni,nj});
+      pq.emplace(s[ni][nj], P{ni,nj});
     }
   };
+  push(p,q);
 
-  add(p,q);
   while(pq.size()) {
-    auto [_, pa] = pq.top(); pq.pop();
-    auto [i,j] = pa;
+    auto [_, pos] = pq.top(); pq.pop();
+    auto [i,j] = pos;
     if (visited[i][j]) continue;
-    if ((double)s[i][j] >= (double)power/x) break;
-    add(i,j);
+    if ((double)s[i][j] >= (double)power/x) continue;
+    push(i,j);
   }
   cout << power << endl;
   return 0;
