56d92da0c55321e295a365f90a6da521cb931b6b
tic40 <ccpzjoh@gmail.com>
2024-12-28 23:42:37 +0900

    Update

--- abc/abc386/d/main.cpp
+++ abc/abc386/d/main.cpp
@@ -7,22 +7,19 @@ const int INF = numeric_limits<int>::max();
 
 int main() {
   int n,m; cin >> n >> m;
-  map<P,char>mp;
+  map<P,char> mp;
   REP(i,m) {
     int x,y; char c; cin >> x >> y >> c;
     mp[P{x,y}] = c;
   }
 
-  bool ok = true;
+  int ok = 1;
   int min_y = INF;
   for(auto [pa,c]: mp) {
     auto [x,y] = pa;
     if (c == 'W') min_y = min(min_y,y);
-    else {
-      if (y >= min_y) ok = false;
-    }
+    else ok &= min_y > y;
   }
-
   cout << (ok ? "Yes" : "No") << endl;
   return 0;
 }
