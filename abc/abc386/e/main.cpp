143c15416d4e2157d8fc21d2142ffb91d81aa4c9
tic40 <ccpzjoh@gmail.com>
2025-01-03 01:36:22 +0900

    Update

--- abc/abc386/e/main.cpp
+++ abc/abc386/e/main.cpp
@@ -5,20 +5,20 @@ using namespace std;
 using ll = long long;
 
 int main() {
-  ll n,k; cin >> n >> k;
+  int n; ll k; cin >> n >> k;
   vector<ll> a(n);
   REP(i,n) cin >> a[i];
 
   ll ans = 0;
-  auto dfs = [&](auto dfs, int i, int cnt, ll now) {
+  auto dfs = [&](auto& dfs, int i, int cnt, ll now) -> void {
     if (cnt == k) { ans = max(ans,now); return; }
-    if (i >= n) return;
+    if (i == n) return;
 
     dfs(dfs,i+1,cnt,now);
     dfs(dfs,i+1,cnt+1,now^a[i]);
   };
 
-  if (k <= n-k) {
+  if (n-k >= k) {
     dfs(dfs,0,0,0);
   } else {
     ll allxor = 0;
