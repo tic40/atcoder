f156e9cdd0bee8aaf65a99cc10e8dc30ef41374c
tic40 <ccpzjoh@gmail.com>
2025-01-12 09:11:43 +0900

    Update

---
+++ abc/abc388/d/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
+
+int main() {
+  int n; cin >> n;
+  vector<int> a(n);
+  REP(i,n) cin >> a[i];
+  vector<int> s(n+1);
+  REP(i,n) {
+    a[i] += s[i];
+    s[i+1]++;
+    s[min(n,i+a[i]+1)]--;
+    s[i+1] += s[i];
+    a[i] = max(0,a[i]-(n-i-1));
+
+    cout << a[i] << " ";
+  }
+  return 0;
+}
