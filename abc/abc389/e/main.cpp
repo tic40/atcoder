27349b09706503e52ca5caf65fd746e6ad16af10
tic40 <ccpzjoh@gmail.com>
2025-01-23 16:22:19 +0900

    Update

--- abc/abc389/e/main.cpp
+++ abc/abc389/e/main.cpp
@@ -9,13 +9,25 @@ int main() {
   vector<int> p(n);
   REP(i,n) cin >> p[i];
 
-  ll tot, num;
+  ll tot, num; // tot := 合計金額, num := 購入個数
+  // judge: c 円未満の商品をすべて買ったとき、m 円以下に収められるか
   auto judge = [&](ll c) {
+    if (c <= 0) return false;
+
     tot = num = 0;
     REP(i,n) {
-      ll k = ((c-1) /p[i]+1)/2;
-      if (k <= 0) continue;
-      if (m/k/k/p[i] == 0) return false;
+      // k := 商品 p[i] を何個買うことができるか
+      // (2k-1) * p < c
+      // (2k-1) * p <= c-1 // c-1 して以下に変換
+      // 2k-1 <= (c-1)/p
+      // 2k <= (c-1)/p + 1
+      // k <= ((c-1)/p + 1) / 2
+      ll k = ((c-1) / p[i] + 1) / 2;
+      if (k == 0) continue;
+      // オーバーフロー対策
+      // k*k*p[i] > m
+      // 1 > m/k/k/p[i]
+      if (m/k/k/p[i] < 1) return false;
       tot += k*k*p[i];
       num += k;
       if (tot > m) return false;
@@ -31,7 +43,9 @@ int main() {
   }
 
   judge(ok);
-  num += (m-tot)/ok;
-  cout << num << endl;
+  // 二分探索後に残金でいくつか商品を買えるケースがある
+  // 二分探索の答えが ok 円(c未満で買えるだけ買う)のとき、ok 円(c円)の品物は必ず存在する
+  ll ans = num + (m-tot)/ok;
+  cout << ans << endl;
   return 0;
 }
