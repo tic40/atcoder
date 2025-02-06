9ad7f635309d4a0ce3622263b05d5a43d06a6a9b
tic40 <ccpzjoh@gmail.com>
2025-02-06 09:34:00 +0900

    Update

--- abc/abc390/e/main.cpp
+++ abc/abc390/e/main.cpp
@@ -4,45 +4,46 @@ using namespace std;
 #define endl '\n'
 using ll = long long;
 using P = pair<int,int>;
-template<class T> void chmax(T& x, T y) { x = max(x,y); }
+const int INF = numeric_limits<int>::max();
 
 int main() {
   int n,x; cin >> n >> x;
-  // fodds[i][j] := ビタミン i が接種できる食べ物 j
-  vector foods(3,vector<P>());
+  vector g(3,vector<P>());
   REP(i,n) {
     int v,a,c; cin >> v >> a >> c; v--;
-    foods[v].emplace_back(a,c);
+    g[v].emplace_back(a,c);
   }
 
-  vector d(3,vector<int>(x+1));
-  REP(v,3) {
+  auto dpf = [&](int v) {
+    // dp[i] := 総カロリー i のときの最大のビタミン v の摂取量
     vector<int> dp(x+1);
-    for(auto [a,c]: foods[v]) {
-      // dp[i] := 接種カロリーが i 以下のときの接種ビタミンの最大値
+    for(auto [a,c]: g[v]) {
       vector<int> old(x+1);
       swap(dp,old);
       REP(i,x+1) {
-        chmax(dp[i],old[i]);
-        if (i-c >= 0) chmax(dp[i],old[i-c]+a);
+        dp[i] = max(dp[i],old[i]);
+        if (i-c >= 0) dp[i] = max(dp[i],old[i-c]+a);
       }
     }
-    d[v] = dp;
-  }
+    return dp;
+  };
 
-  // ビタミン1,2,3 から最も少ないビタミン摂取量を r 以上にできるか？
-  auto f = [&](int r) {
+  vector dp(3,vector<int>());
+  REP(v,3) dp[v] = dpf(v);
+
+  // すべてのビタミンを mid 以上接種できるか？
+  auto f = [&](int mid) {
     int tot = 0;
     REP(v,3) {
-      if (d[v][x] < r) return false;
-      int need = lower_bound(d[v].begin(),d[v].end(),r) - d[v].begin();
-      tot += need;
+      auto it =lower_bound(dp[v].begin(),dp[v].end(),mid);
+      if (it == dp[v].end()) return false;
+      tot += it - dp[v].begin();
     }
     return tot <= x;
   };
 
   int ok = 0, ng = 1e9+5;
-  while(ng-ok > 1) {
+  while(ng-ok>1) {
     int mid = (ok+ng)/2;
     if (f(mid)) ok = mid;
     else ng = mid;
