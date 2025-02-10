fecc996e7b6fee5c4332b7279e8bd7729788aa26
tic40 <ccpzjoh@gmail.com>
2025-02-10 15:55:56 +0900

    Update

--- abc/abc392/f/main.cpp
+++ abc/abc392/f/main.cpp
@@ -4,30 +4,23 @@ using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
-
 int op(int a, int b) { return a+b; }
 int e() { return 0; }
 
 int main() {
   int n; cin >> n;
   vector<int> p(n);
-  REP(i,n) { cin >> p[i]; p[i]--; }
+  REP(i,n) cin >> p[i], p[i]--;
 
   segtree<int,op,e> seg(vector<int>(n,1));
-  auto get_kth = [&](int k) {
-    auto f = [&](int x) { return x <= k; };
-    return seg.max_right(0,f);
-  };
-
   vector<int> ans(n);
-  // 後ろから埋めていく
-  for(int i = n-1; i >= 0; i--) {
-    // k番目の index を取得
-    int j = get_kth(p[i]);
-    ans[j] = i+1;
-    seg.set(j,0);
-  }
 
-  for(auto v :ans) cout << v << " ";
+  for(int i = n-1; i >= 0; i--) {
+    // segtree 上を二分探索して k番目の index を取得
+    int kth = seg.max_right(0,[&](int x){ return x <= p[i]; });
+    ans[kth] = i+1;
+    seg.set(kth,0);
+  }
+  for(auto v: ans) cout << v << " ";
   return 0;
 }
