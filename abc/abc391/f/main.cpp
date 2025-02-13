6565f916c5902765b508799efd599bf1f527e1b3
tic40 <ccpzjoh@gmail.com>
2025-02-13 09:44:02 +0900

    Update

--- abc/abc391/f/main.cpp
+++ abc/abc391/f/main.cpp
@@ -15,7 +15,6 @@ int main() {
   sort(c.rbegin(),c.rend());
 
   priority_queue<tuple<ll,int,int,int>> q;
-  set<tuple<int,int,int>> used;
   auto push = [&](int i, int j, int k) {
     if (i >= n || j >= n || k >= n) return;
     ll val = a[i]*b[j] + b[j]*c[k] + c[k]*a[i];
@@ -24,12 +23,13 @@ int main() {
 
   push(0,0,0);
   REP(_,k-1) {
-    auto [val,i,j,k] = q.top(); q.pop();
+    auto [_val,i,j,k] = q.top(); q.pop();
     push(i+1,j,k);
     if (i==0) push(i,j+1,k);
     if (i==0 && j==0) push(i,j,k+1);
   }
-  ll ans = get<0>(q.top());
+
+  auto [ans,_i,_j,_k] = q.top();
   cout << ans << endl;
   return 0;
 }
