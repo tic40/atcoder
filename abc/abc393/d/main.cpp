3b2b4b452f06cd12d06fbb236a15285254de2bd7
tic40 <ccpzjoh@gmail.com>
2025-02-18 10:07:45 +0900

    Update

--- abc/abc393/d/main.cpp
+++ abc/abc393/d/main.cpp
@@ -7,41 +7,22 @@ using ll = long long;
 int main() {
   int n; string s; cin >> n >> s;
 
-  // 中央値を取る
-  auto solve1 = [&]() {
-    vector<int> p;
-    REP(i,n) if (s[i] == '1') p.push_back(i);
-    int k = p.size();
-    REP(i,k) p[i] -= i;
-    int med = p[k/2];
-
-    ll ans = 0;
-    REP(i,k) ans += abs(med-p[i]);
-    cout << ans << endl;
-  };
-
-  // 左右
-  auto solve2 = [&]() {
-    vector<ll> dl(n+1),dr(n+1);
-    REP(ri,2) {
-      ll now = 0;
-      int one = 0;
-      REP(i,n) {
-        if (s[i] == '0') now += one;
-        else one++;
-        dl[i+1] = now;
-      }
-      swap(dl,dr);
-      reverse(dl.begin(),dl.end());
-      reverse(dr.begin(),dr.end());
-      reverse(s.begin(),s.end());
+  auto f = [&](string s) {
+    vector<ll> res(n+1);
+    int one = 0;
+    REP(i,n) {
+      res[i+1] = res[i];
+      if (s[i] == '1') one++;
+      else res[i+1]+=one;
     }
-    ll ans = 1e18;
-    REP(i,n+1) ans = min(ans,dl[i]+dr[i]);
-    cout << ans << endl;
+    return res;
   };
 
-  // solve1();
-  solve2();
+  auto l  = f(s);
+  reverse(s.begin(),s.end());
+  auto r  = f(s);
+  ll ans = 1e18;
+  REP(i,n+1) ans = min(ans,l[i]+r[n-i]);
+  cout << ans << endl;
   return 0;
 }
