41cb577e587120151ca47dd2df6b1ea9dc87fa36
tic40 <ccpzjoh@gmail.com>
2025-02-20 09:51:54 +0900

    Update

--- abc/abc393/e/main.cpp
+++ abc/abc393/e/main.cpp
@@ -2,26 +2,25 @@
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
-using ll = long long;
 
 int main() {
   int n,k; cin >> n >> k;
   vector<int> a(n);
   REP(i,n) cin >> a[i];
-  const int m = 1e6;
 
-  // num[i] := a の分布
-  // cnt[i] := j で割り切れる a の数のカウント
-  // ans[i] := i を含む k 個の要素を a から選んだときの最大のgcd
-  vector<int> num(m+1),cnt(m+1),ans(m+1);
-  REP(i,n) num[a[i]]++;
-  for(int j = 1; j <= m; j++) {
-    for(int i = j; i <= m; i+=j) cnt[j]+=num[i];
+  const int m = 1e6;
+  // cnt[i] := A にある i で割り切れる数の個数
+  vector<int> cnt(m+1);
+  REP(i,n) cnt[a[i]]++;
+
+  for(int i = 1; i <= m; i++) {
+    for(int j = i+i; j <= m; j+=i) cnt[i] += cnt[j];
   }
 
-  // c[gcd] >= k := gcd で割り切れる a の数が k 以上あるとき
-  for(int gcd = 1; gcd <= m; gcd++) if (cnt[gcd] >= k) {
-    for(int i = gcd; i <= m; i+=gcd) ans[i] = gcd;
+  // ans[i] := A から i を含む k 個を選んだときの gcd の最大値
+  vector<int> ans(m+1);
+  for(int i = 1; i <= m; i++) if (cnt[i] >= k) {
+    for(int j = i; j <= m; j+=i) ans[j] = i;
   }
 
   REP(i,n) cout << ans[a[i]] << endl;
