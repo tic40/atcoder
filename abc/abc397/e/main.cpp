65f89adb1f4339945e02be799fd59b3de3a545e7
tic40 <ccpzjoh@gmail.com>
2025-03-17 15:23:43 +0900

    Update

--- abc/abc397/e/main.cpp
+++ abc/abc397/e/main.cpp
@@ -7,7 +7,7 @@ int main() {
   int n,k; cin >> n >> k;
   int nk = n*k;
   vector g(nk,vector<int>());
-  REP(i,n*k-1) {
+  REP(i,nk-1) {
     int u,v; cin >> u >> v; u--; v--;
     g[u].push_back(v);
     g[v].push_back(u);
@@ -15,13 +15,13 @@ int main() {
 
   // DFS: まだ分解していない頂点 i の部分木のパスの長さを返す
   auto dfs = [&](auto& dfs, int i, int p) -> int {
-    int cnt = 0, len = 1;
-    for (auto v: g[i]) if (v!=p) {
+    int len = 1, cnt = 0;
+    for(auto v: g[i]) if (v!=p) {
       int res = dfs(dfs,v,i);
       if (res == -1) return -1;
-      if (res > 0) cnt++;
-      len += res;
+      if (res > 0) { len+=res; cnt++; }
     }
+
     if (cnt <= 1) return len == k ? 0 : len;
     if (cnt == 2) return len == k ? 0 : -1;
     return -1;
