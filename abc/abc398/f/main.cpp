e645e3339025633f6a8486a4ff7e539100abe9d9
tic40 <ccpzjoh@gmail.com>
2025-03-24 22:06:48 +0900

    Update

--- abc/abc398/f/main.cpp
+++ abc/abc398/f/main.cpp
@@ -4,39 +4,54 @@ using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
-using ll = long long;
-using P = pair<int,int>;
-const int INF = numeric_limits<int>::max();
-const ll LINF = numeric_limits<ll>::max();
-const int MOD = 1e9+7;
-// using mint = modint998244353; // modint1000000007;
 
 int main() {
   string s; cin >> s;
   int n = s.size();
-  if (n == 1) { cout << s << endl; return 0; }
 
-  auto f = [&](string& s) {
-    int n = s.size();
-    vector<int> pi(n);
-    for(int i = 1; i < n; i++) {
-      int j = pi[i-1];
-      while(j > 0 && s[i] != s[j]) j = pi[j-1];
-      if (s[i] == s[j]) j++;
-      pi[i] = j;
-    }
-    return pi;
+  auto solve1 = [&]() -> void {
+    auto f = [&](string& s) {
+      int n = s.size();
+      vector<int> pi(n);
+      for(int i = 1; i < n; i++) {
+        int j = pi[i-1];
+        while(j > 0 && s[i] != s[j]) j = pi[j-1];
+        if (s[i] == s[j]) j++;
+        pi[i] = j;
+      }
+      return pi;
+    };
+
+    auto sr = s;
+    reverse(sr.begin(),sr.end());
+    auto t = sr + "_" + s;
+    auto pi = f(t);
+    int r = pi[t.size()-1];
+    auto ans = s;
+    auto add = s.substr(0,n-r);
+    reverse(add.begin(),add.end());
+    ans += add;
+    cout << ans << endl;
   };
 
-  auto sr = s;
-  reverse(sr.begin(),sr.end());
-  auto t = sr + "_" + s;
-  auto pi = f(t);
-  int r = pi[t.size()-1];
-  auto ans = s;
-  auto add = s.substr(0,n-r);
-  reverse(add.begin(),add.end());
-  ans += add;
-  cout << ans << endl;
+  // 解説放送解
+  auto solve2 = [&]() -> void {
+    auto t = s;
+    reverse(t.begin(),t.end());
+    t += s;
+    vector<int> z = z_algorithm(t);
+
+    REP(i,n) {
+      if (z[n+i] == n-i) {
+        string ans = s;
+        ans += t.substr(n-i,i);
+        cout << ans << endl;
+        return;
+      }
+    }
+  };
+
+  // solve1();
+  solve2();
   return 0;
 }
