676290915b969fe24e63a285585f7000461d1f0a
tic40 <ccpzjoh@gmail.com>
2025-04-06 22:20:08 +0900

    Update

--- abc/abc400/d/main.cpp
+++ abc/abc400/d/main.cpp
@@ -2,9 +2,8 @@
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
-using ll = long long;
-const int INF = numeric_limits<int>::max();
 using P = pair<int,int>;
+const int INF = numeric_limits<int>::max();
 const vector<int> di = {-1,0,1,0};
 const vector<int> dj = {0,1,0,-1};
 
@@ -15,17 +14,17 @@ int main() {
   int a,b,c,d; cin >> a >> b >> c >> d;
   a--; b--; c--; d--;
 
-  vector dist(h,vector<int>(w,INF));
   deque<P> q;
+  vector dist(h,vector<int>(w,INF));
   q.emplace_front(a,b);
   dist[a][b] = 0;
+
   while(q.size()) {
     auto [i,j] = q.front(); q.pop_front();
-    int nd = dist[i][j];
-
     REP(k,4) {
-      int ni = i+di[k];
-      int nj = j+dj[k];
+      int ni = i+di[k], nj = j+dj[k];
+      int nd = dist[i][j];
+
       if (ni < 0 || nj < 0 || ni >= h || nj >= w) continue;
 
       if (s[ni][nj] == '.') {
@@ -33,18 +32,16 @@ int main() {
           dist[ni][nj] = nd;
           q.emplace_front(ni,nj);
         }
-        continue;
-      }
-
-      if (dist[ni][nj] > nd+1) {
-        dist[ni][nj] = nd+1;
-        q.emplace_back(ni,nj);
-      }
-      ni += di[k];
-      nj += dj[k];
-      if (ni >= 0 && nj >= 0 && ni < h && nj < w) {
-        if (dist[ni][nj] > nd+1) {
-          dist[ni][nj] = nd+1;
+      } else {
+        nd++;
+        if (dist[ni][nj] > nd) {
+          dist[ni][nj] = nd;
+          q.emplace_back(ni,nj);
+        }
+        ni += di[k]; nj += dj[k];
+        if (ni < 0 || nj < 0 || ni >= h || nj >= w) continue;
+        if (dist[ni][nj] > nd) {
+          dist[ni][nj] = nd;
           q.emplace_back(ni,nj);
         }
       }
