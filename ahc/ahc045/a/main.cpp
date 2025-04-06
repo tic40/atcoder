36216b31d5e1130e7df0c36c030ddce89d7cf2d5
tic40 <ccpzjoh@gmail.com>
2025-04-07 00:53:18 +0900

    submit3

--- ahc/ahc045/a/main.cpp
+++ ahc/ahc045/a/main.cpp
@@ -1,10 +1,17 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 using ll = long long;
 using P = pair<int,int>;
 const int INF = numeric_limits<int>::max();
+
+struct Edge {
+  int u,v;
+  ll w;
+};
 
 vector<P> query(const vector<int>& c) {
   cout << "? " << c.size();
@@ -29,6 +36,40 @@ void answer(const vector<vector<int>>& groups, const vector<vector<P>>& edges) {
   }
 }
 
+ll compute_dist(int i, int j, const vector<int>& x, const vector<int>& y) {
+  ll dx = (ll)x[i] - x[j];
+  ll dy = (ll)y[i] - y[j];
+  return dx*dx + dy*dy;
+}
+
+vector<P> compute_mst(const vector<int>& group, const vector<int>& x, const vector<int>& y) {
+  int sz = group.size();
+  vector<Edge> edges;
+  REP(i,(int)sz) for (int j = i+1; j < sz; j++) {
+    ll d = compute_dist(group[i],group[j],x,y);
+    edges.push_back({group[i],group[j],d});
+  }
+
+  sort(edges.begin(), edges.end(), [](const Edge& a, const Edge& b){ return a.w < b.w; });
+
+  dsu uf(sz);
+  unordered_map<int,int> mp;
+  REP(i,(int)sz) mp[group[i]] = i;
+
+  vector<P> res;
+  for (auto &e: edges) {
+    int u_idx = mp[e.u];
+    int v_idx = mp[e.v];
+    if (!uf.same(u_idx, v_idx)) {
+      uf.merge(u_idx, v_idx);
+      if(e.u > e.v) swap(e.u, e.v);
+      res.emplace_back(e.u, e.v);
+    }
+    if ((int)res.size() == sz - 1) break;
+  }
+  return res;
+}
+
 int main() {
   int n, m, q, l, w;
   cin >> n >> m >> q >> l >> w;
@@ -38,16 +79,16 @@ int main() {
   vector<int> lx(n), rx(n), ly(n), ry(n);
   REP(i,n) cin >> lx[i] >> rx[i] >> ly[i] >> ry[i];
 
-  // 中心座標を計算
-  vector<int> x(n), y(n);
+  // 各都市の中心座標
+  vector<int> cx(n), cy(n);
   REP(i,n) {
-    x[i] = (lx[i] + rx[i]) / 2;
-    y[i] = (ly[i] + ry[i]) / 2;
+    cx[i] = (lx[i] + rx[i]) / 2;
+    cy[i] = (ly[i] + ry[i]) / 2;
   }
 
   // 全都市の重心を求める
   double sum_x = 0, sum_y = 0;
-  REP(i,n) { sum_x += x[i]; sum_y += y[i]; }
+  REP(i,n) { sum_x += cx[i]; sum_y += cy[i]; }
   double center_x = sum_x / n;
   double center_y = sum_y / n;
 
@@ -55,8 +96,8 @@ int main() {
   vector<int> cities(n);
   iota(cities.begin(), cities.end(), 0);
   sort(cities.begin(), cities.end(), [&](int i, int j) {
-      double angle_i = atan2(y[i] - center_y, x[i] - center_x);
-      double angle_j = atan2(y[j] - center_y, x[j] - center_x);
+      double angle_i = atan2(cy[i] - center_y, cx[i] - center_x);
+      double angle_j = atan2(cy[j] - center_y, cx[j] - center_x);
       return angle_i < angle_j;
   });
 
@@ -69,19 +110,10 @@ int main() {
     start_idx += v;
   }
 
-  vector edges(m,vector<P>);
-  REP(k,m) {
-    for (int i = 0; i < g[k] - 1; i += 2) {
-      if (i < g[k] - 2) {
-        vector<int> subvec = {groups[k][i], groups[k][i + 1], groups[k][i + 2]};
-        auto ret = query(subvec);
-        edges[k].insert(edges[k].end(), ret.begin(), ret.end());
-      } else {
-        edges[k].emplace_back(groups[k][i], groups[k][i + 1]);
-      }
-    }
-  }
+  vector edges(m,vector<P>());
+  REP(k,m) edges[k] = compute_mst(groups[k], cx, cy);
 
   answer(groups, edges);
   return 0;
 }
+
