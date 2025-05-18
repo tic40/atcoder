31b2be204a7b7cf6b28d62386e122cd43131a8f8
tic40 <ccpzjoh@gmail.com>
2025-05-18 12:58:06 +0900

    Update

--- abc/abc406/d/main.cpp
+++ abc/abc406/d/main.cpp
@@ -5,27 +5,26 @@ using namespace std;
 
 int main() {
   int h,w,n; cin >> h >> w >> n;
-  vector row(h,vector<int>());
-  vector col(w,vector<int>());
-  vector<int> rowdel(h);
-  vector<int> coldel(w);
+  vector<set<int>> row(h), col(w);
   REP(i,n) {
     int x,y; cin >> x >> y; x--; y--;
-    row[x].push_back(y);
-    col[y].push_back(x);
+    row[x].insert(y);
+    col[y].insert(x);
   }
 
   int q; cin >> q;
   REP(_,q) {
     int t,x; cin >> t >> x; x--;
-    if (t == 2) { swap(row,col); swap(rowdel,coldel); }
-
-    int ans = max(0, (int)row[x].size() - rowdel[x]);
-    cout << ans << endl;
-    for(auto v: row[x]) coldel[v]++;
-    row[x].clear();
-
-    if (t == 2) { swap(row,col); swap(rowdel,coldel); }
+    if (t == 1) {
+      cout << row[x].size() << endl;
+      for(auto v: row[x]) col[v].erase(x);
+      row[x].clear();
+    }
+    if (t == 2) {
+      cout << col[x].size() << endl;
+      for(auto v: col[x]) row[v].erase(x);
+      col[x].clear();
+    }
   }
   return 0;
 }
