e6fa3fc153d0c1e0694c5d800f64d70bba443df7
tic40 <ccpzjoh@gmail.com>
2025-05-22 00:19:10 +0900

    Update

--- abc/abc154/e/main.cpp
+++ abc/abc154/e/main.cpp
@@ -1,33 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i, n) for(int i = 0; i < n; i++)
-typedef long long ll;
-
-int dp[105][4][2];
+#define REP(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
+using ll = long long;
 
 int main() {
-  string s; cin >> s;
-  int n = s.size();
-  int K; cin >> K;
+  string s; int k; cin >> s >> k;
 
+  int n = s.size();
+  vector dp(n+1, vector(2, vector<ll>(k+1)));
   dp[0][0][0] = 1;
-  // i: i桁目まで
-  // j: j個の非0を使って
-  // k: 0: そこまでの桁はNと一致
-  //    1: そこまでの桁ですでにN未満であることが確定
-  REP(i,n) REP(j,4) REP(k,2) {
-    int nd = s[i]-'0';
-    REP(d,10) {
-      int ni = i+1, nj = j, nk = k;
-      if (d != 0) nj++;
-      if (nj > K) continue;
-      if (k == 0) {
-        if (d > nd) continue;
-        if (d < nd) nk = 1;
+
+  REP(i,n) REP(j,2) REP(p,k+1) {
+    int d = s[i] - '0';
+    REP(x,10) {
+      int nj = j;
+      int np = x == 0 ? p : p+1;
+      if (np > k) continue;
+      if (j == 0) {
+        if (d < x) continue;
+        if (d > x) nj = 1;
       }
-      dp[ni][nj][nk] += dp[i][j][k];
+      dp[i+1][nj][np] += dp[i][j][p];
     }
   }
-  int ans = dp[n][K][0] + dp[n][K][1];
-  cout << ans << endl;
+
+  cout << dp[n][1][k] + dp[n][0][k] << endl;
+  return 0;
 }
