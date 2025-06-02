73d1eb443594d05f07a3cc06089b3e3a44654606
tic40 <ccpzjoh@gmail.com>
2025-06-02 10:05:51 +0900

    Update

--- abc/abc408/d/main.cpp
+++ abc/abc408/d/main.cpp
@@ -2,24 +2,19 @@
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
-const int INF = 1e9;
-template<class T> void chmin(T& a, T b) { a = min(a,b); }
 
 int main() {
-  auto solve = []() {
+  int t; cin >> t;
+  REP(_,t) {
     int n; string s; cin >> n >> s;
-
-    // dp[i][j] := i まで決めて、末尾を j にするときの最小コスト
-    vector dp(n+1,vector<int>(3,INF));
+    // dp[i][j] := i まで決めたとき、末尾の値が j のときの最小コスト
+    vector dp(n+1,vector<int>(3,1e9));
     dp[0][0] = 0;
-    REP(i,n) REP(j,3) {
-      int cost = (s[i]-'0' == j%2) ? 0 : 1;
-      REP(pj,j+1) chmin(dp[i+1][j],dp[i][pj]+cost);
+    REP(i,n) REP(j,3) REP(pj,j+1) {
+      int cost = s[i]-'0' == j%2 ? 0 : 1;
+      dp[i+1][j] = min(dp[i+1][j], dp[i][pj] + cost);
     }
     cout << *min_element(dp[n].begin(),dp[n].end()) << endl;
-  };
-
-  int t; cin >> t;
-  REP(_,t) solve();
+  }
   return 0;
 }
