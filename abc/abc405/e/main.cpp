23703ab6994c3d3ff0f9bae25f1917a568f219b4
tic40 <ccpzjoh@gmail.com>
2025-06-06 09:04:25 +0900

    Update

--- abc/abc405/e/main.cpp
+++ abc/abc405/e/main.cpp
@@ -6,7 +6,6 @@ using namespace std;
 #define endl '\n'
 using mint = modint998244353; // modint1000000007;
 
-// Combination ライブラリ
 struct Combination {
   vector<mint> fact, ifact;
   Combination(int n):fact(n+1),ifact(n+1) {
@@ -23,19 +22,31 @@ struct Combination {
 
 int main() {
   int a,b,c,d; cin >> a >> b >> c >> d;
-  mint ans = 0;
   Combination comb(4e6+1);
+  mint ans = 0;
 
   /*
     a > c
     a > d
     b > d
-
-    [a,b混合][b,c混合][c,d混合]
+    左から以下の順序になる
+    [a or b][b or c][c or d]
+    l := [a or b] の部分に置く b の個数
   */
   REP(l,b+1) {
+    // a-1+l個の要素から l個選ぶ組み合わせ
+    // これは左端のaとbの並び方を表す
+    // -1 しているのは、[a or b] の部分の右端は必ず A になるからその分を -1
     mint now = comb(a-1+l,l);
-    now *= comb((b-l+d)+c,c);
+
+    // (b-l+d)+c個の要素から c個選ぶ組み合わせ
+    // これは残りのb,c,dの並び方を表す
+    // b-l: 残りのbの個数
+    // d: dの個数
+    // c: cの個数
+    now *= comb((b-l)+d+c,c);
+
+    // 全ての組み合わせを足し合わせる
     ans += now;
   }
   cout << ans.val() << endl;
