efe40e7e841dc354af7a47a0288998e84e87ee96
tic40 <ccpzjoh@gmail.com>
2025-06-21 00:08:44 +0900

    Update

--- abc/abc410/d/main.cpp
+++ abc/abc410/d/main.cpp
@@ -6,33 +6,33 @@ using P = pair<int,int>;
 
 int main() {
   int n,m; cin >> n >> m;
-  const int MX = 1<<10;
+  int mx = 1<<10;
   vector g(n,vector<P>());
   REP(i,m) {
     int a,b,w; cin >> a >> b >> w; a--; b--;
     g[a].emplace_back(b,w);
   }
 
-  vector<bool> visited(n * MX);
+  vector<bool> visited(n*mx);
   queue<int> q;
-
-  auto push = [&](int v, int x) -> void {
-    int vid = v * MX + x;
-    if (visited[vid]) return;
-    visited[vid] = true;
-    q.push(vid);
-  };
-
-  push(0,0);
+  q.push(0);
+  visited[0] = true;
   while(q.size()) {
     int uid = q.front(); q.pop();
-    int u = uid / MX;
-    int x = uid % MX;
-    for(auto [v,w]: g[u]) push(v,x^w);
+    int u = uid / mx;
+    int x = uid % mx;
+    for(auto [v,w]: g[u]) {
+      int vid = v * mx + (x^w);
+      if (visited[vid]) continue;
+      visited[vid] = true;
+      q.push(vid);
+    }
   }
 
   int ans = -1;
-  REP(x,MX) if (visited[(n-1) * MX + x]) { ans = x; break; }
+  REP(x,mx) if (visited[(n-1) * mx + x]) {
+    ans = x; break;
+  }
   cout << ans << endl;
   return 0;
 }
