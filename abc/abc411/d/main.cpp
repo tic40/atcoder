d3bcee6d056e05be5479efe2f94425c2ea7c661d
tic40 <ccpzjoh@gmail.com>
2025-06-27 10:32:01 +0900

    Update

--- abc/abc411/d/main.cpp
+++ abc/abc411/d/main.cpp
@@ -8,24 +8,22 @@ int main() {
   vector<tuple<int,int,string>> e;
   REP(i,q) {
     int t,p; string s;
-    cin >> t >> p; // p--;
+    cin >> t >> p; p--;
     if (t == 2) { cin >> s; reverse(s.begin(),s.end()); }
     e.emplace_back(t,p,s);
   }
   reverse(e.begin(),e.end());
 
   string ans = "";
-  int i = 0;
+  int si = -1; // サーバー文字列となっている pc 番号
   for(auto [t,p,s]: e) {
-    if (t == 1) {
-      if (i == p) i = 0;
-    }
-    if (t == 2) {
-      if (i == p) ans += s;
-    }
-    if (t == 3) {
-      if (i == 0) i = p;
-    }
+    // pc[p] をサーバー文字列にする
+    if (t == 1) { if (si == p) si = -1; }
+    // pc[p] の末尾に s 追加
+    if (t == 2) { if (si == p) ans += s; }
+    // サーバー文字列を pc[p] にする
+    if (t == 3) { if (si == -1) si = p; }
+    cout << si << " " << ans << endl;
   }
 
   reverse(ans.begin(),ans.end());
