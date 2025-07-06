4dbebb9eb0e1119325d4a3b0e060ecde1b8b6545
tic40 <ccpzjoh@gmail.com>
2025-07-06 16:59:57 +0900

    Update

--- abc/abc413/d/main.cpp
+++ abc/abc413/d/main.cpp
@@ -9,22 +9,18 @@ int main() {
     int n; cin >> n;
     vector<int> a(n);
     REP(i,n) cin >> a[i];
-
-    sort(a.begin(),a.end(),[&](int x, int y){ return abs(x) < abs(y); });
+    sort(a.begin(),a.end(),[&](int x, int y) { return abs(x) < abs(y); });
 
     bool ok = true;
-    REP(i,n-2) {
-      // a[i] / a[i+1] = a[i+1] / a[i+2]
-      // a[i] * a[i+2] = a[i+1] * a[i+1]
-      if ((ll)a[i]*a[i+2] != (ll)a[i+1]*a[i+1]) ok = false;
-    }
+    REP(i,n-2) ok &= ((ll)a[i+1]*a[i+1] == (ll)a[i+2]*a[i]);
     if (ok) return true;
 
     if (abs(a[0]) == abs(a.back())) {
-      int pos = 0, neg = 0;
+      int neg = 0, pos = 0;
       REP(i,n) a[i] < 0 ? neg++ : pos++;
-      if (abs(neg-pos) <= 1) return true;
+      if (abs(pos-neg) <= 1) return true;
     }
+
     return false;
   };
 
