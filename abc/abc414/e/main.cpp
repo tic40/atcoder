21ad79945d826e42c5b2a6913ba8a16f00637cc8
tic40 <ccpzjoh@gmail.com>
2025-07-14 22:35:13 +0900

    Update

--- abc/abc414/e/main.cpp
+++ abc/abc414/e/main.cpp
@@ -9,10 +9,12 @@ using mint = modint998244353;
 
 int main() {
   ll n; cin >> n;
-  // a > b の総数
-  // ここから a % b == 0 のケースを引く
+  // ∑(n-b - (⌊n/b⌋-1)) を求めればよい // ∑は b=1 to n
+  //   ∑(n-b+1) = n,n-1,n-2...1 の等差数列 = (n+1)*n/2
+  // ここから a % b == 0 のケースを引いていく
   mint ans = mint(n+1)*n/2;
   ll b = 1;
+  // 商列挙テクニック
   while(b <= n) {
     ll y = n/b;
     ll nb = n/y+1;
