35f3b5f55d1b7150e7cca01e567ec1bbc9769ad7
tic40 <ccpzjoh@gmail.com>
2025-08-24 09:12:48 +0900

    Update

--- abc/abc411/e/main.cpp
+++ abc/abc411/e/main.cpp
@@ -8,24 +8,21 @@ using P = pair<int,int>;
 using mint = modint998244353;
 
 mint op(mint a, mint b) { return a*b; }
-mint e() { return mint(1); }
+mint e() { return mint::raw(1); }
+const int m = 6;
+const mint invm = mint(m).inv();
 
 int main() {
   int n; cin >> n;
-  const int m = 6;
   vector<P> pa;
-  REP(i,n) REP(j,m) {
-    int a; cin >> a;
-    pa.emplace_back(a,i);
-  }
+  REP(i,n) REP(j,m) { int a; cin >> a; pa.emplace_back(a,i); }
   sort(pa.begin(),pa.end());
-  const mint inv6 = mint(6).inv();
 
-  segtree<mint,op,e> seg(vector<mint>(n,mint(0)));
+  segtree<mint,op,e> seg(vector<mint>(n, mint::raw(0)));
   mint ans = 0;
   for(auto [a,i]: pa) {
-    ans += a * inv6 * seg.prod(0,i) * seg.prod(i+1,n);
-    seg.set(i, seg.get(i)+inv6);
+    ans += a * invm * seg.prod(0,i) * seg.prod(i+1,n);
+    seg.set(i, seg.get(i)+invm);
   }
   cout << ans.val() << endl;
   return 0;
