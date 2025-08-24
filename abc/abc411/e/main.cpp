dabf37e2ea7a8eed7931e8f4c28985d9b771bd3e
tic40 <ccpzjoh@gmail.com>
2025-08-24 09:08:53 +0900

    Update

--- abc/abc411/e/main.cpp
+++ abc/abc411/e/main.cpp
@@ -4,40 +4,29 @@ using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
-using ll = long long;
 using P = pair<int,int>;
 using mint = modint998244353;
+
+mint op(mint a, mint b) { return a*b; }
+mint e() { return mint(1); }
 
 int main() {
   int n; cin >> n;
   const int m = 6;
-  vector a(n,vector<int>(m));
-  REP(i,n) REP(j,m) cin >> a[i][j];
-
-  vector<P> xs;
-  REP(i,n) REP(j,m) xs.emplace_back(a[i][j],i);
-  sort(xs.begin(),xs.end());
-
-  mint ans = 0;
-  int pre = 0;
-
-  mint prod = 1;
-  int zero = n;
-  vector<int> cnt(n);
-
-  for(auto [x,i]: xs) {
-    mint p = 0;
-    if (zero == 0) p = prod / mint(m).pow(n);
-    ans += (mint(1)-p) * (x-pre);
-    pre = x;
-
-    if (cnt[i] == 0) zero--;
-    else prod /= cnt[i];
-
-    cnt[i]++;
-    prod *= cnt[i];
+  vector<P> pa;
+  REP(i,n) REP(j,m) {
+    int a; cin >> a;
+    pa.emplace_back(a,i);
   }
+  sort(pa.begin(),pa.end());
+  const mint inv6 = mint(6).inv();
 
+  segtree<mint,op,e> seg(vector<mint>(n,mint(0)));
+  mint ans = 0;
+  for(auto [a,i]: pa) {
+    ans += a * inv6 * seg.prod(0,i) * seg.prod(i+1,n);
+    seg.set(i, seg.get(i)+inv6);
+  }
   cout << ans.val() << endl;
   return 0;
 }
