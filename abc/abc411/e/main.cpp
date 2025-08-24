dbdf42d34321c7ea086a56033b23bb3349d825f0
tic40 <ccpzjoh@gmail.com>
2025-08-24 21:10:02 +0900

    Update

--- abc/abc411/e/main.cpp
+++ abc/abc411/e/main.cpp
@@ -10,19 +10,22 @@ using mint = modint998244353;
 mint op(mint a, mint b) { return a*b; }
 mint e() { return mint::raw(1); }
 const int m = 6;
-const mint invm = mint(m).inv();
+const mint inv_m = mint(m).inv();
 
 int main() {
   int n; cin >> n;
-  vector<P> pa;
+  vector<P> pa; // {出目a, サイコロ番号} ペアを出目aの昇順で持つ
   REP(i,n) REP(j,m) { int a; cin >> a; pa.emplace_back(a,i); }
   sort(pa.begin(),pa.end());
 
-  segtree<mint,op,e> seg(vector<mint>(n, mint::raw(0)));
+  segtree<mint,op,e> seg(vector<mint>(n,mint::raw(0)));
   mint ans = 0;
   for(auto [a,i]: pa) {
-    ans += a * invm * seg.prod(0,i) * seg.prod(i+1,n);
-    seg.set(i, seg.get(i)+invm);
+    // a := 現在の出目の最大値
+    // a * inv_m := サイコロ i で a が出る期待値
+    // seg.prod(0,i) * seg.prod(i+1,n) := サイコロ i 以外が全て a 以下のときの確率
+    ans += a * inv_m * seg.prod(0,i) * seg.prod(i+1,n);
+    seg.set(i,seg.get(i)+inv_m);
   }
   cout << ans.val() << endl;
   return 0;
