54d89b4c826d2f337f1d735e5d95ea305d76b52e
tic40 <ccpzjoh@gmail.com>
2025-08-28 16:59:47 +0900

    Update

--- abc/abc420/e/main.cpp
+++ abc/abc420/e/main.cpp
@@ -11,25 +11,25 @@ int main() {
   // col[i] := 頂点iの色が黒(1), 白(0)
   // cnt[i] := 頂点iを含むiとの連結している黒頂点の数
   vector<int> col(n),cnt(n);
-  REP(_,q) {
-    int t,v; cin >> t >> v; v--;
 
+  REP(_,q) {
+    int t; cin >> t;
     if (t == 1) {
-      int u; cin >> u; u--;
+      int u,v; cin >> u >> v; u--; v--;
       u = uf.leader(u);
       v = uf.leader(v);
       if (u == v) continue;
-
       uf.merge(u,v);
       cnt[uf.leader(u)] = cnt[u]+cnt[v];
     }
     if (t == 2) {
-      cnt[uf.leader(v)] -= col[v];
-      col[v] ^= 1;
-      cnt[uf.leader(v)] += col[v];
+      int u; cin >> u; u--;
+      col[u] ^= 1;
+      cnt[uf.leader(u)] += col[u] ? 1 : -1;
     }
     if (t == 3) {
-      cout << (cnt[uf.leader(v)] > 0 ? "Yes" : "No") << endl;
+      int u; cin >> u; u--;
+      cout << (cnt[uf.leader(u)] > 0 ? "Yes" : "No") << endl;
     }
   }
   return 0;
