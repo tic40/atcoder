25d7c6ea0d92279d0f44ec1bbc70112d2e0b5498
tic40 <ccpzjoh@gmail.com>
2026-09-23 21:18:54 +0900

    Update

--- abc/435/e/main.cpp
+++ abc/435/e/main.cpp
@@ -2,39 +2,31 @@
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
-using P = pair<int,int>;
 
 int main() {
   int n,q; cin >> n >> q;
-  set<P> s;
   int ans = n;
-
+  map<int,int> mp;
   REP(i,q) {
-    int l, r; cin >> l >> r;
+    int l,r; cin >> l >> r;
     int nl = l, nr = r;
+    auto it = mp.lower_bound(l);
+    if (it != mp.begin()) --it;
 
-    auto it = s.lower_bound({l, -1});
-    if (it != s.begin()) --it;
+    while(it != mp.end()) {
+      l = it->first;
+      r = it->second;
+      if (r+1 < nl) { ++it; continue; }
+      if (nr + 1 < l) break;
 
-    while (it != s.end()) {
-      auto [l,r] = *it;
+      nl = min(l,nl);
+      nr = max(r,nr);
 
-      // 完全に左側
-      if (r < nl - 1) { ++it; continue; }
-
-      // 完全に右側
-      if (l > nr + 1) break;
-
-      // 重なる or 隣接する
-      nl = min(nl,l);
-      nr = max(nr,r);
-
-      ans += r - l + 1;
-      it = s.erase(it);
+      ans += r-l+1;
+      it = mp.erase(it);
     }
-
-    s.emplace(nl,nr);
-    ans -= nr - nl + 1;
+    mp[nl] = nr;
+    ans -= nr-nl+1;
     cout << ans << endl;
   }
   return 0;
