2e2976cbe8cf3fab8fa3ca9f42ce290075e3e7a4
tic40 <ccpzjoh@gmail.com>
2026-09-23 21:30:51 +0900

    Update

--- abc/435/e/main.cpp
+++ abc/435/e/main.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 int main() {
   int n,q; cin >> n >> q;
-  int ans = n;
   map<int,int> mp;
-  REP(i,q) {
+  int ans = n;
+  REP(_,q) {
     int l,r; cin >> l >> r;
     int nl = l, nr = r;
     auto it = mp.lower_bound(l);
@@ -16,14 +16,14 @@ int main() {
     while(it != mp.end()) {
       l = it->first;
       r = it->second;
+
       if (r+1 < nl) { ++it; continue; }
-      if (nr + 1 < l) break;
+      if (nr < l-1) break;
 
-      nl = min(l,nl);
-      nr = max(r,nr);
-
-      ans += r-l+1;
+      nl = min(nl,l);
+      nr = max(nr,r);
       it = mp.erase(it);
+      ans += r-l+1;
     }
     mp[nl] = nr;
     ans -= nr-nl+1;
