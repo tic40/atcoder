d2bcb2ab1cdc2b22cff67b9b7cf08548afad80ce
tic40 <ccpzjoh@gmail.com>
2026-09-25 23:57:33 +0900

    Update

--- abc/abc437/e/main.cpp
+++ abc/abc437/e/main.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
-using ll = long long;
 
 struct Node {
   map<int,int> next;
@@ -13,29 +12,26 @@ int main() {
   int n; cin >> n;
   vector<Node> trie(1);
   vector<int> pos(n+1);
-
-  pos[0] = 0;
-  for (int i = 1; i <= n; i++) {
-    int x, y; cin >> x >> y;
-
-    int v = pos[x];
-    if (!trie[v].next.count(y)) {
-      trie[v].next[y] = trie.size();
-      trie.push_back(Node{});
+  for(int i = 1; i <= n; i++) {
+    int x,y; cin >> x >> y;
+    int now = pos[x];
+    if (trie[now].next.count(y) == 0) {
+      trie[now].next[y] = trie.size();
+      trie.push_back(Node());
     }
 
-    int to = trie[v].next[y];
-    pos[i] = to;
+    int to = trie[now].next[y];
     trie[to].ids.push_back(i);
+    pos[i] = to;
   }
 
   vector<int> ans;
-  auto dfs = [&](auto dfs, int v) -> void {
-    for (int id: trie[v].ids) ans.push_back(id);
-    for (auto [y,to]: trie[v].next) dfs(dfs, to);
+  auto dfs = [&](auto dfs, int i) -> void {
+    for(auto v: trie[i].ids) ans.push_back(v);
+    for(auto [_,v]: trie[i].next) dfs(dfs,v);
   };
-  dfs(dfs, 0);
+  dfs(dfs,0);
+  for(auto v: ans) cout << v << " ";
 
-  REP(i,n) cout << ans[i] << " ";
   return 0;
 }
