43ac0b4197e523366f5e7d2683b72be7b819956a
tic40 <ccpzjoh@gmail.com>
2026-09-26 21:22:39 +0900

    Update

--- abc/abc438/d/main.cpp
+++ abc/abc438/d/main.cpp
@@ -1,8 +1,15 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace atcoder;
 using namespace std;
 #define REP(i,n) for(int i=0;i<n;i++)
 #define endl '\n'
 using ll = long long;
+using P = pair<int,int>;
+const int INF = numeric_limits<int>::max();
+const ll LINF = numeric_limits<ll>::max();
+const int MOD = 1e9+7;
+// using mint = modint998244353; // modint1000000007;
 
 int main() {
   int n; cin >> n;
@@ -12,27 +19,22 @@ int main() {
   REP(i,n) cin >> c[i];
 
   vector<ll> sa(n+1),sb(n+1),sc(n+1);
-  REP(i,n) sa[i+1] = sa[i] + a[i];
-  REP(i,n) sb[i+1] = sb[i] + b[i];
-  REP(i,n) sc[i+1] = sc[i] + c[i];
+  REP(i,n) sa[i+1] = sa[i]+a[i];
+  REP(i,n) sb[i+1] = sb[i]+b[i];
+  REP(i,n) sc[i+1] = sc[i]+c[i];
 
-  vector<ll> suffixmax(n+1,-1e19);
-  for(int i = n-1; i > 0; i--) {
-    suffixmax[i] = max(sb[i] - sc[i], suffixmax[i+1]);
+  vector<ll> prefixmax(n,-1e12);
+  for(int x = 1; x < n-1; x++) {
+    prefixmax[x] = max(prefixmax[x-1], sa[x] - sb[x]);
   }
 
   ll ans = 0;
-  // x を固定
-  for(int x = 1; x <= n-2; x++) {
+  for(int y = 2; y < n; y++) {
     // sa[x] + (sb[y] - sb[x]) + (sc[n] - sc[y])
     // sa[x] - sb[x] + sb[y] - sc[y] + sc[n]
-    // sb[y] - sc[y] を最大化したい
-    ll now = 0;
-    now = sa[x] - sb[x] + sc[n];
-    now += suffixmax[x+1];
-    ans = max(ans,now);
+    ll now = sb[y] - sc[y] + sc[n] + prefixmax[y-1];
+    ans = max(ans, now);
   }
-
   cout << ans << endl;
   return 0;
 }
